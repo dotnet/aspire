a5eea2d60eb403a6c37e5b38893d9988b275bf64
David Fowler <davidfowl@gmail.com>
2023-10-01 15:53:45 -0700

    Expose some APIs from the host (#7)
    - Make ExecutableComponent implement IDistributedApplicationComponentWithEnvironment
    - Expose OtlpEndpoint API

--- src/Aspire.Hosting/Otlp/OtlpConfigurationExtensions.cs
+++ src/Aspire.Hosting/Otlp/OtlpConfigurationExtensions.cs
@@ -6,28 +6,27 @@ using Microsoft.Extensions.Hosting;
 
 namespace Aspire.Hosting.Otlp;
 
-internal static class OtlpConfigurationExtensions
+public static class OtlpConfigurationExtensions
 {
     private const string DashboardOtlpUrlVariableName = "DOTNET_DASHBOARD_OTLP_ENDPOINT_URL";
     private const string DashboardOtlpUrlDefaultValue = "http://localhost:18889";
 
-    public static IDistributedApplicationComponentBuilder<T> ConfigureOtlpEnvironment<T>(this IDistributedApplicationComponentBuilder<T> builder) where T : IDistributedApplicationComponent
+    public static IDistributedApplicationComponentBuilder<T> ConfigureOtlpEnvironment<T>(this IDistributedApplicationComponentBuilder<T> builder) where T : IDistributedApplicationComponentWithEnvironment
     {
         // Configure OpenTelemetry in projects using environment variables.
         // https://github.com/open-telemetry/opentelemetry-specification/blob/main/specification/configuration/sdk-environment-variables.md
 
-        return builder.WithAnnotation(
-            new EnvironmentCallbackAnnotation((config) =>
-            {
-                config["OTEL_EXPORTER_OTLP_ENDPOINT"] = builder.ApplicationBuilder.Configuration[DashboardOtlpUrlVariableName] ?? DashboardOtlpUrlDefaultValue;
+        return builder.WithEnvironment((config) =>
+        {
+            config["OTEL_EXPORTER_OTLP_ENDPOINT"] = builder.ApplicationBuilder.Configuration[DashboardOtlpUrlVariableName] ?? DashboardOtlpUrlDefaultValue;
 
-                // Set a small batch schedule delay in development.
-                // This reduces the delay that OTLP exporter waits to sends telemetry and makes the dashboard telemetry pages responsive.
-                if (builder.ApplicationBuilder.Environment.IsDevelopment())
-                {
-                    config["OTEL_BLRP_SCHEDULE_DELAY"] = "1000"; // milliseconds
-                    config["OTEL_BSP_SCHEDULE_DELAY"] = "1000"; // milliseconds
-                }
-            }));
+            // Set a small batch schedule delay in development.
+            // This reduces the delay that OTLP exporter waits to sends telemetry and makes the dashboard telemetry pages responsive.
+            if (builder.ApplicationBuilder.Environment.IsDevelopment())
+            {
+                config["OTEL_BLRP_SCHEDULE_DELAY"] = "1000"; // milliseconds
+                config["OTEL_BSP_SCHEDULE_DELAY"] = "1000"; // milliseconds
+            }
+        });
     }
 }
