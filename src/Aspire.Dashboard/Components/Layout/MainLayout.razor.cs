84aad887ff7cd5e49f21793bf5aa20607bc1e6c8
Vincent Baaij <vnbaaij@outlook.com>
2023-10-12 10:03:27 +0200

    Update MainLayout.razor.cs
    Use standard luminosity values and use BaseLayerLuminance design token

--- src/Aspire.Dashboard/Components/Layout/MainLayout.razor.cs
+++ src/Aspire.Dashboard/Components/Layout/MainLayout.razor.cs
@@ -3,20 +3,30 @@
 
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Fast.Components.FluentUI;
+using Microsoft.Fast.Components.FluentUI.DesignTokens;
 using Microsoft.JSInterop;
 
 namespace Aspire.Dashboard.Components.Layout;
 
 public partial class MainLayout
 {
+    [Inject]
+    private IJSRuntime JS { get; set; } = default!;
+
+    [Inject]
+    private BaseLayerLuminance BaseLayerLuminance { get; set; } = default!;
+
+    [Inject]    
+    private PersistentComponentState ApplicationState { get; set; } = default!;
+
     [CascadingParameter]
     public HttpContext? HttpContext { get; set; }
 
-    private const float LightModeLuminosity = 0.99f;
-    private const float DarkModeLuminosity = 0.15f;
-
-    private float _baseLayerLuminance = 0.9f;
+    private StandardLuminance _baseLayerLuminance = StandardLuminance.LightMode;
     private PersistingComponentStateSubscription _persistingSubscription;
+
+    private ElementReference _container = default!;
 
     protected override void OnParametersSet()
     {
@@ -26,11 +36,11 @@ public partial class MainLayout
 
             // Look to see if we have a cookie saying what the last system theme was
             // and set the base layer luminance based on that
-            var lastSystemTheme = HttpContext.Request.Cookies["lastSystemTheme"];
+            var lastSystemTheme = HttpContext.Request.Cookies["lastSystemTheme"];            
             _baseLayerLuminance = lastSystemTheme switch
             {
-                "dark" => DarkModeLuminosity,
-                _ => LightModeLuminosity
+                "dark" => StandardLuminance.DarkMode,
+                _ => StandardLuminance.LightMode
             };
         }
     }
@@ -39,9 +49,9 @@ public partial class MainLayout
     {
         // See if we got a base layer luminance value from the cookie and set the value
         // This will avoid a flash of white if the last system theme and current system theme are both dark
-        if (ApplicationState.TryTakeFromJson<float>("baseLayerLuminance", out var restoredBaseLayerLuminance))
+        if (ApplicationState.TryTakeFromJson<StandardLuminance>("baseLayerStandardLuminance", out var restoredBaseLayerStandardLuminance))
         {
-            _baseLayerLuminance = restoredBaseLayerLuminance;
+            _baseLayerLuminance = restoredBaseLayerStandardLuminance;
             StateHasChanged();
         }
     }
@@ -56,7 +66,9 @@ public partial class MainLayout
             // If it has, we might have a flash of (last system theme color) before the current
             // system theme color takes effect.
             var isSystemThemeDark = await JS.InvokeAsync<bool>("setThemeCookie");
-            _baseLayerLuminance = isSystemThemeDark ? DarkModeLuminosity : LightModeLuminosity;
+            _baseLayerLuminance = isSystemThemeDark ? StandardLuminance.DarkMode : StandardLuminance.LightMode;
+
+            await BaseLayerLuminance.SetValueFor(_container, _baseLayerLuminance.GetLuminanceValue());
             StateHasChanged();
         }
     }
@@ -65,7 +77,22 @@ public partial class MainLayout
     {
         // Persist the base layer luminance value from pre-rendering (when we pull it out of the
         // cookie) to rendering (when setting it is important.
-        ApplicationState.PersistAsJson("baseLayerLuminance", _baseLayerLuminance);
+        ApplicationState.PersistAsJson("baseLayerStandardLuminance", _baseLayerLuminance);
         return Task.CompletedTask;
     }
 }
+
+// Uncomment the code below to overload the GetLuminanceValue extension method and use custom luminosity values.
+// Probably needs to be move to it's own file too.
+/*
+public static class StandardLuminanceExtensions
+{
+    private const float LightMode = 1.0f;
+    private const float DarkMode = 0.15f;
+
+    public static float GetLuminanceValue(this StandardLuminance value)
+    {
+        return value == StandardLuminance.LightMode ? LightMode : DarkMode;
+    }
+}
+*/
