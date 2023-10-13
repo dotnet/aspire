c312c3001b1e98b8919e8e3bf714646ecbf84f80
Tim Mulholland <tlmii@users.noreply.github.com>
2023-10-12 22:41:58 -0700

    Workaround for .NET 8 RTM Blazor Initializer Changes (#278)

---
+++ src/Aspire.Dashboard/wwwroot/Aspire.Dashboard.lib.module.js
@@ -0,0 +1,9 @@
+import * as LibraryExports from "/_content/Microsoft.Fast.Components.FluentUI/Microsoft.Fast.Components.FluentUI.lib.module.js";
+
+export function beforeServerStart(blazor) {
+    LibraryExports.beforeStart(blazor);
+}
+
+export function afterServerStarted(blazor) {
+    LibraryExports.afterStarted(blazor);
+}
