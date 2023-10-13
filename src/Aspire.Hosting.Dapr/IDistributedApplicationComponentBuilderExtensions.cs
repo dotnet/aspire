6fcccd739ef9d2f6aa0c887ab49799dc80bb52a1
David Fowler <davidfowl@gmail.com>
2023-10-12 20:00:11 -0700

    Fix dapr too

--- src/Aspire.Hosting.Dapr/IDistributedApplicationComponentBuilderExtensions.cs
+++ src/Aspire.Hosting.Dapr/IDistributedApplicationComponentBuilderExtensions.cs
@@ -2,8 +2,9 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using Aspire.Hosting.ApplicationModel;
+using Aspire.Hosting.Dapr;
 
-namespace Aspire.Hosting.Dapr;
+namespace Aspire.Hosting;
 
 /// <summary>
 /// Extensions to <see cref="IDistributedApplicationResourceBuilder{T}"/> related to Dapr.
