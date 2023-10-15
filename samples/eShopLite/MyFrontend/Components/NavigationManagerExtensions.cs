1348c9d24b7890487dbf7c21825b760659c0bfc7
Damian Edwards <damian@damianedwards.com>
2023-10-15 14:57:25 -0700

    Further workaround to Blazor issue

---
+++ samples/eShopLite/MyFrontend/Components/NavigationManagerExtensions.cs
@@ -0,0 +1,19 @@
+namespace Microsoft.AspNetCore.Components;
+
+public static class NavigationManagerExtensions
+{
+    public static string ToAbsolutePath(this NavigationManager navigationManager, string uri)
+    {
+        // Workaround for Blazor issue: https://github.com/dotnet/aspnetcore/issues/51380
+
+        if (uri.StartsWith(navigationManager.BaseUri, StringComparison.Ordinal))
+        {
+            // The absolute URI must be of the form "{baseUri}something" (where baseUri ends with a slash),
+            // and from that we return "something". If baseUri includes a path, we return that path too.
+            return new Uri(uri).AbsolutePath;
+        }
+
+        var message = $"The URI '{uri}' is not contained by the base URI '{navigationManager.BaseUri}'.";
+        throw new ArgumentException(message);
+    }
+}
