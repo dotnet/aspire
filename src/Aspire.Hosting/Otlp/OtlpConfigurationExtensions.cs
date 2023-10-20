90e60550a263f7a5d525c955cf22e3391bb52324
David Fowler <davidfowl@gmail.com>
2023-10-20 10:01:01 -0700

    Forgot one

--- src/Aspire.Hosting/Otlp/OtlpConfigurationExtensions.cs
+++ src/Aspire.Hosting/Otlp/OtlpConfigurationExtensions.cs
@@ -49,6 +49,8 @@ public static class OtlpConfigurationExtensions
     /// 2. It sets the service name and instance id to the resource name and UID. Values are injected by the orchestrator.
     /// 3. It sets a small batch schedule delay in development. This reduces the delay that OTLP exporter waits to sends telemetry and makes the dashboard telemetry pages responsive.
     /// </summary>
+    /// <typeparam name="T">The resource type.</typeparam>
+    /// <param name="builder">The resource builder.</param>
     /// <returns>The <see cref="IDistributedApplicationResourceBuilder{T}"/>.</returns>
     public static IDistributedApplicationResourceBuilder<T> WithOtlpExporter<T>(this IDistributedApplicationResourceBuilder<T> builder) where T : IDistributedApplicationResourceWithEnvironment
     {
