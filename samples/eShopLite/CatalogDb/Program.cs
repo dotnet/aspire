be2d6a572683d7d4425d0e43c5aa351103ec3e7c
Damian Edwards <damian@damianedwards.com>
2023-10-20 16:00:23 -0700

    Move catalog DB model & migrations to separate app (#428)
    * Move catalog DB model & migrations to separate app
    
    - The app is a web app that is a resource in the AppHost modeled to depend on the catalogdb
    - It runs db initialization (migrations & seeding) in a BackgroundService
    - It exposes the status via the /readiness health check endpoint (Degraded=in progress, Healthy=complete, Unhealthy=failed)
    - The new app is configured to make at least some of the `dotnet ef` commands work against it via a no-op connection string in its appsettings.Development.json
    
    * Update sample manifest

---
+++ samples/eShopLite/CatalogDb/Program.cs
@@ -0,0 +1,18 @@
+using CatalogDb;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.AddServiceDefaults();
+builder.AddNpgsqlDbContext<CatalogDbContext>("catalogdb");
+
+builder.Services.AddSingleton<CatalogDbInitializer>();
+builder.Services.AddHostedService(sp => sp.GetRequiredService<CatalogDbInitializer>());
+builder.Services.AddHealthChecks()
+    .AddCheck<CatalogDbInitializerHealthCheck>("DbInitializer", null);
+
+var app = builder.Build();
+
+app.MapDefaultEndpoints();
+
+await app.RunAsync();
+
