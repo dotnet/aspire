f00487f63028d44d7e65f47857058c2590a411db
David Fowler <davidfowl@gmail.com>
2023-10-25 00:51:56 -0700

    Refactor migration logic - Move migration into a database strategy for retries - Create an activity for the migration operation

--- samples/eShopLite/CatalogDb/Program.cs
+++ samples/eShopLite/CatalogDb/Program.cs
@@ -5,6 +5,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.AddServiceDefaults();
 builder.AddNpgsqlDbContext<CatalogDbContext>("catalogdb");
 
+builder.Services.AddOpenTelemetry()
+    .WithTracing(tracing => tracing.AddSource(CatalogDbInitializer.ActivitySourceName));
+
 builder.Services.AddSingleton<CatalogDbInitializer>();
 builder.Services.AddHostedService(sp => sp.GetRequiredService<CatalogDbInitializer>());
 builder.Services.AddHealthChecks()
