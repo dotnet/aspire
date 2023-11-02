be7dc159ede7a2387433791177a74638c6bd73db
Kevin Pilch <me@pilchie.com>
2023-11-02 12:51:31 -0700

    Azure cosmosdb support in aspire (#359)
    * Initial addition of CosmosDB support, based on SqlServer
    
    * Remove Healthchecks support from CosmosDB EF Component
    
    * Cleanup connection string handling in Cosmos EF
    
    * Cleanup connection string handling in Cosmos component
    
    * Update CosmosDB package to get OTel support
    
    * Use the parent name for the connection
    
    * Udpate manifest strings
    
    * Add CosmosDB components to Progress and Telemetry
    
    * Rename CosmosDB components to Aspire.Azure.Data.Cosmos[.EntityFrameworkCore]
    
    * Rename options -> settings
    
    * Rename Cosmos Components to follow naming guidelines
    
    * Update to CosmosDB preview package and pin to get OpenTelemetry support
    
    * Update comments and add Keyed DI to Aspire.Microsoft.Azure.Cosmos
    
    * Add log categories to Cosmos Component schemas
    
    * Add basic support for CosmosClientOptions (no IConfiguration binding yet)
    
    * Remove healthchecks support from CosmosDB Component
    
    * Add README for Aspire.Microsoft.Azure.Cosmos
    
    * Add README for Aspire.Microsoft.EntityFrameworkCore.Cosmos, and rename a couple of things
    
    * Update config schema to be nested for Aspire.Microsoft.EntityFramework.Cosmos and Aspire.Microsoft.Azure.Cosmos
    
    * Rename AzureDataCosmosSettings -> AzureCosmosDBSettings
    
    * Update Aspire_Components_Progress.md
    
    * Add PackageTags, Descriptions, and Icons
    
    * Add AccountEndpoint to ConfigurationScheama.json
    
    * Fix DB context builder config
    
    * Add xml doc comments for CosmosDB hosting methods and types
    
    * Move Cosmos DB hosting to Aspire.Hosting.Azure
    
    * Update manifest type names
    
    * Respond to PR feedback

---
+++ src/Aspire.Hosting.Azure/AzureCosmosDBConnectionResource.cs
@@ -0,0 +1,21 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Aspire.Hosting.ApplicationModel;
+
+namespace Aspire.Hosting.Azure.Data.Cosmos;
+
+/// <summary>
+/// Represents a connection to an Azure Cosmos DB account.
+/// </summary>
+/// <param name="name">The resource name.</param>
+/// <param name="connectionString">The connection string to use to connect.</param>
+public class AzureCosmosDBConnectionResource(string name, string? connectionString)
+    : Resource(name), IResourceWithConnectionString
+{
+    /// <summary>
+    /// Gets the connection string to use for this database.
+    /// </summary>
+    /// <returns>The connection string to use for this database.</returns>
+    public string? GetConnectionString() => connectionString;
+}
