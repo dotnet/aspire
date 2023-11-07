71d864a4bfef73cd696907974b8faea3846844a2
James Newton-King <james@newtonking.com>
2023-11-07 22:13:31 +0800

    Fix build

--- src/Aspire.Dashboard/Model/Otlp/SpanWaterfallViewModel.cs
+++ src/Aspire.Dashboard/Model/Otlp/SpanWaterfallViewModel.cs
@@ -2,7 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.Diagnostics.CodeAnalysis;
-using System.Text;
 using Aspire.Dashboard.Otlp.Model;
 using Grpc.Core;
 
