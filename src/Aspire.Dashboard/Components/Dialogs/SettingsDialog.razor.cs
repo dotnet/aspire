fe15a8d2c1c6f8fcdb715a5fd4a2e36b351957fb
Vincent Baaij <vnbaaij@outlook.com>
2023-11-13 22:41:03 +0100

    Settings dialog ->Settings Panel Show version in panel

--- src/Aspire.Dashboard/Components/Dialogs/SettingsDialog.razor.cs
+++ src/Aspire.Dashboard/Components/Dialogs/SettingsDialog.razor.cs
@@ -1,6 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Reflection;
 using Microsoft.FluentUI.AspNetCore.Components;
 using Microsoft.JSInterop;
 
@@ -15,8 +16,14 @@ public partial class SettingsDialog : IDialogContentComponent, IAsyncDisposable
     private const string ThemeSettingLight = "Light";
 
     private string _currentSetting = ThemeSettingSystem;
+    private string? _version;
 
     private IJSObjectReference? _jsModule;
+
+    protected override void OnInitialized()
+    {
+        _version = Assembly.GetExecutingAssembly().GetCustomAttribute<AssemblyFileVersionAttribute>()?.Version;
+    }
 
     protected override async Task OnAfterRenderAsync(bool firstRender)
     {
