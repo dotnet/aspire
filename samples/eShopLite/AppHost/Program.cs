c4eb8d73d7bbdd31c91a03e83093b7d0a364cebe
Mitch Denny <midenn@microsoft.com>
2023-11-16 14:47:36 +1100

    Fix for endpoint not having correct port.

--- samples/eShopLite/AppHost/Program.cs
+++ samples/eShopLite/AppHost/Program.cs
@@ -7,8 +7,7 @@ var catalogDb = builder.AddPostgresContainer("postgres").AddDatabase("catalogdb"
 var basketCache = builder.AddRedisContainer("basketcache");
 
 var catalogService = builder.AddProject<Projects.CatalogService>("catalogservice")
-                     .WithReference(catalogDb)
-                     .WithReplicas(2);
+                     .WithReference(catalogDb);
 
 var messaging = builder.AddRabbitMQContainer("messaging");
 
@@ -18,7 +17,24 @@ var basketService = builder.AddProject<Projects.BasketService>("basketservice")
 
 builder.AddProject<Projects.MyFrontend>("frontend")
        .WithReference(basketService)
-       .WithReference(catalogService.GetEndpoint("http"));
+       .WithReference(catalogService.GetEndpoint("http"))
+       .WithEnvironment("TESTVALUE_X", catalogService.GetEndpoint("http"))
+       .WithEnvironment(context =>
+       {
+           if (!catalogService.Resource.TryGetServiceBindings(out var serviceBindings))
+           {
+               return;
+           }
+
+           var binding = serviceBindings.FirstOrDefault(b => b.Name == "http");
+
+           if (binding is null)
+           {
+               return;
+           }
+
+           context.EnvironmentVariables["TESTVALUE_Y"] = binding.UriScheme + "://localhost:" + binding.Port;
+       });
 
 builder.AddProject<Projects.OrderProcessor>("orderprocessor")
        .WithReference(messaging)
