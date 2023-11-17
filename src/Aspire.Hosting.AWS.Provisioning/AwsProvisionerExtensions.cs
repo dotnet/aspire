1cf1b590917f38ccc561602e363e495025767d19
denizirgin@gmail.com <denizirgin@gmail.com>
2023-11-18 00:38:39 +0300

    add aws provisioner support

---
+++ src/Aspire.Hosting.AWS.Provisioning/AwsProvisionerExtensions.cs
@@ -0,0 +1,79 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Amazon.S3;
+using Aspire.Hosting.ApplicationModel;
+using Aspire.Hosting.AWS.Provisioning;
+using Aspire.Hosting.AWS.Provisioning.Provisioners;
+using Aspire.Hosting.Azure;
+using Aspire.Hosting.Lifecycle;
+using LocalStack.Client.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Aspire.Hosting;
+
+/// <summary>
+/// Provides extension methods for adding support for generating Azure resources dynamically during application startup.
+/// </summary>
+public static class AwsProvisionerExtensions
+{
+    /// <summary>
+    /// Adds support for generating azure resources dynamically during application startup.
+    /// The application must configure the appropriate subscription, location.
+    /// </summary>
+    public static IDistributedApplicationBuilder AddAwsProvisioning(this IDistributedApplicationBuilder builder)
+    {
+        builder.Services.AddLifecycleHook<AwsProvisioner>();
+
+        builder.Services.AddLocalStack(builder.Configuration);
+        builder.Services.AddAwsService<IAmazonS3>();
+
+        // Attempt to read aws configuration from configuration
+        builder.Services.AddOptions<AwsProvisionerOptions>()
+            .BindConfiguration("AWS");
+
+        builder.AddAwsProvisioner<AwsS3BucketResource, S3Provisioner>();
+
+        // We're adding 2 because there's no easy way to enumerate all keys and all service types
+        //builder.AddAzureProvisioner<AzureKeyVaultResource, KeyVaultProvisioner>();
+        //builder.AddResourceEnumerator(resourceGroup => resourceGroup.GetKeyVaults(), resource => resource.Data.Tags);
+
+        //builder.AddAzureProvisioner<AzureStorageResource, StorageProvisioner>();
+        //builder.AddResourceEnumerator(resourceGroup => resourceGroup.GetStorageAccounts(), resource => resource.Data.Tags);
+
+        //builder.AddAzureProvisioner<AzureServiceBusResource, ServiceBusProvisioner>();
+        //builder.AddResourceEnumerator(resourceGroup => resourceGroup.GetServiceBusNamespaces(), resource => resource.Data.Tags);
+
+        //builder.AddAzureProvisioner<AzureRedisResource, AzureRedisProvisioner>();
+        //builder.AddResourceEnumerator(resourceGroup => resourceGroup.GetAllRedis(), resource => resource.Data.Tags);
+
+        //builder.AddAzureProvisioner<AzureAppConfigurationResource, AppConfigurationProvisioner>();
+        //builder.AddResourceEnumerator(resourceGroup => resourceGroup.GetAppConfigurationStores(), resource => resource.Data.Tags);
+
+        //builder.AddAzureProvisioner<AzureCosmosDBResource, AzureCosmosDBProvisioner>();
+        //builder.AddResourceEnumerator(resourceGroup => resourceGroup.GetCosmosDBAccounts(), resource => resource.Data.Tags);
+
+        //builder.AddAzureProvisioner<AzureSqlServerResource, SqlServerProvisioner>();
+        //builder.AddResourceEnumerator(resourceGroup => resourceGroup.GetSqlServers(), resource => resource.Data.Tags);
+
+        return builder;
+    }
+
+    internal static IDistributedApplicationBuilder AddAwsProvisioner<TResource, TProvisioner>(this IDistributedApplicationBuilder builder)
+        where TResource : IAwsResource
+        where TProvisioner : AwsResourceProvisioner<TResource>
+    {
+        // This lets us avoid using open generics in the caller, we can use keyed lookup instead
+        builder.Services.AddKeyedSingleton<IAwsResourceProvisioner, TProvisioner>(typeof(TResource));
+        return builder;
+    }
+
+    //internal static IDistributedApplicationBuilder AddResourceEnumerator<TResource>(this IDistributedApplicationBuilder builder,
+    //    Func<ResourceGroupResource, IAsyncEnumerable<TResource>> getResources,
+    //    Func<TResource, IDictionary<string, string>> getTags)
+    //    where TResource : ArmResource
+    //{
+    //    builder.Services.AddSingleton<IAzureResourceEnumerator>(new AzureResourceEnumerator<TResource>(getResources, getTags));
+    //    return builder;
+    //}
+}
---
+++ src/Aspire.Hosting.AWS.Provisioning/Provisioners/AwsProvisioner.cs
@@ -0,0 +1,348 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Aspire.Hosting.ApplicationModel;
+using Aspire.Hosting.AWS.Provisioning;
+using Aspire.Hosting.Lifecycle;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace Aspire.Hosting.Azure;
+
+// Provisions aws resources for development purposes
+internal sealed class AwsProvisioner(
+    IOptions<AwsProvisionerOptions> options,
+    //IOptions<PublishingOptions> publishingOptions,
+    IConfiguration configuration,
+    IHostEnvironment environment,
+    ILogger<AwsProvisioner> logger,
+    IServiceProvider serviceProvider
+    //IEnumerable<IAzureResourceEnumerator> resourceEnumerators
+    ) : IDistributedApplicationLifecycleHook
+{
+    internal const string AspireResourceNameTag = "aspire-resource-name";
+
+    private readonly AwsProvisionerOptions _options = options.Value;
+
+    public async Task BeforeStartAsync(DistributedApplicationModel appModel, CancellationToken cancellationToken = default)
+    {
+        // TODO: Make this more general purpose
+        //if (publishingOptions.Value.Publisher == "manifest")
+        //{
+        //    return;
+        //}
+       
+        var awsResources = appModel.Resources.OfType<IAwsResource>();
+        if (!awsResources.OfType<IAwsResource>().Any())
+        {
+            return;
+        }
+
+        try
+        {
+            await ProvisionAwsResources(configuration, environment, logger, awsResources, cancellationToken).ConfigureAwait(false);
+        }
+        catch (MissingConfigurationException ex)
+        {
+            logger.LogWarning(ex, "Required configuration is missing.");
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "Error provisioning Aws resources.");
+        }
+    }
+
+    private async Task ProvisionAwsResources(IConfiguration configuration, IHostEnvironment _, ILogger<AwsProvisioner> logger, IEnumerable<IAwsResource> awsResources, CancellationToken cancellationToken)
+    {
+        //var credential = new DefaultAzureCredential(new DefaultAzureCredentialOptions()
+        //{
+        //    ExcludeManagedIdentityCredential = true,
+        //    ExcludeWorkloadIdentityCredential = true,
+        //    ExcludeAzurePowerShellCredential = true,
+        //    CredentialProcessTimeout = TimeSpan.FromSeconds(15)
+        //});
+
+        //var subscriptionId = _options.SubscriptionId ?? throw new MissingConfigurationException("An azure subscription id is required. Set the Azure:SubscriptionId configuration value.");
+        //var location = _options.Location switch
+        //{
+        //    null => throw new MissingConfigurationException("An azure location/region is required. Set the Azure:Location configuration value."),
+        //    string loc => new AzureLocation(loc)
+        //};
+
+        //var armClient = new ArmClient(credential, subscriptionId);
+
+        // TODO: use AWSSDK.CloudFormation here
+
+        //var subscriptionLazy = new Lazy<Task<SubscriptionResource>>(async () =>
+        //{
+        //    logger.LogInformation("Getting default subscription...");
+
+        //    var value = await armClient.GetDefaultSubscriptionAsync(cancellationToken).ConfigureAwait(false);
+
+        //    logger.LogInformation("Default subscription: {name} ({subscriptionId})", value.Data.DisplayName, value.Id);
+
+        //    return value;
+        //});
+
+        //Lazy<Task<(ResourceGroupResource, AzureLocation)>> resourceGroupAndLocationLazy = new(async () =>
+        //{
+        //    // Name of the resource group to create based on the machine name and application name
+        //    var (resourceGroupName, createIfAbsent) = _options.ResourceGroup switch
+        //    {
+        //        null => ($"{Environment.MachineName.ToLowerInvariant()}-{environment.ApplicationName.ToLowerInvariant()}-rg", true),
+        //        string rg => (rg, _options.AllowResourceGroupCreation ?? false)
+        //    };
+
+        //    var subscription = await subscriptionLazy.Value.ConfigureAwait(false);
+
+        //    var resourceGroups = subscription.GetResourceGroups();
+        //    ResourceGroupResource? resourceGroup = null;
+        //    AzureLocation location = new(_options.Location);
+        //    try
+        //    {
+        //        var response = await resourceGroups.GetAsync(resourceGroupName, cancellationToken).ConfigureAwait(false);
+        //        resourceGroup = response.Value;
+        //        location = resourceGroup.Data.Location;
+
+        //        logger.LogInformation("Using existing resource group {rgName}.", resourceGroup.Data.Name);
+        //    }
+        //    catch (Exception)
+        //    {
+        //        if (!createIfAbsent)
+        //        {
+        //            throw;
+        //        }
+
+        //        // REVIEW: Is it possible to do this without an exception?
+
+        //        logger.LogInformation("Creating resource group {rgName} in {location}...", resourceGroupName, location);
+
+        //        var rgData = new ResourceGroupData(location);
+        //        rgData.Tags.Add("aspire", "true");
+        //        var operation = await resourceGroups.CreateOrUpdateAsync(WaitUntil.Completed, resourceGroupName, rgData, cancellationToken).ConfigureAwait(false);
+        //        resourceGroup = operation.Value;
+
+        //        logger.LogInformation("Resource group {rgName} created.", resourceGroup.Data.Name);
+        //    }
+
+        //    return (resourceGroup, location);
+        //});
+
+        //var principalLazy = new Lazy<Task<UserPrincipal>>(async () => await GetUserPrincipalAsync(credential, cancellationToken).ConfigureAwait(false));
+
+        //var resourceMapLazy = new Lazy<Task<Dictionary<string, ArmResource>>>(async () =>
+        //{
+        //    var resourceMap = new Dictionary<string, ArmResource>();
+
+        //    var (resourceGroup, _) = await resourceGroupAndLocationLazy.Value.ConfigureAwait(false);
+
+        //    // Enumerate all known resources and look for aspire tags
+        //    foreach (var enumerator in resourceEnumerators)
+        //    {
+        //        await PopulateExistingAspireResources(
+        //             resourceGroup,
+        //             enumerator.GetResources,
+        //             enumerator.GetTags,
+        //             resourceMap,
+        //             cancellationToken).ConfigureAwait(false);
+        //    }
+
+        //    return resourceMap;
+        //});
+
+        var tasks = new List<Task>();
+
+        // Try to find the user secrets path so that provisioners can persist connection information.
+        //static string? GetUserSecretsPath()
+        //{
+        //    return Assembly.GetEntryAssembly()?.GetCustomAttribute<UserSecretsIdAttribute>()?.UserSecretsId switch
+        //    {
+        //        null => Environment.GetEnvironmentVariable("DOTNET_USER_SECRETS_ID"),
+        //        string id => UserSecretsPathHelper.GetSecretsPathFromSecretsId(id)
+        //    };
+        //}
+
+        //var userSecretsPath = GetUserSecretsPath();
+
+        //ResourceGroupResource? resourceGroup = null;
+        //SubscriptionResource? subscription = null;
+        //Dictionary<string, ArmResource>? resourceMap = null;
+        // UserPrincipal? principal = null;
+        ProvisioningContext? provisioningContext = null;
+        var usedResources = new HashSet<string>();
+
+        //var userSecrets = userSecretsPath is not null && File.Exists(userSecretsPath)
+        //    ? JsonNode.Parse(await File.ReadAllTextAsync(userSecretsPath, cancellationToken).ConfigureAwait(false))!.AsObject()
+        //    : [];
+
+        foreach (var resource in awsResources)
+        {
+            usedResources.Add(resource.Name);
+
+            var provisioner = serviceProvider.GetKeyedService<IAwsResourceProvisioner>(resource.GetType());
+
+            if (provisioner is null)
+            {
+                logger.LogWarning("No provisioner found for {resourceType} skipping.", resource.GetType().Name);
+                continue;
+            }
+
+            if (!provisioner.ShouldProvision(configuration, resource))
+            {
+                logger.LogInformation("Skipping {resourceName} because it is not configured to be provisioned.", resource.Name);
+                continue;
+            }
+
+            provisioner.ConfigureResource(configuration, resource);
+
+            //if (provisioner.ConfigureResource(configuration, resource))
+            //{
+            //    logger.LogInformation("Using connection information stored in user secrets for {resourceName}.", resource.Name);
+
+            //    continue;
+            //}
+
+            //subscription ??= await subscriptionLazy.Value.ConfigureAwait(false);
+
+            //if (resourceGroup is null)
+            //{
+            //    (resourceGroup, location) = await resourceGroupAndLocationLazy.Value.ConfigureAwait(false);
+            //}
+
+            //resourceMap ??= await resourceMapLazy.Value.ConfigureAwait(false);
+           // principal ??= await principalLazy.Value.ConfigureAwait(false);
+            provisioningContext ??= new ProvisioningContext(_options.Region);
+
+            var task = provisioner.GetOrCreateResourceAsync(
+                    resource,
+                    provisioningContext,
+                    cancellationToken);
+
+            tasks.Add(task);
+        }
+
+        if (tasks.Count > 0)
+        {
+            await Task.WhenAll(tasks).ConfigureAwait(false);
+
+            // If we created any resources then save the user secrets
+            //if (userSecretsPath is not null)
+            //{
+            //    // Ensure directory exists before attempting to create secrets file
+            //    Directory.CreateDirectory(Path.GetDirectoryName(userSecretsPath)!);
+            //    await File.WriteAllTextAsync(userSecretsPath, userSecrets.ToString(), cancellationToken).ConfigureAwait(false);
+
+            //    logger.LogInformation("Azure resource connection strings saved to user secrets.");
+            //}
+        }
+
+        // Do this in the background to avoid blocking startup
+        //_ = Task.Run(async () =>
+        //{
+        //    logger.LogInformation("Cleaning up unused resources...");
+
+        //    resourceMap ??= await resourceMapLazy.Value.ConfigureAwait(false);
+
+        //    // Clean up any left over resources that are no longer in the model
+        //    foreach (var (name, sa) in resourceMap)
+        //    {
+        //        if (usedResources.Contains(name))
+        //        {
+        //            continue;
+        //        }
+
+        //        var response = await armClient.GetGenericResources().GetAsync(sa.Id, cancellationToken).ConfigureAwait(false);
+
+        //        logger.LogInformation("Deleting unused resource {keyVaultName} which maps to resource name {name}.", sa.Id, name);
+
+        //        await response.Value.DeleteAsync(WaitUntil.Started, cancellationToken).ConfigureAwait(false);
+        //    }
+        //},
+        //cancellationToken);
+    }
+
+    //private static async Task PopulateExistingAspireResources<TResource>(
+    //    ResourceGroupResource resourceGroup,
+    //    Func<ResourceGroupResource, IAsyncEnumerable<TResource>> getCollection,
+    //    Func<TResource, IDictionary<string, string>> getTags,
+    //    Dictionary<string, ArmResource> map,
+    //    CancellationToken cancellationToken)
+    //    where TResource : ArmResource
+    //{
+    //    await foreach (var r in getCollection(resourceGroup).WithCancellation(cancellationToken))
+    //    {
+    //        var tags = getTags(r);
+    //        if (tags.TryGetValue(AspireResourceNameTag, out var aspireResourceName))
+    //        {
+    //            map[aspireResourceName] = r;
+    //        }
+    //    }
+    //}
+
+    //internal async Task<UserPrincipal> GetUserPrincipalAsync(TokenCredential credential, CancellationToken cancellationToken)
+    //{
+    //    var response = await credential.GetTokenAsync(new(["https://graph.windows.net/.default"]), cancellationToken).ConfigureAwait(false);
+
+    //    static UserPrincipal ParseToken(in AccessToken response)
+    //    {
+    //        // Parse the access token to get the user's object id (this is their principal id)
+    //        var oid = string.Empty;
+    //        var upn = string.Empty;
+    //        var parts = response.Token.Split('.');
+    //        var part = parts[1];
+    //        var convertedToken = part.ToString().Replace('_', '/').Replace('-', '+');
+
+    //        switch (part.Length % 4)
+    //        {
+    //            case 2:
+    //                convertedToken += "==";
+    //                break;
+    //            case 3:
+    //                convertedToken += "=";
+    //                break;
+    //        }
+    //        var bytes = Convert.FromBase64String(convertedToken);
+    //        Utf8JsonReader reader = new(bytes);
+    //        while (reader.Read())
+    //        {
+    //            if (reader.TokenType == JsonTokenType.PropertyName)
+    //            {
+    //                var header = reader.GetString();
+    //                if (header == "oid")
+    //                {
+    //                    reader.Read();
+    //                    oid = reader.GetString()!;
+    //                    if (!string.IsNullOrEmpty(upn))
+    //                    {
+    //                        break;
+    //                    }
+    //                }
+    //                else if (header is "upn" or "email")
+    //                {
+    //                    reader.Read();
+    //                    upn = reader.GetString()!;
+    //                    if (!string.IsNullOrEmpty(oid))
+    //                    {
+    //                        break;
+    //                    }
+    //                }
+    //                else
+    //                {
+    //                    reader.Read();
+    //                }
+    //            }
+    //        }
+    //        return new UserPrincipal(Guid.Parse(oid), upn);
+    //    }
+
+    //    return ParseToken(response);
+    //}
+
+    sealed class MissingConfigurationException(string message) : Exception(message)
+    {
+
+    }
+}
