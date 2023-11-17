2601dd40fece6c02c4130cbf7b75a47cddc5cbb5
Kia Raad <kia.panahirad@gmail.com>
2023-11-17 21:56:38 +0330

    Add test to assert LaunchSetttingsSerializerContext does not throw when json has comments in it.

---
+++ tests/Aspire.Hosting.Tests/LaunchSettingsSerializerContextTests.cs
@@ -0,0 +1,33 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Text.Json;
+using Xunit;
+
+namespace Aspire.Hosting.Tests;
+public class LaunchSettingsSerializerContextTests
+{
+    [Fact]
+    public void CommentsInLaunchSettingsJsonDoesNotThrow()
+    {
+        const string launchSettingsJson = """
+        {
+          "$schema": "http://json.schemastore.org/launchsettings.json",
+          "profiles": {
+            // comment
+            "http": {
+              "commandName": "Project",
+              "dotnetRunMessages": true,
+              "launchBrowser": true,
+              "applicationUrl": "http://localhost:5261",
+              "environmentVariables": {
+                "ASPNETCORE_ENVIRONMENT": "Development"
+              }
+            }
+          }
+        }
+        """;
+        var exception = Record.Exception(() => JsonSerializer.Deserialize(launchSettingsJson, LaunchSetttingsSerializerContext.Default.LaunchSettings));
+        Assert.Null(exception);
+    }
+}
