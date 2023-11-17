a64653f4c4ef8a3902b3fbff04b936a64a7695ae
Bradley Grainger <bgrainger@gmail.com>
2023-11-16 17:55:26 -0800

    Update MySqlConnector to 2.3.1.
    This allows us to use the built-in support for keyed services.

--- src/Components/Aspire.MySqlConnector/AspireMySqlConnectorExtensions.cs
+++ src/Components/Aspire.MySqlConnector/AspireMySqlConnectorExtensions.cs
@@ -1,7 +1,6 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System.Data.Common;
 using Aspire;
 using Aspire.MySqlConnector;
 using HealthChecks.MySql;
@@ -108,7 +107,6 @@ public static class AspireMySqlConnectorExtensions
     {
         if (serviceKey is null)
         {
-            // delay validating the ConnectionString until the DataSource is requested. This ensures an exception doesn't happen until a Logger is established.
             builder.Services.AddMySqlDataSource(settings.ConnectionString ?? string.Empty, dataSourceBuilder =>
             {
                 ValidateConnection();
@@ -116,23 +114,13 @@ public static class AspireMySqlConnectorExtensions
         }
         else
         {
-            // Currently MySqlConnector does not support Keyed DI Registration, so we implement it on our own.
-            // Register a MySqlDataSource factory method, based on https://github.com/mysql-net/MySqlConnector/blob/d895afc013a5849d33a123a7061442e2cbb9ce76/src/MySqlConnector.DependencyInjection/MySqlConnectorServiceCollectionExtensions.cs#L57-L60
-            builder.Services.AddKeyedSingleton<MySqlDataSource>(serviceKey, (serviceProvider, _) =>
+            builder.Services.AddKeyedMySqlDataSource(serviceKey, settings.ConnectionString ?? string.Empty, dataSourceBuilder =>
             {
                 ValidateConnection();
-
-                var dataSourceBuilder = new MySqlDataSourceBuilder(settings.ConnectionString);
-                dataSourceBuilder.UseLoggerFactory(serviceProvider.GetService<ILoggerFactory>());
-                return dataSourceBuilder.Build();
             });
-            // Common Services, based on https://github.com/mysql-net/MySqlConnector/blob/d895afc013a5849d33a123a7061442e2cbb9ce76/src/MySqlConnector.DependencyInjection/MySqlConnectorServiceCollectionExtensions.cs#L64-L70
-            // They let the users resolve MySqlConnection directly.
-            builder.Services.AddKeyedSingleton<DbDataSource>(serviceKey, static (serviceProvider, key) => serviceProvider.GetRequiredKeyedService<MySqlDataSource>(key));
-            builder.Services.AddKeyedTransient<MySqlConnection>(serviceKey, static (serviceProvider, key) => serviceProvider.GetRequiredKeyedService<MySqlDataSource>(key).CreateConnection());
-            builder.Services.AddKeyedTransient<DbConnection>(serviceKey, static (serviceProvider, key) => serviceProvider.GetRequiredKeyedService<MySqlConnection>(key));
         }
 
+        // delay validating the ConnectionString until the DataSource is requested. This ensures an exception doesn't happen until a Logger is established.
         void ValidateConnection()
         {
             if (string.IsNullOrEmpty(settings.ConnectionString))
