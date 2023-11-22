a6bab717b1c7613f2c70c9b43d44aaf94a9db308
Weihan Li <weihanli@outlook.com>
2023-11-23 00:19:34 +0800

    add test case

---
+++ tests/Aspire.Hosting.Tests/Utils/FileUtilTests.cs
@@ -0,0 +1,20 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Xunit;
+using Aspire.Hosting.Utils;
+
+namespace Aspire.Hosting.Tests.Utils;
+
+public class FileUtilTests
+{
+    [Fact]
+    public void FindFullPath()
+    {
+        var dotnetPath = FileUtil.FindFullPathFromPath("dotnet");
+        var dir = Path.GetDirectoryName(dotnetPath);
+        Assert.NotNull(dir);
+        var ext = Path.GetExtension(dotnetPath);
+        Assert.Equals(FileNameSuffixes.CurrentPlatform.Exe, ext);
+    }
+}
