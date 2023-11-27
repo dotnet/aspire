2783162428a696513cfe01953aec3b60dfd7ec16
Tim Mulholland <timulhol@microsoft.com>
2023-11-27 12:31:15 -0800

    Use invariant culture when generating CSS

--- src/Aspire.Dashboard/Components/Pages/Traces.razor.cs
+++ src/Aspire.Dashboard/Components/Pages/Traces.razor.cs
@@ -1,6 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Globalization;
 using Aspire.Dashboard.Model;
 using Aspire.Dashboard.Model.Otlp;
 using Aspire.Dashboard.Otlp.Model;
@@ -43,7 +44,7 @@ public partial class Traces
             percentage = trace.Duration / ViewModel.MaxDuration * 100.0;
         }
 
-        return $"background: linear-gradient(to right, var(--neutral-fill-input-alt-active) {percentage:0.##}%, transparent {percentage:0.##}%);";
+        return string.Create(CultureInfo.InvariantCulture, $"background: linear-gradient(to right, var(--neutral-fill-input-alt-active) {percentage:0.##}%, transparent {percentage:0.##}%);");
     }
 
     private static string GetTooltip(IGrouping<OtlpApplication, OtlpSpan> applicationSpans)
