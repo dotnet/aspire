9e3a873fab1d27e596b324ccc0692e372be5ef52
David Fowler <davidfowl@gmail.com>
2023-11-28 00:44:54 -0800

    Add explicit methods for TryAdd*

--- src/Aspire.Hosting/Lifecycle/LifecycleHookServiceCollectionExtensions.cs
+++ src/Aspire.Hosting/Lifecycle/LifecycleHookServiceCollectionExtensions.cs
@@ -18,6 +18,16 @@ public static class LifecycleHookServiceCollectionExtensions
     /// <param name="services">The <see cref="IServiceCollection"/> to add the distributed application lifecycle hook to.</param>
     public static void AddLifecycleHook<T>(this IServiceCollection services) where T : class, IDistributedApplicationLifecycleHook
     {
+        services.AddSingleton<IDistributedApplicationLifecycleHook, T>();
+    }
+
+    /// <summary>
+    /// Attempts to add a distributed application lifecycle hook to the service collection.
+    /// </summary>
+    /// <typeparam name="T">The type of the distributed application lifecycle hook to add.</typeparam>
+    /// <param name="services">The <see cref="IServiceCollection"/> to add the distributed application lifecycle hook to.</param>
+    public static void TryAddLifecycleHook<T>(this IServiceCollection services) where T : class, IDistributedApplicationLifecycleHook
+    {
         services.TryAddEnumerable(ServiceDescriptor.Singleton<IDistributedApplicationLifecycleHook, T>());
     }
 
@@ -28,6 +38,17 @@ public static class LifecycleHookServiceCollectionExtensions
     /// <param name="services">The service collection to add the hook to.</param>
     /// <param name="implementationFactory">A factory function that creates the hook implementation.</param>
     public static void AddLifecycleHook<T>(this IServiceCollection services, Func<IServiceProvider, T> implementationFactory) where T : class, IDistributedApplicationLifecycleHook
+    {
+        services.AddSingleton<IDistributedApplicationLifecycleHook, T>(implementationFactory);
+    }
+
+    /// <summary>
+    /// Attempts to add a distributed application lifecycle hook to the service collection. 
+    /// </summary>
+    /// <typeparam name="T">The type of the distributed application lifecycle hook.</typeparam>
+    /// <param name="services">The service collection to add the hook to.</param>
+    /// <param name="implementationFactory">A factory function that creates the hook implementation.</param>
+    public static void TryAddLifecycleHook<T>(this IServiceCollection services, Func<IServiceProvider, T> implementationFactory) where T : class, IDistributedApplicationLifecycleHook
     {
         services.TryAddEnumerable(ServiceDescriptor.Singleton<IDistributedApplicationLifecycleHook, T>(implementationFactory));
     }
