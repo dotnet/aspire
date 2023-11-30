eaa396453de11f1a8387531ba3a11432d42d12ce
David Fowler <davidfowl@gmail.com>
2023-11-29 18:25:14 -0800

    Made some dapr improvements based on feedback - Default appid to the resource name - Name the dapr side car resource name-dapr - Look for the side car annotation on all resources.

--- src/Aspire.Hosting.Dapr/IDistributedApplicationComponentBuilderExtensions.cs
+++ src/Aspire.Hosting.Dapr/IDistributedApplicationComponentBuilderExtensions.cs
@@ -12,6 +12,17 @@ namespace Aspire.Hosting;
 public static class IDistributedApplicationResourceBuilderExtensions
 {
     /// <summary>
+    /// Ensures that a Dapr sidecar is started for the resource. The default app is the resource name.
+    /// </summary>
+    /// <typeparam name="T">The type of the resource.</typeparam>
+    /// <param name="builder">The resource builder instance.</param>
+    /// <returns>The resource builder instance.</returns>
+    public static IResourceBuilder<T> WithDaprSidecar<T>(this IResourceBuilder<T> builder) where T : IResource
+    {
+        return builder.WithDaprSidecar(builder.Resource.Name);
+    }
+
+    /// <summary>
     /// Ensures that a Dapr sidecar is started for the resource.
     /// </summary>
     /// <typeparam name="T">The type of the resource.</typeparam>
