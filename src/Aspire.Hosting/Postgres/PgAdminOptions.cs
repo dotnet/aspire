46635e6dd1256a36a6ce76d25da5b11db947da9b
Andre Lins <andrevlins@gmail.com>
2023-11-30 01:17:34 -0300

    doc for PgAdminOptions

--- src/Aspire.Hosting/Postgres/PgAdminOptions.cs
+++ src/Aspire.Hosting/Postgres/PgAdminOptions.cs
@@ -2,8 +2,18 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 namespace Aspire.Hosting.Postgres;
+
+/// <summary>
+/// PgAdmin container image options.
+/// </summary>
 public sealed class PgAdminOptions
 {
+    /// <summary>
+    /// Gets or sets the default email to access the application.
+    /// </summary>
     public string DefaultEmail { get; set; } = "user@domain.com";
+    /// <summary>
+    /// Gets or sets the default password to access the application.
+    /// </summary>
     public string DefaultPassword { get; set; } = "SuperSecret";
 }
