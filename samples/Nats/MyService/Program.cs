a4dcc115ba1d90ddc0b0b3aa38508abcafe0d71a
Ziya Suzen <ziya@suzen.net>
2023-12-02 04:41:43 +0000

    NATS Aspire container

---
+++ samples/Nats/MyService/Program.cs
@@ -0,0 +1,17 @@
+using NATS.Client.Core;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.AddServiceDefaults();
+
+builder.AddNats("nats");
+
+var app = builder.Build();
+
+app.MapGet("/", async (INatsConnection nats) => $"""
+                                                 Hello NATS!
+                                                 rtt: {await nats.PingAsync()}
+                                                 {((NatsConnection)nats).ServerInfo}
+                                                 """);
+
+app.Run();
