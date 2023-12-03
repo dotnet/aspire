7f3c0488b4b0184eedd6cff6948c827d78c0f78a
Joe Whiteaker <47674962+josephaw1022@users.noreply.github.com>
2023-12-03 11:32:08 -0500

    fix db extensions to have to make it work with volume (file header and remove unnecassary volume mount)

--- src/Aspire.Hosting/Prometheus/PrometheusDBExtensions.cs
+++ src/Aspire.Hosting/Prometheus/PrometheusDBExtensions.cs
@@ -1,3 +1,6 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
 using System.Net.Sockets;
 using Aspire.Hosting.ApplicationModel;
 
@@ -13,7 +16,6 @@ public static class PrometheusBuilderExtensions
         return builder.AddResource(prometheusContainer)
                       .WithAnnotation(new ServiceBindingAnnotation(ProtocolType.Tcp, containerPort: 9090))
                       .WithAnnotation(new ContainerImageAnnotation { Image = "prom/prometheus", Tag = "latest" })
-                      .WithVolumeMount(dataVolumeName, "/prometheus")
-                      .WithVolumeMount(configFilePath, "/etc/prometheus/prometheus.yml");
+                      .WithVolumeMount(configFilePath, "/etc/prometheus");
     }
 }
