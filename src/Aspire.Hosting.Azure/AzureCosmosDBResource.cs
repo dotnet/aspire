45d2c917c9769d6e4d742248959c5844c2055e30
David Pine <david.pine@microsoft.com>
2023-12-06 14:27:31 -0600

    Remove unused usings.

--- src/Aspire.Hosting.Azure/AzureCosmosDBResource.cs
+++ src/Aspire.Hosting.Azure/AzureCosmosDBResource.cs
@@ -1,8 +1,6 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System.Globalization;
-using System.Text;
 using Aspire.Hosting.ApplicationModel;
 
 namespace Aspire.Hosting.Azure.Data.Cosmos;
