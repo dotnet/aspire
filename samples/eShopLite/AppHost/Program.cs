9f111f06d9f8be4350bbdaf97c4c05f66a058633
Kahbazi <akahbazi@gmail.com>
2023-12-07 19:02:13 +0330

    Undo the sample changes

--- samples/eShopLite/AppHost/Program.cs
+++ samples/eShopLite/AppHost/Program.cs
@@ -1,33 +1,32 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-//var catalogDb = builder.AddPostgresContainer("postgres").AddDatabase("catalogdb");
+var catalogDb = builder.AddPostgresContainer("postgres").AddDatabase("catalogdb");
 
-//var basketCache = builder.AddRedisContainer("basketcache");
+var basketCache = builder.AddRedisContainer("basketcache");
 
-//var catalogService = builder.AddProject<Projects.CatalogService>("catalogservice")
-//                     .WithReference(catalogDb)
-//                     .WithReplicas(2);
+var catalogService = builder.AddProject<Projects.CatalogService>("catalogservice")
+                     .WithReference(catalogDb)
+                     .WithReplicas(2);
 
-//var messaging = builder.AddRabbitMQContainer("messaging");
+var messaging = builder.AddRabbitMQContainer("messaging");
 
-//var basketService = builder.AddProject("basketservice", @"..\BasketService\BasketService.csproj")
-//                    .WithReference(basketCache)
-//                    .WithReference(messaging);
+var basketService = builder.AddProject("basketservice", @"..\BasketService\BasketService.csproj")
+                    .WithReference(basketCache)
+                    .WithReference(messaging);
 
 builder.AddProject<Projects.MyFrontend>("frontend")
-       //.WithReference(basketService)
-       //.WithReference(catalogService.GetEndpoint("http"))
-       .WithReference("petstore", new Uri("https://petstore.swagger.io"));
+       .WithReference(basketService)
+       .WithReference(catalogService.GetEndpoint("http"));
 
-//builder.AddProject<Projects.OrderProcessor>("orderprocessor")
-//       .WithReference(messaging)
-//       .WithLaunchProfile("OrderProcessor");
+builder.AddProject<Projects.OrderProcessor>("orderprocessor")
+       .WithReference(messaging)
+       .WithLaunchProfile("OrderProcessor");
 
-//builder.AddProject<Projects.ApiGateway>("apigateway")
-//       .WithReference(basketService)
-//       .WithReference(catalogService);
+builder.AddProject<Projects.ApiGateway>("apigateway")
+       .WithReference(basketService)
+       .WithReference(catalogService);
 
-//builder.AddProject<Projects.CatalogDb>("catalogdbapp")
-//       .WithReference(catalogDb);
+builder.AddProject<Projects.CatalogDb>("catalogdbapp")
+       .WithReference(catalogDb);
 
 builder.Build().Run();
