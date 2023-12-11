1f9abbc758b196122230b1337a294295bd2af54a
Adam Ratzman false <adamratzman@microsoft.com>
2023-12-11 12:12:47 -0500

    add copy to clipboard project file path

--- src/Aspire.Dashboard/Components/Controls/GridValue.razor.cs
+++ src/Aspire.Dashboard/Components/Controls/GridValue.razor.cs
@@ -13,6 +13,12 @@ public partial class GridValue
     public string? Value { get; set; }
 
     /// <summary>
+    /// If set, copies this value instead of <see cref="Value"/>.
+    /// </summary>
+    [Parameter]
+    public string? ValueToCopy { get; set; }
+
+    /// <summary>
     /// Determines whether or not masking support is enabled for this value
     /// </summary>
     [Parameter]
