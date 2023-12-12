2d27718c0e8b40492fb810eb814c5da2095fc6e8
Joe Whiteaker <47674962+josephaw1022@users.noreply.github.com>
2023-12-11 21:27:02 -0500

    remove azure hosting import

--- samples/eShopLite/AppHost/Program.cs
+++ samples/eShopLite/AppHost/Program.cs
@@ -1,7 +1,5 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddAzureProvisioning();
-
 builder.AddPrometheusContainer("prometheus", "../prometheus", "prom-data");
 
 var catalogDb = builder.AddPostgresContainer("postgres").AddDatabase("catalogdb");
