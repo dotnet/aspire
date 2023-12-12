dfb08377dbdfc57ff60a1f48b59bd3be72a0e53e
Joe Whiteaker <47674962+josephaw1022@users.noreply.github.com>
2023-12-12 18:17:17 -0500

    add comments to the prometheus container

--- src/Aspire.Hosting/Prometheus/PrometheusDBExtensions.cs
+++ src/Aspire.Hosting/Prometheus/PrometheusDBExtensions.cs
@@ -8,11 +8,31 @@ namespace Aspire.Hosting;
 
 public static class PrometheusBuilderExtensions
 {
+    /// <summary>
+    /// AddPrometheusContainer adds a Prometheus container to the application.
+    /// </summary>
+    /// <param name="builder">
+    /// <see cref="IDistributedApplicationBuilder"/>
+    /// </param>
+    /// <param name="name">
+    /// the name of the container
+    /// </param>
+    /// <param name="configFilePath">
+    /// the path to the Prometheus configuration file. The directory should contain prometheus.yml
+    /// </param>
+    /// <param name="dataVolumeName">
+    /// the name of the data volume to use for Prometheus data
+    /// </param>
+    /// <returns>
+    /// <see cref="IResourceBuilder{PrometheusContainerResource}"/>
+    /// </returns>
     public static IResourceBuilder<PrometheusContainerResource> AddPrometheusContainer(
         this IDistributedApplicationBuilder builder, string name, string configFilePath, string dataVolumeName)
     {
+        // Define the resource
         var prometheusContainer = new PrometheusContainerResource(name, configFilePath, dataVolumeName);
 
+        // Add the resource to the application
         return builder.AddResource(prometheusContainer)
                       .WithAnnotation(new ServiceBindingAnnotation(ProtocolType.Tcp, containerPort: 9090))
                       .WithAnnotation(new ContainerImageAnnotation { Image = "prom/prometheus", Tag = "latest" })
