d8467c78aec83aaf789962bd5045cba9cd48e0fa
Arvin Kahbazi <a.kahbazi@dotin.ir>
2023-12-13 10:28:07 +0330

    Revert "Include Executable WithArgs in manifest (#1366)"
    This reverts commit 0c3d01451c7e3a0f69bb166dce0e813615041f1c.

--- src/Aspire.Hosting/Publishing/ManifestPublisher.cs
+++ src/Aspire.Hosting/Publishing/ManifestPublisher.cs
@@ -132,26 +132,13 @@ public class ManifestPublisher(ILogger<ManifestPublisher> logger,
         context.Writer.WriteString("workingDirectory", relativePathToProjectFile);
 
         context.Writer.WriteString("command", executable.Command);
+        context.Writer.WriteStartArray("args");
 
-        var args = new List<string>(executable.Args ?? []);
-        if (executable.TryGetAnnotationsOfType<ExecutableArgsCallbackAnnotation>(out var argsCallback))
+        foreach (var arg in executable.Args ?? [])
         {
-            foreach (var callback in argsCallback)
-            {
-                callback.Callback(args);
-            }
+            context.Writer.WriteStringValue(arg);
         }
-
-        if (args.Count > 0)
-        {
-            context.Writer.WriteStartArray("args");
-
-            foreach (var arg in args)
-            {
-                context.Writer.WriteStringValue(arg);
-            }
-            context.Writer.WriteEndArray();
-        }
+        context.Writer.WriteEndArray();
 
         context.WriteEnvironmentVariables(executable);
         context.WriteBindings(executable);
