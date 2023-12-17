88a5cdabfbadbbe7ce52025cf77531a4664261b5
Drew Noakes <git@drewnoakes.com>
2023-12-18 10:56:03 +1100

    Complete ResourceViewModel construction from Resource message

--- src/Aspire.Dashboard/Model/ResourceServiceMessageExtensions.cs
+++ src/Aspire.Dashboard/Model/ResourceServiceMessageExtensions.cs
@@ -1,7 +1,8 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.Collections.Frozen;
+using System.Collections.Immutable;
 using Aspire.Dashboard.Utils;
 using Aspire.V1;
 
@@ -14,16 +15,37 @@ internal static class ResourceServiceMessageExtensions
         return new()
         {
             Name = resource.Name,
-            ResourceType = resource.Id.ResourceType,
+            ResourceType = resource.ResourceType,
             DisplayName = resource.DisplayName,
-            Uid = resource.Id.Uid,
+            Uid = resource.Uid,
             CreationTimeStamp = resource.CreatedAt.ToDateTime(),
             Properties = resource.Properties.ToFrozenDictionary(data => data.Name, data => data.Value, StringComparers.ResourceDataKey),
-            Endpoints = [], // TODO
-            Environment = [], // TODO
-            ExpectedEndpointsCount = 0, // TODO
-            Services = [], // TODO
+            Endpoints = GetEndpoints(),
+            Environment = GetEnvironment(),
+            ExpectedEndpointsCount = resource.ExpectedEndpointsCount,
+            Services = GetServices(),
             State = resource.HasState ? resource.State : null,
         };
+
+        ImmutableArray<ResourceServiceViewModel> GetServices()
+        {
+            return resource.Services
+                .Select(s => new ResourceServiceViewModel(s.Name, s.AllocatedAddress, s.AllocatedPort))
+                .ToImmutableArray();
+        }
+
+        ImmutableArray<EnvironmentVariableViewModel> GetEnvironment()
+        {
+            return resource.Environment
+                .Select(s => new EnvironmentVariableViewModel(s.Name, s.Value, s.IsFromSpec))
+                .ToImmutableArray();
+        }
+
+        ImmutableArray<EndpointViewModel> GetEndpoints()
+        {
+            return resource.Endpoints
+                .Select(e => new EndpointViewModel(e.EndpointUrl, e.ProxyUrl))
+                .ToImmutableArray();
+        }
     }
 }
