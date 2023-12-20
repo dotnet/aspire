875bd60133b43753915761182f3c95aea24607e0
Adam Ratzman <adamratzman@microsoft.com>
2023-12-20 15:01:14 -0500

    Use anchor + badge for badge link

--- src/Aspire.Dashboard/Components/ResourcesGridColumns/UnreadLogErrorsBadge.razor.cs
+++ src/Aspire.Dashboard/Components/ResourcesGridColumns/UnreadLogErrorsBadge.razor.cs
@@ -2,17 +2,13 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using Aspire.Dashboard.Model;
-using Microsoft.AspNetCore.Components;
 
 namespace Aspire.Dashboard.Components;
 
 public partial class UnreadLogErrorsBadge
 {
-    [Inject]
-    public required NavigationManager NavigationManager { get; set; }
-
-    private void ViewResourceErrorStructuredLogsUrl(ResourceViewModel resource)
+    private static string GetResourceErrorStructuredLogsUrl(ResourceViewModel resource)
     {
-        NavigationManager.NavigateTo($"/StructuredLogs/{resource.Uid}?level=error");
+        return $"/StructuredLogs/{resource.Uid}?level=error";
     }
 }
