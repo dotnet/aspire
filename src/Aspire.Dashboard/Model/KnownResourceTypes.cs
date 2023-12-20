a7e298c0a72ea2a773433f53e1a25c3eef8adaec
Drew Noakes <git@drewnoakes.com>
2023-12-21 10:16:34 +1100

    Remove obsolete TODO

--- src/Aspire.Dashboard/Model/KnownResourceTypes.cs
+++ src/Aspire.Dashboard/Model/KnownResourceTypes.cs
@@ -1,9 +1,8 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
 namespace Aspire.Dashboard.Model;
 
-// TODO use these constants throughout
 public static class KnownResourceTypes
 {
     public const string Executable = "Executable";
