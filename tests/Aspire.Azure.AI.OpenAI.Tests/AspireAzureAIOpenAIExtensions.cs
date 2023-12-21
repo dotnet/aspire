f51a735d6cc4ed1f07618f562004733fc228a36a
Sebastien Ros <sebastienros@gmail.com>
2023-12-20 16:37:11 -0800

    Fix tests

--- tests/Aspire.Azure.AI.OpenAI.Tests/AspireAzureAIOpenAIExtensions.cs
+++ tests/Aspire.Azure.AI.OpenAI.Tests/AspireAzureAIOpenAIExtensions.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Xunit;
 
-namespace Aspire.Azure.Messaging.ServiceBus.Tests;
+namespace Aspire.Azure.AI.OpenAI.Tests;
 
 public class AspireServiceBusExtensionsTests
 {
--- tests/Aspire.Azure.AI.OpenAI.Tests/ConformanceTests.cs
+++ tests/Aspire.Azure.AI.OpenAI.Tests/ConformanceTests.cs
@@ -1,16 +1,16 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using Aspire.Azure.AI.OpenAI;
 using Aspire.Components.ConformanceTests;
 using Azure.AI.OpenAI;
 using Azure.Identity;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace Aspire.Azure.Messaging.ServiceBus.Tests;
+namespace Aspire.Azure.AI.OpenAI.Tests;
 
-public abstract class ConformanceTests : ConformanceTests<OpenAIClient, AzureOpenAISettings>
+public class ConformanceTests : ConformanceTests<OpenAIClient, AzureOpenAISettings>
 {
     // Fake connection string for cases when credentials are unavailable and need to switch to raw connection string
     protected const string ServiceUri = "https://aspireopenaitests.openai.azure.com/";
@@ -18,7 +18,11 @@ public abstract class ConformanceTests : ConformanceTests<OpenAIClient, AzureOpe
 
     protected override ServiceLifetime ServiceLifetime => ServiceLifetime.Singleton;
 
-    protected override string[] RequiredLogCategories => new string[] { "Azure.AI.OpenAI" };
+    protected override string[] RequiredLogCategories => new string[] {
+        "Azure.Core",
+        // Not triggered because no connection is made
+        // "Azure.AI.OpenAI",
+    };
 
     protected override bool SupportsKeyedRegistrations => true;
 
@@ -51,19 +55,22 @@ public abstract class ConformanceTests : ConformanceTests<OpenAIClient, AzureOpe
     protected override (string json, string error)[] InvalidJsonToErrorMessage => new[]
         {
             ("""{"Aspire": { "Azure": { "AI":{ "OpenAI": {"ServiceUri": "YOUR_URI"}}}}}""", "Value does not match format \"uri\""),
-            ("""{"Aspire": { "Azure": { "AI":{ "OpenAI": {"ServiceUri": "http://YOUR_URI", "Trace": "false"}}}}}""", "Value is \"string\" but should be \"boolean\""),
-            ("""{"Aspire": { "Azure": { "AI":{ "OpenAI": {"ClientOptions": {"CustomEndpointAddress": "EndPoint"}}}}}}""", "Value does not match format \"uri\""),
-            ("""{"Aspire": { "Azure": { "AI":{ "OpenAI": {"ClientOptions": {"EnableCrossEntityTransactions": "false"}}}}}}""", "Value is \"string\" but should be \"boolean\""),
-            ("""{"Aspire": { "Azure": { "AI":{ "OpenAI": {"ClientOptions": {"RetryOptions": {"Mode": "Fast"}}}}}}}""", "Value should match one of the values specified by the enum"),
-            ("""{"Aspire": { "Azure": { "AI":{ "OpenAI": {"ClientOptions": {"RetryOptions": {"TryTimeout": "3S"}}}}}}}""", "Value does not match format \"duration\""),
-            ("""{"Aspire": { "Azure": { "AI":{ "OpenAI": {"ClientOptions": {"TransportType": "HTTP"}}}}}}""", "Value should match one of the values specified by the enum")
+            ("""{"Aspire": { "Azure": { "AI":{ "OpenAI": {"ServiceUri": "http://YOUR_URI", "Tracing": "false"}}}}}""", "Value is \"string\" but should be \"boolean\""),
         };
+
+    protected override string ActivitySourceName => throw new NotImplementedException();
 
     // When credentials are not available, we switch to using raw connection string (otherwise we get CredentialUnavailableException)
     protected KeyValuePair<string, string?> GetMainConfigEntry(string? key)
         => CanConnectToServer
                 ? new(CreateConfigKey("Aspire:Azure:AI:OpenAI", key, nameof(AzureOpenAISettings.ServiceUri)), ServiceUri)
                 : new(CreateConfigKey("Aspire:Azure:AI:OpenAI", key, nameof(AzureOpenAISettings.Key)), Key);
+
+    protected override void PopulateConfiguration(ConfigurationManager configuration, string? key = null)
+        => configuration.AddInMemoryCollection(new KeyValuePair<string, string?>[]
+        {
+            new(CreateConfigKey("Aspire:Azure:AI:OpenAI", key, "ServiceUri"), ServiceUri)
+        });
 
     protected override void RegisterComponent(HostApplicationBuilder builder, Action<AzureOpenAISettings>? configure = null, string? key = null)
     {
@@ -82,13 +89,20 @@ public abstract class ConformanceTests : ConformanceTests<OpenAIClient, AzureOpe
             {
                 settings.Credential = new DefaultAzureCredential();
             }
+
             configure?.Invoke(settings);
         }
     }
+
+    protected override void SetHealthCheck(AzureOpenAISettings options, bool enabled)
+        => throw new NotImplementedException();
 
     protected override void SetMetrics(AzureOpenAISettings options, bool enabled)
         => throw new NotImplementedException();
 
     protected override void SetTracing(AzureOpenAISettings options, bool enabled)
         => options.Tracing = enabled;
+
+    protected override void TriggerActivity(OpenAIClient service)
+        => service.GetCompletions(new CompletionsOptions { DeploymentName = "dummy-gpt" });
 }
