9f3307612f22e97b019d20564a742f61a86e96ca
Drew Noakes <git@drewnoakes.com>
2023-12-21 14:53:42 +1100

    Remove redundant route on gRPC endpoint
    A browser will request using HTTP/1.1, and we only accept HTTP/2, so this route doesn't achieve anything.

--- src/Aspire.Hosting/Dashboard/DashboardServiceHost.cs
+++ src/Aspire.Hosting/Dashboard/DashboardServiceHost.cs
@@ -51,7 +51,6 @@ internal sealed class DashboardServiceHost : IHostedService
         _app = builder.Build();
 
         _app.MapGrpcService<DashboardService>();
-        _app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
         return;
 
