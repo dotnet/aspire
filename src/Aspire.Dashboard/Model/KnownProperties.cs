e7c02f0bbc833cf3079712b8f77fd23828dc582f
Drew Noakes <git@drewnoakes.com>
2023-12-21 14:13:38 +1100

    Add API doc

--- src/Aspire.Dashboard/Model/KnownProperties.cs
+++ src/Aspire.Dashboard/Model/KnownProperties.cs
@@ -1,8 +1,17 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using Aspire.Dashboard.Utils;
+
 namespace Aspire.Dashboard.Model;
 
+/// <summary>
+/// Models some known property names for known types of resources.
+/// </summary>
+/// <remarks>
+/// Used as keys in the "properties" dictionary on resource snapshots and view models.
+/// Should be compared using <see cref="StringComparers.ResourcePropertyName"/>.
+/// </remarks>
 public static class KnownProperties
 {
     public static class Resource
