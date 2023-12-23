4167fc09a03df74479fe80985a91442ce29c48ff
Mitch Denny <midenn@microsoft.com>
2023-12-23 12:33:35 +1100

    First stab at secret resource implementation.

--- samples/eShopLite/AppHost/Program.cs
+++ samples/eShopLite/AppHost/Program.cs
@@ -1,5 +1,7 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var apiKey = builder.AddSecretStore("secretstore").AddSecret("apikey");
+
 var catalogDb = builder.AddPostgres("postgres").AddDatabase("catalogdb");
 
 var basketCache = builder.AddRedis("basketcache");
@@ -16,6 +18,7 @@ var basketService = builder.AddProject("basketservice", @"..\BasketService\Baske
 
 builder.AddProject<Projects.MyFrontend>("frontend")
        .WithReference(basketService)
+       .WithEnvironment("API_KEY", apiKey)
        .WithReference(catalogService.GetEndpoint("http"));
 
 builder.AddProject<Projects.OrderProcessor>("orderprocessor")
