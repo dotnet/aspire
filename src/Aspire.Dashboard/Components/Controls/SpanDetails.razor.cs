214cff2caac11f9957612c484b9f73b8a5e895cb
James Newton-King <james@newtonking.com>
2024-01-02 11:29:29 +0800

    Filtering

--- src/Aspire.Dashboard/Components/Controls/SpanDetails.razor.cs
+++ src/Aspire.Dashboard/Components/Controls/SpanDetails.razor.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using Aspire.Dashboard.Model;
+using Aspire.Dashboard.Otlp.Model;
 using Microsoft.AspNetCore.Components;
 using Microsoft.FluentUI.AspNetCore.Components;
 
@@ -24,6 +25,9 @@ public partial class SpanDetails
                 vm.Name.Contains(_filter, StringComparison.CurrentCultureIgnoreCase) ||
                 vm.Value?.Contains(_filter, StringComparison.CurrentCultureIgnoreCase) == true
         )?.AsQueryable();
+
+    private IQueryable<OtlpSpanEvent> FilteredSpanEvents =>
+        ViewModel.Span.Events.Where(e => e.Name.Contains(_filter, StringComparison.CurrentCultureIgnoreCase)).AsQueryable();
 
     private string _filter = "";
 
