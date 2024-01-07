fecc2050e37d2dc5e6f2cfd7f037b010b66a0e59
David Pine <david.pine.7@gmail.com>
2024-01-07 12:24:15 -0600

    Address feedback

--- src/Aspire.Hosting.Azure/AzureCosmosDBResource.cs
+++ src/Aspire.Hosting.Azure/AzureCosmosDBResource.cs
@@ -33,7 +33,7 @@ public class AzureCosmosDBResource(string name, string? connectionString)
 
     private int GetEmulatorPort(string endpointName) =>
         Annotations
-            .OfType<EndpointAnnotation>()
+            .OfType<AllocatedEndpointAnnotation>()
             .FirstOrDefault(x => x.Name == endpointName)
             ?.Port
         ?? throw new DistributedApplicationException($"Azure Cosmos DB resource does not have endpoint annotation with name '{endpointName}'.");
