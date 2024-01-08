dde16515ab002e855387fa2b0fb956b4fed26500
Virgil Voillot <voillot.virgil@gmail.com>
2024-01-08 14:58:42 -0500

    Remove useless method

--- src/Aspire.Hosting/Keycloak/KeycloakContainerResource.cs
+++ src/Aspire.Hosting/Keycloak/KeycloakContainerResource.cs
@@ -9,21 +9,4 @@ namespace Aspire.Hosting.ApplicationModel;
 /// <param name="name">The name of the resource.</param>
 public class KeycloakContainerResource(string name) : ContainerResource(name), IResourceWithEnvironment, IResourceWithServiceDiscovery
 {
-
-    /// <summary>
-    /// Gets the endpoint string for the Keycloak server.
-    /// </summary>
-    /// <returns>A endpoint string for the Keycloak server in the form "host:port".</returns>
-    public string? GetEndpoints()
-    {
-        if (!this.TryGetAllocatedEndPoints(out var allocatedEndpoints))
-        {
-            throw new DistributedApplicationException("Expected allocated endpoints!");
-        }
-
-        var allocatedEndpoint = allocatedEndpoints.Single(); // We should only have one endpoint.
-
-        var endpoint = $"{allocatedEndpoint.Address}:{allocatedEndpoint.Port}";
-        return endpoint;
-    }
 }
