fb73fcef3dc67be99b636e7d17426c3985ec4033
Virgil Voillot <virgil.voillot@momentum-tech.ca>
2024-01-08 14:08:52 -0500

    Add Keycloak container resource

---
+++ src/Aspire.Hosting/Keycloak/KeycloakBuilderExtension.cs
@@ -0,0 +1,35 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Net.Sockets;
+using Aspire.Hosting.ApplicationModel;
+using Aspire.Hosting.Publishing;
+
+namespace Aspire.Hosting;
+
+public static class KeycloakBuilderExtensions
+{
+    private const int DefaultContainerPort = 8080;
+
+    public static IResourceBuilder<KeycloakContainerResource> AddKeycloakContainer(
+        this IDistributedApplicationBuilder builder,
+        string name,
+        int? port = null)
+    {
+        var keycloakContainer = new KeycloakContainerResource(name);
+
+        return builder
+            .AddResource(keycloakContainer)
+            .WithManifestPublishingCallback(WriteKeycloakContainerToManifest)
+            .WithAnnotation(new EndpointAnnotation(ProtocolType.Tcp, uriScheme: "http", port: port, containerPort: DefaultContainerPort))
+            .WithAnnotation(new ContainerImageAnnotation { Image = "quay.io/keycloak/keycloak", Tag = "latest" })
+            .WithEnvironment("KEYCLOAK_ADMIN","admin")
+            .WithEnvironment("KEYCLOAK_ADMIN_PASSWORD","admin")
+            .WithArgs("start-dev");
+    }
+
+    private static void WriteKeycloakContainerToManifest(this ManifestPublishingContext context)
+    {
+        context.Writer.WriteString("type", "keycloak.server.v0");
+    }
+}
---
+++ src/Aspire.Hosting/Keycloak/KeycloakContainerResource.cs
@@ -0,0 +1,29 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace Aspire.Hosting.ApplicationModel;
+
+/// <summary>
+/// A resource that represents a Keycloak container.
+/// </summary>
+/// <param name="name">The name of the resource.</param>
+public class KeycloakContainerResource(string name) : ContainerResource(name), IResourceWithEnvironment, IResourceWithServiceDiscovery
+{
+
+    /// <summary>
+    /// Gets the endpoint string for the Keycloak server.
+    /// </summary>
+    /// <returns>A endpoint string for the Keycloak server in the form "host:port".</returns>
+    public string? GetEndpoints()
+    {
+        if (!this.TryGetAllocatedEndPoints(out var allocatedEndpoints))
+        {
+            throw new DistributedApplicationException("Expected allocated endpoints!");
+        }
+
+        var allocatedEndpoint = allocatedEndpoints.Single(); // We should only have one endpoint.
+
+        var endpoint = $"{allocatedEndpoint.Address}:{allocatedEndpoint.Port}";
+        return endpoint;
+    }
+}
