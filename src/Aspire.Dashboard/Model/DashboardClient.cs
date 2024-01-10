403f579d229e5e53a373586300c953314b0ec30e
Drew Noakes <git@drewnoakes.com>
2024-01-10 15:22:06 +1100

    Add DashboardClient tests

--- src/Aspire.Dashboard/Model/DashboardClient.cs
+++ src/Aspire.Dashboard/Model/DashboardClient.cs
@@ -117,6 +117,9 @@ internal sealed class DashboardClient : IDashboardClient
         }
     }
 
+    // For testing purposes
+    internal int OutgoingResourceSubscriberCount => _outgoingChannels.Count;
+
     private void EnsureInitialized()
     {
         var priorState = Interlocked.CompareExchange(ref _state, StateInitialized, comparand: StateNone);
@@ -286,6 +289,8 @@ internal sealed class DashboardClient : IDashboardClient
     {
         EnsureInitialized();
 
+        var clientCancellationToken = _cts.Token;
+
         lock (_lock)
         {
             // There are two types of channel in this class. This is not a gRPC channel.
@@ -300,13 +305,13 @@ internal sealed class DashboardClient : IDashboardClient
                 InitialState: _resourceByName.Values.ToImmutableArray(),
                 Subscription: StreamUpdates());
 
-            async IAsyncEnumerable<ResourceViewModelChange> StreamUpdates([EnumeratorCancellation] CancellationToken cancellationToken = default)
+            async IAsyncEnumerable<ResourceViewModelChange> StreamUpdates([EnumeratorCancellation] CancellationToken enumeratorCancellationToken = default)
             {
                 try
                 {
-                    using var combinedTokens = CancellationTokenSource.CreateLinkedTokenSource(_cts.Token, cancellationToken);
+                    using var cts = CancellationTokenSource.CreateLinkedTokenSource(clientCancellationToken, enumeratorCancellationToken);
 
-                    await foreach (var batch in channel.Reader.ReadAllAsync(combinedTokens.Token).ConfigureAwait(false))
+                    await foreach (var batch in channel.Reader.ReadAllAsync(cts.Token).ConfigureAwait(false))
                     {
                         yield return batch;
                     }
@@ -346,6 +351,8 @@ internal sealed class DashboardClient : IDashboardClient
     {
         if (Interlocked.Exchange(ref _state, StateDisposed) is not StateDisposed)
         {
+            _outgoingChannels = [];
+
             await _cts.CancelAsync().ConfigureAwait(false);
 
             _cts.Dispose();
