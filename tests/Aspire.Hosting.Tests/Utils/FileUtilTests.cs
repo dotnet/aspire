097f71b0d6061d60218c23f590970ccf44b5d875
Drew Noakes <git@drewnoakes.com>
2024-01-10 13:47:40 +1100

    Make testable and improve test quality
    The tests no longer rely on external state such as:
    
    - Environment variables
    - Path separators
    - Querying the file system
    - The current platform (for the executable suffix)
    
    Tests now pass these values in explicitly, in order to control and observe all interaction with the function.

--- tests/Aspire.Hosting.Tests/Utils/FileUtilTests.cs
+++ tests/Aspire.Hosting.Tests/Utils/FileUtilTests.cs
@@ -6,41 +6,58 @@ using Aspire.Hosting.Utils;
 
 namespace Aspire.Hosting.Tests.Utils;
 
-public class FileUtilTests
+public static class FileUtilTests
 {
+    private static string PathA { get; } = Path.Combine("A", "dotnet.exe");
+    private static string PathB { get; } = Path.Combine("B", "dotnet.exe");
+    private static string PathC { get; } = Path.Combine("C", "dotnet.exe");
+
     [Fact]
-    public void FindFullPath()
+    public static void FindFullPathFromPath_Found()
     {
-        var fullPath = FileUtil.FindFullPathFromPath("dotnet");
-        
-        var dir = Path.GetDirectoryName(fullPath);
-        Assert.False(string.IsNullOrEmpty(dir));
-        
-        var ext = Path.GetExtension(fullPath);
-        Assert.Equal(FileNameSuffixes.CurrentPlatform.Exe, ext);
+        const string pathVariable = @"A;B;C:\Program Files\dotnet\bin;C";
+
+        var dotnetPath = Path.Combine(@"C:\Program Files\dotnet\bin", "dotnet.exe");
+
+        var filesChecked = new List<string>();
+
+        var fullPath = FileUtil.FindFullPathFromPath(
+            "dotnet",
+            pathVariable,
+            FileNameSuffixes.DotNet,
+            pathSeparator: ';',
+            // Remember the files we check, and look for our known dotnet.exe path.
+            p => { filesChecked.Add(p); return p == dotnetPath; });
+
+        // PathC is not checked once a match is found
+        Assert.Equal(
+            [PathA, PathB, dotnetPath],
+            (IEnumerable<string>)filesChecked);
+
+        Assert.Equal(dotnetPath, fullPath);
     }
 
     [Fact]
-    public void FindFullPath_ExecutableWithExtension_NotFound()
+    public static void FindFullPathFromPath_NotFound()
     {
-        var executable = $"dotnet-dotnet-dotnet{FileNameSuffixes.CurrentPlatform.Exe}";
-        var fullPath = FileUtil.FindFullPathFromPath(executable);
-        
-        var dir = Path.GetDirectoryName(fullPath);
-        Assert.True(string.IsNullOrEmpty(dir));
-        
-        Assert.Equal(executable, fullPath);
-    }
-    
-    [Fact]
-    public void FindFullPath_ExecutableWithoutExtension_NotFound()
-    {
-        var executable = "dotnet-dotnet-dotnet";
-        var fullPath = FileUtil.FindFullPathFromPath(executable);
-        
-        var dir = Path.GetDirectoryName(fullPath);
-        Assert.True(string.IsNullOrEmpty(dir));
-        
-        Assert.Equal($"{executable}{FileNameSuffixes.CurrentPlatform.Exe}", fullPath);
+        const string pathVariable = @"A;B;C";
+
+        var filesChecked = new List<string>();
+
+        var fullPath = FileUtil.FindFullPathFromPath(
+            "dotnet",
+            pathVariable,
+            FileNameSuffixes.DotNet,
+            pathSeparator: ';',
+            // Remember the files we check, and never indicate a match.
+            p => { filesChecked.Add(p); return false; });
+
+        // All possible paths were checked.
+        Assert.Equal(
+            [PathA, PathB, PathC],
+            (IEnumerable<string>)filesChecked);
+
+        // The executable has had ".exe" appended.
+        Assert.Equal("dotnet.exe", fullPath);
     }
 }
