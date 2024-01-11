8e1a12aa27df55a40303e10bf0c003dd6a032cc4
David Pine <david.pine@microsoft.com>
2024-01-11 06:38:28 -0600

    Add a `UseEmulator` extension method for Azure Cosmos DB (#1244)

--- src/Aspire.Hosting.Azure/AzureCosmosDBResource.cs
+++ src/Aspire.Hosting.Azure/AzureCosmosDBResource.cs
@@ -14,6 +14,8 @@ namespace Aspire.Hosting.Azure.Data.Cosmos;
 public class AzureCosmosDBResource(string name, string? connectionString)
     : Resource(name), IResourceWithConnectionString, IAzureResource
 {
+    private readonly Collection<AzureCosmosDBDatabaseResource> _databases = new();
+
     /// <summary>
     /// Gets or sets the connection string for the Azure Cosmos DB resource.
     /// </summary>
@@ -23,11 +25,26 @@ public class AzureCosmosDBResource(string name, string? connectionString)
     /// Gets the connection string to use for this database.
     /// </summary>
     /// <returns>The connection string to use for this database.</returns>
-    public string? GetConnectionString() => ConnectionString;
+    public string? GetConnectionString() => IsEmulator
+        ? AzureCosmosDBEmulatorConnectionString.Create(GetEmulatorPort("emulator"))
+        : ConnectionString;
 
-    private readonly Collection<AzureCosmosDBDatabaseResource> _databases = new();
+    /// <summary>
+    /// Gets a value indicating whether the Azure Cosmos DB resource is running in the local emulator.
+    /// </summary>
+    public bool IsEmulator => this.IsContainer();
 
+    /// <summary>
+    /// Gets a collection of Azure Cosmos DB database resources.
+    /// </summary>
     public IReadOnlyCollection<AzureCosmosDBDatabaseResource> Databases => _databases;
+
+    private int GetEmulatorPort(string endpointName) =>
+        Annotations
+            .OfType<AllocatedEndpointAnnotation>()
+            .FirstOrDefault(x => x.Name == endpointName)
+            ?.Port
+        ?? throw new DistributedApplicationException($"Azure Cosmos DB resource does not have endpoint annotation with name '{endpointName}'.");
 
     internal void AddDatabase(AzureCosmosDBDatabaseResource database)
     {
@@ -37,4 +54,17 @@ public class AzureCosmosDBResource(string name, string? connectionString)
         }
         _databases.Add(database);
     }
+}
+
+file static class AzureCosmosDBEmulatorConnectionString
+{
+    /// <summary>
+    /// Gets the well-known and documented Azure Cosmos DB emulator account key.
+    /// See <a href="https://learn.microsoft.com/azure/cosmos-db/emulator#authentication"></a>
+    /// </summary>
+    private const string ConnectionStringHeader = """
+        AccountKey=C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==;
+        """;
+
+    public static string Create(int port) => $"{ConnectionStringHeader}AccountEndpoint=https://127.0.0.1:{port};";
 }
