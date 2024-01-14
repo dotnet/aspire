732c8f16220d11b97e779630e32b1eb090cb6307
Ziya Suzen <ziya@suzen.net>
2024-01-14 20:31:42 +0000

    Add JetStream option

--- src/Aspire.Hosting.Nats/NatsBuilderExtensions.cs
+++ src/Aspire.Hosting.Nats/NatsBuilderExtensions.cs
@@ -10,13 +10,38 @@ namespace Aspire.Hosting.Nats;
 public static class NatsBuilderExtensions
 {
     public static IResourceBuilder<NatsContainerResource> AddNatsContainer(
-        this IDistributedApplicationBuilder builder, string name, int? port = null)
+        this IDistributedApplicationBuilder builder, string name, int? port = null, bool enableJetStream = false, string? mountPath = null)
     {
         var nats = new NatsContainerResource(name);
-        return builder.AddResource(nats)
+        var resourceBuilder = builder.AddResource(nats)
             .WithManifestPublishingCallback(WriteNatsResourceToManifest)
             .WithAnnotation(new ServiceBindingAnnotation(ProtocolType.Tcp, uriScheme: "nats", port: port, containerPort: 4222))
             .WithAnnotation(new ContainerImageAnnotation { Image = "nats", Tag = "latest" });
+
+        if (enableJetStream)
+        {
+            resourceBuilder
+                .WithAnnotation(new ExecutableArgsCallbackAnnotation(updatedArgs =>
+                {
+                    if (enableJetStream)
+                    {
+                        updatedArgs.Add("-js");
+                    }
+
+                    if (mountPath != null)
+                    {
+                        updatedArgs.Add("-sd");
+                        updatedArgs.Add("/data");
+                    }
+                }));
+
+            if (mountPath != null)
+            {
+                resourceBuilder.WithAnnotation(new VolumeMountAnnotation(mountPath, "/data"));
+            }
+        }
+
+        return resourceBuilder;
     }
 
     private static void WriteNatsResourceToManifest(ManifestPublishingContext context)
