fbba8cf3d65519ed8590d6b551f6187e1e8a5f65
Mitch Denny <midenn@microsoft.com>
2024-01-21 15:35:41 +1100

    Remove STJ serializer.

--- src/Playgrounds/CosmosEndToEnd/CosmosEndToEnd.ApiService/Program.cs
+++ src/Playgrounds/CosmosEndToEnd/CosmosEndToEnd.ApiService/Program.cs
@@ -1,26 +1,18 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System.Text.Json.Serialization;
-using CosmosEndToEnd.ApiService;
 using Microsoft.Azure.Cosmos;
+using Newtonsoft.Json;
 
 var sessionId = Guid.NewGuid().ToString();
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.AddServiceDefaults();
-builder.AddAzureCosmosDB("db",
-                         settings =>
-                            {
-                                settings.IgnoreEmulatorCertificate = true;
-                            },
-                         clientOptions =>
-                         {
-                             // Default serializer for Cosmos V3 client is JSON.NET, this changes
-                             // us to use S.T.J for this playground.
-                             clientOptions.Serializer = new StjSerializer(new System.Text.Json.JsonSerializerOptions());
-                         });
+builder.AddAzureCosmosDB("db", settings =>
+{
+    settings.IgnoreEmulatorCertificate = true;
+});
 
 var app = builder.Build();
 
@@ -30,7 +22,8 @@ app.MapGet("/", async (CosmosClient cosmosClient) =>
     var container = (await db.CreateContainerIfNotExistsAsync("entries", "/sessionId").ConfigureAwait(false)).Container;
 
     // Add an entry to the database on each request.
-    await container.CreateItemAsync(new Entry(Guid.NewGuid().ToString(), sessionId)).ConfigureAwait(false);
+    var newEntry = new Entry() { Id = Guid.NewGuid().ToString(), SessionId = Guid.NewGuid().ToString() };
+    await container.CreateItemAsync(newEntry).ConfigureAwait(false);
 
     var entries = new List<Entry>();
     var iterator = container.GetItemQueryIterator<Entry>(requestOptions: new QueryRequestOptions() { MaxItemCount = 5 });
@@ -56,11 +49,11 @@ app.MapGet("/", async (CosmosClient cosmosClient) =>
 
 app.Run();
 
-public class Entry(string id, string sessionId)
+public class Entry
 {
-    [JsonPropertyName("id")]
-    public string Id { get; set; } = id;
+    [JsonProperty("id")]
+    public string? Id { get; set; }
 
-    [JsonPropertyName("sessionId")]
-    public string SessionId { get; set; } = sessionId;
+    [JsonProperty("sessionId")]
+    public string? SessionId { get; set; }
 }
