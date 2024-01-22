b39155a309e6327f1eb5ddc4721cd4ea2125d085
James Newton-King <james@newtonking.com>
2024-01-23 06:31:40 +0800

    Fix

--- src/Aspire.Hosting/Extensions/ContainerResourceBuilderExtensions.cs
+++ src/Aspire.Hosting/Extensions/ContainerResourceBuilderExtensions.cs
@@ -1,7 +1,6 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System.Net.Sockets;
 using Aspire.Hosting.ApplicationModel;
 
 namespace Aspire.Hosting;
