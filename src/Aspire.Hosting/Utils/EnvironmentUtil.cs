719fdc6e46240a9b8aad1ab8c32e29ed445544c4
Drew Noakes <git@drewnoakes.com>
2024-01-23 11:28:56 +1100

    Require parsed URI to be absolute

--- src/Aspire.Hosting/Utils/EnvironmentUtil.cs
+++ src/Aspire.Hosting/Utils/EnvironmentUtil.cs
@@ -7,6 +7,13 @@ namespace Aspire.Hosting.Utils;
 
 internal static class EnvironmentUtil
 {
+    /// <summary>
+    /// Parses a environment variable's semicolon-delimited value into an array of <see cref="Uri"/> objects.
+    /// </summary>
+    /// <param name="variableName">The name of the environment variable.</param>
+    /// <param name="defaultValue">A default value, for when the environment variable is unspecified or white space. May be <see langword="null"/>.</param>
+    /// <returns>The parsed values, or the default value if specified and parsing failed. Returns <see langword="null"/> if <paramref name="defaultValue"/> is <see langword="null"/> and parsing failed.</returns>
+    /// <exception cref="InvalidOperationException">The environment variable could not be accessed, or contained an unparseable value.</exception>
     [return: NotNullIfNotNull(nameof(defaultValue))]
     public static Uri[]? GetAddressUris(string variableName, Uri? defaultValue)
     {
@@ -26,7 +33,7 @@ internal static class EnvironmentUtil
             {
                 return urls
                     .Split(';', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)
-                    .Select(url => new Uri(url))
+                    .Select(url => new Uri(url, UriKind.Absolute))
                     .ToArray();
             }
         }
