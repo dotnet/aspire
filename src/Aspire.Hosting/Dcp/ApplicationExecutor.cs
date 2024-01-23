bac6099fe85226c665b23272181fdeb5387359b3
Drew Noakes <git@drewnoakes.com>
2024-01-23 15:38:45 +1100

    Ensure dashboard is started first
    When F5'ing one of the playground solutions in the dotnet/aspire repo, the dashboard is added as the last resource. This means it starts after all other resources, which is not what we want. This change ensures it is always started first.

--- src/Aspire.Hosting/Dcp/ApplicationExecutor.cs
+++ src/Aspire.Hosting/Dcp/ApplicationExecutor.cs
@@ -81,9 +81,19 @@ internal sealed class ApplicationExecutor(ILogger<ApplicationExecutor> logger,
         AspireEventSource.Instance.DcpModelCreationStart();
         try
         {
-            if (!_model.Resources.Any(static r => StringComparers.ResourceName.Equals(r.Name, KnownResourceNames.AspireDashboard)))
+            var (dashboardIndex, dashboardResource) = _model.Resources.IndexOf(static r => !StringComparers.ResourceName.Equals(r.Name, KnownResourceNames.AspireDashboard));
+
+            if (dashboardIndex is -1)
             {
+                // No dashboard is specified, so start one.
+                // TODO validate that the dashboard has not been suppressed
                 await StartDashboardAsync(cancellationToken).ConfigureAwait(false);
+            }
+            else if (dashboardIndex is not 0)
+            {
+                // A dashboard exists in the set. Move it to the front so it starts first.
+                _model.Resources.RemoveAt(dashboardIndex);
+                _model.Resources.Insert(0, dashboardResource);
             }
 
             PrepareServices();
