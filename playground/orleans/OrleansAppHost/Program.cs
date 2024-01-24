f6bb40bee591e8489ed2ff5389dab61d9359c39b
David Negstad <David.Negstad@microsoft.com>
2024-01-23 17:30:54 -0800

    Update comments on default path to dashboard

--- playground/orleans/OrleansAppHost/Program.cs
+++ playground/orleans/OrleansAppHost/Program.cs
@@ -33,8 +33,8 @@ builder.AddProject<Projects.OrleansServer>("silo")
 // This project is only added in playground projects to support development/debugging
 // of the dashboard. It is not required in end developer code. Comment out this code
 // to test end developer dashboard launch experience. Refer to Directory.Build.props
-// for the path to the dashboard binary (defaults to a relative path to the artifacts
-// directory).
+// for the path to the dashboard binary (defaults to the Aspire.Dashboard bin output
+// in the artifacts dir).
 builder.AddProject<Projects.Aspire_Dashboard>(KnownResourceNames.AspireDashboard)
     .WithEnvironment("DOTNET_DASHBOARD_GRPC_ENDPOINT_URL", "http://localhost:5555")
     .ExcludeFromManifest();
