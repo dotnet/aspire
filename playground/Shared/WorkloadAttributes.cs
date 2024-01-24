f97026f6cd66f1b9e75acbb3394b93058ee953c2
David Negstad <50252651+danegsta@users.noreply.github.com>
2024-01-23 18:16:35 -0800

    [dashboard-oop] Update dashboard attribute config for playground projects (#1799)
    * Update attribute handling for playground projects
    
    * Update comments on default path to dashboard

--- playground/Shared/WorkloadAttributes.cs
+++
@@ -1,9 +0,0 @@
-// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-
-using System.Reflection;
-
-// In our released product this attribute is added automatically by the SDK, here we reference
-// the path relative to the AppHost working directory and code inside the ApplicationExecutor
-// resolves it to the full path.
-[assembly:AssemblyMetadata("aspiredashboardpath", "..\\..\\..\\artifacts\\bin\\Aspire.Dashboard\\Debug\\net8.0\\Aspire.Dashboard.dll")]
