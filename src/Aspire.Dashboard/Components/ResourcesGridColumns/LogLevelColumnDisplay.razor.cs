9d4c6c20348307358158499cfa17fce81d86492a
Adam Ratzman <adamratzman@microsoft.com>
2024-01-24 10:54:34 -0500

    add tooltip with error info to log

---
+++ src/Aspire.Dashboard/Components/ResourcesGridColumns/LogLevelColumnDisplay.razor.cs
@@ -0,0 +1,26 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Globalization;
+using System.Text;
+using Aspire.Dashboard.Otlp.Model;
+
+namespace Aspire.Dashboard.Components;
+
+public partial class LogLevelColumnDisplay
+{
+    private string? GetErrorInformation()
+    {
+        if (LogEntry.Properties.GetValue("exception.type") is { } exceptionType)
+        {
+            var tooltip = new StringBuilder(LogEntry.Message);
+            tooltip
+                .Append(Environment.NewLine)
+                .Append(CultureInfo.CurrentCulture, $"{exceptionType}: {LogEntry.Properties.GetValue("exception.message")}")
+                .Append(Environment.NewLine)
+                .Append(LogEntry.Properties.GetValue("exception.stacktrace"));
+        }
+
+        return null;
+    }
+}
