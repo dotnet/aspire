3f8f9d07691bca121b79bbbaebb37b9683eb5d20
David Negstad <David.Negstad@microsoft.com>
2024-01-23 17:17:40 -0800

    Update attribute handling for playground projects

--- playground/CosmosEndToEnd/CosmosEndToEnd.AppHost/Program.cs
+++ playground/CosmosEndToEnd/CosmosEndToEnd.AppHost/Program.cs
@@ -12,7 +12,7 @@ builder.AddProject<Projects.CosmosEndToEnd_ApiService>("api")
 
 // This project is only added in playground projects to support development/debugging
 // of the dashboard. It is not required in end developer code. Comment out this code
-// to test end developer dashboard launch experience. Refer to WorkloadAttributes.cs
+// to test end developer dashboard launch experience. Refer to Directory.Build.props
 // for the path to the dashboard binary (defaults to a relative path to the artifacts
 // directory).
 builder.AddProject<Projects.Aspire_Dashboard>(KnownResourceNames.AspireDashboard)
--- playground/eShopLite/AppHost/Program.cs
+++ playground/eShopLite/AppHost/Program.cs
@@ -34,7 +34,7 @@ builder.AddProject<Projects.CatalogDb>("catalogdbapp")
 
 // This project is only added in playground projects to support development/debugging
 // of the dashboard. It is not required in end developer code. Comment out this code
-// to test end developer dashboard launch experience. Refer to WorkloadAttributes.cs
+// to test end developer dashboard launch experience. Refer to Directory.Build.props
 // for the path to the dashboard binary (defaults to a relative path to the artifacts
 // directory).
 builder.AddProject<Projects.Aspire_Dashboard>(KnownResourceNames.AspireDashboard)
