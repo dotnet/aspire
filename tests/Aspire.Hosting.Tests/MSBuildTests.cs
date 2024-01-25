1bb197fd81821106a882e76b9a1fc044b72ecc59
Jose Perez Rodriguez <joperezr@microsoft.com>
2024-01-25 11:16:26 -0800

    Updating container base images and tests to 9.0

--- tests/Aspire.Hosting.Tests/MSBuildTests.cs
+++ tests/Aspire.Hosting.Tests/MSBuildTests.cs
@@ -26,7 +26,7 @@ public class MSBuildTests
 <Project Sdk="Microsoft.NET.Sdk">
 
   <PropertyGroup>
-    <TargetFramework>net8.0</TargetFramework>
+    <TargetFramework>net9.0</TargetFramework>
     <ImplicitUsings>enable</ImplicitUsings>
     <Nullable>enable</Nullable>
   </PropertyGroup>
@@ -49,7 +49,7 @@ public class Class1
 
   <PropertyGroup>
     <OutputType>Exe</OutputType>
-    <TargetFramework>net8.0</TargetFramework>
+    <TargetFramework>net9.0</TargetFramework>
     <ImplicitUsings>enable</ImplicitUsings>
     <Nullable>enable</Nullable>
     <IsAspireHost>true</IsAspireHost>
