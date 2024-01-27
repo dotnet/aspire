88bbe5bbeb412841753e71ad60630bd050b2e854
Norm Johanson <normj@amazon.com>
2024-01-27 01:15:34 -0800

    Minor cleanup

--- samples/AWS/AWS.AppHost/Program.cs
+++ samples/AWS/AWS.AppHost/Program.cs
@@ -10,13 +10,13 @@ var awsConfig = builder.AddAWSSDKConfig("frontend-aws-config")
                         .WithRegion(RegionEndpoint.USEast2);
 
 // Provision application level resources like SQS queues and SNS topics defined in the CloudFormation template file app-resources.template.
-var awsResource = builder.AddAWSCloudFormationProvisioning("AspireSampleDevResources", "app-resources.template")
+var awsResources = builder.AddAWSCloudFormationProvisioning("AspireSampleDevResources", "app-resources.template")
                         // Add the SDK configuration so the AppHost knows what account/region to provision the resources.
                         .WithAWSSDKReference(awsConfig);
 
 builder.AddProject<Projects.Frontend>("frontend")
         // Reference the CloudFormation resource to project. The output parameters will added to the IConfiguration of the project.
-        .WithAWSCloudFormationReference(awsResource)
+        .WithAWSCloudFormationReference(awsResources)
         // Assign the SDK config to the project. The service clients created in the project relying on environment config
         // will pick up these configuration.
         .WithAWSSDKReference(awsConfig);
