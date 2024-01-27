f548bdc576bbf5220f7a6464fcb90a8a581e43a4
Mitch Denny <midenn@microsoft.com>
2024-01-27 15:10:12 +1100

    End to end SQL playground.

---
+++ playground/SqlServerEndToEnd/SqlServerEndToEnd.ApiService/Program.cs
@@ -0,0 +1,50 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.AddServiceDefaults();
+
+builder.AddSqlServerDbContext<MyDbContext>("db");
+
+var app = builder.Build();
+
+app.MapGet("/", async (MyDbContext context) =>
+{
+    // You wouldn't normally do this on every call,
+    // but doing it here just to make this simple.
+    context.Database.EnsureCreated();
+
+    var entry = new Entry();
+    await context.Entries.AddAsync(entry);
+    await context.SaveChangesAsync();
+
+    var entries = await context.Entries.ToListAsync();
+
+    return new
+    {
+        totalEntries = entries.Count,
+        entries = entries
+    };
+});
+
+app.Run();
+
+public class MyDbContext(DbContextOptions<MyDbContext> options) : DbContext(options)
+{
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Entry>().HasKey(e => e.Id);
+    }
+
+    public DbSet<Entry> Entries { get; set; }
+}
+
+public class Entry
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+}
