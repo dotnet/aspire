8a50b10db7ba4148496c682a9357aac3db362299
James Newton-King <james@newtonking.com>
2024-01-29 12:35:40 +0800

    Improve environment variable debugging in the app model (#1746)

---
+++ src/Aspire.Hosting/ApplicationModel/EnvironmentAnnotation.cs
@@ -0,0 +1,20 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Diagnostics;
+
+namespace Aspire.Hosting.ApplicationModel;
+
+// The purpose of this type is to improve the debugging experience when inspecting environment variables set without callback.
+[DebuggerDisplay("Type = {GetType().Name,nq}, Name = {_name}, Value = {_value}")]
+internal class EnvironmentAnnotation : EnvironmentCallbackAnnotation
+{
+    private readonly string _name;
+    private readonly string _value;
+
+    public EnvironmentAnnotation(string name, string value) : base(name, () => value)
+    {
+        _name = name;
+        _value = value;
+    }
+}
