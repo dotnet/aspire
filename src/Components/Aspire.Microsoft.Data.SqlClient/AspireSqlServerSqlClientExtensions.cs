1685d1bb210c4ee1aa6ad512a737e8cdf31c4761
martincostello <martin@martincostello.com>
2024-01-29 15:46:55 +0000

    Update OpenTelemetry packages
    - Update all the OpenTelemetry packages to their latest versions.
    - Use built-in methods to add .NET metrics.
    - Add suppression for new AoT warning on `AddSqlClientInstrumentation()`.

--- src/Components/Aspire.Microsoft.Data.SqlClient/AspireSqlServerSqlClientExtensions.cs
+++ src/Components/Aspire.Microsoft.Data.SqlClient/AspireSqlServerSqlClientExtensions.cs
@@ -89,7 +89,9 @@ public static class AspireSqlServerSqlClientExtensions
         {
             builder.Services.AddOpenTelemetry().WithTracing(tracerProviderBuilder =>
             {
+#pragma warning disable IL2026 // Members annotated with 'RequiresUnreferencedCodeAttribute' require dynamic access otherwise can break functionality when trimming application code
                 tracerProviderBuilder.AddSqlClientInstrumentation();
+#pragma warning restore IL2026 // Members annotated with 'RequiresUnreferencedCodeAttribute' require dynamic access otherwise can break functionality when trimming application code
             });
         }
 
