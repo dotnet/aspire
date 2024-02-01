45e6f2f60b6e175f5056964fde35e02a93503be7
Levi <levimatheri@gmail.com>
2024-01-31 20:09:13 -0500

    Fix Merge conflicts

--- tests/Aspire.Azure.AI.Search.Tests/ConformanceTests.cs
+++ tests/Aspire.Azure.AI.Search.Tests/ConformanceTests.cs
@@ -25,8 +25,6 @@ public class ConformanceTests : ConformanceTests<SearchIndexClient, AzureAISearc
 
     protected override bool SupportsKeyedRegistrations => true;
 
-    protected override string JsonSchemaPath => "src/Components/Aspire.Azure.AI.Search/ConfigurationSchema.json";
-
     protected override string ValidJsonConfig => """
         {
           "Aspire": {
