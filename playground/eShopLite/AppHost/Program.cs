9b1f37d94471ce8f4a6dbce0cf85beb70e843f48
Mitch Denny <midenn@microsoft.com>
2024-02-03 13:35:10 +1100

    Make ParameterEndToEnd use SQL Server.

--- playground/eShopLite/AppHost/Program.cs
+++ playground/eShopLite/AppHost/Program.cs
@@ -1,5 +1,7 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+builder.AddPostgres("pg2");
+
 var catalogDb = builder.AddPostgres("postgres")
                        .WithPgAdmin()
                        .AddDatabase("catalogdb");
