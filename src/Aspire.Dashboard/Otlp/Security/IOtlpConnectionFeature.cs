c9ecfe482b950c1eabe8ad671c026413c91e1684
James Newton-King <james@newtonking.com>
2024-02-07 16:51:08 +0800

    Clean up

--- src/Aspire.Dashboard/Otlp/Security/IOtlpConnectionFeature.cs
+++ src/Aspire.Dashboard/Otlp/Security/IOtlpConnectionFeature.cs
@@ -1,9 +1,11 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
 namespace Aspire.Dashboard.Otlp.Security;
 
+/// <summary>
+/// This feature's presence on a connection indicates that the connection is for OTLP.
+/// </summary>
 internal interface IOtlpConnectionFeature
 {
-
 }
