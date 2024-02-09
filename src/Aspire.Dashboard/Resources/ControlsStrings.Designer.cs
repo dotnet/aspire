d45d6f10689b253093181775087bc39755289cc4
Adam Ratzman <adamratzman@microsoft.com>
2024-02-09 11:22:54 -0500

    increase opacity in dark theme, add replica set suffix to set options

--- src/Aspire.Dashboard/Resources/ControlsStrings.Designer.cs
+++ src/Aspire.Dashboard/Resources/ControlsStrings.Designer.cs
@@ -285,6 +285,15 @@ namespace Aspire.Dashboard.Resources {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to replica set.
+        /// </summary>
+        public static string ReplicaSetLower {
+            get {
+                return ResourceManager.GetString("ReplicaSetLower", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to {0} (replica of {1}).
         /// </summary>
         public static string ResourceDropdownReplicaAccessibleTitle {
