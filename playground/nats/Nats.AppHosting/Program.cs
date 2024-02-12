7d94f673670d16c39d8069624c40024ae48fbc69
Ziya Suzen <ziya@suzen.net>
2024-02-12 13:31:56 +0000

    Moved sample to playgrounds

--- playground/nats/Nats.AppHosting/Program.cs
+++ playground/nats/Nats.AppHosting/Program.cs
@@ -4,7 +4,7 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var nats = builder.AddNatsContainer("nats");
 
-builder.AddProject<Projects.MyService>("myservice")
+builder.AddProject<Projects.Nats_ApiService>("apiservice")
     .WithReference(nats);
 
 builder.Build().Run();
