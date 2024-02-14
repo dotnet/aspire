2eb8acd6de2e1e8649cb584600924c439485d08c
Ziya Suzen <ziya@suzen.net>
2024-02-14 06:31:44 +0000

    NATS health check

--- src/Components/Aspire.Nats.Client/AspireNatsClientExtensions.cs
+++ src/Components/Aspire.Nats.Client/AspireNatsClientExtensions.cs
@@ -1,10 +1,12 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using Aspire;
 using Aspire.Nats.Client;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Logging;
 using NATS.Client.Core;
 using NATS.Client.JetStream;
@@ -94,6 +96,18 @@ public static class AspireNatsClientExtensions
             builder.Services.TryAddKeyedSingleton<NatsConnection>(serviceKey, (provider, _) => Factory(provider));
             builder.Services.TryAddKeyedSingleton<INatsConnection>(serviceKey, static (provider, key) => provider.GetRequiredKeyedService<NatsConnection>(key));
         }
+
+        if (settings.HealthChecks)
+        {
+            builder.TryAddHealthCheck(new HealthCheckRegistration(
+                serviceKey is null ? "NATS" : $"NATS_{connectionName}",
+                sp => new NatsHealthCheck(serviceKey is null
+                    ? sp.GetRequiredService<INatsConnection>()
+                    : sp.GetRequiredKeyedService<INatsConnection>(serviceKey)),
+                failureStatus: default,
+                tags: default,
+                timeout: default));
+        }
     }
 
     public static void AddNatsJetStream(this IHostApplicationBuilder builder)
@@ -104,5 +118,15 @@ public static class AspireNatsClientExtensions
         {
             return new NatsJSContextFactory().CreateContext(provider.GetService<INatsConnection>()!);
         });
+    }
+}
+
+public class NatsHealthCheck(INatsConnection connection) : IHealthCheck
+{
+    public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+    {
+        return Task.FromResult(connection.ConnectionState == NatsConnectionState.Open
+            ? HealthCheckResult.Healthy()
+            : HealthCheckResult.Unhealthy());
     }
 }
