f772de851b23fcb81f1c460e5e466e068d60d027
Ziya Suzen <ziya@suzen.net>
2024-02-14 06:56:03 +0000

    NATS client extension docs

--- src/Components/Aspire.Nats.Client/AspireNatsClientExtensions.cs
+++ src/Components/Aspire.Nats.Client/AspireNatsClientExtensions.cs
@@ -13,44 +13,45 @@ using NATS.Client.JetStream;
 
 namespace Microsoft.Extensions.Hosting;
 
+/// <summary>
+/// Extension methods for connecting NATS server with NATS client
+/// </summary>
 public static class AspireNatsClientExtensions
 {
     private const string DefaultConfigSectionName = "Aspire:Nats:Client";
 
-    public static void AddNats(this IHostApplicationBuilder builder,
-        string connectionName,
-        Action<NatsClientSettings>? configureSettings = null,
-        Func<NatsOpts, NatsOpts>? configureOptions = null)
-        => AddNats(
-            builder,
-            configurationSectionName: DefaultConfigSectionName,
-            connectionName: connectionName,
-            serviceKey: null,
-            configureSettings: configureSettings,
-            configureOptions: configureOptions);
+    /// <summary>
+    /// Registers <see cref="INatsConnection"/> service for connecting NATS server with NATS client.
+    /// Configures health check and logging for the NATS client.
+    /// </summary>
+    /// <param name="builder">The <see cref="IHostApplicationBuilder" /> to read config from and add services to.</param>
+    /// <param name="connectionName">A name used to retrieve the connection string from the ConnectionStrings configuration section.</param>
+    /// <param name="configureSettings">An optional delegate that can be used for customizing options. It's invoked after the settings are read from the configuration.</param>
+    /// <param name="configureOptions">An optional delegate that can be used for customizing NATS options that aren't exposed as standard configuration.</param>
+    /// <exception cref="ArgumentNullException">Thrown if mandatory <paramref name="builder"/> is null.</exception>
+    /// <exception cref="InvalidOperationException">Thrown when mandatory <see cref="NatsClientSettings.ConnectionString"/> is not provided.</exception>
+    public static void AddNats(this IHostApplicationBuilder builder, string connectionName, Action<NatsClientSettings>? configureSettings = null, Func<NatsOpts, NatsOpts>? configureOptions = null)
+        => AddNats(builder, configurationSectionName: DefaultConfigSectionName, connectionName: connectionName, serviceKey: null, configureSettings: configureSettings, configureOptions: configureOptions);
 
-    public static void AddKeyedNats(this IHostApplicationBuilder builder,
-        string name,
-        Action<NatsClientSettings>? configureSettings = null,
-        Func<NatsOpts, NatsOpts>? configureOptions = null)
+    /// <summary>
+    /// Registers <see cref="INatsConnection"/> as a keyed service for given <paramref name="name"/> for connecting NATS server with NATS client.
+    /// Configures health check and logging for the NATS client.
+    /// </summary>
+    /// <param name="builder">The <see cref="IHostApplicationBuilder" /> to read config from and add services to.</param>
+    /// <param name="name">The name of the component, which is used as the <see cref="ServiceDescriptor.ServiceKey"/> of the service and also to retrieve the connection string from the ConnectionStrings configuration section.</param>
+    /// <param name="configureSettings">An optional delegate that can be used for customizing options. It's invoked after the settings are read from the configuration.</param>
+    /// <param name="configureOptions">An optional delegate that can be used for customizing NATS options that aren't exposed as standard configuration.</param>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="builder"/> or <paramref name="name"/> is null.</exception>
+    /// <exception cref="ArgumentException">Thrown if mandatory <paramref name="name"/> is empty.</exception>
+    /// <exception cref="InvalidOperationException">Thrown when mandatory <see cref="NatsClientSettings.ConnectionString"/> is not provided.</exception>
+    public static void AddKeyedNats(this IHostApplicationBuilder builder, string name, Action<NatsClientSettings>? configureSettings = null, Func<NatsOpts, NatsOpts>? configureOptions = null)
     {
         ArgumentException.ThrowIfNullOrEmpty(name);
 
-        AddNats(
-            builder,
-            configurationSectionName: $"{DefaultConfigSectionName}:{name}",
-            connectionName: name,
-            serviceKey: name,
-            configureSettings: configureSettings,
-            configureOptions: configureOptions);
+        AddNats(builder, configurationSectionName: $"{DefaultConfigSectionName}:{name}", connectionName: name, serviceKey: name, configureSettings: configureSettings, configureOptions: configureOptions);
     }
 
-    private static void AddNats(this IHostApplicationBuilder builder,
-        string configurationSectionName,
-        string connectionName,
-        object? serviceKey,
-        Action<NatsClientSettings>? configureSettings,
-        Func<NatsOpts, NatsOpts>? configureOptions)
+    private static void AddNats(this IHostApplicationBuilder builder, string configurationSectionName, string connectionName, object? serviceKey, Action<NatsClientSettings>? configureSettings, Func<NatsOpts, NatsOpts>? configureOptions)
     {
         ArgumentNullException.ThrowIfNull(builder);
 
@@ -110,6 +111,11 @@ public static class AspireNatsClientExtensions
         }
     }
 
+    /// <summary>
+    /// Registers <see cref="INatsJSContext"/> service for NATS JetStream operations.
+    /// </summary>
+    /// <param name="builder">The <see cref="IHostApplicationBuilder" /> to read config from and add services to.</param>
+    /// <exception cref="ArgumentNullException">Thrown if mandatory <paramref name="builder"/> is null.</exception>
     public static void AddNatsJetStream(this IHostApplicationBuilder builder)
     {
         ArgumentNullException.ThrowIfNull(builder);
