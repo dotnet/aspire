d780f6ac4022fc0087c3fbd1bf0ed2df96f761e9
Sébastien Ros <sebastienros@gmail.com>
2024-02-15 09:27:27 -0800

    Update src/Aspire.Dashboard/DashboardWebApplication.cs
    Co-authored-by: James Newton-King <james@newtonking.com>

--- src/Aspire.Dashboard/DashboardWebApplication.cs
+++ src/Aspire.Dashboard/DashboardWebApplication.cs
@@ -85,8 +85,9 @@ public class DashboardWebApplication
 
         if (GetType().Assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion is string informationalVersion)
         {
+            // Write version at info level so it's written to the console by default. Help us debug user issues.
             // Display version and commit like 8.0.0-preview.2.23619.3+17dd83f67c6822954ec9a918ef2d048a78ad4697
-            logger.LogInformation("Version: {InformationalVersion}", informationalVersion);
+            logger.LogInformation("Aspire version: {Version}", informationalVersion);
         }
 
         if (dashboardUris.FirstOrDefault() is { } reportedDashboardUri)
