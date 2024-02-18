25cbfe981f30f70e9f264a4004face8f81cb4961
James Newton-King <james@newtonking.com>
2024-02-18 10:23:28 +0800

    Add database migration playground and skip connection string validation for EF tooling (#2263)

---
+++ playground/DatabaseMigration/DatabaseMigration.ApiModel/MyDb1Context.cs
@@ -0,0 +1,14 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Microsoft.EntityFrameworkCore;
+
+public class MyDb1Context(DbContextOptions<MyDb1Context> options) : DbContext(options)
+{
+    public DbSet<Entry> Entries { get; set; }
+}
+
+public class Entry
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+}
