e04483765b821cc5e6758a804e5ad9c7d192fcf7
Natalia Venditto <nataliafrontend@gmail.com>
2024-02-22 16:30:47 +0100

    Reinstate using directive to previous Aspire.Hosting.Utils, as per linter message

--- src/Aspire.Hosting/JavaScript/JavaScriptExtensions.cs
+++ src/Aspire.Hosting/JavaScript/JavaScriptExtensions.cs
@@ -1,7 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 using Aspire.Hosting.ApplicationModel;
-using Aspire.Hosting;
+using Aspire.Hosting.Utils;
 using Microsoft.Extensions.Hosting;
 
 namespace Aspire.Hosting;
