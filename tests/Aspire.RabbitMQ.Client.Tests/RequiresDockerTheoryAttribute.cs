0a2f2d0e9e532cc58dcf0ef58b89abf902f27e62
Eric Erhardt <eric.erhardt@microsoft.com>
2024-02-22 07:45:49 -0600

    Use correct helix env var

--- tests/Aspire.RabbitMQ.Client.Tests/RequiresDockerTheoryAttribute.cs
+++ tests/Aspire.RabbitMQ.Client.Tests/RequiresDockerTheoryAttribute.cs
@@ -8,7 +8,7 @@ namespace Aspire.RabbitMQ.Client.Tests;
 // TODO: remove these attributes when Helix has a Windows agent with Docker support
 public class RequiresDockerTheoryAttribute : TheoryAttribute
 {
-    public static bool IsSupported => !OperatingSystem.IsWindows() || Environment.GetEnvironmentVariable("helix") is null;
+    public static bool IsSupported => !OperatingSystem.IsWindows() || Environment.GetEnvironmentVariable("HELIX_WORKITEM_ROOT") is null;
 
     public override string Skip
     {
