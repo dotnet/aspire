658da44208df092d78048bb9b53ab90567e59cc0
James Newton-King <james@newtonking.com>
2024-02-23 12:43:02 +0800

    Add transaction back

--- playground/DatabaseMigration/DatabaseMigration.MigrationService/ApiDbInitializer.cs
+++ playground/DatabaseMigration/DatabaseMigration.MigrationService/ApiDbInitializer.cs
@@ -3,6 +3,8 @@
 
 using System.Diagnostics;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
 using Microsoft.Extensions.Logging;
 
 namespace DatabaseMigration.MigrationService;
@@ -18,20 +20,32 @@ public class ApiDbInitializer(
     {
         using var scope = serviceProvider.CreateScope();
         var dbContext = scope.ServiceProvider.GetRequiredService<MyDb1Context>();
+        var dbCreator = dbContext.GetService<IRelationalDatabaseCreator>();
 
-        await InitializeDatabaseAsync(dbContext, cancellationToken);
+        await InitializeDatabaseAsync(dbContext, dbCreator, cancellationToken);
 
         hostApplicationLifetime.StopApplication();
     }
 
-    private static async Task InitializeDatabaseAsync(MyDb1Context dbContext, CancellationToken cancellationToken)
+    private static async Task InitializeDatabaseAsync(MyDb1Context dbContext, IRelationalDatabaseCreator dbCreator, CancellationToken cancellationToken)
     {
         using var activity = s_activitySource.StartActivity("Migrating database", ActivityKind.Client);
 
+        // Database container might not be ready yet, so we need to retry.
         var strategy = dbContext.Database.CreateExecutionStrategy();
         await strategy.ExecuteAsync(async () =>
         {
+            // Create the database if it does not exist.
+            // Do this first so there is then a database to start a transaction against.
+            if (!await dbCreator.ExistsAsync(cancellationToken))
+            {
+                await dbCreator.CreateAsync(cancellationToken);
+            }
+
+            // Run migration in a transaction to avoid partial migration if it fails.
+            await using var transaction = await dbContext.Database.BeginTransactionAsync(cancellationToken);
             await dbContext.Database.MigrateAsync(cancellationToken);
+            await transaction.CommitAsync(cancellationToken);
         });
     }
 }
