eae03aab05eadc63f1828c40d50a129c7be8aee2
Tim Mulholland <timulhol@microsoft.com>
2024-02-26 22:27:47 -0800

    Suppress JSDisconnectedException

--- src/Aspire.Dashboard/Components/Layout/MainLayout.razor.cs
+++ src/Aspire.Dashboard/Components/Layout/MainLayout.razor.cs
@@ -187,6 +187,14 @@ public partial class MainLayout
 
     public async ValueTask DisposeAsync()
     {
-        await JS.InvokeVoidAsync("window.unregisterGlobalKeydownListener", _keyboardHandlers);
+        try
+        {
+            await JS.InvokeVoidAsync("window.unregisterGlobalKeydownListener", _keyboardHandlers);
+        }
+        catch (JSDisconnectedException)
+        {
+            // Per https://learn.microsoft.com/aspnet/core/blazor/javascript-interoperability/?view=aspnetcore-7.0#javascript-interop-calls-without-a-circuit
+            // this is one of the calls that will fail if the circuit is disconnected, and we just need to catch the exception so it doesn't pollute the logs
+        }
     }
 }
