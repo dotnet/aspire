5c1bae6d0a15c68a00fa0bacb1864175dae4bc24
Karol Zadora-Przylecki <karolz@microsoft.com>
2024-02-27 10:28:20 -0800

    Use streaming to get logs for Executables and Containers (#2435)
    The new implementation is currently enabled via configuration setting. Once the issues found during testing are resolved, we will enable the new implementation permanently.

--- src/Aspire.Hosting/Dcp/DcpVersion.cs
+++ src/Aspire.Hosting/Dcp/DcpVersion.cs
@@ -5,5 +5,5 @@ namespace Aspire.Hosting.Dcp;
 
 internal static class DcpVersion
 {
-    public static Version MinimumVersionInclusive = new Version(0, 1, 52);
+    public static Version MinimumVersionInclusive = new Version(0, 1, 55);
 }
