39472c862ac5621bfacc134de2ae7ac80ef9c9bc
Ankit Jain <radical@gmail.com>
2024-02-28 23:00:32 -0500

    remaining tidbits

---
+++ tests/testproject/Common/TestResourceNames.cs
@@ -0,0 +1,38 @@
+
+namespace Aspire.TestProject;
+
+public enum TestResourceNames
+{
+    cosmos,
+    dashboard,
+    kafka,
+    mongodb,
+    mysql,
+    oracledatabase,
+    pomelo,
+    postgres,
+    rabbitmq,
+    redis,
+    sqlserver,
+}
+
+public static class TestResourceNamesExtensions
+{
+    public static IList<TestResourceNames> Parse(IEnumerable<string> resourceNames)
+    {
+        List<TestResourceNames> resourcesToSkip = new();
+        foreach (var resourceName in resourceNames)
+        {
+            if (Enum.TryParse<TestResourceNames>(resourceName, ignoreCase: true, out var name))
+            {
+                resourcesToSkip.Add(name);
+            }
+            else
+            {
+                throw new ArgumentException($"Unknown resource name: {resourceName}");
+            }
+        }
+
+        return resourcesToSkip;
+    }
+}
