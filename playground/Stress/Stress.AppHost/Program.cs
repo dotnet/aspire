e9ca4f63c5f0e0d58174b4ec05524db3fb017c3b
James Newton-King <james@newtonking.com>
2024-03-01 13:50:52 +0800

    Add stress playground

---
+++ playground/Stress/Stress.AppHost/Program.cs
@@ -0,0 +1,28 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+var builder = DistributedApplication.CreateBuilder(args);
+
+for (var i = 0; i < 10; i++)
+{
+    builder.AddTestResource($"test-{i:0000}");
+}
+
+var serviceBuilder = builder.AddProject<Projects.Stress_ApiService>("stress-apiservice", launchProfileName: null);
+
+for (var i = 0; i < 30; i++)
+{
+    var port = 5180 + i;
+    serviceBuilder.WithHttpEndpoint(port, $"http-{port}");
+}
+
+builder.AddProject<Projects.Stress_TelemetryService>("stress-telemetryservice");
+
+// This project is only added in playground projects to support development/debugging
+// of the dashboard. It is not required in end developer code. Comment out this code
+// to test end developer dashboard launch experience. Refer to Directory.Build.props
+// for the path to the dashboard binary (defaults to the Aspire.Dashboard bin output
+// in the artifacts dir).
+builder.AddProject<Projects.Aspire_Dashboard>(KnownResourceNames.AspireDashboard);
+
+builder.Build().Run();
