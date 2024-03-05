c7e11e4bca82fdf4192f906cdeceec8edb1afa7f
James Newton-King <james@newtonking.com>
2024-03-05 15:00:59 +0800

    Metrics chart HTML encoding and testing (#2634)

---
+++ src/Aspire.Dashboard/Model/IInstrumentUnitResolver.cs
@@ -0,0 +1,11 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Aspire.Dashboard.Otlp.Model;
+
+namespace Aspire.Dashboard.Model;
+
+public interface IInstrumentUnitResolver
+{
+    string ResolveDisplayedUnit(OtlpInstrument instrument);
+}
