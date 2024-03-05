f31d49f5e70b093756b4b9047199c042001548fb
Ziya Suzen <ziya@suzen.net>
2024-03-05 05:57:33 +0000

    nats health checks tweak

--- src/Components/Aspire.NATS.Net/NatsHealthCheck.cs
+++ src/Components/Aspire.NATS.Net/NatsHealthCheck.cs
@@ -1,17 +1,24 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Text;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 using NATS.Client.Core;
 
 namespace Aspire.NATS.Net;
 
-public class NatsHealthCheck(INatsConnection connection) : IHealthCheck
+internal class NatsHealthCheck(INatsConnection connection) : IHealthCheck
 {
     public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
     {
-        return Task.FromResult(connection.ConnectionState == NatsConnectionState.Open
-            ? HealthCheckResult.Healthy()
-            : HealthCheckResult.Unhealthy());
+        var result = connection.ConnectionState switch
+        {
+            NatsConnectionState.Open => HealthCheckResult.Healthy(),
+            NatsConnectionState.Connecting or NatsConnectionState.Reconnecting => HealthCheckResult.Degraded(),
+            NatsConnectionState.Closed => HealthCheckResult.Unhealthy(),
+            _ => new HealthCheckResult(context.Registration.FailureStatus)
+        };
+
+        return Task.FromResult(result);
     }
 }
