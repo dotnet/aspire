894be23eac734c58421bd2ebe2d4aef4aacb8f72
Ankit Jain <radical@gmail.com>
2024-03-08 14:41:33 -0500

    address review feedback

--- tests/Shared/RequiresDockerTheoryAttribute.cs
+++ tests/Shared/RequiresDockerTheoryAttribute.cs
@@ -8,10 +8,7 @@ namespace Aspire.InternalTesting;
 // TODO: remove these attributes when Helix has a Windows agent with Docker support
 public class RequiresDockerTheoryAttribute : TheoryAttribute
 {
-    // Not available on windows/CI
-    public static bool IsSupported => !OperatingSystem.IsWindows() ||
-                                      (Environment.GetEnvironmentVariable("HELIX_WORKITEM_ROOT") is null
-                                      && Environment.GetEnvironmentVariable("BUILD_BUILDID") is null);
+    public static bool IsSupported => !OperatingSystem.IsWindows() || Environment.GetEnvironmentVariable("HELIX_WORKITEM_ROOT") is null;
 
     public override string Skip
     {
