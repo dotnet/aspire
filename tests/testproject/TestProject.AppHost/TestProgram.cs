33cf0c88acd6290d4969f418983bf061aabff48a
ReubenBond <rebond@microsoft.com>
2024-03-08 14:41:14 -0800

    Specify launch profile in test projects

--- tests/testproject/TestProject.AppHost/TestProgram.cs
+++ tests/testproject/TestProject.AppHost/TestProgram.cs
@@ -37,9 +37,9 @@ public class TestProgram : IDisposable
 
         var serviceAPath = Path.Combine(Projects.TestProject_AppHost.ProjectPath, @"..\TestProject.ServiceA\TestProject.ServiceA.csproj");
 
-        ServiceABuilder = AppBuilder.AddProject("servicea", serviceAPath);
-        ServiceBBuilder = AppBuilder.AddProject<Projects.ServiceB>("serviceb");
-        ServiceCBuilder = AppBuilder.AddProject<Projects.ServiceC>("servicec");
+        ServiceABuilder = AppBuilder.AddProject("servicea", serviceAPath, launchProfileName: "http");
+        ServiceBBuilder = AppBuilder.AddProject<Projects.ServiceB>("serviceb", launchProfileName: "http");
+        ServiceCBuilder = AppBuilder.AddProject<Projects.ServiceC>("servicec", launchProfileName: "http");
         WorkerABuilder = AppBuilder.AddProject<Projects.WorkerA>("workera");
 
         if (includeNodeApp)
