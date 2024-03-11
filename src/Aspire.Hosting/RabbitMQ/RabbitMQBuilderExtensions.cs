98176786b3737940860e8269a36b405b01eeb6e9
Stephan Bauer <67107950+stbau04@users.noreply.github.com>
2024-03-11 16:09:56 +0100

    Add WithVolumeMountForData (#1317)
    * Make it easier to use external volumes for database containers
    
    * Remove optional volume name
    
    * Update existing code
    
    * Add methods directly to types
    
    * Fix naming of volumes
    
    * Adapt docs
    
    * Add oracle database resource extensions
    
    * Add kafka extensions

--- src/Aspire.Hosting/RabbitMQ/RabbitMQBuilderExtensions.cs
+++ src/Aspire.Hosting/RabbitMQ/RabbitMQBuilderExtensions.cs
@@ -40,4 +40,24 @@ public static class RabbitMQBuilderExtensions
     {
         return builder.WithManifestPublishingCallback(context => context.WriteContainerAsync(builder.Resource));
     }
+
+    /// <summary>
+    /// Adds a named volume for the data folder to a RabbitMQ container resource.
+    /// </summary>
+    /// <param name="builder">The resource builder.</param>
+    /// <param name="name">The name of the volume. Defaults to an auto-generated name based on the resource name. </param>
+    /// <param name="isReadOnly">A flag that indicates if this is a read-only volume.</param>
+    /// <returns>The <see cref="IResourceBuilder{T}"/>.</returns>
+    public static IResourceBuilder<RabbitMQServerResource> WithDataVolume(this IResourceBuilder<RabbitMQServerResource> builder, string? name = null, bool isReadOnly = false)
+        => builder.WithVolume(name ?? $"{builder.Resource.Name}-data", "/var/lib/rabbitmq", isReadOnly);
+
+    /// <summary>
+    /// Adds a bind mount for the data folder to a RabbitMQ container resource.
+    /// </summary>
+    /// <param name="builder">The resource builder.</param>
+    /// <param name="source">The source directory on the host to mount into the container.</param>
+    /// <param name="isReadOnly">A flag that indicates if this is a read-only mount.</param>
+    /// <returns>The <see cref="IResourceBuilder{T}"/>.</returns>
+    public static IResourceBuilder<RabbitMQServerResource> WithDataBindMount(this IResourceBuilder<RabbitMQServerResource> builder, string source, bool isReadOnly = false)
+        => builder.WithBindMount(source, "/var/lib/rabbitmq", isReadOnly);
 }
