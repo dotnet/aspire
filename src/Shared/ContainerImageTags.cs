3468c8513a7a9a979e322595d1da8b482482041e
Ankit Jain <radical@gmail.com>
2024-03-13 16:01:28 -0400

    Fix failing test - `Aspire.Hosting.Tests.Redis.AddRedisTests.VerifyManifest`

--- src/Shared/ContainerImageTags.cs
+++ src/Shared/ContainerImageTags.cs
@@ -1,16 +1,9 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-#if BUILDING_TESTS
-namespace Aspire.Components.Common.Tests;
-#else
 namespace Aspire.Components.Common;
-#endif
 
-#if BUILDING_TESTS
-public
-#endif
-sealed class ContainerImageTags
+internal sealed class ContainerImageTags
 {
     public static (string image, string tag) Redis = ("redis", "7.2.4");
 }
