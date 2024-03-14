6338f6e1281a2e5782516e6e073b452a7ed4bc46
Mitch Denny <midenn@microsoft.com>
2024-03-14 17:26:01 +1100

    Removed old bicep resource for appconfig.

--- src/Aspire.Hosting.Azure/Extensions/AzureAppConfigurationExtensions.cs
+++ src/Aspire.Hosting.Azure/Extensions/AzureAppConfigurationExtensions.cs
@@ -18,22 +18,6 @@ public static class AzureAppConfigurationExtensions
     /// </summary>
     /// <param name="builder">The <see cref="IDistributedApplicationBuilder"/>.</param>
     /// <param name="name">The name of the resource. This name will be used as the connection string name when referenced in a dependency.</param>
-    /// <returns>A reference to the <see cref="IResourceBuilder{T}"/>.</returns>
-    public static IResourceBuilder<AzureAppConfigurationResource> AddAzureAppConfiguration(this IDistributedApplicationBuilder builder, string name)
-    {
-        var resource = new AzureAppConfigurationResource(name);
-        return builder.AddResource(resource)
-                .WithParameter("configName", resource.CreateBicepResourceName())
-                .WithParameter(AzureBicepResource.KnownParameters.PrincipalId)
-                .WithParameter(AzureBicepResource.KnownParameters.PrincipalType)
-                .WithManifestPublishingCallback(resource.WriteToManifest);
-    }
-
-    /// <summary>
-    /// Adds an Azure App Configuration resource to the application model.
-    /// </summary>
-    /// <param name="builder">The <see cref="IDistributedApplicationBuilder"/>.</param>
-    /// <param name="name">The name of the resource. This name will be used as the connection string name when referenced in a dependency.</param>
     /// <param name="configureResource"></param>
     /// <returns>A reference to the <see cref="IResourceBuilder{T}"/>.</returns>
     public static IResourceBuilder<AzureAppConfigurationConstructResource> AddAzureAppConfigurationConstruct(this IDistributedApplicationBuilder builder, string name, Action<IResourceBuilder<AzureAppConfigurationConstructResource>, ResourceModuleConstruct, AppConfigurationStore>? configureResource = null)
