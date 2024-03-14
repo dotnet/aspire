baae9c042fad020b2443ccedb11c909e9f2b2d7f
Mitch Denny <midenn@microsoft.com>
2024-03-15 10:09:40 +1100

    Delete Redis bicep types.

--- src/Aspire.Hosting.Azure/AzureRedisResource.cs
+++ src/Aspire.Hosting.Azure/AzureRedisResource.cs
@@ -9,41 +9,6 @@ namespace Aspire.Hosting.Azure;
 /// Represents an Azure Redis resource.
 /// </summary>
 /// <param name="innerResource">The inner resource.</param>
-public class AzureRedisResource(RedisResource innerResource) :
-    AzureBicepResource(innerResource.Name, templateResourceName: "Aspire.Hosting.Azure.Bicep.redis.bicep"),
-    IResourceWithConnectionString
-{
-    /// <summary>
-    /// Gets the "connectionString" output reference from the bicep template for the Azure Redis resource.
-    /// </summary>
-    public BicepSecretOutputReference ConnectionString => new("connectionString", this);
-
-    /// <summary>
-    /// Gets the connection string template for the manifest for the Azure Redis resource.
-    /// </summary>
-    public string ConnectionStringExpression => ConnectionString.ValueExpression;
-
-    /// <summary>
-    /// Gets the connection string for the Azure Redis resource.
-    /// </summary>
-    /// <param name="cancellationToken"> A <see cref="CancellationToken"/> to observe while waiting for the task to complete.</param>
-    /// <returns>The connection string for the Azure Redis resource.</returns>
-    public ValueTask<string?> GetConnectionStringAsync(CancellationToken cancellationToken = default)
-    {
-        return ConnectionString.GetValueAsync(cancellationToken);
-    }
-
-    /// <inheritdoc/>
-    public override string Name => innerResource.Name;
-
-    /// <inheritdoc />
-    public override ResourceAnnotationCollection Annotations => innerResource.Annotations;
-}
-
-/// <summary>
-/// Represents an Azure Redis resource.
-/// </summary>
-/// <param name="innerResource">The inner resource.</param>
 /// <param name="configureConstruct"></param>
 public class AzureRedisConstructResource(RedisResource innerResource, Action<ResourceModuleConstruct> configureConstruct) :
     AzureConstructResource(innerResource.Name, configureConstruct),
--- src/Aspire.Hosting.Azure/Extensions/AzureRedisExtensions.cs
+++ src/Aspire.Hosting.Azure/Extensions/AzureRedisExtensions.cs
@@ -18,62 +18,6 @@ public static class AzureRedisExtensions
     /// Configures the resource to be published as Azure Cache for Redis when deployed via Azure Developer CLI.
     /// </summary>
     /// <param name="builder">The <see cref="IResourceBuilder{RedisResource}"/> builder.</param>
-    /// <param name="callback">Callback to configure Azure resource.</param>
-    /// <returns>A reference to the <see cref="IResourceBuilder{RedisResource}"/> builder.</returns>
-    public static IResourceBuilder<RedisResource> PublishAsAzureRedis(this IResourceBuilder<RedisResource> builder, Action<IResourceBuilder<AzureRedisResource>>? callback = null)
-    {
-        var resource = new AzureRedisResource(builder.Resource);
-        var azureRedisBuilder = builder.ApplicationBuilder.CreateResourceBuilder(resource).ConfigureDefaults();
-
-        if (callback != null)
-        {
-            callback(azureRedisBuilder);
-        }
-
-        return builder;
-    }
-
-    /// <summary>
-    /// Configures resource to use Azure for local development and when doing a deployment via the Azure Developer CLI.
-    /// </summary>
-    /// <param name="builder">The <see cref="IResourceBuilder{RedisResource}"/> builder.</param>
-    /// <param name="callback">Callback to configure Azure resource.</param>
-    /// <returns>A reference to the <see cref="IResourceBuilder{RedisResource}"/> builder.</returns>
-    public static IResourceBuilder<RedisResource> AsAzureRedis(this IResourceBuilder<RedisResource> builder, Action<IResourceBuilder<AzureRedisResource>>? callback = null)
-    {
-        var resource = new AzureRedisResource(builder.Resource);
-        var azureRedisBuilder = builder.ApplicationBuilder.CreateResourceBuilder(resource).ConfigureDefaults();
-
-        // Used to hold a reference to the azure surrogate for use with the provisioner.
-        builder.WithAnnotation(new AzureBicepResourceAnnotation(resource));
-        builder.WithConnectionStringRedirection(resource);
-
-        // Remove the container annotation so that DCP doesn't do anything with it.
-        if (builder.Resource.Annotations.OfType<ContainerImageAnnotation>().SingleOrDefault() is { } containerAnnotation)
-        {
-            builder.Resource.Annotations.Remove(containerAnnotation);
-        }
-
-        if (callback != null)
-        {
-            callback(azureRedisBuilder);
-        }
-
-        return builder;
-    }
-
-    private static IResourceBuilder<AzureRedisResource> ConfigureDefaults(this IResourceBuilder<AzureRedisResource> builder)
-    {
-        var resource = builder.Resource;
-        return builder.WithParameter("redisCacheName", resource.CreateBicepResourceName())
-                      .WithParameter(AzureBicepResource.KnownParameters.KeyVaultName)
-                      .WithManifestPublishingCallback(resource.WriteToManifest);
-    }
-
-    /// <summary>
-    /// Configures the resource to be published as Azure Cache for Redis when deployed via Azure Developer CLI.
-    /// </summary>
-    /// <param name="builder">The <see cref="IResourceBuilder{RedisResource}"/> builder.</param>
     /// <param name="configureResource">Callback to configure Azure resource.</param>
     /// <returns>A reference to the <see cref="IResourceBuilder{RedisResource}"/> builder.</returns>
     public static IResourceBuilder<RedisResource> PublishAsAzureRedisConstruct(this IResourceBuilder<RedisResource> builder, Action<ResourceModuleConstruct, RedisCache>? configureResource = null)
