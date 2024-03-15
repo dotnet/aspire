fb429c1a7b3f23951394e3fd1c7b41d829712378
Eric Erhardt <eric.erhardt@microsoft.com>
2024-03-15 18:21:20 -0500

    fix MSBuild test

--- tests/Aspire.Hosting.Tests/MSBuildTests.cs
+++ tests/Aspire.Hosting.Tests/MSBuildTests.cs
@@ -74,12 +74,12 @@ builder.Build().Run();
     <SkipAspireWorkloadManifest>true</SkipAspireWorkloadManifest>
   </PropertyGroup>
 
-  <Import Project="{repoRoot}\src\Aspire.Hosting\build\Aspire.Hosting.props" />
+  <Import Project="{repoRoot}\src\Aspire.Hosting.AppHost\build\Aspire.Hosting.AppHost.props" />
 </Project>
 """);
             File.WriteAllText(Path.Combine(appHostDirectory, "Directory.Build.targets"), $"""
 <Project>
-  <Import Project="{repoRoot}\src\Aspire.Hosting\build\Aspire.Hosting.targets" />
+  <Import Project="{repoRoot}\src\Aspire.Hosting.AppHost\build\Aspire.Hosting.AppHost.targets" />
   <Import Project="{repoRoot}\src\Aspire.Hosting.Sdk\SDK\Sdk.targets" />
 </Project>
 """);
