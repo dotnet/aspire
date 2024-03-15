825ca71b1143052bc10d18ae8b0be1bcd1aac9e5
Mitch Denny <midenn@microsoft.com>
2024-03-15 11:36:26 +1100

    Remove Postgres bicep resources.

--- src/Aspire.Hosting.Azure/AzurePostgresResource.cs
+++ src/Aspire.Hosting.Azure/AzurePostgresResource.cs
@@ -9,41 +9,6 @@ namespace Aspire.Hosting.Azure;
 /// Represents an resource for Azure Postgres Flexible Server.
 /// </summary>
 /// <param name="innerResource"><see cref="PostgresServerResource"/> that this resource wraps.</param>
-public class AzurePostgresResource(PostgresServerResource innerResource) :
-    AzureBicepResource(innerResource.Name, templateResourceName: "Aspire.Hosting.Azure.Bicep.postgres.bicep"),
-    IResourceWithConnectionString
-{
-    /// <summary>
-    /// Gets the "connectionString" secret output reference from the bicep template for the Azure Postgres Flexible Server.
-    /// </summary>
-    public BicepSecretOutputReference ConnectionString => new("connectionString", this);
-
-    /// <summary>
-    /// Gets the connection template for the manifest for the Azure Postgres Flexible Server.
-    /// </summary>
-    public string ConnectionStringExpression => ConnectionString.ValueExpression;
-
-    /// <summary>
-    /// Gets the connection string for the Azure Postgres Flexible Server.
-    /// </summary>
-    /// <param name="cancellationToken"> A <see cref="CancellationToken"/> to observe while waiting for the task to complete.</param>
-    /// <returns>The connection string.</returns>
-    public ValueTask<string?> GetConnectionStringAsync(CancellationToken cancellationToken = default)
-    {
-        return ConnectionString.GetValueAsync(cancellationToken);
-    }
-
-    /// <inheritdoc/>
-    public override string Name => innerResource.Name;
-
-    /// <inheritdoc />
-    public override ResourceAnnotationCollection Annotations => innerResource.Annotations;
-}
-
-/// <summary>
-/// Represents an resource for Azure Postgres Flexible Server.
-/// </summary>
-/// <param name="innerResource"><see cref="PostgresServerResource"/> that this resource wraps.</param>
 /// <param name="configureConstruct">Callback to configure construct.</param>
 public class AzurePostgresConstructResource(PostgresServerResource innerResource, Action<ResourceModuleConstruct> configureConstruct) :
     AzureConstructResource(innerResource.Name, configureConstruct),
