2102b5183f32cccdae5e53bf8c5b6dcd24b792b6
Ankit Jain <radical@gmail.com>
2024-03-20 17:45:20 -0400

    [tests] Refactor PostgreSql tests to use testcontainers (#2862)
    * Refactor PostgreSQL tests to use testcontainers
    
    * Use ContainerImageTags.PostgreSql instead of hardcoding the image:tag
    
    # Conflicts:
    #	src/Aspire.Hosting/ContainerImageTags.cs
    
    * Merge main
    
    * Move ContainerImageTags from Aspire.Components.Common to Aspire.Hosting
    
    * Remove unncessary RequiresDocker attributes
    
    .. and use a fallback localhost connection string when docker isn't
    available.
    
    * cleanup
    
    * Update tests/Aspire.Npgsql.EntityFrameworkCore.PostgreSQL.Tests/ConformanceTests.cs
    
    Co-authored-by: Eric Erhardt <eric.erhardt@microsoft.com>
    
    * Address review feedback from @ eerhardt
    
    * Address review feedback from @ eerhardt
    
    * Address review feedback from @ eerhardt
    
    Bring back `RemoteExecutor` because:
    
    ```
        // This method can have side effects (setting AppContext switch, enabling activity source by name).
        // That is why it needs to be executed in a standalone process.
        // We use RemoteExecutor for that, but it does not support abstract classes
        // (it can not determine the type to instantiate), so that is why this "test"
        // is here and derived types call it
        protected void ActivitySourceTest(string? key)
    ```
    
    * Track file path change
    
    * Track changes from main to ContainerImageTags
    
    * Dispose RemoteExecutor
    
    * Address review feedback from @ eerhardt
    
    * Use remoteExecutor for tracing tests
    
    ---------
    
    Co-authored-by: Eric Erhardt <eric.erhardt@microsoft.com>

---
+++ tests/Aspire.Npgsql.Tests/PostgreSQLContainerFixture.cs
@@ -0,0 +1,36 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Aspire.Components.Common.Tests;
+using Aspire.Hosting.Postgres;
+using Testcontainers.PostgreSql;
+using Xunit;
+
+namespace Aspire.Npgsql.Tests;
+
+public sealed class PostgreSQLContainerFixture : IAsyncLifetime
+{
+    public PostgreSqlContainer? Container { get; private set; }
+
+    public string GetConnectionString() => Container?.GetConnectionString() ??
+        throw new InvalidOperationException("The test container was not initialized.");
+
+    public async Task InitializeAsync()
+    {
+        if (RequiresDockerTheoryAttribute.IsSupported)
+        {
+            Container = new PostgreSqlBuilder()
+                .WithImage($"{PostgresContainerImageTags.Image}:{PostgresContainerImageTags.Tag}")
+                .Build();
+            await Container.StartAsync();
+        }
+    }
+
+    public async Task DisposeAsync()
+    {
+        if (Container is not null)
+        {
+            await Container.DisposeAsync();
+        }
+    }
+}
