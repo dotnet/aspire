3a3200f20ea4ad045ef541f1f0497afdd59041de
Ankit Jain <radical@gmail.com>
2024-03-20 14:39:34 -0400

    Use remoteExecutor for tracing tests

--- tests/Aspire.Npgsql.EntityFrameworkCore.PostgreSQL.Tests/ConformanceTests.cs
+++ tests/Aspire.Npgsql.EntityFrameworkCore.PostgreSQL.Tests/ConformanceTests.cs
@@ -4,6 +4,7 @@
 using Aspire.Components.Common.Tests;
 using Aspire.Components.ConformanceTests;
 using Aspire.Npgsql.Tests;
+using Microsoft.DotNet.RemoteExecutor;
 using Microsoft.EntityFrameworkCore.Internal;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -129,8 +130,8 @@ public class ConformanceTests : ConformanceTests<TestDbContext, NpgsqlEntityFram
     }
 
     [RequiresDockerFact]
-    public Task TracingEnablesTheRightActivitySource()
-        => RunWithFixtureAsync(obj => obj.ActivitySourceTest(key: null));
+    public void TracingEnablesTheRightActivitySource()
+        => RemoteExecutor.Invoke(() => RunWithFixtureAsync(obj => obj.ActivitySourceTest(key: null))).Dispose();
 
     private static async Task RunWithFixtureAsync(Action<ConformanceTests> test)
     {
