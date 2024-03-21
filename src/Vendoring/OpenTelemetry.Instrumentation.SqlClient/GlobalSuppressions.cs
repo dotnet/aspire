3a56580fa4e5fb012cfcfe2a13236b14b690082e
Sebastien Ros <sebastienros@gmail.com>
2024-03-20 17:29:52 -0700

    Remove dependency from OpenTelemetry.Instrumentation.SqlClient

---
+++ src/Vendoring/OpenTelemetry.Instrumentation.SqlClient/GlobalSuppressions.cs
@@ -0,0 +1,11 @@
+// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Style", "IDE0028:Simplify collection initialization", Justification = "Vendor code", Scope = "namespaceanddescendants", Target = "~N:OpenTelemetry.Instrumentation.SqlClient")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Vendor code", Scope = "namespaceanddescendants", Target = "~N:OpenTelemetry.Instrumentation")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Vendor code", Scope = "namespaceanddescendants", Target = "~N:OpenTelemetry.Instrumentation.SqlClient")]
+[assembly: SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "Vendor code", Scope = "namespaceanddescendants", Target = "~N:OpenTelemetry.Instrumentation")]
---
+++ src/Vendoring/README.md
@@ -0,0 +1,30 @@
+# OpenTelemetry.Instrumentation.Share
+
+```
+git clone https://github.com/open-telemetry/opentelemetry-dotnet.git
+```
+
+## Instructions
+
+- Copy required files from `src/Shared`:
+    - `DiagnosticSourceInstrumentation\*.cs`
+    - `ExceptionExtensions.cs`
+    - `Guard.cs`
+    - `SemanticConventions.cs`
+
+# OpenTelemetry.Instrumentation.SqlClient
+
+```
+git clone https://github.com/open-telemetry/opentelemetry-dotnet.git
+```
+
+## Instructions
+
+- Copy files from `src/OpenTelemetry.Instrumentation.SqlClient`:
+    - `OpenTelemetry.Instrumentation.SqlClient.csproj`
+    - `**\*.cs`
+
+## Customizations
+
+- Added `#nullable disable` in files that require it.
+- Added `GlobalSuppressions.cs` to fix incompatible coding style.
