0c220737e5030be4f6348f912223d91dbeeceaa3
Mitch Denny <midenn@microsoft.com>
2024-03-21 20:46:46 +1100

    Move sql.

--- src/Aspire.Hosting.Azure/BicepResourceAnnotation.cs
+++ src/Aspire.Hosting.Azure/BicepResourceAnnotation.cs
@@ -19,8 +19,7 @@ namespace Aspire.Hosting.Azure;
 ///     <para>
 ///         For example, the following code adds a <see cref="SqlServerServerResource"/> resource to the application model. This type
 ///         does not derive from <see cref="AzureBicepResource"/> but can be annotated with <see cref="AzureBicepResourceAnnotation"/>
-///         by using the <see cref="AzureSqlExtensions.AsAzureSqlDatabase(IResourceBuilder{SqlServerServerResource})"/> extension
-///         method.
+///         by using the AzureSqlExtensions.AsAzureSqlDatabase()) extension method.
 ///     </para>
 ///     <code>
 ///         var builder = DistributedApplication.CreateBuilder();
