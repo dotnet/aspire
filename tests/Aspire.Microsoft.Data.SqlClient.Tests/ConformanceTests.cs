968ab3de1f02fd13869ca1d649a636d9d80fcfb5
Ankit Jain <radical@gmail.com>
2024-03-22 15:31:40 -0400

    Remove RequiredLogCategories for SqlClient as none are supported, see src/Components/Telemetry.md

--- tests/Aspire.Microsoft.Data.SqlClient.Tests/ConformanceTests.cs
+++ tests/Aspire.Microsoft.Data.SqlClient.Tests/ConformanceTests.cs
@@ -24,11 +24,7 @@ public class ConformanceTests : ConformanceTests<SqlConnection, MicrosoftDataSql
     // https://github.com/open-telemetry/opentelemetry-dotnet/blob/031ed48714e16ba4a5b099b6e14647994a0b9c1b/src/OpenTelemetry.Instrumentation.SqlClient/Implementation/SqlActivitySourceHelper.cs#L31
     protected override string ActivitySourceName => "Aspire.Microsoft.Data.SqlClient";
 
-    protected override string[] RequiredLogCategories => [
-        "Microsoft.Extensions.Hosting.Internal.ApplicationLifetime",
-        "Microsoft.Extensions.Hosting.Internal.Host",
-        "Microsoft.Hosting.Lifetime"
-    ];
+    protected override string[] RequiredLogCategories => Array.Empty<string>();
 
     protected override bool SupportsKeyedRegistrations => true;
 
