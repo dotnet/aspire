3dc39dc1a9e47010bde2837c692af292ddd4dee5
Mitch Denny <midenn@microsoft.com>
2024-03-22 17:15:35 +1100

    Just use a var.

--- playground/OpenAIEndToEnd/OpenAIEndToEnd.AppHost/Program.cs
+++ playground/OpenAIEndToEnd/OpenAIEndToEnd.AppHost/Program.cs
@@ -5,16 +5,14 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddAzureProvisioning();
 
-#pragma warning disable ASPIRE0001 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
-var openai = builder.AddAzureOpenAI("openai", (_, _, _, deployments) => {
-    var deployment = deployments.Single();
-    deployment.AddOutput("modelName", x => x.Name);
-}).AddDeployment(new("gpt-35-turbo", "gpt-35-turbo", "0613"));
-#pragma warning restore ASPIRE0001 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+var deploymentAndModelName = "gpt-35-turbo";
+var openai = builder.AddAzureOpenAI("openai").AddDeployment(
+    new(deploymentAndModelName, deploymentAndModelName, "0613")
+    );
 
 builder.AddProject<Projects.OpenAIEndToEnd_WebStory>("webstory")
        .WithReference(openai)
-       .WithEnvironment("OpenAI__DeploymentName", openai.GetOutput("modelName"));
+       .WithEnvironment("OpenAI__DeploymentName", deploymentAndModelName);
 
 // This project is only added in playground projects to support development/debugging
 // of the dashboard. It is not required in end developer code. Comment out this code
