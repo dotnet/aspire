a150d643b57ad4e0f63d473dd1d053e15aec7de6
Ankit Jain <radical@gmail.com>
2024-03-22 21:56:31 -0400

    wip

--- tests/testproject/TestProject.IntegrationServiceA/Cosmos/CosmosExtensions.cs
+++ tests/testproject/TestProject.IntegrationServiceA/Cosmos/CosmosExtensions.cs
@@ -1,7 +1,6 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System.Text;
 using Microsoft.Azure.Cosmos;
 using Polly;
 
@@ -14,22 +13,13 @@ public static class CosmosExtensions
 
     private static async Task<IResult> VerifyCosmosAsync(CosmosClient cosmosClient)
     {
-        StringBuilder log = new();
         try
         {
-            TimeSpan totalTimeout = TimeSpan.FromSeconds(90);
-            // Stopwatch sw = new();
             var policy = Policy
                 .Handle<HttpRequestException>()
                 // retry 60 times with a 1 second delay between retries
                 .WaitAndRetryAsync(20, retryAttempt => TimeSpan.FromSeconds(1));
-                    // 10,
-                    // retryAttempt => TimeSpan.FromSeconds(1));
-                    // retryAttempt => (totalTimeout.TotalMilliseconds - sw.ElapsedMilliseconds) > 5000 ? TimeSpan.FromSeconds(1) : TimeSpan.Zero,
-                    // onRetry: (exception, sleepDuration)  =>
-                    //     log.AppendLine(CultureInfo.InvariantCulture, $"Retry due to {exception.Message}"));
 
-            // sw.Start();
             var db = await policy.ExecuteAsync(async () => (
                         await cosmosClient.CreateDatabaseIfNotExistsAsync("db")).Database);
 
@@ -48,7 +38,7 @@ public static class CosmosExtensions
         }
         catch (Exception e)
         {
-            return Results.Problem(e.ToString() + $" Log: {log}");
+            return Results.Problem(e.ToString());
         }
     }
 }
