0cb8f8c27f3d06e385d911ee39621f4d8b7e26b9
Reuben Bond <reuben.bond@gmail.com>
2024-03-25 15:11:37 -0700

    Hide IServiceEndPointSelector

--- src/Microsoft.Extensions.ServiceDiscovery/LoadBalancing/IServiceEndPointSelector.cs
+++ src/Microsoft.Extensions.ServiceDiscovery/LoadBalancing/IServiceEndPointSelector.cs
@@ -1,12 +1,12 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-namespace Microsoft.Extensions.ServiceDiscovery;
+namespace Microsoft.Extensions.ServiceDiscovery.LoadBalancing;
 
 /// <summary>
 /// Selects endpoints from a collection of endpoints.
 /// </summary>
-public interface IServiceEndPointSelector
+internal interface IServiceEndPointSelector
 {
     /// <summary>
     /// Sets the collection of endpoints which this instance will select from.
