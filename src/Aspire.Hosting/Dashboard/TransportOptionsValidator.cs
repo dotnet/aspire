8dcdf0cd61341e13d3268bede9b79875d2be09a5
Mitch Denny <midenn@microsoft.com>
2024-03-27 10:56:25 +1100

    Return validation result instead of throwing.

--- src/Aspire.Hosting/Dashboard/TransportOptionsValidator.cs
+++ src/Aspire.Hosting/Dashboard/TransportOptionsValidator.cs
@@ -12,22 +12,21 @@ internal class TransportOptionsValidator(IConfiguration configuration) : IValida
     {
         if (configuration[KnownEnvironmentVariables.AspNetCoreUrls] is not { } applicationUrls)
         {
-            throw new DistributedApplicationException($"AppHost does not have applicationUrl in launch profile, or {KnownEnvironmentVariables.AspNetCoreUrls} environment variable set.");
+            return ValidateOptionsResult.Fail($"AppHost does not have applicationUrl in launch profile, or {KnownEnvironmentVariables.AspNetCoreUrls} environment variable set.");
         }
 
         var firstApplicationUrl = applicationUrls.Split(";").First();
 
         if (!Uri.TryCreate(firstApplicationUrl, UriKind.Absolute, out var parsedFirstApplicationUrl))
         {
-            throw new DistributedApplicationException($"The 'applicationUrl' setting of the launch profile has value '{firstApplicationUrl}' which could not be parsed as a URI.");
+            return ValidateOptionsResult.Fail($"The 'applicationUrl' setting of the launch profile has value '{firstApplicationUrl}' which could not be parsed as a URI.");
         }
 
         if (parsedFirstApplicationUrl.Scheme == "http" && !options.AllowUnsecureTransport.GetValueOrDefault(false))
         {
-            throw new DistributedApplicationException($"Use of a non-TLS URL in the 'applicationUrl' setting of the launch profile unless the 'ASPIRE_ALLOW_UNSECURED_TRANSPORT' environment variable is set to 'true'. See https://aka.ms/dotnet/aspire/allowunsecuredtransport for more details.");
+            return ValidateOptionsResult.Fail($"Use of a non-TLS URL in the 'applicationUrl' setting of the launch profile unless the 'ASPIRE_ALLOW_UNSECURED_TRANSPORT' environment variable is set to 'true'. See https://aka.ms/dotnet/aspire/allowunsecuredtransport for more details.");
         }
 
-        _ = configuration;
         return ValidateOptionsResult.Success;
     }
 }
