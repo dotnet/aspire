80c62a0bbc5b2ec0c477c2bdd1fc2cb4e8fc43ff
James Newton-King <james@newtonking.com>
2024-03-27 18:40:32 +0800

    Clean up

--- src/Aspire.Dashboard/Components/Pages/Token.razor.js
+++ src/Aspire.Dashboard/Components/Pages/Token.razor.js
@@ -1,7 +1,4 @@
 export async function validateToken(token) {
-    var response = await fetch('/validate-token?token=sdfsdf', {
-        
-    });
-
+    var response = await fetch(`/validate-token?token=${encodeURIComponent(token)}`);
     return response.text();
 }
