b8109dcf7254b15fdbd146e1dd761c7fc631f971
Tim Mulholland <timulhol@microsoft.com>
2024-03-27 13:07:18 -0700

    Add styling to token page

--- src/Aspire.Dashboard/Components/Pages/Token.razor.cs
+++ src/Aspire.Dashboard/Components/Pages/Token.razor.cs
@@ -1,18 +1,22 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using Aspire.Dashboard.Extensions;
 using Aspire.Dashboard.Model;
 using Aspire.Dashboard.Utils;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.FluentUI.AspNetCore.Components;
 using Microsoft.JSInterop;
 
 namespace Aspire.Dashboard.Components.Pages;
 
 public partial class Token : IAsyncDisposable
 {
+    private static readonly string? s_version = typeof(Token).Assembly.GetDisplayVersion();
     private IJSObjectReference? _jsModule;
+    private FluentTextField? _tokenTextField;
 
     private TokenFormModel _formModel = default!;
     public EditContext EditContext { get; private set; } = default!;
@@ -51,6 +55,8 @@ public partial class Token : IAsyncDisposable
         if (firstRender)
         {
             _jsModule = await JS.InvokeAsync<IJSObjectReference>("import", "/Components/Pages/Token.razor.js");
+
+            _tokenTextField?.FocusAsync();
         }
     }
 
