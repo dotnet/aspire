adad6a9f01c17243e1c7ac16028896f4e02fd274
Mitch Denny <midenn@microsoft.com>
2024-03-27 21:50:38 +1100

    Fix unnecesary namespace.

--- src/Aspire.Hosting/DistributedApplicationBuilder.cs
+++ src/Aspire.Hosting/DistributedApplicationBuilder.cs
@@ -12,7 +12,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Logging.Console;
 
 namespace Aspire.Hosting;
 
