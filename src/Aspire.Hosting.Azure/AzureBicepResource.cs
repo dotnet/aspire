d0c8d709ac44a567114b71c6202d406b52a7b527
Mitch Denny <midenn@microsoft.com>
2024-03-30 16:59:45 +1100

    Fix path.

--- src/Aspire.Hosting.Azure/AzureBicepResource.cs
+++ src/Aspire.Hosting.Azure/AzureBicepResource.cs
@@ -73,8 +73,12 @@ public class AzureBicepResource(string name, string? templateFile = null, string
         {
             isTempFile = directory is null;
 
+            //path = TempDirectory is null
+            //    ? Path.GetTempFileName() + ".bicep"
+            //    : Path.Combine(TempDirectory, $"{Name.ToLowerInvariant()}.bicep");
+
             path = TempDirectory is null
-                ? Path.GetTempFileName() + ".bicep"
+                ? Path.Combine(directory ?? Directory.CreateTempSubdirectory("aspire").FullName, $"{Name.ToLowerInvariant()}.bicep")
                 : Path.Combine(TempDirectory, $"{Name.ToLowerInvariant()}.bicep");
 
             if (TemplateResourceName is null)
