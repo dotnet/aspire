9c4533e939faafbc26202694808e37039e22d835
Mitch Denny <midenn@microsoft.com>
2024-04-01 08:36:55 +1100

    Make KeyVault parent explicit. (#3276)
    * Make KeyVault parent explicit.
    
    * Fix Cosmos as well.

--- src/Aspire.Hosting.Azure.PostgreSQL/AzurePostgresExtensions.cs
+++ src/Aspire.Hosting.Azure.PostgreSQL/AzurePostgresExtensions.cs
@@ -83,7 +83,7 @@ public static class AzurePostgresExtensions
             }
 
             var keyVault = KeyVault.FromExisting(construct, "keyVaultName");
-            _ = new KeyVaultSecret(construct, "connectionString", postgres.GetConnectionString(administratorLogin, administratorLoginPassword));
+            _ = new KeyVaultSecret(construct, "connectionString", postgres.GetConnectionString(administratorLogin, administratorLoginPassword), keyVault);
 
             var azureResource = (AzurePostgresResource)construct.Resource;
             var azureResourceBuilder = builder.ApplicationBuilder.CreateResourceBuilder(azureResource);
