3a5094bf61a99e1a370f77a5d109e81edd025459
Eric Erhardt <eric.erhardt@microsoft.com>
2024-04-02 13:59:39 -0500

    Fix ConfigurationSchemaGenerator to use correct TimeSpan format (#3320)
    * Fix ConfigurationSchemaGenerator to use correct TimeSpan format
    
    'duration' isn't an acceptable description of how to represent a TimeSpan. Instead use a custom regular expression to represent TimeSpan formats.
    
    * Add tests for the timespan regex.
    
    * Fix outdated JSON config schema tests
    
    * Fix more tests

--- tests/Aspire.Azure.Storage.Blobs.Tests/ConformanceTests.cs
+++ tests/Aspire.Azure.Storage.Blobs.Tests/ConformanceTests.cs
@@ -47,7 +47,7 @@ public class ConformanceTests : ConformanceTests<BlobServiceClient, AzureStorage
                     "TrimBlobNameSlashes": true,
                     "Retry": {
                       "Mode": "Exponential",
-                      "Delay": "PT3S"
+                      "Delay": "00:00:03"
                     }
                   }
                 }
@@ -62,7 +62,7 @@ public class ConformanceTests : ConformanceTests<BlobServiceClient, AzureStorage
             ("""{"Aspire": { "Azure": { "Storage":{ "Blobs": { "ServiceUri": "YOUR_URI"}}}}}""", "Value does not match format \"uri\""),
             ("""{"Aspire": { "Azure": { "Storage":{ "Blobs": { "ServiceUri": "http://YOUR_URI", "HealthChecks": "false"}}}}}""", "Value is \"string\" but should be \"boolean\""),
             ("""{"Aspire": { "Azure": { "Storage":{ "Blobs": { "ServiceUri": "http://YOUR_URI", "ClientOptions": {"Retry": {"Mode": "Fast"}}}}}}}""", "Value should match one of the values specified by the enum"),
-            ("""{"Aspire": { "Azure": { "Storage":{ "Blobs": { "ServiceUri": "http://YOUR_URI", "ClientOptions": {"Retry": {"NetworkTimeout": "3S"}}}}}}}""", "Value does not match format \"duration\"")
+            ("""{"Aspire": { "Azure": { "Storage":{ "Blobs": { "ServiceUri": "http://YOUR_URI", "ClientOptions": {"Retry": {"NetworkTimeout": "3S"}}}}}}}""", "The string value is not a match for the indicated regular expression")
         };
 
     protected override void PopulateConfiguration(ConfigurationManager configuration, string? key = null)
