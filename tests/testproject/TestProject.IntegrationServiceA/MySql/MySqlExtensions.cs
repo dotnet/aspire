83c064ad832e641677148dca235bae10f7f831eb
Ankit Jain <radical@gmail.com>
2024-04-04 20:03:01 -0400

    [tests] Ignore `CA1305` for tests
    https://learn.microsoft.com/en-us/dotnet/fundamentals/code-analysis/quality-rules/ca1305
    
    ```
    dotnet_diagnostic.CA1305.severity = none
    ```
    
    Addresses review feedback from @ eerhardt .

--- tests/testproject/TestProject.IntegrationServiceA/MySql/MySqlExtensions.cs
+++ tests/testproject/TestProject.IntegrationServiceA/MySql/MySqlExtensions.cs
@@ -1,7 +1,6 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System.Globalization;
 using System.Text;
 using Aspire.TestProject;
 using MySqlConnector;
@@ -21,7 +20,7 @@ public static class MySqlExtensions
         {
             ResiliencePipelineBuilder pipeline = ResilienceUtils.GetDefaultResiliencePipelineBuilder<MySqlException>(args =>
             {
-                errorMessageBuilder.AppendLine(CultureInfo.InvariantCulture, $"{Environment.NewLine}Service retry #{args.AttemptNumber} due to {args.Outcome.Exception}");
+                errorMessageBuilder.AppendLine($"{Environment.NewLine}Service retry #{args.AttemptNumber} due to {args.Outcome.Exception}");
                 return ValueTask.CompletedTask;
             });
 
