cdb73cf036074ceb2cb07efb176c6cdd356e0e10
Ankit Jain <radical@gmail.com>
2024-04-11 18:21:50 -0400

    [tests] Use ResiliencePipeline in IntegrationTests (#3385)
    * [tests] Use ResiliencePipeline in IntegrationTests
    
    .. with a policy to retry with 1 sec delays, and a total timeout of
    90 secs on the service side. The test(client) side has an overall http
    request timeout for 120 secs which would allow the real errors from the
    service to get surfaced to the client.
    
    * address review feedback from @ eerhardt
    
    * Use the pipeline for MySql also
    
    * IntegrationServiceFixture.DumpComponentLogsAsync: error out if more than one resource is given
    
    * Rename TestUtils -> ResilienceUtils - feedback from @ eerhardt
    
    * [tests] Ignore `CA1305` for tests
    
    https://learn.microsoft.com/en-us/dotnet/fundamentals/code-analysis/quality-rules/ca1305
    
    ```
    dotnet_diagnostic.CA1305.severity = none
    ```
    
    Addresses review feedback from @ eerhardt .
    
    * Remove Polly reference
    
    * Make code consistent.
    
    * add comment addressing review feedback from @ eerhardt
    
    ---------
    
    Co-authored-by: Eric Erhardt <eric.erhardt@microsoft.com>

--- tests/testproject/TestProject.IntegrationServiceA/Oracle/OracleDatabaseExtensions.cs
+++ tests/testproject/TestProject.IntegrationServiceA/Oracle/OracleDatabaseExtensions.cs
@@ -1,6 +1,8 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Text;
+using Aspire.TestProject;
 using Microsoft.EntityFrameworkCore;
 using Oracle.ManagedDataAccess.Client;
 using Polly;
@@ -14,14 +16,16 @@ public static class OracleDatabaseExtensions
 
     private static IResult VerifyOracleDatabase(MyDbContext context)
     {
+        StringBuilder errorMessageBuilder = new();
         try
         {
-            var policy = Policy
-                .Handle<OracleException>()
-                // retry 60 times with a 1 second delay between retries
-                .WaitAndRetry(60, retryAttempt => TimeSpan.FromSeconds(1));
+            ResiliencePipeline pipeline = ResilienceUtils.GetDefaultResiliencePipelineBuilder<OracleException>(args =>
+            {
+                errorMessageBuilder.AppendLine($"{Environment.NewLine}Service retry #{args.AttemptNumber} due to {args.Outcome.Exception}");
+                return ValueTask.CompletedTask;
+            }).Build();
 
-            return policy.Execute(() =>
+            return pipeline.Execute(() =>
             {
                 var results = context.Database.SqlQueryRaw<int>("SELECT 1 FROM DUAL");
                 return results.Any() ? Results.Ok("Success!") : Results.Problem("Failed");
@@ -29,7 +33,7 @@ public static class OracleDatabaseExtensions
         }
         catch (Exception e)
         {
-            return Results.Problem(e.ToString());
+            return Results.Problem($"Error: {e}{Environment.NewLine}** Previous retries: {errorMessageBuilder}");
         }
     }
 }
