20c704ecf69d1e460440007400ed7e783969bb7a
paule96 <paul-jeschke@outlook.com>
2024-04-11 07:41:03 +0000

    revert changes in playground

--- playground/dapr/AppHost/Program.cs
+++ playground/dapr/AppHost/Program.cs
@@ -3,12 +3,12 @@ var builder = DistributedApplication.CreateBuilder(args);
 var stateStore = builder.AddDaprStateStore("statestore");
 var pubSub = builder.AddDaprPubSub("pubsub");
 
-builder.AddProject<Projects.DaprServiceA>("servicea", "https")
+builder.AddProject<Projects.DaprServiceA>("servicea")
        .WithDaprSidecar()
        .WithReference(stateStore)
        .WithReference(pubSub);
 
-builder.AddProject<Projects.DaprServiceB>("serviceb", "https")
+builder.AddProject<Projects.DaprServiceB>("serviceb")
        .WithDaprSidecar()
        .WithReference(pubSub);
 
