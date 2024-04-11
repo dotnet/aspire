2a2e6c75ae5ed50810867fd0ec961c8834bfa053
Drew Noakes <git@drewnoakes.com>
2024-04-12 07:59:50 +1000

    Allow configuring claim requirements for frontend authorization (#3595)
    Adds optional configuration that checks a given claim of a user authenticated via OIDC.
    
    Two new configuration values are:
    
    - `Dashboard:Frontend:OpenIdConnect:RequireClaimType` specifies the (optional) claim that be present for authorized users. Defaults to empty.
    - `Dashboard:Frontend:OpenIdConnect:RequireClaimValue` specifies the (optional) value of the required claim. Only used if `Dashboard:Frontend:OpenIdConnect:RequireClaimType` is also specified. Defaults to empty.

--- src/Aspire.Dashboard/Configuration/DashboardOptions.cs
+++ src/Aspire.Dashboard/Configuration/DashboardOptions.cs
@@ -180,6 +180,20 @@ public sealed class OpenIdConnectOptions
     public string NameClaimType { get; set; } = "name";
     public string UsernameClaimType { get; set; } = "preferred_username";
 
+    /// <summary>
+    /// Gets the optional name of a claim that users authenticated via OpenID Connect are required to have.
+    /// If specified, users without this claim will be rejected. If <see cref="RequiredClaimValue"/>
+    /// is also specified, then the value of this claim must also match <see cref="RequiredClaimValue"/>.
+    /// </summary>
+    public string RequiredClaimType { get; set; } = "";
+
+    /// <summary>
+    /// Gets the optional value of the <see cref="RequiredClaimType"/> claim for users authenticated via
+    /// OpenID Connect. If specified, users not having this value for the corresponding claim type are
+    /// rejected.
+    /// </summary>
+    public string RequiredClaimValue { get; set; } = "";
+
     public string[] GetNameClaimTypes()
     {
         Debug.Assert(_nameClaimTypes is not null, "Should have been parsed during validation.");
