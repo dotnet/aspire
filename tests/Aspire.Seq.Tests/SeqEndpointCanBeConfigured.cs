892893af07569e90d680efdd7af75684998c7df5
Liam McLennan <lmclennan@datalust.co>
2024-04-15 12:58:24 +1000

    Add test

---
+++ tests/Aspire.Seq.Tests/SeqEndpointCanBeConfigured.cs
@@ -0,0 +1,25 @@
+﻿
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Microsoft.Extensions.Hosting;
+using OpenTelemetry.Exporter;
+using Xunit;
+
+namespace Aspire.Seq.Tests;
+
+public class SeqTests
+{
+    [Fact]
+    public void SeqEndpointCanBeConfigured()
+    {
+        var builder = Host.CreateEmptyApplicationBuilder(null);
+        builder.AddSeqEndpoint("seq", s =>
+        {
+            s.LogExporterOptions.TimeoutMilliseconds = 1000;
+            s.TraceExporterOptions.Protocol = OtlpExportProtocol.Grpc;
+        });
+
+        using var host = builder.Build();
+    }
+}
