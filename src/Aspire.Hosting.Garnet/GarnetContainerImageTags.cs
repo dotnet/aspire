5c35d924e2ff56222440ded6dc832946c61cb748
ZLoo <Kozlov.s.v.1992@gmail.com>
2024-04-23 10:06:10 +0300

    Freeze version sha-d4c89fd

--- src/Aspire.Hosting.Garnet/GarnetContainerImageTags.cs
+++ src/Aspire.Hosting.Garnet/GarnetContainerImageTags.cs
@@ -7,5 +7,5 @@ internal static class GarnetContainerImageTags
 {
     public const string Registry = "ghcr.io";
     public const string Image = "microsoft/garnet";
-    public const string Tag = "latest";
+    public const string Tag = "sha-d4c89fd";
 }
