ba574f6e987554ab30709b48c91989b3c0f2bb0e
Amaury Levé <amauryleve@microsoft.com>
2024-04-23 14:38:15 +0200

    Add support for NUnit

---
+++ src/Aspire.ProjectTemplates/templates/aspire-nunit/IntegrationTest1.cs
@@ -0,0 +1,31 @@
+using System.Net;
+
+namespace Aspire.Tests1;
+
+public class IntegrationTest1
+{
+    // Instructions:
+    // 1. Add a project reference to the target AppHost project, e.g.:
+    //
+    //    <ItemGroup>
+    //        <ProjectReference Include="../MyAspireApp.AppHost/MyAspireApp.AppHost.csproj" />
+    //    </ItemGroup>
+    //
+    // 2. Uncomment the following example test and update 'Projects.MyAspireApp_AppHost' to match your AppHost project:
+    //
+    // [Test]
+    // public async Task GetWebResourceRootReturnsOkStatusCode()
+    // {
+    //     // Arrange
+    //     var appHost = await DistributedApplicationTestingBuilder.CreateAsync<Projects.MyAspireApp_AppHost>();
+    //     await using var app = await appHost.BuildAsync();
+    //     await app.StartAsync();
+
+    //     // Act
+    //     var httpClient = app.CreateHttpClient("webfrontend");
+    //     var response = await httpClient.GetAsync("/");
+
+    //     // Assert
+    //     ClassicAssert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+    // }
+}
