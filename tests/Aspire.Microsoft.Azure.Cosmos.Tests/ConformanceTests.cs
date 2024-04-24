355d8c7443711f719a172a562a6baf1edf98919a
Debdatta Kunda <dkunda@microsoft.com>
2024-04-24 11:15:52 -0700

    Code changes to fix broken tests.

--- tests/Aspire.Microsoft.Azure.Cosmos.Tests/ConformanceTests.cs
+++ tests/Aspire.Microsoft.Azure.Cosmos.Tests/ConformanceTests.cs
@@ -83,9 +83,9 @@ public class ConformanceTests : ConformanceTests<CosmosClient, MicrosoftAzureCos
             new KeyValuePair<string, string?>("ConnectionStrings:cosmosdb3", "AccountEndpoint=https://example3.documents.azure.com:443/;AccountKey=fake;"),
         ]);
 
-        builder.AddAzureCosmosDBClient("cosmosdb1");
-        builder.AddKeyedAzureCosmosDbClient("cosmosdb2");
-        builder.AddKeyedAzureCosmosDbClient("cosmosdb3");
+        builder.AddAzureCosmosClient("cosmosdb1");
+        builder.AddKeyedAzureCosmosClient("cosmosdb2");
+        builder.AddKeyedAzureCosmosClient("cosmosdb3");
 
         using var host = builder.Build();
 
