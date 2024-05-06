f14a0e5180d5de3141fac7fc6b1aafaf590b1d99
Arno Noordover <anoordover@users.noreply.github.com>
2024-05-06 23:19:13 +0200

    Added ActiveMQ hosting

---
+++ src/Aspire.Hosting.ActiveMQ/ActiveMQContainerImageTags.cs
@@ -0,0 +1,11 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace Aspire.Hosting.ActiveMQ;
+
+internal static class ActiveMQContainerImageTags
+{
+    public const string Registry = "docker.io";
+    public const string Image = "apache/activemq-classic";
+    public const string Tag = "6.1.0";
+}
