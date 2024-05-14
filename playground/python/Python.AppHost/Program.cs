30c850ff468457b1b7f5402b75db9330b797c12a
Willem Meints <1550763+wmeints@users.noreply.github.com>
2024-05-14 06:43:12 +0200

    Fix playground sample for python

--- playground/python/Python.AppHost/Program.cs
+++ playground/python/Python.AppHost/Program.cs
@@ -1,13 +1,9 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using Aspire.Hosting.Python;
-
 var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddPythonProject("script-only", "../script_only", "main.py");
 builder.AddPythonProject("instrumented-script", "../instrumented_script", "main.py");
-builder.AddFlaskProjectWithVirtualEnvironment("flask-app", "../flask_app", "main");
-builder.AddFlaskProjectWithVirtualEnvironment("instrumented-flask-app", "../instrumented_flask_app", "main");
 
 builder.Build().Run();
