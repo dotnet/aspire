3b2898f773828c1d554dfe8834e2f97b24997e2b
Sebastien Ros <sebastienros@gmail.com>
2024-05-17 15:04:09 -0700

    Fix multiple emulators support

--- src/Aspire.Hosting.Azure.EventHubs/AzureEventHubsEmulatorConfigWriterHook.cs
+++ src/Aspire.Hosting.Azure.EventHubs/AzureEventHubsEmulatorConfigWriterHook.cs
@@ -11,52 +11,52 @@ internal sealed class AzureEventHubsEmulatorConfigWriterHook : IDistributedAppli
 {
     public Task AfterEndpointsAllocatedAsync(DistributedApplicationModel appModel, CancellationToken cancellationToken)
     {
-        if (appModel.Resources.OfType<AzureEventHubsResource>().SingleOrDefault() is not { } emulatorResource || !emulatorResource.IsEmulator)
+        var eventHubsEmulatorResources = appModel.Resources.OfType<AzureEventHubsResource>().Where(x => x is { } eventHubsResource && eventHubsResource.IsEmulator);
+
+        if (!eventHubsEmulatorResources.Any())
         {
             // No-op if there is no Azure Event Hubs emulator resource.
             return Task.CompletedTask;
         }
 
-        var configFileMount = emulatorResource.Annotations.OfType<ContainerMountAnnotation>().Single(v => v.Target == AzureEventHubsEmulatorResource.EmulatorConfigJsonPath);
-
-        if (!emulatorResource.Hubs.Any())
+        foreach (var emulatorResource in eventHubsEmulatorResources)
         {
-            // No-op if there are no Azure Event Hubs resources present.
-            return Task.CompletedTask;
+            var configFileMount = emulatorResource.Annotations.OfType<ContainerMountAnnotation>().Single(v => v.Target == AzureEventHubsEmulatorResource.EmulatorConfigJsonPath);
+
+            using var stream = new FileStream(configFileMount.Source!, FileMode.Create);
+            using var writer = new Utf8JsonWriter(stream);
+
+            writer.WriteStartObject();                      // {
+            writer.WriteStartObject("UserConfig");          //   "UserConfig": {
+            writer.WriteStartArray("NamespaceConfig");      //     "NamespaceConfig": [
+            writer.WriteStartObject();                      //       {
+            writer.WriteString("Type", "EventHub");         //         "Type": "EventHub",
+            writer.WriteString("Name", "emulatorNs1");      //         "Name": "emulatorNs1"
+            writer.WriteStartArray("Entities");             //         "Entities": [
+
+            foreach (var hub in emulatorResource.Hubs)
+            {
+                // The default consumer group ('$default') is automatically created
+
+                writer.WriteStartObject();                  //           {
+                writer.WriteString("Name", hub.Name);       //             "Name": "hub",
+                writer.WriteString("PartitionCount", "2");  //             "PartitionCount": "2",
+                writer.WriteStartArray("ConsumerGroups");   //             "ConsumerGroups": [
+                writer.WriteEndArray();                     //             ]
+                writer.WriteEndObject();                    //           }
+            }
+
+            writer.WriteEndArray();                         //         ] (/Entities)
+            writer.WriteEndObject();                        //       } 
+            writer.WriteEndArray();                         //     ], (/NamespaceConfig)
+            writer.WriteStartObject("LoggingConfig");       //     "LoggingConfig": {
+            writer.WriteString("Type", "File");             //       "Type": "File"
+            writer.WriteEndObject();                        //     } (/LoggingConfig)
+
+            writer.WriteEndObject();                        //   } (/UserConfig)
+            writer.WriteEndObject();                        // } (/Root)
+
         }
-
-        using var stream = new FileStream(configFileMount.Source!, FileMode.Create);
-        using var writer = new Utf8JsonWriter(stream);
-
-        writer.WriteStartObject();                      // {
-        writer.WriteStartObject("UserConfig");          //   "UserConfig": {
-        writer.WriteStartArray("NamespaceConfig");      //     "NamespaceConfig": [
-        writer.WriteStartObject();                      //       {
-        writer.WriteString("Type", "EventHub");         //         "Type": "EventHub",
-        writer.WriteString("Name", "emulatorNs1");      //         "Name": "emulatorNs1"
-        writer.WriteStartArray("Entities");             //         "Entities": [
-
-        foreach (var hub in emulatorResource.Hubs)
-        {
-            // The default consumer group ('$default') is automatically created
-
-            writer.WriteStartObject();                  //           {
-            writer.WriteString("Name", hub.Name);       //             "Name": "hub",
-            writer.WriteString("PartitionCount", "2");  //             "PartitionCount": "2",
-            writer.WriteStartArray("ConsumerGroups");   //             "ConsumerGroups": [
-            writer.WriteEndArray();                     //             ]
-            writer.WriteEndObject();                    //           }
-        }
-
-        writer.WriteEndArray();                         //         ] (/Entities)
-        writer.WriteEndObject();                        //       } 
-        writer.WriteEndArray();                         //     ], (/NamespaceConfig)
-        writer.WriteStartObject("LoggingConfig");       //     "LoggingConfig": {
-        writer.WriteString("Type", "File");             //       "Type": "File"
-        writer.WriteEndObject();                        //     } (/LoggingConfig)
-
-        writer.WriteEndObject();                        //   } (/UserConfig)
-        writer.WriteEndObject();                        // } (/Root)
 
         return Task.CompletedTask;
     }
