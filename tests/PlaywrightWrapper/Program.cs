571a3c9c76213b4502e77ccf446259ec117adc38
Ankit Jain <radical@gmail.com>
2024-05-17 17:38:52 -0400

    Remove PlaywrightWrapper project
    .. and instead use a pre-generated `runtimeconfig.json` to directly run
    `Microsoft.Playwright.dll`.
    
    Based on feedback from @ eerhardt .

--- tests/PlaywrightWrapper/Program.cs
+++
@@ -1,8 +0,0 @@
-// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-//
-sealed class Program
-{
-    public static int Main(string[] args)
-        => Microsoft.Playwright.Program.Main(args);
-}
