f03d884512cef0874ad7aa422b1fdd23ac976ab8
Adam Ratzman <adamratzman@microsoft.com>
2024-05-21 11:50:36 -0400

    add mobile nav menu

---
+++ src/Aspire.Dashboard/Components/Layout/MobileNavigationHamburger.razor.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.FluentUI.AspNetCore.Components;
+
+namespace Aspire.Dashboard.Components.Layout;
+
+public partial class MobileNavigationHamburger : ComponentBase
+{
+}
+
+record NavMenuItemEntry(string Text, Func<Task> OnClick, Icon? Icon = null);
