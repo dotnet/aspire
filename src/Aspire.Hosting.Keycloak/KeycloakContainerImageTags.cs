fd7a507a9fd54088b2a85bcaac16e46e5bad743b
Julio Casal <julioc@dotnetmicroservices.com>
2024-05-24 14:30:05 -0700

    Add Keycloak component

---
+++ src/Aspire.Hosting.Keycloak/KeycloakContainerImageTags.cs
@@ -0,0 +1,11 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace Aspire.Hosting.Keycloak;
+
+internal static class KeycloakContainerImageTags
+{
+    public const string Registry = "quay.io";
+    public const string Image = "keycloak/keycloak";
+    public const string Tag = "24.0.4";
+}
---
+++ src/Aspire.Hosting.Keycloak/KeycloakResouceBuilderExtensions.cs
@@ -0,0 +1,69 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Aspire.Hosting.ApplicationModel;
+using Aspire.Hosting.Keycloak;
+using Aspire.Hosting.Utils;
+
+namespace Aspire.Hosting;
+
+/// <summary>
+/// Provides extension methods for adding Keycloak resources to an <see cref="IDistributedApplicationBuilder"/>.
+/// </summary>
+public static class KeycloakResouceBuilderExtensions
+{
+    private const string AdminEnvVarName = "KEYCLOAK_ADMIN";
+    private const string AdminPasswordEnvVarName = "KEYCLOAK_ADMIN_PASSWORD";
+    private const string DefaultAdmin = "admin";
+    private const string DefaultAdminPassword = "admin";
+    private const int DefaultContainerPort = 8080;
+
+    /// <summary>
+    /// Adds a Keycloak container to the application model.
+    /// </summary>
+    /// <param name="builder">The <see cref="IDistributedApplicationBuilder"/>.</param>
+    /// <param name="name">The name of the resource. </param>
+    /// <param name="port">The host port that the underlying container is bound to when running locally.</param>
+    /// <param name="admin">The parameter used as the admin for the Keycloak resource. If <see langword="null"/> a default value will be used.</param>
+    /// <param name="adminPassword">The parameter used as the admin password for the Keycloak resource. If <see langword="null"/> a default password will be used.</param>
+    /// <returns>A reference to the <see cref="IResourceBuilder{T}"/>.</returns>
+    public static IResourceBuilder<KeycloakResource> AddKeycloak(
+        this IDistributedApplicationBuilder builder,
+        string name,
+        int? port = null,
+        IResourceBuilder<ParameterResource>? admin = null,
+        IResourceBuilder<ParameterResource>? adminPassword = null)
+    {
+        var resource = new KeycloakResource(name);
+
+        var keycloak = builder
+            .AddResource(resource)
+            .WithImage(KeycloakContainerImageTags.Image)
+            .WithImageRegistry(KeycloakContainerImageTags.Registry)
+            .WithImageTag(KeycloakContainerImageTags.Tag)
+            .WithHttpEndpoint(port: port, targetPort: DefaultContainerPort)
+            .WithEnvironment(AdminEnvVarName, admin?.Resource.Value ?? DefaultAdmin)
+            .WithEnvironment(AdminPasswordEnvVarName, adminPassword?.Resource.Value ?? DefaultAdminPassword);
+
+        if (builder.ExecutionContext.IsRunMode)
+        {
+            keycloak.WithArgs("start-dev");
+        }
+        else
+        {
+            keycloak.WithArgs("start");
+        }
+
+        return keycloak;
+    }
+
+    /// <summary>
+    /// Adds a named volume for the data folder to a Keycloak container resource.
+    /// </summary>
+    /// <param name="builder">The resource builder.</param>
+    /// <param name="name">The name of the volume. Defaults to an auto-generated name based on the application and resource names.</param>
+    /// <param name="isReadOnly">A flag that indicates if this is a read-only volume.</param>
+    /// <returns>The <see cref="IResourceBuilder{T}"/>.</returns>
+    public static IResourceBuilder<KeycloakResource> WithDataVolume(this IResourceBuilder<KeycloakResource> builder, string? name = null, bool isReadOnly = false)
+        => builder.WithVolume(name ?? VolumeNameGenerator.CreateVolumeName(builder, "data"), "/opt/keycloak/data", isReadOnly);
+}
