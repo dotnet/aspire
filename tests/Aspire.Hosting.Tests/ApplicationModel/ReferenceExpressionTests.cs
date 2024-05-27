57042e6369e08f4d687172302746a34237475add
Jeff Layton <jeffl8n@users.noreply.github.com>
2024-05-27 14:13:13 -0400

    Format to match editorconfig rules

--- tests/Aspire.Hosting.Tests/ApplicationModel/ReferenceExpressionTests.cs
+++ tests/Aspire.Hosting.Tests/ApplicationModel/ReferenceExpressionTests.cs
@@ -4,39 +4,46 @@
 using Xunit;
 
 namespace Aspire.Hosting.Tests.ApplicationModel;
-public class ReferenceExpressionTests {
+public class ReferenceExpressionTests
+{
     [Fact]
-    public void ReferenceExpressionHandlesValueWithNonParameterBrackets() {
+    public void ReferenceExpressionHandlesValueWithNonParameterBrackets()
+    {
         var s = "{x}";
         var expr = ReferenceExpression.Create($"{s}").ValueExpression;
         Assert.Equal("{x}", expr);
     }
     [Fact]
-    public void ReferenceExpressionHandlesValueWithDoubleNonParameterBrackets() {
+    public void ReferenceExpressionHandlesValueWithDoubleNonParameterBrackets()
+    {
         var s = "{{x}}";
         var expr = ReferenceExpression.Create($"{s}").ValueExpression;
         Assert.Equal("{{x}}", expr);
     }
     [Fact]
-    public void ReferenceExpressionHandlesValueWithUnmatchedNonParameterStartBracket() {
+    public void ReferenceExpressionHandlesValueWithUnmatchedNonParameterStartBracket()
+    {
         var s = "{x";
         var expr = ReferenceExpression.Create($"{s}").ValueExpression;
         Assert.Equal("{x", expr);
     }
     [Fact]
-    public void ReferenceExpressionHandlesValueWithUnmatchedNonParameterEndBracket() {
+    public void ReferenceExpressionHandlesValueWithUnmatchedNonParameterEndBracket()
+    {
         var s = "x}";
         var expr = ReferenceExpression.Create($"{s}").ValueExpression;
         Assert.Equal("x}", expr);
     }
     [Fact]
-    public void ReferenceExpressionHandlesValueWithParameterBrackets() {
+    public void ReferenceExpressionHandlesValueWithParameterBrackets()
+    {
         var s = "{0}";
         var expr = ReferenceExpression.Create($"{s}", [new HostUrl("test")], ["abc123"]).ValueExpression;
         Assert.Equal("abc123", expr);
     }
     [Fact]
-    public void ReferenceExpressionHandlesValueWithoutBrackets() {
+    public void ReferenceExpressionHandlesValueWithoutBrackets()
+    {
         var s = "Test";
         var expr = ReferenceExpression.Create($"{s}").ValueExpression;
         Assert.Equal("Test", expr);
