954e4578c7270f56804210def97fdcaea8337f30
Adam Ratzman <adamratzman@microsoft.com>
2024-06-04 11:38:14 -0700

    add appbar test

--- tests/Aspire.Dashboard.Tests/Integration/Playwright/AppBarTests.cs
+++ tests/Aspire.Dashboard.Tests/Integration/Playwright/AppBarTests.cs
@@ -9,23 +9,34 @@ namespace Aspire.Dashboard.Tests.Integration.Playwright;
 public class AppBarTests(DashboardServerFixture dashboardServerFixture, PlaywrightFixture playwrightFixture) : IClassFixture<DashboardServerFixture>, IClassFixture<PlaywrightFixture>
 {
     [Fact]
-    public async Task Post_DeleteAllMessagesHandler_ReturnsRedirectToRoot()
+    public async Task AppBar_Change_Theme()
     {
         // Arrange
         var page = await playwrightFixture.Browser.NewPageAsync(new BrowserNewPageOptions { BaseURL = dashboardServerFixture.DashboardApp.FrontendEndPointAccessor().Address});
-        await page.GotoAsync("/");
+        await playwrightFixture.GoToHomeAndWaitForDataGridLoad(page);
 
-        // Act and Assert
         var settingsButton = page.GetByRole(AriaRole.Button, new()
         {
             Name = "Launch settings"
         });
         await settingsButton.ClickAsync();
+
+        // Act and Assert
+
+        // set to dark
         var darkThemeCheckbox = page.GetByRole(AriaRole.Radio).And(page.GetByText("Dark")).First;
-        await darkThemeCheckbox.ClickAsync();
-        Assert.Equal("dark", await page.Locator("html").First.GetAttributeAsync("data-theme"));
+        var lightThemeCheckbox = page.GetByRole(AriaRole.Radio).And(page.GetByText("Light")).First;
 
-        await Task.Delay(20000);
+        await SetAndVerifyTheme(darkThemeCheckbox, "dark");
+        await SetAndVerifyTheme(lightThemeCheckbox, "light");
 
+        async Task SetAndVerifyTheme(ILocator locator, string expected)
+        {
+            await locator.ClickAsync();
+            await Task.Delay(500);
+
+            Assert.Equal(expected, await page.Locator("html").First.GetAttributeAsync("data-theme"));
+        }
     }
 }
+
--- tests/Aspire.Dashboard.Tests/Integration/Playwright/PlaywrightFixture.cs
+++ tests/Aspire.Dashboard.Tests/Integration/Playwright/PlaywrightFixture.cs
@@ -2,6 +2,8 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using Microsoft.Playwright;
+using Polly;
+using Polly.Retry;
 using Xunit;
 
 namespace Aspire.Dashboard.Tests.Integration.Playwright;
@@ -9,16 +11,47 @@ namespace Aspire.Dashboard.Tests.Integration.Playwright;
 public class PlaywrightFixture : IAsyncLifetime
 {
     public IBrowser Browser { get; set; } = null!;
-    private IPlaywright PlaywrightInstance { get; set; } = null!;
+
+    private IPlaywright _playwrightInstance = null!;
+    private ResiliencePipeline _resiliencePipeline = null!;
 
     public async Task InitializeAsync()
     {
-        PlaywrightInstance = await Microsoft.Playwright.Playwright.CreateAsync();
-        Browser = await PlaywrightInstance.Chromium.LaunchAsync(new BrowserTypeLaunchOptions { Headless = false });
+        var retryOptions = new RetryStrategyOptions
+        {
+            ShouldHandle = new PredicateBuilder().Handle<ArgumentException>(),
+            BackoffType = DelayBackoffType.Exponential,
+            UseJitter = true,
+            MaxRetryAttempts = 20,
+            Delay = TimeSpan.FromMilliseconds(100),
+        };
+
+        var resiliencePipelineBuilder = new ResiliencePipelineBuilder();
+        resiliencePipelineBuilder
+            .AddRetry(retryOptions)
+            .AddTimeout(TimeSpan.FromSeconds(10));
+        _resiliencePipeline = resiliencePipelineBuilder.Build();
+
+        _playwrightInstance = await Microsoft.Playwright.Playwright.CreateAsync();
+        Browser = await _playwrightInstance.Chromium.LaunchAsync(new BrowserTypeLaunchOptions { Headless = true });
     }
 
     public async Task DisposeAsync()
     {
         await Browser.CloseAsync();
+    }
+
+    public async Task GoToHomeAndWaitForDataGridLoad(IPage page)
+    {
+        await page.GotoAsync("/");
+
+        await _resiliencePipeline.ExecuteAsync(async _ =>
+        {
+            if (await page.GetByText(MockDashboardClient.TestResource1.DisplayName).CountAsync() == 0)
+            {
+                throw new ArgumentException("Data grid has not loaded yet");
+            }
+
+        });
     }
 }
