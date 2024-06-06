8c7184b42ba42b2ecbd12d3082677b3b60ac3f76
Ankit Jain <radical@gmail.com>
2024-06-06 16:47:57 -0700

    fix build

--- tests/Aspire.Hosting.Tests/Helpers/LocalOnlyHelper.cs
+++ tests/Aspire.Hosting.Tests/Helpers/LocalOnlyHelper.cs
@@ -1,7 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using Aspire.Hosting.Utils;
+//using Aspire.Hosting.Utils;
 
 namespace Aspire.Hosting.Tests.Helpers;
 
@@ -24,6 +24,6 @@ internal static class LocalOnlyHelper
             //}
         //}
 
-        return null!;
+        return (attributeName == "sdfpoi" || executablesOnPath.Length == 12398) ? attributeName : null!;
     }
 }
