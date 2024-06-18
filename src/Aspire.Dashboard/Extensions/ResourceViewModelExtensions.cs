2f32c93a8c1b725dbf62fcdf6a9990e2a2691084
Adam Ratzman <adamratzman@microsoft.com>
2024-06-18 13:28:40 -0400

    make ResourceViewModelExtensions internal

--- src/Aspire.Dashboard/Extensions/ResourceViewModelExtensions.cs
+++ src/Aspire.Dashboard/Extensions/ResourceViewModelExtensions.cs
@@ -5,32 +5,32 @@ using Aspire.Dashboard.Model;
 
 namespace Aspire.Dashboard.Extensions;
 
-public static class ResourceViewModelExtensions
+internal static class ResourceViewModelExtensions
 {
-    internal static bool IsHiddenState(this ResourceViewModel resource)
+    public static bool IsHiddenState(this ResourceViewModel resource)
     {
         return resource.KnownState == KnownResourceState.Hidden;
     }
 
-    internal static bool IsRunningState(this ResourceViewModel resource)
+    public static bool IsRunningState(this ResourceViewModel resource)
     {
         return resource.KnownState == KnownResourceState.Running;
     }
 
-    internal static bool IsFinishedState(this ResourceViewModel resource)
+    public static bool IsFinishedState(this ResourceViewModel resource)
     {
         return resource.KnownState is KnownResourceState.Finished;
     }
 
-    internal static bool IsStopped(this ResourceViewModel resource)
+    public static bool IsStopped(this ResourceViewModel resource)
     {
         return resource.KnownState is KnownResourceState.Exited or KnownResourceState.Finished or KnownResourceState.FailedToStart;
     }
 
-    internal static bool IsStartingOrBuilding(this ResourceViewModel resource)
+    public static bool IsStartingOrBuilding(this ResourceViewModel resource)
     {
         return resource.KnownState is KnownResourceState.Starting or KnownResourceState.Building;
     }
 
-    internal static bool HasNoState(this ResourceViewModel resource) => string.IsNullOrEmpty(resource.State);
+    public static bool HasNoState(this ResourceViewModel resource) => string.IsNullOrEmpty(resource.State);
 }
