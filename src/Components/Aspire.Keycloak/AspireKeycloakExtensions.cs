044b89c15e488eb625854456b6eeeec5d59526bd
Julio Casal <julioc@dotnetmicroservices.com>
2024-06-19 06:54:40 -0700

    Improve docs

--- src/Components/Aspire.Keycloak/AspireKeycloakExtensions.cs
+++ src/Components/Aspire.Keycloak/AspireKeycloakExtensions.cs
@@ -21,13 +21,17 @@ public static class AspireKeycloakExtensions
     /// Adds Keycloak JWT Bearer authentication to the application.
     /// </summary>
     /// <param name="builder">The <see cref="AuthenticationBuilder" /> to add services to.</param>
-    /// <param name="connectionName">The connection name to use to find a connection string.</param>
+    /// <param name="serviceName">The name of the service used to resolve the Keycloak server URL.</param>
     /// <param name="realm">The realm of the Keycloak server to connect to.</param>
     /// <param name="authenticationScheme">The authentication scheme name. Default is "Bearer".</param>
     /// <param name="configureJwtBearerOptions">An optional action to configure the <see cref="JwtBearerOptions"/>.</param>
+    /// <remarks>
+    /// The <paramref name="serviceName"/> is used to resolve the Keycloak server URL and is combined with the realm to form the authority URL.
+    /// For example, if <paramref name="serviceName"/> is "keycloak" and <paramref name="realm"/> is "myrealm", the authority URL will be "https+http://keycloak/realms/myrealm".
+    /// </remarks>
     public static void AddKeycloakJwtBearer(
         this AuthenticationBuilder builder,
-        string connectionName,
+        string serviceName,
         string realm,
         string authenticationScheme = JwtBearerDefaults.AuthenticationScheme,
         Action<JwtBearerOptions>? configureJwtBearerOptions = null)
@@ -43,7 +47,7 @@ public static class AspireKeycloakExtensions
                .Configure<IConfiguration, IHttpClientFactory, IHostEnvironment>((options, configuration, httpClientFactory, hostEnvironment) =>
                {
                    options.Backchannel = httpClientFactory.CreateClient(KeycloakBackchannel);
-                   options.Authority = GetAuthorityUri(connectionName, realm);
+                   options.Authority = GetAuthorityUri(serviceName, realm);
 
                    configureJwtBearerOptions?.Invoke(options);
                });
@@ -53,14 +57,18 @@ public static class AspireKeycloakExtensions
     /// Adds Keycloak OpenID Connect authentication to the application.
     /// </summary>
     /// <param name="builder">The <see cref="AuthenticationBuilder" /> to add services to.</param>
-    /// <param name="connectionName">The connection name to use to find a connection string.</param>
+    /// <param name="serviceName">The name of the service used to resolve the Keycloak server URL.</param>
     /// <param name="realm">The realm of the Keycloak server to connect to.</param>
     /// <param name="openIdConnectScheme">The OpenID Connect authentication scheme name. Default is "OpenIdConnect".</param>
     /// <param name="cookieScheme">The cookie authentication scheme name. Default is "Cookie".</param>
     /// <param name="configureOpenIdConnectOptions">An optional action to configure the <see cref="OpenIdConnectOptions"/>.</param>
+    /// <remarks>
+    /// The <paramref name="serviceName"/> is used to resolve the Keycloak server URL and is combined with the realm to form the authority URL.
+    /// For example, if <paramref name="serviceName"/> is "keycloak" and <paramref name="realm"/> is "myrealm", the authority URL will be "https+http://keycloak/realms/myrealm".
+    /// </remarks>
     public static void AddKeycloakOpenIdConnect(
         this AuthenticationBuilder builder,
-        string connectionName,
+        string serviceName,
         string realm,
         string openIdConnectScheme = OpenIdConnectDefaults.AuthenticationScheme,
         string cookieScheme = CookieAuthenticationDefaults.AuthenticationScheme,
@@ -78,7 +86,7 @@ public static class AspireKeycloakExtensions
                .Configure<IConfiguration, IHttpClientFactory, IHostEnvironment>((options, configuration, httpClientFactory, hostEnvironment) =>
                {
                    options.Backchannel = httpClientFactory.CreateClient(KeycloakBackchannel);
-                   options.Authority = GetAuthorityUri(connectionName, realm);
+                   options.Authority = GetAuthorityUri(serviceName, realm);
                    options.SignInScheme = cookieScheme;
                    options.SignOutScheme = openIdConnectScheme;
 
