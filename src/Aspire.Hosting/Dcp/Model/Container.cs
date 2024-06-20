b1b06822547f5abe29095531d3662db586c1007c
Mitch Denny <midenn@microsoft.com>
2024-06-19 22:36:45 -0700

    Update container logs available condition to allow streaming build logs. (#4601)

--- src/Aspire.Hosting/Dcp/Model/Container.cs
+++ src/Aspire.Hosting/Dcp/Model/Container.cs
@@ -319,7 +319,9 @@ internal sealed class Container : CustomResource<ContainerSpec, ContainerStatus>
     }
 
     public bool LogsAvailable =>
-        this.Status?.State == ContainerState.Running
+        this.Status?.State == ContainerState.Starting
+        || this.Status?.State == ContainerState.Building
+        || this.Status?.State == ContainerState.Running
         || this.Status?.State == ContainerState.Paused
         || this.Status?.State == ContainerState.Stopping
         || this.Status?.State == ContainerState.Exited
