b308822be6925283d1579d71e39de18e0963439d
Willem Meints <1550763+wmeints@users.noreply.github.com>
2024-06-24 19:27:04 +0200

    Improve test case behavior

--- tests/Aspire.Hosting.Tests/Python/AddPythonProjectTests.cs
+++ tests/Aspire.Hosting.Tests/Python/AddPythonProjectTests.cs
@@ -1,75 +1,159 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using Aspire.Hosting.Tests.Helpers;
 using Xunit;
 using Microsoft.Extensions.DependencyInjection;
-using System.Text.RegularExpressions;
+using Aspire.Hosting.Utils;
+using Aspire.Hosting.Tests.Utils;
 
 namespace Aspire.Hosting.Tests.Python;
 
 public class AddPythonProjectTests
 {
-    private static readonly Regex s_pythonExecutablePattern  = new Regex("python(\\.(exe|bat|cmd|sh|ps1))?$", RegexOptions.IgnoreCase);
-    private static readonly Regex s_telemetryExecutablePattern = new Regex("opentelemetry-instrument(\\.(exe|bat|cmd|sh|ps1))?$", RegexOptions.IgnoreCase);
-    private static readonly string s_playgroundDirectory = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "../../../../../playground/python/"));
-
-    [LocalOnlyFact("python")]
-    public void AddPythonProject_SetsResourcePropertiesCorrectly()
+    [Fact]
+    public async Task AddPythonProject_SetsResourcePropertiesCorrectly()
     {
-        var pythonProjectDirectory = Path.Combine(s_playgroundDirectory, "script_only");
-        var appBuilder = DistributedApplication.CreateBuilder();
+        using var builder = TestDistributedApplicationBuilder.Create();
 
-        appBuilder.AddPythonProject("python", pythonProjectDirectory, "main.py");
+        var (projectDirectory, pythonExecutable, scriptName) = CreateTempPythonProject();
+        
+        builder.AddPythonProject("pythonProject", projectDirectory, scriptName);
 
-        var app = appBuilder.Build();
+        var app = builder.Build();
         var appModel = app.Services.GetRequiredService<DistributedApplicationModel>();
         var executableResources = appModel.GetExecutableResources();
 
         var pythonProjectResource = Assert.Single(executableResources);
 
-        Assert.Equal("python", pythonProjectResource.Name);
-        Assert.Equal(pythonProjectDirectory, pythonProjectResource.WorkingDirectory);
+        Assert.Equal("pythonProject", pythonProjectResource.Name);
+        Assert.Equal(projectDirectory, pythonProjectResource.WorkingDirectory);
+
+        if(OperatingSystem.IsWindows())
+        {
+            Assert.Equal(Path.Join(projectDirectory, ".venv", "Scripts", "python.exe"), pythonProjectResource.Command);
+        }
+        else
+        {
+            Assert.Equal(Path.Join(projectDirectory, ".venv", "bin", "python"), pythonProjectResource.Command);
+        }
+
+        var commandArguments = await ArgumentEvaluator.GetArgumentListAsync(pythonProjectResource);
+
+        Assert.Equal(scriptName, commandArguments[0]);
     }
 
-    [LocalOnlyFact("python")]
-    public void AddPythonProject_SetsPythonExecutable()
+    [Fact]
+    public async Task AddPythonProjectWithInstrumentation_SwitchesExecutableToInstrumentationExecutable()
     {
-        var pythonProjectDirectory = Path.Combine(s_playgroundDirectory, "script_only");
-        var appBuilder = DistributedApplication.CreateBuilder();
+        using var builder = TestDistributedApplicationBuilder.Create();
 
-        appBuilder.AddPythonProject("python", pythonProjectDirectory, "main.py");
+        var (projectDirectory, pythonExecutable, scriptName) = CreateTempPythonProject(instrument: true);
+        
+        builder.AddPythonProject("pythonProject", projectDirectory, scriptName, virtualEnvironmentPath: ".venv");
 
-        var app = appBuilder.Build();
+        var app = builder.Build();
         var appModel = app.Services.GetRequiredService<DistributedApplicationModel>();
         var executableResources = appModel.GetExecutableResources();
 
         var pythonProjectResource = Assert.Single(executableResources);
+        var commandArguments = await ArgumentEvaluator.GetArgumentListAsync(pythonProjectResource);
 
-        Assert.Equal("python", pythonProjectResource.Name);
-        Assert.Equal(pythonProjectDirectory, pythonProjectResource.WorkingDirectory);
-        Assert.Contains(".venv", pythonProjectResource.Command);
-        Assert.Matches(s_pythonExecutablePattern, pythonProjectResource.Command);
-        Assert.Equal("python", pythonProjectResource.Name);
+        if (OperatingSystem.IsWindows())
+        {
+            Assert.Equal(Path.Join(projectDirectory, ".venv", "Scripts", "opentelemetry-instrument.exe"), pythonProjectResource.Command);
+        }
+        else
+        {
+            Assert.Equal(Path.Join(projectDirectory, ".venv", "bin", "opentelemetry-instrument"), pythonProjectResource.Command);
+        }
+
+        Assert.Equal("--traces_exporter", commandArguments[0]);
+        Assert.Equal("otlp", commandArguments[1]);
+        Assert.Equal("--logs_exporter", commandArguments[2]);
+        Assert.Equal("console,otlp", commandArguments[3]);
+        Assert.Equal("--metrics_exporter", commandArguments[4]);
+        Assert.Equal("otlp", commandArguments[5]);
+        Assert.Equal(pythonExecutable, commandArguments[6]);
+        Assert.Equal(scriptName, commandArguments[7]);
     }
 
-    [LocalOnlyFact("python")]
-    public void AddInstrumentedPythonProject_ExecutesInstrumentationTool()
+    [Fact]
+    public async Task AddPythonProjectWithScriptArgs_IncludesTheArguments()
     {
-        var pythonProjectDirectory = Path.Combine(s_playgroundDirectory, "instrumented_script");
-        var appBuilder = DistributedApplication.CreateBuilder();
+        using var builder = TestDistributedApplicationBuilder.Create();
 
-        appBuilder.AddPythonProject("python", pythonProjectDirectory, "main.py");
+        var (projectDirectory, pythonExecutable, scriptName) = CreateTempPythonProject();
 
-        var app = appBuilder.Build();
+        builder.AddPythonProject("pythonProject", projectDirectory, scriptName, scriptArgs: "test");
+
+        var app = builder.Build();
         var appModel = app.Services.GetRequiredService<DistributedApplicationModel>();
         var executableResources = appModel.GetExecutableResources();
 
         var pythonProjectResource = Assert.Single(executableResources);
 
-        Assert.Equal("python", pythonProjectResource.Name);
-        Assert.Equal(pythonProjectDirectory, pythonProjectResource.WorkingDirectory);
-        Assert.Contains(".venv", pythonProjectResource.Command);
-        Assert.Matches(s_telemetryExecutablePattern, pythonProjectResource.Command);
+        Assert.Equal("pythonProject", pythonProjectResource.Name);
+        Assert.Equal(projectDirectory, pythonProjectResource.WorkingDirectory);
+
+        if (OperatingSystem.IsWindows())
+        {
+            Assert.Equal(Path.Join(projectDirectory, ".venv", "Scripts", "python.exe"), pythonProjectResource.Command);
+        }
+        else
+        {
+            Assert.Equal(Path.Join(projectDirectory, ".venv", "bin", "python"), pythonProjectResource.Command);
+        }
+
+        var commandArguments = await ArgumentEvaluator.GetArgumentListAsync(pythonProjectResource);
+
+        Assert.Equal(scriptName, commandArguments[0]);
+        Assert.Equal("test", commandArguments[1]);
+    }
+
+    private static (string projectDirectory, string pythonExecutable, string scriptName) CreateTempPythonProject(bool instrument = false)
+    {
+        
+
+        var projectDirectory = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+        Directory.CreateDirectory(projectDirectory);
+
+        // Create a fake virtual environment.
+        var virtualEnvDirectory = Path.Combine(projectDirectory, ".venv");
+        Directory.CreateDirectory(virtualEnvDirectory);
+
+        string pythonExecutable = Path.Combine(virtualEnvDirectory, OperatingSystem.IsWindows() ? "Scripts\\python.exe" : "bin/python");
+
+        if (OperatingSystem.IsWindows())
+        {
+            var scriptsDirectory = Path.Join(virtualEnvDirectory, "Scripts");
+            Directory.CreateDirectory(scriptsDirectory);
+
+            File.WriteAllText(pythonExecutable, "");
+
+            if(instrument)
+            {
+                File.WriteAllText(Path.Join(scriptsDirectory, "opentelemetry-instrument.exe"), "");
+            }
+        }
+        else
+        {
+            var binariesDirectory = Path.Join(virtualEnvDirectory, "bin");
+            Directory.CreateDirectory(binariesDirectory);
+
+            File.WriteAllText(pythonExecutable, "");
+
+            if (instrument)
+            {
+                File.WriteAllText(Path.Join(binariesDirectory, "opentelemetry-instrument"), "");
+            }
+        }
+
+        // Create the main script with some bogus content.
+        var scriptName = "main.py";
+        var scriptPath = Path.Combine(projectDirectory, scriptName);
+
+        File.WriteAllText(scriptPath, "print('Hello, World!')");
+
+        return (projectDirectory, pythonExecutable, scriptName);
     }
 }
