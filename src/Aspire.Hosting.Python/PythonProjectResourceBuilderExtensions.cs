533109bd99c92ec910f530d19a2e8f256ea95068
Mitch Denny <midenn@microsoft.com>
2024-06-25 11:56:15 +1000

    Expand remarks and fix up solution file.

--- src/Aspire.Hosting.Python/PythonProjectResourceBuilderExtensions.cs
+++ src/Aspire.Hosting.Python/PythonProjectResourceBuilderExtensions.cs
@@ -15,15 +15,6 @@ public static class PythonProjectResourceBuilderExtensions
     /// <summary>
     /// Adds a python application with a virtual environment to the application model.
     /// </summary>
-    /// <example>
-    /// Use in application host
-    /// <code lang="csharp">
-    /// var builder = DistributedApplication.CreateBuilder(args);
-    ///
-    /// builder.AddPythonProject("python-project", "PythonProject", "main.py");
-    /// 
-    /// builder.Build().Run(); 
-    /// </code>
     /// </example>
     /// <param name="builder">The <see cref="IDistributedApplicationBuilder"/> to add the resource to.</param>
     /// <param name="name">The name of the resource.</param>
@@ -33,10 +24,39 @@ public static class PythonProjectResourceBuilderExtensions
     /// <param name="scriptArgs">The arguments for the script.</param>
     /// <returns>A reference to the <see cref="IResourceBuilder{T}"/>.</returns>
     /// <remarks>
+    /// <para>
+    /// The virtual environment must be initialized before running the project. By default the virtual environment folder is expected
+    /// to be named <c>.venv</c> and be located in the project directory. If the virtual environment is located in a different directory
+    /// this default can be overridden using the <paramref name="virtualEnvironmentPath"/> parameter.
+    /// </para>
+    /// <para>
+    /// The virtual environment is setup individually for each project to allow each project to use a different version of
+    /// Python and dependencies. To setup a virtual environment use the <c>python -m venv .venv</c> command in the project
+    /// directory. This will create a virtual environment in the <c>.venv</c> directory.
+    /// </para>
+    /// <para>
+    /// To restore dependencies in the virtual environment first activate the environment by executing the activation
+    /// script and then use the <c>pip install -r requirements.txt</c> command to restore dependencies.
+    /// </para>
+    /// <para>
     /// To receive traces, logs, and metrics from the python project in the dashboard, the project must be instrumented with OpenTelemetry.
     /// You can instrument your project by adding the <c>opentelemetry-distro</c>, and <c>opentelemetry-exporter-otlp</c> to
     /// your Python project.
+    /// </para>
     /// </remarks>
+    /// <example>
+    /// Add a python project to the application model. In this example the project is located in the <c>PythonProject</c> directory
+    /// if this path is relative then it is assumed to be relative to the AppHost directory, and the virtual enviroment path if relative
+    /// is relative to the project directory. In the example below, if the app host directory is <c>$HOME/repos/MyApp/src/MyApp.AppHost</c> then
+    /// the ProjectPath would be <c>$HOME/repos/MyApp/src/MyApp.AppHost/PythonProject</c> and the virtual environment path (defaulted) would
+    /// be <c>$HOME/repos/MyApp/src/MyApp.AppHost/PythonProject/.venv</c>.
+    /// <code lang="csharp">
+    /// var builder = DistributedApplication.CreateBuilder(args);
+    ///
+    /// builder.AddPythonProject("python-project", "PythonProject", "main.py");
+    /// 
+    /// builder.Build().Run(); 
+    /// </code>
     public static IResourceBuilder<PythonProjectResource> AddPythonProject(
         this IDistributedApplicationBuilder builder, string name, string projectDirectory, string scriptPath,
         string? virtualEnvironmentPath = null, params string[] scriptArgs)
