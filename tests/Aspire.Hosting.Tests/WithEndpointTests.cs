1329b49a59f1c3b78e0a29db9654c95886cd7fce
David Fowler <davidfowl@gmail.com>
2024-06-27 21:34:30 -0700

    Fix regression in port allocation for non http/https endpoints (#4696)
    * Fix regression in port allocation
    - We should only consider the http and https schemes for default port values (null)
    - Updated the test to account for this
    
    * Updated the manifest
    
    * Explicit check when adding schemes and fixed naming of set

--- tests/Aspire.Hosting.Tests/WithEndpointTests.cs
+++ tests/Aspire.Hosting.Tests/WithEndpointTests.cs
@@ -459,7 +459,8 @@ public class WithEndpointTests
             .WithHttpEndpoint(name: "hp")       // Won't get targetPort since it's the first http
             .WithHttpEndpoint(name: "hp2")      // Will get a targetPort
             .WithHttpsEndpoint(name: "hps")     // Won't get targetPort since it's the first https
-            .WithHttpsEndpoint(name: "hps2");   // Will get a targetPort
+            .WithHttpsEndpoint(name: "hps2")   // Will get a targetPort
+            .WithEndpoint(scheme: "tcp", name: "tcp0");  // Will get a targetPort
 
         var manifest = await ManifestUtils.GetManifest(project.Resource);
 
@@ -498,6 +499,12 @@ public class WithEndpointTests
                   "protocol": "tcp",
                   "transport": "http",
                   "targetPort": 8001
+                },
+                "tcp0": {
+                  "scheme": "tcp",
+                  "protocol": "tcp",
+                  "transport": "tcp",
+                  "targetPort": 8002
                 }
               }
             }
