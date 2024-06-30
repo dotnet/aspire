515d34d3fc4637c714bd9fd513b76bd66d44623a
Ankit Jain <radical@gmail.com>
2024-06-29 20:19:53 -0400

    wip

---
+++ tests/Aspire.Playground.Tests/PlagroundAppFixture.cs
@@ -0,0 +1,198 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Xunit;
+using Xunit.Abstractions;
+using Aspire.Workload.Tests;
+
+namespace Aspire.EndToEnd.Tests;
+
+/// <summary>
+/// This fixture ensures the TestProject.AppHost application is started before a test is executed.
+///
+/// Represents the the IntegrationServiceA project in the test application used to send HTTP requests
+/// to the project's endpoints.
+/// </summary>
+public sealed class PlaygroundAppFixture : IAsyncLifetime
+{
+#if TESTS_RUNNING_OUTSIDE_OF_REPO
+    public static bool TestsRunningOutsideOfRepo = true;
+#else
+    public static bool TestsRunningOutsideOfRepo;
+#endif
+
+    public static string? TestScenario { get; } = EnvironmentVariables.TestScenario;
+    public Dictionary<string, ProjectInfo> Projects => Project?.InfoTable ?? throw new InvalidOperationException("Project is not initialized");
+
+    private readonly string _relativeAppHostProjectDir;
+
+    // private TestResourceNames _resourcesToSkip;
+    private readonly IMessageSink _diagnosticMessageSink;
+    private readonly TestOutputWrapper _testOutput;
+    private AspireProject? _project;
+
+    public BuildEnvironment BuildEnvironment { get; init; }
+    // public ProjectInfo IntegrationServiceA => Projects["integrationservicea"];
+    public AspireProject Project => _project ?? throw new InvalidOperationException("Project is not initialized");
+
+    public PlaygroundAppFixture(string relativeAppHostProjectDir, IMessageSink diagnosticMessageSink)
+    {
+        _relativeAppHostProjectDir = relativeAppHostProjectDir;
+        _diagnosticMessageSink = diagnosticMessageSink;
+        _testOutput = new TestOutputWrapper(messageSink: _diagnosticMessageSink);
+        BuildEnvironment = new(useSystemDotNet: !TestsRunningOutsideOfRepo);
+        if (TestsRunningOutsideOfRepo)
+        {
+            if (!BuildEnvironment.HasWorkloadFromArtifacts)
+            {
+                throw new InvalidOperationException("Expected to have sdk+workload from artifacts when running tests outside of the repo");
+            }
+            BuildEnvironment.EnvVars["TestsRunningOutsideOfRepo"] = "true";
+        }
+        else
+        {
+            // inside the repo
+            if (BuildEnvironment.RepoRoot is null)
+            {
+                throw new InvalidOperationException("These tests should be run from inside the repo when using `TestsRunningOutsideOfRepo=false`");
+            }
+
+            BuildEnvironment.TestAssetsPath = Path.Combine(BuildEnvironment.RepoRoot.FullName, "tests");
+            if (!Directory.Exists(BuildEnvironment.TestAssetsPath))
+            {
+                throw new ArgumentException($"Cannot find TestAssetsPath={BuildEnvironment.TestAssetsPath}");
+            }
+        }
+    }
+
+    public async Task InitializeAsync()
+    {
+        string pgProjectDir = Path.Combine(BuildEnvironment.PlaygroundAppsPath, _relativeAppHostProjectDir);
+        if (!Directory.Exists(pgProjectDir))
+        {
+            throw new DirectoryNotFoundException($"Playground project directory not found: {pgProjectDir}");
+        }
+        _project = new AspireProject(Path.GetFileName(_relativeAppHostProjectDir), pgProjectDir, _testOutput, BuildEnvironment);
+        if (TestsRunningOutsideOfRepo)
+        {
+            _testOutput.WriteLine("");
+            _testOutput.WriteLine($"****************************************");
+            _testOutput.WriteLine($"   Running EndToEnd tests outside-of-repo");
+            _testOutput.WriteLine($"   Playground project: {Project.AppHostProjectDirectory}");
+            _testOutput.WriteLine($"****************************************");
+            _testOutput.WriteLine("");
+        }
+
+        await Project.BuildAsync();
+
+        string extraArgs = "";
+        // _resourcesToSkip = GetResourcesToSkip();
+        // if (_resourcesToSkip != TestResourceNames.None && _resourcesToSkip.ToCSVString() is string skipArg)
+        // {
+        //     extraArgs += $"--skip-resources {skipArg}";
+        // }
+        await Project.StartAppHostAsync([extraArgs]);
+
+        foreach (var project in Projects.Values)
+        {
+            project.Client = AspireProject.Client.Value;
+        }
+    }
+
+    // public Task DumpComponentLogsAsync(TestResourceNames resource, ITestOutputHelper? testOutputArg = null)
+    // {
+    //     if (resource == TestResourceNames.None)
+    //     {
+    //         return Task.CompletedTask;
+    //     }
+    //     if (resource == TestResourceNames.All || !Enum.IsDefined<TestResourceNames>(resource))
+    //     {
+    //         throw new ArgumentException($"Only one resource is supported at a time. resource: {resource}");
+    //     }
+
+    //     string component = resource switch
+    //     {
+    //         TestResourceNames.cosmos => "cosmos",
+    //         TestResourceNames.kafka => "kafka",
+    //         TestResourceNames.mongodb => "mongodb",
+    //         TestResourceNames.mysql or TestResourceNames.efmysql => "mysql",
+    //         TestResourceNames.oracledatabase => "oracledatabase",
+    //         TestResourceNames.postgres or TestResourceNames.efnpgsql => "postgres",
+    //         TestResourceNames.rabbitmq => "rabbitmq",
+    //         TestResourceNames.redis => "redis",
+    //         TestResourceNames.garnet => "garnet",
+    //         TestResourceNames.valkey => "valkey",
+    //         TestResourceNames.sqlserver => "sqlserver",
+    //         TestResourceNames.milvus => "milvus",
+    //         TestResourceNames.eventhubs => "eventhubs",
+    //         _ => throw new ArgumentException($"Unknown resource: {resource}")
+    //     };
+
+    //     return Project.DumpComponentLogsAsync(component, testOutputArg);
+    // }
+
+    public async Task DisposeAsync()
+    {
+        if (_project is not null)
+        {
+            await _project.DisposeAsync();
+        }
+    }
+
+    // public void EnsureAppHasResources(TestResourceNames expectedResourceNames)
+    // {
+    //     foreach (var ename in Enum.GetValues<TestResourceNames>())
+    //     {
+    //         if (ename != TestResourceNames.None && expectedResourceNames.HasFlag(ename) && _resourcesToSkip.HasFlag(ename))
+    //         {
+    //             throw new InvalidOperationException($"The required resource '{ename}' was skipped for the app run for TestScenario: {TestScenario}. Make sure that the TEST_SCENARIO environment variable matches the intended scenario for the test. Resources that were skipped: {string.Join(",", _resourcesToSkip)}. TestScenario: {TestScenario} ");
+    //         }
+    //     }
+    // }
+
+    // private static TestResourceNames GetResourcesToSkip()
+    // {
+    //     TestResourceNames resourcesToInclude = TestScenario switch
+    //     {
+    //         "oracle" => TestResourceNames.oracledatabase,
+    //         "cosmos" => TestResourceNames.cosmos,
+    //         "eventhubs" => TestResourceNames.eventhubs,
+    //         "basicservices" => TestResourceNames.kafka
+    //                           | TestResourceNames.mongodb
+    //                           | TestResourceNames.rabbitmq
+    //                           | TestResourceNames.redis
+    //                           | TestResourceNames.garnet
+    //                           | TestResourceNames.valkey
+    //                           | TestResourceNames.postgres
+    //                           | TestResourceNames.efnpgsql
+    //                           | TestResourceNames.mysql
+    //                           | TestResourceNames.efmysql
+    //                           | TestResourceNames.sqlserver
+    //                           | TestResourceNames.milvus,
+    //         "" or null => TestResourceNames.All,
+    //         _ => throw new ArgumentException($"Unknown test scenario '{TestScenario}'")
+    //     };
+
+    //     TestResourceNames resourcesToSkip = TestResourceNames.All & ~resourcesToInclude;
+
+    //     // always skip cosmos on macos/arm64
+    //     if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX) && RuntimeInformation.ProcessArchitecture == Architecture.Arm64)
+    //     {
+    //         resourcesToSkip |= TestResourceNames.cosmos;
+    //     }
+    //     if (string.IsNullOrEmpty(TestScenario))
+    //     {
+    //         // no scenario specified
+    //         if (BuildEnvironment.IsRunningOnCI)
+    //         {
+    //             resourcesToSkip |= TestResourceNames.cosmos;
+    //             resourcesToSkip |= TestResourceNames.oracledatabase;
+    //         }
+    //     }
+
+    //     // always skip the dashboard
+    //     resourcesToSkip |= TestResourceNames.dashboard;
+
+    //     return resourcesToSkip;
+    // }
+}
---
+++ tests/Aspire.Playground.Tests/PlaygroundTestsBase.cs
@@ -0,0 +1,162 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Microsoft.Playwright;
+// using Xunit;
+using Xunit.Abstractions;
+using Aspire.Workload.Tests;
+// using static Aspire.Workload.Tests.TestExtensions;
+
+namespace Aspire.Playground.Tests;
+
+public class PlaygroundTestsBase
+{
+    private static Lazy<IBrowser> Browser => new(CreateBrowser);
+    protected readonly TestOutputWrapper _testOutput;
+
+    // public static readonly string[] TestFrameworkTypes = ["none", "mstest", "nunit", "xunit.net"];
+
+    public PlaygroundTestsBase(ITestOutputHelper testOutput)
+        => _testOutput = new TestOutputWrapper(testOutput);
+
+    private static IBrowser CreateBrowser()
+    {
+        var t = Task.Run(async () => await PlaywrightProvider.CreateBrowserAsync());
+
+        // default timeout for playwright.Chromium.LaunchAsync is 30secs,
+        // so using a timeout here as a fallback
+        if (!t.Wait(45 * 1000))
+        {
+            throw new TimeoutException("Browser creation timed out");
+        }
+
+        return t.Result;
+    }
+
+    public static Task<IBrowserContext> CreateNewBrowserContextAsync()
+        => BuildEnvironment.HasPlaywrightSupport
+                ? Browser.Value.NewContextAsync(new BrowserNewContextOptions { IgnoreHTTPSErrors = true })
+                : throw new InvalidOperationException("Playwright is not available");
+#if false
+    protected Task<ResourceRow[]> CheckDashboardHasResourcesAsync(IPage dashboardPage, IEnumerable<ResourceRow> expectedResources, int timeoutSecs = 120)
+        => CheckDashboardHasResourcesAsync(dashboardPage, expectedResources, _testOutput, timeoutSecs);
+
+    protected static async Task<ResourceRow[]> CheckDashboardHasResourcesAsync(IPage dashboardPage, IEnumerable<ResourceRow> expectedResources, ITestOutputHelper testOutput, int timeoutSecs = 120)
+    {
+        // FIXME: check the page has 'Resources' label
+        // fluent-toolbar/h1 resources
+
+        testOutput.WriteLine($"Waiting for resources to appear on the dashboard");
+        await Task.Delay(500);
+
+        Dictionary<string, ResourceRow> expectedRowsTable = expectedResources.ToDictionary(r => r.Name);
+        HashSet<string> foundNames = [];
+        List<ResourceRow> foundRows = [];
+
+        var cts = new CancellationTokenSource();
+        cts.CancelAfter(TimeSpan.FromSeconds(timeoutSecs));
+
+        while (foundNames.Count < expectedRowsTable.Count && !cts.IsCancellationRequested)
+        {
+            await Task.Delay(500);
+
+            // _testOutput.WriteLine($"Checking for rows again");
+            ILocator rowsLocator = dashboardPage.Locator("//fluent-data-grid-row[@class='resource-row']");
+            var allRows = await rowsLocator.AllAsync();
+            // _testOutput.WriteLine($"found rows#: {allRows.Count}");
+            if (allRows.Count == 0)
+            {
+                // Console.WriteLine ($"** no rows found ** elapsed: {sw.Elapsed.TotalSeconds} secs");
+                continue;
+            }
+
+            foreach (ILocator rowLoc in allRows)
+            {
+                // get the cells
+                IReadOnlyList<ILocator> cellLocs = await rowLoc.Locator("//fluent-data-grid-cell[@role='gridcell']").AllAsync();
+                Assert.Equal(8, cellLocs.Count);
+
+                // is the resource name expected?
+                string resourceName = await cellLocs[1].InnerTextAsync();
+                if (!expectedRowsTable.TryGetValue(resourceName, out var expectedRow))
+                {
+                    Assert.Fail($"Row with unknown name found: {resourceName}");
+                }
+                if (foundNames.Contains(resourceName))
+                {
+                    continue;
+                }
+
+                string resourceNameInCell = await cellLocs[1].InnerTextAsync().ConfigureAwait(false);
+                resourceNameInCell.Trim();
+                AssertEqual(expectedRow.Name, resourceNameInCell, $"Name for {resourceName}");
+
+                string actualState = await cellLocs[2].InnerTextAsync().ConfigureAwait(false);
+                actualState = actualState.Trim();
+                if (expectedRow.State != actualState && actualState != "Finished" && !actualState.Contains("failed", StringComparison.OrdinalIgnoreCase))
+                {
+                    testOutput.WriteLine($"[{expectedRow.Name}] expected state: '{expectedRow.State}', actual state: '{actualState}'");
+                    continue;
+                }
+                AssertEqual(expectedRow.State, await cellLocs[2].InnerTextAsync(), $"State for {resourceName}");
+
+                // Match endpoints
+
+                int matchingEndpoints = 0;
+                var expectedEndpoints = expectedRow.Endpoints;
+
+                string[] endpointsFound =
+                    (await rowLoc.Locator("//div[@class='fluent-overflow-item']").AllAsync())
+                        .Select(async e => await e.InnerTextAsync())
+                        .Select(t => t.Result.Trim(','))
+                        .ToArray();
+                // FIXME: this could still return "+2"
+                if (endpointsFound.Length == 0)
+                {
+                    var cellText = await cellLocs[5].InnerTextAsync();
+                    endpointsFound = cellText.Trim().Split(',', StringSplitOptions.RemoveEmptyEntries);
+                }
+                if (expectedEndpoints.Length != endpointsFound.Length)
+                {
+                    // _testOutput.WriteLine($"For resource '{resourceName}, found ")
+                    // _testOutput.WriteLine($"-- expected: {expectedEndpoints.Length} found: {endpointsFound.Length}, expected: {string.Join(',', expectedEndpoints)} found: {string.Join(',', endpointsFound)} for {resourceName}");
+                    continue;
+                }
+
+                AssertEqual(expectedEndpoints.Length, endpointsFound.Length, $"#endpoints for {resourceName}");
+
+                // endpointsFound: ["foo", "https://localhost:7589/weatherforecast"]
+                foreach (var endpointFound in endpointsFound)
+                {
+                    // matchedEndpoints: ["https://localhost:7589/weatherforecast"]
+                    string[] matchedEndpoints = expectedEndpoints.Where(e => Regex.IsMatch(endpointFound, e)).ToArray();
+                    if (matchedEndpoints.Length == 0)
+                    {
+                        Assert.Fail($"Unexpected endpoint found: {endpointFound} for resource named {resourceName}. Expected endpoints: {string.Join(',', expectedEndpoints)}");
+                    }
+                    matchingEndpoints++;
+                }
+
+                AssertEqual(expectedEndpoints.Length, matchingEndpoints, $"Expected number of endpoints for {resourceName}");
+
+                // Check 'Source' column
+                AssertEqual(expectedRow.Source, await cellLocs[4].InnerTextAsync(), $"Source for {resourceName}");
+
+                foundRows.Add(expectedRow with { Endpoints = endpointsFound });
+                foundNames.Add(resourceName);
+            }
+        }
+
+        if (foundNames.Count != expectedRowsTable.Count)
+        {
+            Assert.Fail($"Expected rows not found: {string.Join(", ", expectedRowsTable.Keys.Except(foundNames))}");
+        }
+
+        return foundRows.ToArray();
+    }
+#endif
+
+    // Don't fixup the prefix so it can have characters meant for testing, like spaces
+    public static string GetNewProjectId(string? prefix = null)
+        => (prefix is null ? "" : $"{prefix}_") + Path.GetRandomFileName();
+}
