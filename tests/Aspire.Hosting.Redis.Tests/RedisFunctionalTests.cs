c8be7b32f4ac4f4739fe119595659cdc72633709
Sebastien Ros <sebastienros@gmail.com>
2024-07-01 07:40:05 -0700

    Mark functional test with RequiresDocker
    Otherwise would try to run it on Windows in CI where Docker is not available

--- tests/Aspire.Hosting.Redis.Tests/RedisFunctionalTests.cs
+++ tests/Aspire.Hosting.Redis.Tests/RedisFunctionalTests.cs
@@ -1,6 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using Aspire.Components.Common.Tests;
 using Aspire.Hosting.Utils;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -13,6 +14,7 @@ namespace Aspire.Hosting.Redis.Tests;
 public class RedisFunctionalTests
 {
     [Fact]
+    [RequiresDocker]
     public async Task VerifyRedisResource()
     {
         var builder = TestDistributedApplicationBuilder.Create();
