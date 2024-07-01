64623584db2fb285b48efe7aa9b1be40fb3350f9
Sebastien Ros <sebastienros@gmail.com>
2024-07-01 08:48:27 -0700

    Create AddDataVolume tests for Redis

--- tests/Aspire.Hosting.Redis.Tests/RedisFunctionalTests.cs
+++ tests/Aspire.Hosting.Redis.Tests/RedisFunctionalTests.cs
@@ -48,4 +48,153 @@ public class RedisFunctionalTests
 
         Assert.Equal("value", value);
     }
+
+    [Fact]
+    [RequiresDocker]
+    public async Task AddDataVolumeShouldPersistStateBetweenUsages()
+    {
+        var volumeName = "myvolume";
+
+        // Use a volume to do a snapshot save
+
+        var builder1 = TestDistributedApplicationBuilder.Create();
+        var redis1 = builder1.AddRedis("redis").WithDataVolume(volumeName);
+
+        using (var app = builder1.Build())
+        {
+            await app.StartAsync();
+
+            var hb = Host.CreateApplicationBuilder();
+
+            // BGSAVE is only available in admin mode, enable it for this instance
+            hb.Configuration.AddInMemoryCollection(new Dictionary<string, string?>
+            {
+                [$"ConnectionStrings:{redis1.Resource.Name}"] = $"{await redis1.Resource.GetConnectionStringAsync()},allowAdmin=true"
+            });
+
+            hb.AddRedisClient(redis1.Resource.Name);
+
+            using (var host = hb.Build())
+            {
+                await host.StartAsync();
+
+                var redisClient = host.Services.GetRequiredService<IConnectionMultiplexer>();
+
+                var db = redisClient.GetDatabase();
+
+                await db.StringSetAsync("key", "value");
+
+                // Force Redis to save the keys (snapshotting)
+                // c.f. https://redis.io/docs/latest/operate/oss_and_stack/management/persistence/
+
+                await redisClient.GetServers().First().SaveAsync(SaveType.BackgroundSave);
+            }
+        }
+
+        // Start a new instance with the same existing volume
+
+        var builder2 = TestDistributedApplicationBuilder.Create();
+        var redis2 = builder2.AddRedis("redis").WithDataVolume(volumeName);
+
+        using (var app = builder2.Build())
+        {
+            await app.StartAsync();
+
+            var hb = Host.CreateApplicationBuilder();
+
+            hb.Configuration.AddInMemoryCollection(new Dictionary<string, string?>
+            {
+                [$"ConnectionStrings:{redis2.Resource.Name}"] = await redis2.Resource.GetConnectionStringAsync()
+            });
+
+            hb.AddRedisClient(redis2.Resource.Name);
+
+            using (var host = hb.Build())
+            {
+                await host.StartAsync();
+
+                var redisClient = host.Services.GetRequiredService<IConnectionMultiplexer>();
+
+                var db = redisClient.GetDatabase();
+
+                var value = await db.StringGetAsync("key");
+
+                Assert.Equal("value", value);
+            }
+        }
+    }
+
+    [Fact]
+    [RequiresDocker]
+    public async Task AddDataVolumeWithCustomPersistenceInterval()
+    {
+        var volumeName = "myvolume";
+        var interval = TimeSpan.FromSeconds(1);
+
+        // Use a volume to do a snapshot save
+
+        var builder1 = TestDistributedApplicationBuilder.Create();
+        var redis1 = builder1.AddRedis("redis").WithDataVolume(volumeName).WithPersistence(interval);
+
+        using (var app = builder1.Build())
+        {
+            await app.StartAsync();
+
+            var hb = Host.CreateApplicationBuilder();
+
+            hb.Configuration.AddInMemoryCollection(new Dictionary<string, string?>
+            {
+                [$"ConnectionStrings:{redis1.Resource.Name}"] = await redis1.Resource.GetConnectionStringAsync()
+            });
+
+            hb.AddRedisClient(redis1.Resource.Name);
+
+            using (var host = hb.Build())
+            {
+                await host.StartAsync();
+
+                var redisClient = host.Services.GetRequiredService<IConnectionMultiplexer>();
+
+                var db = redisClient.GetDatabase();
+
+                await db.StringSetAsync("key", "value");
+
+                // Wait 1 second more than the interval
+                await Task.Delay(interval + TimeSpan.FromSeconds(1));
+            }
+        }
+
+        // Start a new instance with the same existing volume
+
+        var builder2 = TestDistributedApplicationBuilder.Create();
+        var redis2 = builder2.AddRedis("redis").WithDataVolume(volumeName);
+
+        using (var app = builder2.Build())
+        {
+            await app.StartAsync();
+
+            var hb = Host.CreateApplicationBuilder();
+
+            hb.Configuration.AddInMemoryCollection(new Dictionary<string, string?>
+            {
+                [$"ConnectionStrings:{redis2.Resource.Name}"] = await redis2.Resource.GetConnectionStringAsync()
+            });
+
+            hb.AddRedisClient(redis2.Resource.Name);
+
+            using (var host = hb.Build())
+            {
+                await host.StartAsync();
+
+                var redisClient = host.Services.GetRequiredService<IConnectionMultiplexer>();
+
+                var db = redisClient.GetDatabase();
+
+                var value = await db.StringGetAsync("key");
+
+                Assert.Equal("value", value);
+            }
+        }
+    }
+
 }
