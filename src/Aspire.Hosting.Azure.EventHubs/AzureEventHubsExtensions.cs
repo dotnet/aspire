4777e4e4a92826789790562cdbf9bdbecaa99cef
Sebastien Ros <sebastienros@gmail.com>
2024-07-03 08:36:30 -0700

    Delete obsolete methods as EH hasn't shipped yet

--- src/Aspire.Hosting.Azure.EventHubs/AzureEventHubsExtensions.cs
+++ src/Aspire.Hosting.Azure.EventHubs/AzureEventHubsExtensions.cs
@@ -181,27 +181,6 @@ public static class AzureEventHubsExtensions
     /// Adds a bind mount for the data folder to an Azure Event Hubs emulator resource.
     /// </summary>
     /// <param name="builder">The builder for the <see cref="AzureEventHubsEmulatorResource"/>.</param>
-    /// <param name="isReadOnly">A flag that indicates if this is a read-only mount.</param>
-    /// <returns>A builder for the <see cref="AzureEventHubsEmulatorResource"/>.</returns>
-    [Obsolete("This method is obsolete and will be removed in a future version. Use the overload without the isReadOnly parameter.")]
-    public static IResourceBuilder<AzureEventHubsEmulatorResource> WithDataBindMount(this IResourceBuilder<AzureEventHubsEmulatorResource> builder, bool isReadOnly)
-        => builder.WithDataBindMount(path: null, isReadOnly);
-
-    /// <summary>
-    /// Adds a bind mount for the data folder to an Azure Event Hubs emulator resource.
-    /// </summary>
-    /// <param name="builder">The builder for the <see cref="AzureEventHubsEmulatorResource"/>.</param>
-    /// <param name="path">Relative path to the AppHost where emulator storage is persisted between runs. Defaults to the path '.eventhubs/{builder.Resource.Name}'</param>
-    /// <param name="isReadOnly">A flag that indicates if this is a read-only mount.</param>
-    /// <returns>A builder for the <see cref="AzureEventHubsEmulatorResource"/>.</returns>
-    [Obsolete("This method is obsolete and will be removed in a future version. Use the overload without the isReadOnly parameter.")]
-    public static IResourceBuilder<AzureEventHubsEmulatorResource> WithDataBindMount(this IResourceBuilder<AzureEventHubsEmulatorResource> builder, string? path, bool isReadOnly)
-        => builder.WithBindMount(path ?? $".eventhubs/{builder.Resource.Name}", "/data", isReadOnly);
-
-    /// <summary>
-    /// Adds a bind mount for the data folder to an Azure Event Hubs emulator resource.
-    /// </summary>
-    /// <param name="builder">The builder for the <see cref="AzureEventHubsEmulatorResource"/>.</param>
     /// <param name="path">Relative path to the AppHost where emulator storage is persisted between runs. Defaults to the path '.eventhubs/{builder.Resource.Name}'</param>
     /// <returns>A builder for the <see cref="AzureEventHubsEmulatorResource"/>.</returns>
     public static IResourceBuilder<AzureEventHubsEmulatorResource> WithDataBindMount(this IResourceBuilder<AzureEventHubsEmulatorResource> builder, string? path = null)
