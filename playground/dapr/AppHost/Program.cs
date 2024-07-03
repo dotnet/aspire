2e471d2386e9639b10d96e295b86f0b67e6bf208
Ankit Jain <radical@gmail.com>
2024-07-03 18:35:15 -0400

    fix dapr

--- playground/dapr/AppHost/Program.cs
+++ playground/dapr/AppHost/Program.cs
@@ -12,13 +12,32 @@ builder.AddProject<Projects.DaprServiceB>("serviceb")
        .WithDaprSidecar()
        .WithReference(pubSub);
 
+#if BUILD_FOR_TEST
+builder.Services.AddLifecycleHook<EndPointWriterHook>();
+#endif
+
+#if !BUILD_FOR_TEST
 // This project is only added in playground projects to support development/debugging
 // of the dashboard. It is not required in end developer code. Comment out this code
 // to test end developer dashboard launch experience. Refer to Directory.Build.props
 // for the path to the dashboard binary (defaults to the Aspire.Dashboard bin output
 // in the artifacts dir).
 builder.AddProject<Projects.Aspire_Dashboard>(KnownResourceNames.AspireDashboard);
+#endif
 
 using var app = builder.Build();
+
+#if BUILD_FOR_TEST
+// Run a task to read from the console and stop the app if an external process sends "Stop".
+// This allows for easier control than sending CTRL+C to the console in a cross-platform way.
+_ = Task.Run(async () =>
+{
+    var s = Console.ReadLine();
+    if (s == "Stop")
+    {
+        await app.StopAsync();
+    }
+});
+#endif
 
 await app.RunAsync();
