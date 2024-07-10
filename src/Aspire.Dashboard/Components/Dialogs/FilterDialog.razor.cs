2362a37202827baf4940cb3620604e57a8ae3344
James Newton-King <james@newtonking.com>
2024-07-10 13:54:57 +0800

    More

--- src/Aspire.Dashboard/Components/Dialogs/FilterDialog.razor.cs
+++ src/Aspire.Dashboard/Components/Dialogs/FilterDialog.razor.cs
@@ -24,7 +24,7 @@ public partial class FilterDialog
     public FilterDialogViewModel Content { get; set; } = default!;
 
     [Inject]
-    public required TelemetryRepository TelemetryRepository { get; set; }
+    public required TelemetryRepository TelemetryRepository { get; init; }
 
     private LogDialogFormModel _formModel = default!;
     private List<SelectViewModel<string>> _parameters = default!;
