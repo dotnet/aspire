502971cb48dc72a53f756a7e67ce1ccbdcf043d0
Ankit Jain <radical@gmail.com>
2024-07-20 01:30:52 -0400

    fix playground build

--- playground/Elasticsearch/Elasticsearch.AppHost/Program.cs
+++ playground/Elasticsearch/Elasticsearch.AppHost/Program.cs
@@ -9,11 +9,13 @@ var elasticsearch = builder.AddElasticsearch("elasticsearch")
 builder.AddProject<Projects.Elasticsearch_ApiService>("elasticsearch-apiservice")
     .WithReference(elasticsearch);
 
+#if !BUILD_FOR_TEST
 // This project is only added in playground projects to support development/debugging
 // of the dashboard. It is not required in end developer code. Comment out this code
 // to test end developer dashboard launch experience. Refer to Directory.Build.props
 // for the path to the dashboard binary (defaults to the Aspire.Dashboard bin output
 // in the artifacts dir).
 builder.AddProject<Projects.Aspire_Dashboard>(KnownResourceNames.AspireDashboard);
+#endif
 
 builder.Build().Run();
