d646d47878e8a38c647ebb4d7673bb7a1f7b1003
Aram Hayrapetyan <aramhrptn@hotmail.com>
2024-07-22 01:37:26 +0400

    Add restart policy to Aspire Hosting ApplicationModel

--- src/Aspire.Hosting/Dcp/Model/Container.cs
+++ src/Aspire.Hosting/Dcp/Model/Container.cs
@@ -3,6 +3,7 @@
 
 using System.Net.Sockets;
 using System.Text.Json.Serialization;
+using Aspire.Hosting.ApplicationModel;
 using k8s.Models;
 
 namespace Aspire.Hosting.Dcp.Model;
@@ -10,109 +11,84 @@ namespace Aspire.Hosting.Dcp.Model;
 internal sealed class ContainerSpec
 {
     // Container name displayed in Docker. If not specified, the metadata name + random suffix is used.
-    [JsonPropertyName("containerName")]
-    public string? ContainerName { get; set; }
+    [JsonPropertyName("containerName")] public string? ContainerName { get; set; }
 
     // Image to be used to create the container
-    [JsonPropertyName("image")]
-    public string? Image { get; set; }
+    [JsonPropertyName("image")] public string? Image { get; set; }
 
     // Optional configuration to build an image from a Dockerfile instead of using a pre-built image
-    [JsonPropertyName("build")]
-    public BuildContext? Build { get; set; }
+    [JsonPropertyName("build")] public BuildContext? Build { get; set; }
 
     // Volumes that should be mounted into the container
-    [JsonPropertyName("volumeMounts")]
-    public List<VolumeMount>? VolumeMounts { get; set; }
+    [JsonPropertyName("volumeMounts")] public List<VolumeMount>? VolumeMounts { get; set; }
 
     // Exposed ports
-    [JsonPropertyName("ports")]
-    public List<ContainerPortSpec>? Ports { get; set; }
+    [JsonPropertyName("ports")] public List<ContainerPortSpec>? Ports { get; set; }
 
     // Environment variables to be used for the container
-    [JsonPropertyName("env")]
-    public List<EnvVar>? Env { get; set; }
+    [JsonPropertyName("env")] public List<EnvVar>? Env { get; set; }
 
     // Environment files to use to populate Container environment during startup
-    [JsonPropertyName("envFiles")]
-    public List<string>? EnvFiles { get; set; }
+    [JsonPropertyName("envFiles")] public List<string>? EnvFiles { get; set; }
 
     // Container restart policy
-    [JsonPropertyName("restartPolicy")]
-    public string? RestartPolicy { get; set; } = ContainerRestartPolicy.None;
+    [JsonPropertyName("restartPolicy")] public string? RestartPolicy { get; set; } = ContainerRestartPolicy.None;
 
     // Command to run in the container (entrypoint)
-    [JsonPropertyName("command")]
-    public string? Command { get; set; }
+    [JsonPropertyName("command")] public string? Command { get; set; }
 
     // Arguments to pass to the command that starts the container
-    [JsonPropertyName("args")]
-    public List<string>? Args { get; set; }
+    [JsonPropertyName("args")] public List<string>? Args { get; set; }
 
     // Optional labels to apply to the container instance
-    [JsonPropertyName("labels")]
-    public List<ContainerLabel>? Labels { get; set; }
+    [JsonPropertyName("labels")] public List<ContainerLabel>? Labels { get; set; }
 
     // Additional arguments to pass to the container run command
-    [JsonPropertyName("runArgs")]
-    public List<string>? RunArgs { get; set; }
+    [JsonPropertyName("runArgs")] public List<string>? RunArgs { get; set; }
 
     // Should this container be created and persisted between DCP runs?
-    [JsonPropertyName("persistent")]
-    public bool? Persistent { get; set; }
+    [JsonPropertyName("persistent")] public bool? Persistent { get; set; }
 
-    [JsonPropertyName("networks")]
-    public List<ContainerNetworkConnection>? Networks { get; set; }
+    [JsonPropertyName("networks")] public List<ContainerNetworkConnection>? Networks { get; set; }
 }
 
 internal sealed class BuildContext
 {
     // The path to the directory that will serve as the root of the image build context
-    [JsonPropertyName("context")]
-    public string? Context { get; set; }
+    [JsonPropertyName("context")] public string? Context { get; set; }
 
     // Optional path to a specific Dockerfile to use in the build (defaults to looking for a Dockerfile in the root Context folder)
-    [JsonPropertyName("dockerfile")]
-    public string? Dockerfile { get; set;}
+    [JsonPropertyName("dockerfile")] public string? Dockerfile { get; set; }
 
     // Optional build --build-args to pass to the build command
-    [JsonPropertyName("args")]
-    public List<EnvVar>? Args { get; set; }
+    [JsonPropertyName("args")] public List<EnvVar>? Args { get; set; }
 
     // Optional build secret mounts to pass to the build command
-    [JsonPropertyName("secrets")]
-    public List<BuildContextSecret>? Secrets { get; set; }
+    [JsonPropertyName("secrets")] public List<BuildContextSecret>? Secrets { get; set; }
 
     // Optional specific stage to use when building a multiple stage Dockerfile
-    [JsonPropertyName("stage")]
-    public string? Stage { get; set; }
+    [JsonPropertyName("stage")] public string? Stage { get; set; }
 
     // Optional additional tags to apply to the built image
-    [JsonPropertyName("tags")]
-    public List<string>? Tags { get; set; }
+    [JsonPropertyName("tags")] public List<string>? Tags { get; set; }
 
     // Optional labels to apply to the built image
-    [JsonPropertyName("labels")]
-    public List<ContainerLabel>? Labels { get; set; }
+    [JsonPropertyName("labels")] public List<ContainerLabel>? Labels { get; set; }
 }
 
 internal sealed class BuildContextSecret
 {
     // The ID of the secret (a secret can be used in a Dockerfile with `RUN --mount-type=secret,id=<id>,target=<targetpath>`)
-    [JsonPropertyName("id")]
-    public string? Id { get; set; }
+    [JsonPropertyName("id")] public string? Id { get; set; }
 
     // Type of the secret, can be "env" or "file".
-    [JsonPropertyName("type")]
-    public string? Type { get; set; }
+    [JsonPropertyName("type")] public string? Type { get; set; }
 
     // Value of the secret to be used in the build when the type of the secret is "env".
-    [JsonPropertyName("value")]
-    public string? Value { get; set; }
+    [JsonPropertyName("value")] public string? Value { get; set; }
 
     // Path to secret file/folder that will be mounted as a build secret using --secret
-    [JsonPropertyName("source")]
-    public string? Source { get; set; }
+    [JsonPropertyName("source")] public string? Source { get; set; }
 }
 
 internal static class VolumeMountType
@@ -126,45 +102,37 @@ internal static class VolumeMountType
 
 internal sealed class VolumeMount
 {
-    [JsonPropertyName("type")]
-    public string Type { get; set; } = VolumeMountType.Bind;
+    [JsonPropertyName("type")] public string Type { get; set; } = VolumeMountType.Bind;
 
     // Bind mounts: the host directory to mount
     // Volume mounts: name of the volume to mount
-    [JsonPropertyName("source")]
-    public string? Source { get; set; }
+    [JsonPropertyName("source")] public string? Source { get; set; }
 
     // The path within the container that the mount will use
-    [JsonPropertyName("target")]
-    public string? Target { get; set; }
+    [JsonPropertyName("target")] public string? Target { get; set; }
 
     // True if the mounted file system is supposed to be read-only
-    [JsonPropertyName("readOnly")]
-    public bool IsReadOnly { get; set; } = false;
+    [JsonPropertyName("readOnly")] public bool IsReadOnly { get; set; } = false;
 }
 
 internal sealed class ContainerNetworkConnection
 {
     // DCP Resource name of a ContainerNetwork to connect to
     // A container won't start running until it can be conneced to all specified networks
-    [JsonPropertyName("name")]
-	public string? Name { get; set; }
+    [JsonPropertyName("name")] public string? Name { get; set; }
 
-	// Aliases of the container on the network
-	// This enables container DNS resolution
-    [JsonPropertyName("aliases")]
-	public List<string>? Aliases { get; set; }
+    // Aliases of the container on the network
+    // This enables container DNS resolution
+    [JsonPropertyName("aliases")] public List<string>? Aliases { get; set; }
 }
 
 internal sealed class ContainerLabel
 {
     // The label key
-    [JsonPropertyName("key")]
-    public string? Key { get; set; }
+    [JsonPropertyName("key")] public string? Key { get; set; }
 
     // The label value
-    [JsonPropertyName("value")]
-    public string? Value { get; set; }
+    [JsonPropertyName("value")] public string? Value { get; set; }
 }
 
 internal static class ContainerRestartPolicy
@@ -232,60 +200,47 @@ internal static class PortProtocol
 internal sealed class ContainerPortSpec
 {
     // Optional: If specified, this must be a valid port number, 0 < x < 65536.
-    [JsonPropertyName("hostPort")]
-    public int? HostPort { get; set; }
+    [JsonPropertyName("hostPort")] public int? HostPort { get; set; }
 
     // Required: This must be a valid port number, 0 < x < 65536.
-    [JsonPropertyName("containerPort")]
-    public int? ContainerPort { get; set; }
+    [JsonPropertyName("containerPort")] public int? ContainerPort { get; set; }
 
     // The network protocol to be used, defaults to TCP
-    [JsonPropertyName("protocol")]
-    public string Protocol { get; set; } = PortProtocol.TCP;
+    [JsonPropertyName("protocol")] public string Protocol { get; set; } = PortProtocol.TCP;
 
     // Optional: What host IP to bind the external port to.
-    [JsonPropertyName("hostIP")]
-    public string? HostIP { get; set; }
+    [JsonPropertyName("hostIP")] public string? HostIP { get; set; }
 }
 
 internal sealed class ContainerStatus : V1Status
 {
     // Container name displayed in Docker
-    [JsonPropertyName("containerName")]
-    public string? ContainerName { get; set; }
+    [JsonPropertyName("containerName")] public string? ContainerName { get; set; }
 
     // Current state of the Container.
-    [JsonPropertyName("state")]
-    public string? State { get; set; }
+    [JsonPropertyName("state")] public string? State { get; set; }
 
     // ID of the Container (if an attempt to start the Container was made)
-    [JsonPropertyName("containerId")]
-    public string? ContainerId { get; set; }
+    [JsonPropertyName("containerId")] public string? ContainerId { get; set; }
 
     // Timestamp of the Container start attempt
-    [JsonPropertyName("startupTimestamp")]
-    public DateTimeOffset? StartupTimestamp { get; set; }
+    [JsonPropertyName("startupTimestamp")] public DateTimeOffset? StartupTimestamp { get; set; }
 
     // Timestamp when the Container was terminated last
-    [JsonPropertyName("finishTimestamp")]
-    public DateTimeOffset? FinishTimestamp { get; set; }
+    [JsonPropertyName("finishTimestamp")] public DateTimeOffset? FinishTimestamp { get; set; }
 
     // Exit code of the Container.
     // Default is -1, meaning the exit code is not known, or the container is still running.
-    [JsonPropertyName("exitCode")]
-    public int ExitCode { get; set; } = Conventions.UnknownExitCode;
+    [JsonPropertyName("exitCode")] public int ExitCode { get; set; } = Conventions.UnknownExitCode;
 
     // Effective values of environment variables, after all substitutions have been applied
-    [JsonPropertyName("effectiveEnv")]
-    public List<EnvVar>? EffectiveEnv { get; set; }
+    [JsonPropertyName("effectiveEnv")] public List<EnvVar>? EffectiveEnv { get; set; }
 
     // Effective values of launch arguments to be passed to the Container, after all substitutions are applied.
-    [JsonPropertyName("effectiveArgs")]
-    public List<string>? EffectiveArgs { get; set; }
+    [JsonPropertyName("effectiveArgs")] public List<string>? EffectiveArgs { get; set; }
 
     // Any ContainerNetworks this container is attached to
-    [JsonPropertyName("networks")]
-    public List<string>? Networks { get; set; }
+    [JsonPropertyName("networks")] public List<string>? Networks { get; set; }
 
     // Note: the ContainerStatus has "Message" property that represents a human-readable information about Container state.
     // It is provided by V1Status base class.
@@ -324,11 +279,13 @@ internal static class ContainerState
 internal sealed class Container : CustomResource<ContainerSpec, ContainerStatus>
 {
     [JsonConstructor]
-    public Container(ContainerSpec spec) : base(spec) { }
-
-    public static Container Create(string name, string image)
+    public Container(ContainerSpec spec) : base(spec)
     {
-        var c = new Container(new ContainerSpec { Image = image });
+    }
+
+    public static Container Create(string name, string image, ResourceAnnotationCollection containerAnnotations)
+    {
+        var c = new Container(new ContainerSpec { Image = image, RestartPolicy = GetRestartPolicy(containerAnnotations) });
 
         c.Kind = Dcp.ContainerKind;
         c.ApiVersion = Dcp.GroupVersion.ToString();
@@ -336,6 +293,26 @@ internal sealed class Container : CustomResource<ContainerSpec, ContainerStatus>
         c.Metadata.NamespaceProperty = string.Empty;
 
         return c;
+    }
+
+    private static string GetRestartPolicy(ResourceAnnotationCollection containerAnnotations)
+    {
+        var policy =
+            (RestartPolicyAnnotation?)containerAnnotations.FirstOrDefault(x =>
+                x.GetType().Name == "RestartPolicyAnnotation");
+        return policy is null ? ContainerRestartPolicy.Always : GetRestartPolicyValue(policy);
+    }
+
+    private static string GetRestartPolicyValue(RestartPolicyAnnotation annotation)
+    {
+        return annotation.RestartPolicy switch
+        {
+            RestartPolicy.Always => ContainerRestartPolicy.Always,
+            RestartPolicy.Never => ContainerRestartPolicy.None,
+            RestartPolicy.OnFailure => ContainerRestartPolicy.OnFailure,
+            RestartPolicy.UnlessStopped => ContainerRestartPolicy.UnlessStopped,
+            _ => ContainerRestartPolicy.None
+        };
     }
 
     public bool LogsAvailable =>
@@ -347,4 +324,3 @@ internal sealed class Container : CustomResource<ContainerSpec, ContainerStatus>
         || this.Status?.State == ContainerState.Exited
         || (this.Status?.State == ContainerState.FailedToStart && this.Status?.ContainerId is not null);
 }
-
