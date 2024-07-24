280a590def2209d6008e6aca48d0c69c5faec4c1
ZLoo <Kozlov.s.v.1992@gmail.com>
2024-07-24 23:37:09 +0300

    CA1062#Aspire.Hosting.Qdrant

---
+++ tests/Aspire.Hosting.Qdrant.Tests/QdrantPublicApiTests.cs
@@ -0,0 +1,167 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Aspire.Hosting.ApplicationModel;
+using Xunit;
+
+namespace Aspire.Hosting.Qdrant.Tests;
+
+public class QdrantPublicApiTests
+{
+    #region QdrantBuilderExtensions
+
+    [Fact]
+    public void AddQdrantContainerShouldThrowsWhenBuilderIsNull()
+    {
+        IDistributedApplicationBuilder builder = null!;
+        const string name = "Qdrant";
+
+        var action = () => builder.AddQdrant(name);
+
+        Assert.Multiple(() =>
+        {
+            var exception = Assert.Throws<ArgumentNullException>(action);
+            Assert.Equal(nameof(builder), exception.ParamName);
+        });
+    }
+
+    [Fact]
+    public void AddQdrantContainerShouldThrowsWhenNameIsNull()
+    {
+        var builder = DistributedApplication.CreateBuilder([]);
+        string name = null!;
+
+        var action = () => builder.AddQdrant(name);
+
+        Assert.Multiple(() =>
+        {
+            var exception = Assert.Throws<ArgumentNullException>(action);
+            Assert.Equal(nameof(name), exception.ParamName);
+        });
+    }
+
+    [Fact]
+    public void WithDataVolumeShouldThrowsWhenBuilderIsNull()
+    {
+        IResourceBuilder<QdrantServerResource> builder = null!;
+
+        var action = () => builder.WithDataVolume();
+
+        Assert.Multiple(() =>
+        {
+            var exception = Assert.Throws<ArgumentNullException>(action);
+            Assert.Equal(nameof(builder), exception.ParamName);
+        });
+    }
+
+    [Fact]
+    public void WithDataBindMountShouldThrowsWhenBuilderIsNull()
+    {
+        IResourceBuilder<QdrantServerResource> builder = null!;
+        const string source = "/qdrant/storage";
+
+        var action = () => builder.WithDataBindMount(source);
+
+        Assert.Multiple(() =>
+        {
+            var exception = Assert.Throws<ArgumentNullException>(action);
+            Assert.Equal(nameof(builder), exception.ParamName);
+        });
+    }
+
+    [Fact]
+    public void WithDataBindMountShouldThrowsWhenNameIsNull()
+    {
+        var distributedApplicationBuilder = DistributedApplication.CreateBuilder([]);
+        const string name = "Qdrant";
+        var apiKeyParameter = ParameterResourceBuilderExtensions.CreateDefaultPasswordParameter(distributedApplicationBuilder, $"{name}-Key", special: false);
+        var resource = new QdrantServerResource(name, apiKeyParameter);
+        var builder = distributedApplicationBuilder.AddResource(resource);
+        string source = null!;
+
+        var action = () => builder.WithDataBindMount(source);
+
+        Assert.Multiple(() =>
+        {
+            var exception = Assert.Throws<ArgumentNullException>(action);
+            Assert.Equal(nameof(source), exception.ParamName);
+        });
+    }
+
+    [Fact]
+    public void WithReferenceShouldThrowsWhenBuilderIsNull()
+    {
+        IResourceBuilder<IResourceWithEnvironment> builder = null!;
+        var distributedApplicationBuilder = DistributedApplication.CreateBuilder([]);
+        const string name = "Qdrant";
+        var apiKeyParameter = ParameterResourceBuilderExtensions.CreateDefaultPasswordParameter(distributedApplicationBuilder, $"{name}-Key", special: false);
+        var resource = new QdrantServerResource(name, apiKeyParameter);
+        var qdrantResource = distributedApplicationBuilder.AddResource(resource);
+        
+        var action = () => builder.WithReference(qdrantResource);
+
+        Assert.Multiple(() =>
+        {
+            var exception = Assert.Throws<ArgumentNullException>(action);
+            Assert.Equal(nameof(builder), exception.ParamName);
+        });
+    }
+
+    [Fact]
+    public void WithReferenceShouldThrowsWhenQdrantResourceIsNull()
+    {
+        var distributedApplicationBuilder = DistributedApplication.CreateBuilder([]);
+        const string name = "Qdrant";
+        var apiKeyParameter = ParameterResourceBuilderExtensions.CreateDefaultPasswordParameter(distributedApplicationBuilder, $"{name}-Key", special: false);
+        var resource = new QdrantServerResource(name, apiKeyParameter);
+        var builder = distributedApplicationBuilder.AddResource(resource);
+        IResourceBuilder<QdrantServerResource> qdrantResource = null!;
+
+        var action = () => builder.WithReference(qdrantResource);
+
+        Assert.Multiple(() =>
+        {
+            var exception = Assert.Throws<ArgumentNullException>(action);
+            Assert.Equal(nameof(qdrantResource), exception.ParamName);
+        });
+    }
+
+    #endregion
+
+    #region QdrantServerResource
+
+    [Fact]
+    public void CtorQdrantServerResourceShouldThrowsWhenNameIsNull()
+    {
+        var distributedApplicationBuilder = DistributedApplication.CreateBuilder([]);
+        string name = null!;
+        const string key = nameof(key);
+        var apiKey = ParameterResourceBuilderExtensions.CreateDefaultPasswordParameter(distributedApplicationBuilder, key, special: false);
+
+        var action = () => new QdrantServerResource(name, apiKey);
+
+        Assert.Multiple(() =>
+        {
+            var exception = Assert.Throws<ArgumentNullException>(action);
+            Assert.Equal(nameof(name), exception.ParamName);
+        });
+    }
+
+    [Fact]
+    public void CtorQdrantServerResourceShouldThrowsWhenApiKeyIsNull()
+    {
+        var distributedApplicationBuilder = DistributedApplication.CreateBuilder([]);
+        const string name = "Qdrant";
+        ParameterResource apiKey = null!;
+
+        var action = () => new QdrantServerResource(name, apiKey);
+
+        Assert.Multiple(() =>
+        {
+            var exception = Assert.Throws<ArgumentNullException>(action);
+            Assert.Equal(nameof(apiKey), exception.ParamName);
+        });
+    }
+
+    #endregion
+}
