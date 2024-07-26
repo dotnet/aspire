470f332947a7ed4cdc278872d0048c023480ce1e
Eric Erhardt <eric.erhardt@microsoft.com>
2024-07-26 08:24:24 -0500

    Address PR feedback

--- tests/Aspire.Hosting.Azure.Tests/AzureStorageEmulatorFunctionalTests.cs
+++ tests/Aspire.Hosting.Azure.Tests/AzureStorageEmulatorFunctionalTests.cs
@@ -4,15 +4,13 @@
 using Aspire.Components.Common.Tests;
 using Aspire.Hosting.Utils;
 using Azure.Storage.Blobs;
-using Azure.Storage.Blobs.Models;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Xunit;
 using Xunit.Abstractions;
 
-namespace Aspire.Hosting.Redis.Tests;
+namespace Aspire.Hosting.Azure.Tests;
 
 public class AzureStorageEmulatorFunctionalTests(ITestOutputHelper testOutputHelper)
 {
@@ -27,20 +25,20 @@ public class AzureStorageEmulatorFunctionalTests(ITestOutputHelper testOutputHel
         await app.StartAsync();
 
         var hb = Host.CreateApplicationBuilder();
-        hb.Configuration.AddInMemoryCollection(new Dictionary<string, string?>
-        {
-            [$"ConnectionStrings:BlobConnection"] = await storage.Resource.ConnectionStringExpression.GetValueAsync(CancellationToken.None)
-        });
+        hb.Configuration["ConnectionStrings:BlobConnection"] = await storage.Resource.ConnectionStringExpression.GetValueAsync(CancellationToken.None);
         hb.AddAzureBlobClient("BlobConnection");
 
         using var host = hb.Build();
         await host.StartAsync();
 
         var serviceClient = host.Services.GetRequiredService<BlobServiceClient>();
-        var accountInfo = (await serviceClient.GetAccountInfoAsync()).Value;
-        Assert.NotNull(accountInfo);
-        Assert.Equal(AccountKind.StorageV2, accountInfo.AccountKind);
-        Assert.Equal(SkuName.StandardRagrs, accountInfo.SkuName);
+        var blobContainer = (await serviceClient.CreateBlobContainerAsync("container")).Value;
+        var blobClient = blobContainer.GetBlobClient("testKey");
+
+        await blobClient.UploadAsync(BinaryData.FromString("testValue"));
+
+        var downloadResult = (await blobClient.DownloadContentAsync()).Value;
+        Assert.Equal("testValue", downloadResult.Content.ToString());
     }
 
     private TestDistributedApplicationBuilder CreateDistributedApplicationBuilder()
