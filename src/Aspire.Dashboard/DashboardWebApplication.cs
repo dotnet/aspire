d0414ac240e0c92f0df74750f926739fe30069c1
Adam Ratzman <adamratzman@microsoft.com>
2024-07-30 14:29:27 -0400

    remove chart view model

--- src/Aspire.Dashboard/DashboardWebApplication.cs
+++ src/Aspire.Dashboard/DashboardWebApplication.cs
@@ -177,8 +177,6 @@ public sealed class DashboardWebApplication : IAsyncDisposable
         builder.Services.AddScoped<ILocalStorage, LocalBrowserStorage>();
         builder.Services.AddScoped<ISessionStorage, SessionBrowserStorage>();
 
-        builder.Services.AddScoped<CurrentChartViewModel>();
-
         builder.Services.AddScoped<DimensionManager>();
 
         builder.Services.AddLocalization();
