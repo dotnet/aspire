4adf0e7643cb9bf3975e7483e6ffecb93604b0ee
Adam Ratzman <adam@adamratzman.com>
2024-07-31 11:48:50 -0400

    Add visible labels to resource select component and filter dialog (#3727)
    * Consolidate (All) strings, add select a resource label to resource select, change console logs no selection to say (None)
    
    * Add labels to filter dialog inputs, use the same All and None labels everywhere, simplify
    
    * Select a resource -> Resource and remove Value placeholder in filter dialog
    
    * Change SelectAResource -> ResourceLabel
    
    * update resx designer
    
    ---------
    
    Co-authored-by: Adam Ratzman <adamratzman@microsoft.com>

--- src/Aspire.Dashboard/Components/Pages/ConsoleLogs.razor.cs
+++ src/Aspire.Dashboard/Components/Pages/ConsoleLogs.razor.cs
@@ -64,7 +64,7 @@ public sealed partial class ConsoleLogs : ComponentBase, IAsyncDisposable, IPage
 
     protected override async Task OnInitializedAsync()
     {
-        _noSelection = new() { Id = null, Name = ControlsStringsLoc[nameof(ControlsStrings.SelectAResource)] };
+        _noSelection = new() { Id = null, Name = ControlsStringsLoc[nameof(ControlsStrings.None)] };
         PageViewModel = new ConsoleLogsViewModel { SelectedOption = _noSelection, SelectedResource = null, Status = Loc[nameof(Dashboard.Resources.ConsoleLogs.ConsoleLogsLoadingResources)] };
 
         var loadingTcs = new TaskCompletionSource();
