14aa1eff43fa7cd7b6e794dcb17e57ec896cb1aa
James Newton-King <james@newtonking.com>
2024-08-01 16:04:29 +0800

    Test

--- src/Aspire.Dashboard/Otlp/Model/DurationFormatter.cs
+++ src/Aspire.Dashboard/Otlp/Model/DurationFormatter.cs
@@ -1,10 +1,13 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Diagnostics;
+
 namespace Aspire.Dashboard.Otlp.Model;
 
 public static class DurationFormatter
 {
+    [DebuggerDisplay("Unit = {Unit}, Ticks = {Ticks}, IsDecimal = {IsDecimal}")]
     private sealed class UnitStep
     {
         public required string Unit { get; init; }
