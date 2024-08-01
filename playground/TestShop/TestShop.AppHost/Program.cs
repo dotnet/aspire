b510cb66b500eca22026dfc7745de8f82281a7d4
Ankit Jain <radical@gmail.com>
2024-08-01 01:46:33 -0400

    [playground] Conditionally AddProject for dashboard reference

--- playground/TestShop/TestShop.AppHost/Program.cs
+++ playground/TestShop/TestShop.AppHost/Program.cs
@@ -40,11 +40,13 @@ builder.AddProject<Projects.ApiGateway>("apigateway")
 builder.AddProject<Projects.CatalogDb>("catalogdbapp")
        .WithReference(catalogDb);
 
+#if !SKIP_DASHBOARD_REFERENCE
 // This project is only added in playground projects to support development/debugging
 // of the dashboard. It is not required in end developer code. Comment out this code
 // to test end developer dashboard launch experience. Refer to Directory.Build.props
 // for the path to the dashboard binary (defaults to the Aspire.Dashboard bin output
 // in the artifacts dir).
 builder.AddProject<Projects.Aspire_Dashboard>(KnownResourceNames.AspireDashboard);
+#endif
 
 builder.Build().Run();
