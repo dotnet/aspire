0868ebf53f87d255d7c6f77c2541d493cf741ecb
Ankit Jain <radical@gmail.com>
2024-08-06 23:07:49 -0400

    wip

--- src/Aspire.Hosting/ApplicationModel/ProjectResource.cs
+++ src/Aspire.Hosting/ApplicationModel/ProjectResource.cs
@@ -48,17 +48,17 @@ public class ProjectResource(string name) : Resource(name), IResourceWithEnviron
     {
         if (string.IsNullOrEmpty(s_aspireProjectRootEnvVar) || !Directory.Exists(s_aspireProjectRootEnvVar) || string.IsNullOrEmpty(originalProjectPath) || File.Exists(originalProjectPath))
         {
-            Console.WriteLine($"[{label}] root: {s_aspireProjectRootEnvVar}, originalProjectPath: {originalProjectPath}");
+            //Console.WriteLine($"[{label}] root: {s_aspireProjectRootEnvVar}, originalProjectPath: {originalProjectPath}");
             return originalProjectPath;
         }
 
-        // Console.WriteLine($"s_aspireProjectRootEnvVar: {root}");
-        Console.WriteLine($">> [{label}] originalProjectPath: {originalProjectPath}");
+        // //Console.WriteLine($"s_aspireProjectRootEnvVar: {root}");
+        //Console.WriteLine($">> [{label}] originalProjectPath: {originalProjectPath}");
 
         string filename = Path.GetFileName(originalProjectPath);
 
         string relativeTargetPath = Path.GetDirectoryName(originalProjectPath)!;
-        Console.WriteLine($"%% [{label}] starting: relativeTargetPath: {relativeTargetPath}");
+        //Console.WriteLine($"%% [{label}] starting: relativeTargetPath: {relativeTargetPath}");
 
         string relativeParentPath = "";
         while (true)
@@ -66,7 +66,7 @@ public class ProjectResource(string name) : Resource(name), IResourceWithEnviron
             string parentName = Path.GetFileName(relativeTargetPath);
             if (string.IsNullOrEmpty(parentName))
             {
-                Console.WriteLine($"%% [{label}] No parent found for {relativeTargetPath} for {originalProjectPath}");
+                //Console.WriteLine($"%% [{label}] No parent found for {relativeTargetPath} for {originalProjectPath}");
                 break;
             }
             // prepend the parent name to the relativeParentPath
@@ -74,18 +74,18 @@ public class ProjectResource(string name) : Resource(name), IResourceWithEnviron
 
             // FIXME: check if this should this be done at the end of the block?
             relativeTargetPath = Path.GetDirectoryName(relativeTargetPath)!;
-            Console.WriteLine($"\t%% [{label}] relativePathToTry: {relativeParentPath}");
+            //Console.WriteLine($"\t%% [{label}] relativePathToTry: {relativeParentPath}");
             if (relativeParentPath == null)
             {
                 break;
             }
 
             string projectPathToTry = Path.Combine(s_aspireProjectRootEnvVar, relativeParentPath, filename);
-            Console.WriteLine($"\t%% [{label}] projectPathToTry: {projectPathToTry}");
+            //Console.WriteLine($"\t%% [{label}] projectPathToTry: {projectPathToTry}");
 
             if (File.Exists(projectPathToTry))
             {
-                Console.WriteLine($"\t\t%% [{label}] Using root: {s_aspireProjectRootEnvVar} => returning {projectPathToTry}");
+                //Console.WriteLine($"\t\t%% [{label}] Using root: {s_aspireProjectRootEnvVar} => returning {projectPathToTry}");
                 return projectPathToTry;
             }
         }
