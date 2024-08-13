3fc0b740425d370fdfc784ed36581fb3c4965671
Sebastien Ros <sebastienros@gmail.com>
2024-08-13 15:06:12 -0700

    Uwe UnsafeAccessor instead of private reflection

--- tests/Aspire.Hosting.Dapr.Tests/DaprTests.cs
+++ tests/Aspire.Hosting.Dapr.Tests/DaprTests.cs
@@ -1,6 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Runtime.CompilerServices;
 using Aspire.Hosting.ApplicationModel;
 using Aspire.Hosting.Tests.Utils;
 using Aspire.Hosting.Utils;
@@ -31,7 +32,8 @@ public class DaprTests
             .WithDaprSidecar();
 
         using var app = builder.Build();
-        await app.ExecuteBeforeStartHooksAsync(default);
+
+        await ExecuteBeforeStartHooksAsync(app, default);
 
         var model = app.Services.GetRequiredService<DistributedApplicationModel>();
 
@@ -130,7 +132,7 @@ public class DaprTests
             });
         }
         using var app = builder.Build();
-        await app.ExecuteBeforeStartHooksAsync(default);
+        await ExecuteBeforeStartHooksAsync(app, default);
 
         var model = app.Services.GetRequiredService<DistributedApplicationModel>();
 
@@ -166,4 +168,7 @@ public class DaprTests
         Assert.Contains($"--app-protocol {expectedSchema}", commandline);
         Assert.NotNull(container.Annotations.OfType<DaprSidecarAnnotation>());
     }
+
+    [UnsafeAccessor(UnsafeAccessorKind.Method, Name = "ExecuteBeforeStartHooksAsync")]
+    private static extern Task ExecuteBeforeStartHooksAsync(DistributedApplication app, CancellationToken cancellationToken);
 }
