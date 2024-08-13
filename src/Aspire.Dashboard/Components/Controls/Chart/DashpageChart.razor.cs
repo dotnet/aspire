ce1d94cfa7acbb5b6393b32daa8264db5b9a4341
Drew Noakes <git@drewnoakes.com>
2024-08-13 22:41:41 +1000

    Add TODO and placeholder for DashpageChart component

---
+++ src/Aspire.Dashboard/Components/Controls/Chart/DashpageChart.razor.cs
@@ -0,0 +1,10 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Microsoft.AspNetCore.Components;
+
+namespace Aspire.Dashboard.Components.Controls;
+
+public class DashpageChart : ComponentBase
+{
+}
