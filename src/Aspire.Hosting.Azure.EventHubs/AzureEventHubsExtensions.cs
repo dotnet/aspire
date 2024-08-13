6dd99f5cc427176da0f6e4fa011cd7c1ea5910af
Mitch Denny <midenn@microsoft.com>
2024-08-14 08:22:09 +1000

    Move eventhubs lifecycle hook into eventing.

--- src/Aspire.Hosting.Azure.EventHubs/AzureEventHubsExtensions.cs
+++ src/Aspire.Hosting.Azure.EventHubs/AzureEventHubsExtensions.cs
@@ -2,10 +2,10 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.Diagnostics.CodeAnalysis;
+using System.Text.Json;
 using Aspire.Hosting.ApplicationModel;
 using Aspire.Hosting.Azure;
 using Aspire.Hosting.Azure.EventHubs;
-using Aspire.Hosting.Lifecycle;
 using Aspire.Hosting.Utils;
 using Azure.Provisioning;
 using Azure.Provisioning.EventHubs;
@@ -132,8 +132,6 @@ public static class AzureEventHubsExtensions
             return builder;
         }
 
-        builder.ApplicationBuilder.Services.TryAddLifecycleHook<AzureEventHubsEmulatorConfigWriterHook>();
-
         // Add emulator container
 
         builder
@@ -173,6 +171,63 @@ public static class AzureEventHubsExtensions
             var surrogateBuilder = builder.ApplicationBuilder.CreateResourceBuilder(surrogate);
             configureContainer(surrogateBuilder);
         }
+
+#pragma warning disable ASPIREEVENTING001 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+        builder.ApplicationBuilder.Eventing.Subscribe<AfterEndpointsAllocatedEvent>((e, ct) =>
+        {
+            var eventHubsEmulatorResources = builder.ApplicationBuilder.Resources.OfType<AzureEventHubsResource>().Where(x => x is { } eventHubsResource && eventHubsResource.IsEmulator);
+
+            if (!eventHubsEmulatorResources.Any())
+            {
+                // No-op if there is no Azure Event Hubs emulator resource.
+                return Task.CompletedTask;
+            }
+
+            foreach (var emulatorResource in eventHubsEmulatorResources)
+            {
+                var configFileMount = emulatorResource.Annotations.OfType<ContainerMountAnnotation>().Single(v => v.Target == AzureEventHubsEmulatorResource.EmulatorConfigJsonPath);
+
+                using var stream = new FileStream(configFileMount.Source!, FileMode.Create);
+                using var writer = new Utf8JsonWriter(stream);
+
+                writer.WriteStartObject();                      // {
+                writer.WriteStartObject("UserConfig");          //   "UserConfig": {
+                writer.WriteStartArray("NamespaceConfig");      //     "NamespaceConfig": [
+                writer.WriteStartObject();                      //       {
+                writer.WriteString("Type", "EventHub");         //         "Type": "EventHub",
+
+                // This name is currently required by the emulator
+                writer.WriteString("Name", "emulatorNs1");      //         "Name": "emulatorNs1"
+                writer.WriteStartArray("Entities");             //         "Entities": [
+
+                foreach (var hub in emulatorResource.Hubs)
+                {
+                    // The default consumer group ('$default') is automatically created
+
+                    writer.WriteStartObject();                  //           {
+                    writer.WriteString("Name", hub.Name);       //             "Name": "hub",
+                    writer.WriteString("PartitionCount", "2");  //             "PartitionCount": "2",
+                    writer.WriteStartArray("ConsumerGroups");   //             "ConsumerGroups": [
+                    writer.WriteEndArray();                     //             ]
+                    writer.WriteEndObject();                    //           }
+                }
+
+                writer.WriteEndArray();                         //         ] (/Entities)
+                writer.WriteEndObject();                        //       }
+                writer.WriteEndArray();                         //     ], (/NamespaceConfig)
+                writer.WriteStartObject("LoggingConfig");       //     "LoggingConfig": {
+                writer.WriteString("Type", "File");             //       "Type": "File"
+                writer.WriteEndObject();                        //     } (/LoggingConfig)
+
+                writer.WriteEndObject();                        //   } (/UserConfig)
+                writer.WriteEndObject();                        // } (/Root)
+
+            }
+
+            return Task.CompletedTask;
+
+        });
+#pragma warning restore ASPIREEVENTING001 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
 
         return builder;
     }
