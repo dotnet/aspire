78be4043c5e9795e6e5ba6d8e3efc65d7a2a832b
ZLoo <Kozlov.s.v.1992@gmail.com>
2024-08-15 02:08:30 +0300

    Aspire.Hosting.AWS

--- src/Aspire.Hosting.AWS/AWSProvisioningException.cs
+++ src/Aspire.Hosting.AWS/AWSProvisioningException.cs
@@ -1,6 +1,9 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+
 namespace Aspire.Hosting.AWS;
 
 /// <summary>
@@ -8,6 +11,14 @@ namespace Aspire.Hosting.AWS;
 /// </summary>
 /// <param name="message"></param>
 /// <param name="innerException"></param>
-public class AWSProvisioningException(string message, Exception? innerException = null) : Exception(message, innerException)
+public class AWSProvisioningException(string message, Exception? innerException = null)
+    : Exception(ThrowIfNullOrEmpty(message), innerException)
 {
+    private static string ThrowIfNullOrEmpty(
+        [NotNull] string? argument,
+        [CallerArgumentExpression(nameof(argument))] string? paramName = null)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(argument, paramName);
+        return argument;
+    }
 }
---
+++ tests/Aspire.Hosting.AWS.Tests/AWSPublicApiTests.cs
@@ -0,0 +1,121 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Amazon;
+using Aspire.Hosting.ApplicationModel;
+using Aspire.Hosting.Utils;
+using Xunit;
+
+namespace Aspire.Hosting.AWS.Tests;
+
+#pragma warning disable IDE0200 //Remove unnecessary lambda expression (IDE0200)
+public class AWSPublicApiTests
+{
+    [Theory]
+    [InlineData(true)]
+    [InlineData(false)]
+    public void CtorAWSProvisioningExceptionShouldThrowWhenMessageIsNullOrEmpty(bool isNull)
+    {
+        var message = isNull ? null! : string.Empty;
+
+        var action = () => new AWSProvisioningException(message, default(Exception?));
+
+        var exception = isNull
+            ? Assert.Throws<ArgumentNullException>(action)
+            : Assert.Throws<ArgumentException>(action);
+        Assert.Equal(nameof(message), exception.ParamName);
+    }
+
+    [Fact]
+    public void AddAWSSDKConfigShouldThrowWhenBuilderIsNull()
+    {
+        IDistributedApplicationBuilder builder = null!;
+
+        var action = () => builder.AddAWSSDKConfig();
+
+        var exception = Assert.Throws<ArgumentNullException>(action);
+        Assert.Equal(nameof(builder), exception.ParamName);
+    }
+
+    [Fact]
+    public void WithProfileShouldThrowWhenConfigIsNull()
+    {
+        IAWSSDKConfig config = null!;
+        var profile = "default";
+
+        var action = () => config.WithProfile(profile);
+
+        var exception = Assert.Throws<ArgumentNullException>(action);
+        Assert.Equal(nameof(config), exception.ParamName);
+    }
+
+    [Theory]
+    [InlineData(true)]
+    [InlineData(false)]
+    public void WithProfileShouldThrowWhenProfileIsNullOrEmpty(bool isNull)
+    {
+        var builder = TestDistributedApplicationBuilder.Create();
+        var config = builder.AddAWSSDKConfig();
+        var profile = isNull ? null! : string.Empty;
+
+        var action = () => config.WithProfile(profile);
+
+        var exception = isNull
+            ? Assert.Throws<ArgumentNullException>(action)
+            : Assert.Throws<ArgumentException>(action);
+        Assert.Equal(nameof(profile), exception.ParamName);
+    }
+
+    [Fact]
+    public void WithRegionShouldThrowWhenConfigIsNull()
+    {
+        IAWSSDKConfig config = null!;
+        var region = RegionEndpoint.EUCentral1;
+
+        var action = () => config.WithRegion(region);
+
+        var exception = Assert.Throws<ArgumentNullException>(action);
+        Assert.Equal(nameof(config), exception.ParamName);
+    }
+
+    [Fact]
+    public void WithRegionShouldThrowWhenRegionIsNull()
+    {
+        var builder = TestDistributedApplicationBuilder.Create();
+        var config = builder.AddAWSSDKConfig();
+        RegionEndpoint region = null!;
+
+        var action = () => config.WithRegion(region);
+
+        var exception = Assert.Throws<ArgumentNullException>(action);
+        Assert.Equal(nameof(region), exception.ParamName);
+    }
+
+    [Fact]
+    public void WithReferenceShouldThrowWhenBuilderIsNull()
+    {
+        IResourceBuilder<ProjectResource> builder = null!;
+        var testBuilder = TestDistributedApplicationBuilder.Create();
+        var awsSdkConfig = testBuilder.AddAWSSDKConfig();
+
+        var action = () => builder.WithReference(awsSdkConfig);
+
+        var exception = Assert.Throws<ArgumentNullException>(action);
+        Assert.Equal(nameof(builder), exception.ParamName);
+    }
+
+    [Fact]
+    public void WithReferenceShouldThrowWhenAwsSdkConfigIsNull()
+    {
+        var testBuilder = TestDistributedApplicationBuilder.Create();
+        var resource = new KafkaServerResource("kafka");
+        var builder = testBuilder.AddResource(resource);
+
+        IAWSSDKConfig awsSdkConfig = null!;
+
+        var action = () => builder.WithReference(awsSdkConfig);
+
+        var exception = Assert.Throws<ArgumentNullException>(action);
+        Assert.Equal(nameof(awsSdkConfig), exception.ParamName);
+    }
+}
