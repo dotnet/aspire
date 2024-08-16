b74f2c6f2ad2335d825795b39301d15b3ed07dab
Ankit Jain <radical@gmail.com>
2024-08-15 23:09:49 -0400

    [tests] DockerUtils.AttemptDeleteDockerVolume - allow throwing on failures (#5315)
    .. failures. This is useful when expecting to create a fresh new volume,
    and this would cause an exception to be thrown if a volume with the same
    name could not be deleted.

--- tests/Aspire.Hosting.RabbitMQ.Tests/RabbitMQFunctionalTests.cs
+++ tests/Aspire.Hosting.RabbitMQ.Tests/RabbitMQFunctionalTests.cs
@@ -71,8 +71,8 @@ public class RabbitMQFunctionalTests(ITestOutputHelper testOutputHelper)
                 // Use a deterministic volume name to prevent them from exhausting the machines if deletion fails
                 volumeName = VolumeNameGenerator.CreateVolumeName(rabbitMQ1, nameof(WithDataShouldPersistStateBetweenUsages));
 
-                // if the volume already exists (because of a crashing previous run), try to delete it
-                DockerUtils.AttemptDeleteDockerVolume(volumeName);
+                // if the volume already exists (because of a crashing previous run), delete it
+                DockerUtils.AttemptDeleteDockerVolume(volumeName, throwOnFailure: true);
                 rabbitMQ1.WithDataVolume(volumeName);
             }
             else
