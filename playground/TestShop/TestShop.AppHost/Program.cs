90f8a6547a6b2bd5193bb2dd308d4882a6fc3857
James Newton-King <james@newtonking.com>
2024-08-16 17:07:21 +0800

    Update

--- playground/TestShop/TestShop.AppHost/Program.cs
+++ playground/TestShop/TestShop.AppHost/Program.cs
@@ -31,11 +31,7 @@ var basketService = builder.AddProject("basketservice", @"..\BasketService\Baske
 builder.AddProject<Projects.MyFrontend>("frontend")
        .WithExternalHttpEndpoints()
        .WithReference(basketService)
-       .WithReference(catalogService)
-       .WithEnvironment(c =>
-       {
-           c.EnvironmentVariables[""] = "hi";
-       });
+       .WithReference(catalogService);
 
 builder.AddProject<Projects.OrderProcessor>("orderprocessor", launchProfileName: "OrderProcessor")
        .WithReference(messaging);
