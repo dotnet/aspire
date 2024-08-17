2beb33cd113ff1abd78b07816216b1c080f6a0cd
ZLoo <Kozlov.s.v.1992@gmail.com>
2024-08-17 16:04:00 +0300

    Aspire.Hosting.Dapr#2

--- tests/Aspire.Hosting.Dapr.Tests/DarpPublicApiTests.cs
+++ tests/Aspire.Hosting.Dapr.Tests/DarpPublicApiTests.cs
@@ -1,6 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using Aspire.Hosting.ApplicationModel;
 using Aspire.Hosting.Utils;
 using Xunit;
 
@@ -41,7 +42,7 @@ public class DarpPublicApiTests
     }
 
     [Fact]
-    public void AddDaprShouldThrowWhenbuilderIsNull()
+    public void AddDaprShouldThrowWhenBuilderIsNull()
     {
         IDistributedApplicationBuilder builder = null!;
 
@@ -52,7 +53,7 @@ public class DarpPublicApiTests
     }
 
     [Fact]
-    public void AddDaprComponentShouldThrowWhenbuilderIsNull()
+    public void AddDaprComponentShouldThrowWhenBuilderIsNull()
     {
         IDistributedApplicationBuilder builder = null!;
         const string name = "darp";
@@ -99,7 +100,7 @@ public class DarpPublicApiTests
     }
 
     [Fact]
-    public void AddDaprPubSubShouldThrowWhenbuilderIsNull()
+    public void AddDaprPubSubShouldThrowWhenBuilderIsNull()
     {
         IDistributedApplicationBuilder builder = null!;
         const string name = "darp";
@@ -127,7 +128,7 @@ public class DarpPublicApiTests
     }
 
     [Fact]
-    public void AddDaprStateStoreShouldThrowWhenbuilderIsNull()
+    public void AddDaprStateStoreShouldThrowWhenBuilderIsNull()
     {
         IDistributedApplicationBuilder builder = null!;
         const string name = "darp";
@@ -152,5 +153,124 @@ public class DarpPublicApiTests
             ? Assert.Throws<ArgumentNullException>(action)
             : Assert.Throws<ArgumentException>(action);
         Assert.Equal(nameof(name), exception.ParamName);
+    }
+
+    [Fact]
+    public void WithDaprSidecarShouldThrowWhenBuilderIsNull()
+    {
+        IResourceBuilder<DaprComponentResource> builder = null!;
+
+        var action = () => builder.WithDaprSidecar();
+
+        var exception = Assert.Throws<ArgumentNullException>(action);
+        Assert.Equal(nameof(builder), exception.ParamName);
+    }
+
+    [Fact]
+    public void WithDaprSidecarWithAppIdShouldThrowWhenBuilderIsNull()
+    {
+        IResourceBuilder<DaprComponentResource> builder = null!;
+        const string appId = "darp";
+
+        var action = () => builder.WithDaprSidecar(appId);
+
+        var exception = Assert.Throws<ArgumentNullException>(action);
+        Assert.Equal(nameof(builder), exception.ParamName);
+    }
+
+    [Theory]
+    [InlineData(true)]
+    [InlineData(false)]
+    public void WithDaprSidecarWithAppIdShouldThrowWhenAppIdIsNullOrEmpty(bool isNull)
+    {
+        IDistributedApplicationBuilder builder = TestDistributedApplicationBuilder.Create();
+        var darp = builder.AddDaprPubSub("darp");
+        var appId = isNull ? null! : string.Empty;
+
+        var action = () => darp.WithDaprSidecar(appId);
+
+        var exception = isNull
+            ? Assert.Throws<ArgumentNullException>(action)
+            : Assert.Throws<ArgumentException>(action);
+        Assert.Equal(nameof(appId), exception.ParamName);
+    }
+
+    [Fact]
+    public void WithDaprSidecarWithConfigureSidecarShouldThrowWhenBuilderIsNull()
+    {
+        IResourceBuilder<DaprComponentResource> builder = null!;
+        Action<IResourceBuilder<IDaprSidecarResource>> configureSidecar = (_) => { };
+
+        var action = () => builder.WithDaprSidecar(configureSidecar);
+
+        var exception = Assert.Throws<ArgumentNullException>(action);
+        Assert.Equal(nameof(builder), exception.ParamName);
+    }
+
+    [Fact]
+    public void WithDaprSidecarWithConfigureSidecarShouldThrowWhenConfigureSidecarIsNull()
+    {
+        IDistributedApplicationBuilder builder = TestDistributedApplicationBuilder.Create();
+        var darp = builder.AddDaprPubSub("darp");
+        Action<IResourceBuilder<IDaprSidecarResource>> configureSidecar = null!;
+
+        var action = () => darp.WithDaprSidecar(configureSidecar);
+
+        var exception = Assert.Throws<ArgumentNullException>(action);
+        Assert.Equal(nameof(configureSidecar), exception.ParamName);
+    }
+
+    [Fact]
+    public void WithOptionsShouldThrowWhenBuilderIsNull()
+    {
+        IResourceBuilder<IDaprSidecarResource> builder = null!;
+        DaprSidecarOptions options = null!;
+
+        var action = () => builder.WithOptions(options);
+
+        var exception = Assert.Throws<ArgumentNullException>(action);
+        Assert.Equal(nameof(builder), exception.ParamName);
+    }
+
+    private sealed class DaprSidecar(string name) : Resource(name), IDaprSidecarResource;
+
+    [Fact]
+    public void WithOptionsShouldThrowWhenOptionsIsNull()
+    {
+        IDistributedApplicationBuilder builder = TestDistributedApplicationBuilder.Create();
+        var darpSidecar = builder.AddResource<IDaprSidecarResource>(new DaprSidecar("darpSidecar"));
+        DaprSidecarOptions options = null!;
+
+        var action = () => darpSidecar.WithOptions(options);
+
+        var exception = Assert.Throws<ArgumentNullException>(action);
+        Assert.Equal(nameof(options), exception.ParamName);
+    }
+
+    [Fact]
+    public void WithReferenceShouldThrowWhenBuilderIsNull()
+    {
+        IResourceBuilder<IDaprSidecarResource> builder = null!;
+
+        IDistributedApplicationBuilder builder2 = TestDistributedApplicationBuilder.Create();
+        var component = builder2.AddResource<IDaprComponentResource>(new DaprComponentResource("darp", "state"));
+
+        var action = () => builder.WithReference(component);
+
+        var exception = Assert.Throws<ArgumentNullException>(action);
+        Assert.Equal(nameof(builder), exception.ParamName);
+    }
+
+    [Fact]
+    public void WithReferenceShouldThrowWhenComponentIsNull()
+    {
+        IDistributedApplicationBuilder builder = TestDistributedApplicationBuilder.Create();
+        var darpSidecar = builder.AddResource<IDaprSidecarResource>(new DaprSidecar("darpSidecar"));
+        IResourceBuilder<IDaprComponentResource> component = null!;
+
+        var action = () => darpSidecar.WithReference(component);
+
+        var exception = Assert.Throws<ArgumentNullException>(action);
+        Assert.Equal(nameof(component), exception.ParamName);
     }
 }
