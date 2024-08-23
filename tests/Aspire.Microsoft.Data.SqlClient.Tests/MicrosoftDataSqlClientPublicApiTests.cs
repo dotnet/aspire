8a07913f43428c20de385f77231e498242133b6a
ZLoo <Kozlov.s.v.1992@gmail.com>
2024-08-23 22:52:30 +0300

    Aspire.Microsoft.Data.SqlClient

---
+++ tests/Aspire.Microsoft.Data.SqlClient.Tests/MicrosoftDataSqlClientPublicApiTests.cs
@@ -0,0 +1,74 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Microsoft.Extensions.Hosting;
+using Xunit;
+
+namespace Aspire.Microsoft.Data.SqlClient.Tests;
+
+public class MicrosoftDataSqlClientPublicApiTests
+{
+    [Fact]
+    public void AddSqlServerClientShouldThrowWhenBuilderIsNull()
+    {
+        IHostApplicationBuilder builder = null!;
+        const string connectionName = "blobs";
+
+        var action = () => builder.AddSqlServerClient(
+            connectionName,
+            default(Action<MicrosoftDataSqlClientSettings>?));
+
+        var exception = Assert.Throws<ArgumentNullException>(action);
+        Assert.Equal(nameof(builder), exception.ParamName);
+    }
+
+    [Theory]
+    [InlineData(true)]
+    [InlineData(false)]
+    public void AddSqlServerClientShouldThrowWhenConnectionNameIsNullOrEmpty(bool isNull)
+    {
+        IHostApplicationBuilder builder = new HostApplicationBuilder();
+        var connectionName = isNull ? null! : string.Empty;
+
+        var action = () => builder.AddSqlServerClient(
+            connectionName,
+            default(Action<MicrosoftDataSqlClientSettings>?));
+
+        var exception = isNull
+            ? Assert.Throws<ArgumentNullException>(action)
+            : Assert.Throws<ArgumentException>(action);
+        Assert.Equal(nameof(connectionName), exception.ParamName);
+    }
+
+    [Fact]
+    public void AddKeyedSqlServerClientShouldThrowWhenBuilderIsNull()
+    {
+        IHostApplicationBuilder builder = null!;
+        const string name = "blobs";
+
+        var action = () => builder.AddKeyedSqlServerClient(
+            name,
+            default(Action<MicrosoftDataSqlClientSettings>?));
+
+        var exception = Assert.Throws<ArgumentNullException>(action);
+        Assert.Equal(nameof(builder), exception.ParamName);
+    }
+
+    [Theory]
+    [InlineData(true)]
+    [InlineData(false)]
+    public void AddKeyedSqlServerClientShouldThrowWhenNameIsNullOrEmpty(bool isNull)
+    {
+        IHostApplicationBuilder builder = new HostApplicationBuilder();
+        var name = isNull ? null! : string.Empty;
+
+        var action = () => builder.AddKeyedSqlServerClient(
+            name,
+            default(Action<MicrosoftDataSqlClientSettings>?));
+
+        var exception = isNull
+            ? Assert.Throws<ArgumentNullException>(action)
+            : Assert.Throws<ArgumentException>(action);
+        Assert.Equal(nameof(name), exception.ParamName);
+    }
+}
