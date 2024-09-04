a7b1c5b693d0ef0c4f2fb9b56403ff324e68cd30
David Fowler <davidfowl@gmail.com>
2024-09-03 18:32:13 -0700

    Remove other interfaces from functions resource

--- src/Aspire.Hosting.Azure.Functions/AzureFunctionsResource.cs
+++ src/Aspire.Hosting.Azure.Functions/AzureFunctionsResource.cs
@@ -10,8 +10,7 @@ namespace Aspire.Hosting.Azure;
 /// Regardless, we'll likely want to use a custom resource type for functions to permit us to
 /// grow from one implementation to the next.
 /// </remarks>
-public class AzureFunctionsProjectResource(string name)
-    : ProjectResource(name), IResourceWithEnvironment, IResourceWithArgs, IResourceWithServiceDiscovery
+public class AzureFunctionsProjectResource(string name) : ProjectResource(name)
 {
     internal AzureStorageResource? HostStorage { get; set; }
 }
