1bf361c71d0be70cfca4eb769071a99402fa92c8
Drew Noakes <git@drewnoakes.com>
2024-09-09 21:19:14 +1000

    Use basic version of plotly lib
    This reduces the uncompressed file size from 4,408 kB to 1,002 kB.

--- src/Aspire.Dashboard/wwwroot/js/app-metrics.js
+++ src/Aspire.Dashboard/wwwroot/js/app-metrics.js
@@ -1,4 +1,4 @@
-import './plotly-2.35.0.min.js'
+import './plotly-basic-2.35.0.min.js'
 
 export function initializeChart(id, traces, exemplarTrace, rangeStartTime, rangeEndTime, serverLocale, chartInterop) {
     registerLocale(serverLocale);
