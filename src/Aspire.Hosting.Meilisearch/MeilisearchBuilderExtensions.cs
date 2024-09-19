925256c31c4036227d30c2cfc7ddfd6fec66a6af
Alireza Baloochi <alireza.baloochi1380@gmail.com>
2024-09-19 17:59:40 +0330

    WIP: Meilisearch integration

---
+++ src/Aspire.Hosting.Meilisearch/MeilisearchBuilderExtensions.cs
@@ -0,0 +1,118 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Aspire.Hosting.ApplicationModel;
+using Aspire.Hosting.Meilisearch;
+using Aspire.Hosting.Utils;
+
+namespace Aspire.Hosting;
+
+/// <summary>
+/// Provides extension methods for adding Meilisearch resources to the application model.
+/// </summary>
+public static class MeilisearchBuilderExtensions
+{
+    private const int MeilisearchPort = 7700;
+
+    /// <summary>
+    /// Adds an Meilisearch container resource to the application model.
+    /// </summary>
+    /// <remarks>
+    /// The default image is "getmeili/meilisearch" and the tag is "v1.10".
+    /// </remarks>
+    /// <param name="builder">The <see cref="IDistributedApplicationBuilder"/>.</param>
+    /// <param name="name">The name of the resource. This name will be used as the connection string name when referenced in a dependency.</param>
+    /// <param name="port">The host port to bind the underlying container to.</param>
+    /// <param name="masterKey">The parameter used to provide the master key for the Meilisearch. If <see langword="null"/> a random master key will be generated.</param>
+    /// <returns>A reference to the <see cref="IResourceBuilder{T}"/>.</returns>
+    /// <example>
+    /// Add an Meilisearch container to the application model and reference it in a .NET project.
+    /// <code lang="csharp">
+    /// var builder = DistributedApplication.CreateBuilder(args);
+    ///
+    /// var meilisearch = builder.Meilisearch("meilisearch");
+    /// var api = builder.AddProject&lt;Projects.Api&gt;("api")
+    ///   .WithReference(meilisearch);
+    ///  
+    /// builder.Build().Run(); 
+    /// </code>
+    /// </example>
+    public static IResourceBuilder<MeilisearchResource> AddMeilisearch(
+        this IDistributedApplicationBuilder builder,
+        string name,
+        IResourceBuilder<ParameterResource>? masterKey = null,
+        int? port = null)
+    {
+        ArgumentNullException.ThrowIfNull(builder);
+        ArgumentNullException.ThrowIfNull(name);
+
+        var masterKeyParameter = masterKey?.Resource ?? ParameterResourceBuilderExtensions.CreateDefaultPasswordParameter(builder, $"{name}-masterKey");
+
+        var meilisearch = new MeilisearchResource(name, masterKeyParameter);
+
+        //todo: add health check
+        return builder.AddResource(meilisearch)
+             .WithImage(MeilisearchContainerImageTags.Image, MeilisearchContainerImageTags.Tag)
+             .WithImageRegistry(MeilisearchContainerImageTags.Registry)
+             .WithHttpEndpoint(targetPort: MeilisearchPort, port: port, name: MeilisearchResource.PrimaryEndpointName)
+             .WithEnvironment(context =>
+             {
+                 context.EnvironmentVariables["MEILI_MASTER_KEY"] = meilisearch.MasterKeyParameter;
+             });
+    }
+
+    /// <summary>
+    /// Adds a named volume for the data folder to a Meilisearch container resource.
+    /// </summary>
+    /// <param name="builder">The resource builder.</param>
+    /// <param name="name">The name of the volume. Defaults to an auto-generated name based on the application and resource names.</param>
+    /// <returns>The <see cref="IResourceBuilder{T}"/>.</returns>
+    /// <example>
+    /// Add an Meilisearch container to the application model and reference it in a .NET project. Additionally, in this
+    /// example a data volume is added to the container to allow data to be persisted across container restarts.
+    /// <code lang="csharp">
+    /// var builder = DistributedApplication.CreateBuilder(args);
+    ///
+    /// var meilisearch = builder.AddMeilisearch("meilisearch")
+    /// .WithDataVolume();
+    /// var api = builder.AddProject&lt;Projects.Api&gt;("api")
+    ///   .WithReference(meilisearch);
+    ///  
+    /// builder.Build().Run(); 
+    /// </code>
+    /// </example>
+    public static IResourceBuilder<MeilisearchResource> WithDataVolume(this IResourceBuilder<MeilisearchResource> builder, string? name = null)
+    {
+        ArgumentNullException.ThrowIfNull(builder);
+
+        return builder.WithVolume(name ?? VolumeNameGenerator.CreateVolumeName(builder, "data"), "/meili_data");
+    }
+
+    /// <summary>
+    /// Adds a bind mount for the data folder to a Meilisearch container resource.
+    /// </summary>
+    /// <param name="builder">The resource builder.</param>
+    /// <param name="source">The source directory on the host to mount into the container.</param>
+    /// <returns>The <see cref="IResourceBuilder{T}"/>.</returns>
+    /// <example>
+    /// Add an Meilisearch container to the application model and reference it in a .NET project. Additionally, in this
+    /// example a bind mount is added to the container to allow data to be persisted across container restarts.
+    /// <code lang="csharp">
+    /// var builder = DistributedApplication.CreateBuilder(args);
+    ///
+    /// var meilisearch = builder.AddMeilisearch("meilisearch")
+    /// .WithDataBindMount("./data/meilisearch/data");
+    /// var api = builder.AddProject&lt;Projects.Api&gt;("api")
+    ///   .WithReference(meilisearch);
+    ///  
+    /// builder.Build().Run(); 
+    /// </code>
+    /// </example>
+    public static IResourceBuilder<MeilisearchResource> WithDataBindMount(this IResourceBuilder<MeilisearchResource> builder, string source)
+    {
+        ArgumentNullException.ThrowIfNull(builder);
+        ArgumentNullException.ThrowIfNull(source);
+
+        return builder.WithBindMount(source, "/meili_data");
+    }
+}
---
+++ src/Aspire.Hosting.Meilisearch/MeilisearchResource.cs
@@ -0,0 +1,39 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace Aspire.Hosting.ApplicationModel;
+
+/// <summary>
+/// A resource that represents a Meilisearch
+/// </summary>
+public class MeilisearchResource : ContainerResource, IResourceWithConnectionString
+{
+    internal const string PrimaryEndpointName = "http";
+
+    /// <param name="name">The name of the resource.</param>
+    /// <param name="masterKey">A parameter that contains the Meilisearch master key.</param>
+    public MeilisearchResource(string name, ParameterResource masterKey) : base(name)
+    {
+        ArgumentNullException.ThrowIfNull(masterKey);
+        MasterKeyParameter = masterKey;
+    }
+
+    private EndpointReference? _primaryEndpoint;
+
+    /// <summary>
+    /// Gets the primary endpoint for the Meilisearch. This endpoint is used for all API calls over HTTP.
+    /// </summary>
+    public EndpointReference PrimaryEndpoint => _primaryEndpoint ??= new(this, PrimaryEndpointName);
+
+    /// <summary>
+    /// Gets the parameter that contains the Meilisearch superuser password.
+    /// </summary>
+    public ParameterResource MasterKeyParameter { get; }
+
+    /// <summary>
+    /// Gets the connection string expression for the Meilisearch
+    /// </summary>
+    public ReferenceExpression ConnectionStringExpression =>
+        ReferenceExpression.Create($"http://{PrimaryEndpoint.Property(EndpointProperty.Host)}:{PrimaryEndpoint.Property(EndpointProperty.Port)}");
+}
+
