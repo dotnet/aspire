c797e10144aa40698b3c5d9d79987921b4d0ef95
James Newton-King <james@newtonking.com>
2024-09-19 11:53:25 +0800

    Update

--- tests/Aspire.Dashboard.Components.Tests/Pages/ConsoleLogsTests.cs
+++ tests/Aspire.Dashboard.Components.Tests/Pages/ConsoleLogsTests.cs
@@ -124,14 +124,13 @@ public partial class ConsoleLogsTests : TestContext
         var loc = Services.GetRequiredService<IStringLocalizer<Resources.ConsoleLogs>>();
 
         // Assert
-        logger.LogInformation("Waiting for selected resource.");
-        cut.WaitForState(() => instance.PageViewModel.SelectedResource == testResource);
-        cut.WaitForState(() => instance.PageViewModel.Status == loc[nameof(Resources.ConsoleLogs.ConsoleLogsWatchingLogs)]);
-
+        logger.LogInformation("Resource and subscription should be set immediately on first render.");
+        Assert.Equal(testResource, instance.PageViewModel.SelectedResource);
+        Assert.Equal(loc[nameof(Resources.ConsoleLogs.ConsoleLogsWatchingLogs)], instance.PageViewModel.Status);
         Assert.Equal("test-resource", Assert.Single(subscribedResourceNames));
 
+        logger.LogInformation("Log results are added to log viewer.");
         consoleLogsChannel.Writer.TryWrite([new ResourceLogLine(1, "Hello world", IsErrorMessage: false)]);
-
         cut.WaitForState(() => instance.LogViewer.LogEntries.EntriesCount > 0);
     }
 
