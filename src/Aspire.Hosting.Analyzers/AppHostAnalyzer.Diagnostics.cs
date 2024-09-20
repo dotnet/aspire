9851e65b0caee907f3deac5385cf26e01681879f
Damian Edwards <damian@damianedwards.com>
2024-09-20 11:51:43 -0700

    Drop version of Roslyn analyzer depends on to 4.8

--- src/Aspire.Hosting.Analyzers/AppHostAnalyzer.Diagnostics.cs
+++ src/Aspire.Hosting.Analyzers/AppHostAnalyzer.Diagnostics.cs
@@ -11,18 +11,18 @@ public partial class AppHostAnalyzer
     [System.Diagnostics.CodeAnalysis.SuppressMessage("MicrosoftCodeAnalysisReleaseTracking", "RS2008:Enable analyzer release tracking")]
     internal static class Diagnostics
     {
+        private const string ModelNameMustBeValidId = "ASPIRE006";
         internal static readonly DiagnosticDescriptor s_modelNameMustBeValid = new(
-            id: "ASPIRE006",
+            id: ModelNameMustBeValidId,
             title: "Application model items must have valid names",
             messageFormat: "{0}",
             category: "Design",
             DiagnosticSeverity.Error,
             isEnabledByDefault: true,
-            helpLinkUri: "https://aka.ms/dotnet/aspire/ASPIRE006");
+            helpLinkUri: $"https://aka.ms/dotnet/aspire/{ModelNameMustBeValidId}");
 
-        public static readonly ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics = [
-            // Resources
+        public static readonly ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics = ImmutableArray.Create(
             s_modelNameMustBeValid
-        ];
+        );
     }
 }
