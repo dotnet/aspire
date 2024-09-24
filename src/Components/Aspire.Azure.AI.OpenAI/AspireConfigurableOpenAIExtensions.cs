14314cd8bbc7a3c70a276b4f2e4c51dd85d4b694
Sebastien Ros <sebastienros@gmail.com>
2024-09-23 19:57:07 -0700

    Fix build

--- src/Components/Aspire.Azure.AI.OpenAI/AspireConfigurableOpenAIExtensions.cs
+++ src/Components/Aspire.Azure.AI.OpenAI/AspireConfigurableOpenAIExtensions.cs
@@ -2,7 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.Data.Common;
-using Aspire.Azure.AI.OpenAI;
 using Azure.AI.OpenAI;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
