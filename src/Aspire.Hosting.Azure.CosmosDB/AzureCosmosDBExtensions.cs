dd5f2fff5c623727b6ea617c78abe92ed3bfc998
Hans Bakker <hansmbakker+github@gmail.com>
2024-09-24 17:37:06 +0200

    Add WithPartitionCount and WithDataVolume to Azure.Hosting.Azure.CosmosDB (#5157)
    * Add WithPartitionCount
    
    * Add comment about default count
    
    * Add WithDataVolume() based on https://github.com/dotnet/aspire/issues/3295
    
    * Fix build
    
    * Align shareddir usage
    
    * Update AzureCosmosDBExtensions.cs
    
    Co-authored-by: Martin Costello <martin@martincostello.com>
    
    * Add functional test
    
    * Fix assertion
    
    * Add test for WithPartitionCount
    
    * Add WithDataVolumeShouldPersistStateBetweenUsages
    
    * Move tests into https://github.com/dotnet/aspire/blob/main/tests/Aspire.Hosting.Azure.Tests/AzureCosmosDBEmulatorFunctionalTests.cs
    
    * remove extra project
    
    * Remove redundant changes from sln
    
    * fix InternalsVisibleTo
    
    * Fix compile errors
    
    * skip two tests
    
    ---------
    
    Co-authored-by: Martin Costello <martin@martincostello.com>

--- src/Aspire.Hosting.Azure.CosmosDB/AzureCosmosDBExtensions.cs
+++ src/Aspire.Hosting.Azure.CosmosDB/AzureCosmosDBExtensions.cs
@@ -5,6 +5,7 @@ using System.Diagnostics.CodeAnalysis;
 using Aspire.Hosting.ApplicationModel;
 using Aspire.Hosting.Azure;
 using Aspire.Hosting.Azure.Cosmos;
+using Aspire.Hosting.Utils;
 using Azure.Identity;
 using Azure.Provisioning;
 using Azure.Provisioning.CosmosDB;
@@ -12,6 +13,7 @@ using Azure.Provisioning.Expressions;
 using Azure.Provisioning.KeyVault;
 using Microsoft.Azure.Cosmos;
 using Microsoft.Extensions.DependencyInjection;
+using System.Globalization;
 
 namespace Aspire.Hosting;
 
@@ -191,6 +193,16 @@ public static class AzureCosmosExtensions
     }
 
     /// <summary>
+    /// Adds a named volume for the data folder to an Azure Cosmos DB emulator resource.
+    /// </summary>
+    /// <param name="builder">The builder for the <see cref="AzureCosmosDBEmulatorResource"/>.</param>
+    /// <param name="name">The name of the volume. Defaults to an auto-generated name based on the application and resource names.</param>
+    /// <returns>A builder for the <see cref="AzureCosmosDBEmulatorResource"/>.</returns>
+    public static IResourceBuilder<AzureCosmosDBEmulatorResource> WithDataVolume(this IResourceBuilder<AzureCosmosDBEmulatorResource> builder, string? name = null)
+        => builder.WithEnvironment("AZURE_COSMOS_EMULATOR_ENABLE_DATA_PERSISTENCE", "true")
+                  .WithVolume(name ?? VolumeNameGenerator.CreateVolumeName(builder, "data"), "/tmp/cosmos/appdata", isReadOnly: false);
+
+    /// <summary>
     /// Configures the gateway port for the Azure Cosmos DB emulator.
     /// </summary>
     /// <param name="builder">Builder for the Cosmos emulator container</param>
@@ -202,6 +214,25 @@ public static class AzureCosmosExtensions
         {
             endpoint.Port = port;
         });
+    }
+
+    /// <summary>
+    /// Configures the partition count for the Azure Cosmos DB emulator.
+    /// </summary>
+    /// <param name="builder">Builder for the Cosmos emulator container</param>
+    /// <param name="count">Desired partition count.</param>
+    /// <returns>Cosmos emulator resource builder.</returns>
+    /// <remarks>Not calling this method will result in the default of 25 partitions. The actual started partitions is always one more than specified.
+    /// See <a href="https://learn.microsoft.com/en-us/azure/cosmos-db/emulator-windows-arguments#change-the-number-of-default-containers">this documentation</a> about setting the partition count.
+    /// </remarks>
+    public static IResourceBuilder<AzureCosmosDBEmulatorResource> WithPartitionCount(this IResourceBuilder<AzureCosmosDBEmulatorResource> builder, int count)
+    {
+        if (count < 1 || count > 250)
+        {
+            throw new ArgumentOutOfRangeException(nameof(count), count, "Count must be between 1 and 250.");
+        }
+
+        return builder.WithEnvironment("AZURE_COSMOS_EMULATOR_PARTITION_COUNT", count.ToString(CultureInfo.InvariantCulture));
     }
 
     /// <summary>
