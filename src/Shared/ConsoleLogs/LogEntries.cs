2ce653981c2e8735facdfc363f5b1ca5989da7b3
James Newton-King <james@newtonking.com>
2024-09-25 22:46:07 +0800

    Comment

--- src/Shared/ConsoleLogs/LogEntries.cs
+++ src/Shared/ConsoleLogs/LogEntries.cs
@@ -6,6 +6,9 @@ using Aspire.Dashboard.Otlp.Storage;
 
 namespace Aspire.Hosting.ConsoleLogs;
 
+// Type is shared by dashboard and hosting.
+// It needs to be public in dashboard so it can be bound to a parameter.
+// It needs to be internal in hosting because we don't want to expose it as public API.
 [DebuggerDisplay("Count = {EntriesCount}")]
 #if ASPIRE_DASHBOARD
 public sealed class LogEntries(int maximumEntryCount)
