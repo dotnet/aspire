896b3e2bfd45c94614cf3796e88c25e88791292a
Ankit Jain <radical@gmail.com>
2024-09-25 21:02:09 -0400

    wip

--- tests/Shared/WorkloadTesting/DotNetNewCommand.cs
+++ tests/Shared/WorkloadTesting/DotNetNewCommand.cs
@@ -7,7 +7,7 @@ namespace Aspire.Workload.Tests;
 
 public class DotNetNewCommand : DotNetCommand
 {
-    private string? _customHive;
+    private readonly string _customHive;
 
     public DotNetNewCommand(
         ITestOutputHelper _testOutput,
@@ -17,16 +17,14 @@ public class DotNetNewCommand : DotNetCommand
         string label = "dotnet-new")
             : base(_testOutput, useDefaultArgs, buildEnv, label)
     {
-        _customHive = hiveDirectory ?? _buildEnvironment.TemplatesCustomHive?.CustomHiveDirectory;
-    }
-
-    public DotNetNewCommand WithCustomHive(string hiveDirectory)
-    {
-        _customHive = hiveDirectory;
-        return this;
+        string? hiveDir = hiveDirectory ?? _buildEnvironment.TemplatesCustomHive?.CustomHiveDirectory;
+        if (hiveDir is null)
+        {
+            throw new ArgumentException("No custom hive directory was provided, and the BuildEnvironment does not have one set either");
+        }
+        _customHive = hiveDir;
     }
 
     protected override string GetFullArgs(params string[] args)
-        => $"new {base.GetFullArgs(args)}"
-                + (_customHive is not null ? $" --debug:custom-hive \"{_customHive}\"" : "");
+        => $"new {base.GetFullArgs(args)} --debug:custom-hive \"{_customHive}\"";
 }
