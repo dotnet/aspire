279e51b7ed3576ad3e257765b0028b29dd363d56
ZLoo <Kozlov.s.v.1992@gmail.com>
2024-09-27 18:52:21 +0300

    upd conflict

--- src/Aspire.Hosting.AWS/AWSProvisioningException.cs
+++ src/Aspire.Hosting.AWS/AWSProvisioningException.cs
@@ -1,24 +1,13 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System.Diagnostics.CodeAnalysis;
-using System.Runtime.CompilerServices;
-
-namespace Aspire.Hosting.AWS;
+namespace Aspire.Hosting.AWS.Provisioning;
 
 /// <summary>
 /// Exception for errors provisioning AWS application resources
 /// </summary>
 /// <param name="message"></param>
 /// <param name="innerException"></param>
-public class AWSProvisioningException(string message, Exception? innerException = null)
-    : Exception(ThrowIfNullOrEmpty(message), innerException)
+public class AWSProvisioningException(string message, Exception? innerException = null) : Exception(message, innerException)
 {
-    private static string ThrowIfNullOrEmpty(
-        [NotNull] string? argument,
-        [CallerArgumentExpression(nameof(argument))] string? paramName = null)
-    {
-        ArgumentException.ThrowIfNullOrEmpty(argument, paramName);
-        return argument;
-    }
 }
