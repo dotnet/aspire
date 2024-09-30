df5df9045df0ca46057765c6d5129ebe4424d74d
Safia Abdalla <safia@microsoft.com>
2024-09-30 12:26:25 -0700

    Update ready state check for Azure Functions tests (#5987)
    * Update ready state check for Azure Functions tests
    
    * Use HttpClient with resilience configured

--- tests/Aspire.Playground.Tests/ProjectSpecificTests.cs
+++ tests/Aspire.Playground.Tests/ProjectSpecificTests.cs
@@ -56,7 +56,6 @@ public class ProjectSpecificTests(ITestOutputHelper _testOutput)
     }
 
     [Fact]
-    [ActiveIssue("https://github.com/dotnet/aspire/issues/5960")]
     [RequiresDocker]
     [RequiresTools(["func"])]
     public async Task AzureFunctionsTest()
@@ -68,17 +67,17 @@ public class ProjectSpecificTests(ITestOutputHelper _testOutput)
         await app.StartAsync();
         await app.WaitForResources().WaitAsync(TimeSpan.FromMinutes(2));
 
-        // Wait for the blobTrigger to be discovered as an indication that the host and worker
-        // has been successfully launched
+        // Wait for the 'Job host started' message as an indication
+        // that the Functions host has initialized correctly
         await WaitForAllTextAsync(app,
             [
-                "MyAzureBlobTrigger: blobTrigger"
+                "Job host started"
             ],
             resourceName: "funcapp",
             timeoutSecs: 160);
 
         // Assert that HTTP triggers work correctly
-        await app.CreateHttpClient("funcapp").GetAsync("/api/injected-resources");
+        await AppHostTests.CreateHttpClientWithResilience(app, "funcapp").GetAsync("/api/injected-resources");
         await WaitForAllTextAsync(app,
             [
                 "Executed 'Functions.injected-resources'"
@@ -86,8 +85,9 @@ public class ProjectSpecificTests(ITestOutputHelper _testOutput)
             resourceName: "funcapp",
             timeoutSecs: 160);
 
+        using var apiServiceClient = AppHostTests.CreateHttpClientWithResilience(app, "apiservice");
         // Assert that Azure Storage Queue triggers work correctly
-        await app.CreateHttpClient("apiservice").GetAsync("/publish/asq");
+        await apiServiceClient.GetAsync("/publish/asq");
         await WaitForAllTextAsync(app,
             [
                 "Executed 'Functions.MyAzureQueueTrigger'"
@@ -96,7 +96,7 @@ public class ProjectSpecificTests(ITestOutputHelper _testOutput)
             timeoutSecs: 160);
 
         // Assert that Azure Storage Blob triggers work correctly
-        await app.CreateHttpClient("apiservice").GetAsync("/publish/blob");
+        await apiServiceClient.GetAsync("/publish/blob");
         await WaitForAllTextAsync(app,
             [
                 "Executed 'Functions.MyAzureBlobTrigger'"
@@ -105,7 +105,7 @@ public class ProjectSpecificTests(ITestOutputHelper _testOutput)
             timeoutSecs: 160);
 
         // Assert that EventHubs triggers work correctly
-        await app.CreateHttpClient("apiservice").GetAsync("/publish/eventhubs");
+        await apiServiceClient.GetAsync("/publish/eventhubs");
         await WaitForAllTextAsync(app,
             [
                 "Executed 'Functions.MyEventHubTrigger'"
@@ -115,7 +115,7 @@ public class ProjectSpecificTests(ITestOutputHelper _testOutput)
 
 #if !SKIP_PROVISIONED_AZURE_RESOURCE
         // Assert that ServiceBus triggers work correctly
-        await app.CreateHttpClient("apiservice").GetAsync("/publish/asb");
+        await apiServiceClient.GetAsync("/publish/asb");
         await WaitForAllTextAsync(app,
             [
                 "Executed 'Functions.MyServiceBusTrigger'"
