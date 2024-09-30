dc0b426f7e14147c796f182c1d1e5209fd8800c5
Mitch Denny <midenn@microsoft.com>
2024-09-30 14:18:44 +1000

    Update src/Aspire.Hosting/Health/ResourceHealthCheckService.cs
    Co-authored-by: David Fowler <davidfowl@gmail.com>

--- src/Aspire.Hosting/Health/ResourceHealthCheckService.cs
+++ src/Aspire.Hosting/Health/ResourceHealthCheckService.cs
@@ -46,7 +46,7 @@ internal class ResourceHealthCheckService(ResourceNotificationService resourceNo
 
         if (!resource.TryGetAnnotationsIncludingAncestorsOfType<HealthCheckAnnotation>(out var annotations))
         {
-            // NOTE: If there are no transitive health check annotations then there
+            // NOTE: If there are no health check annotations then there
             //       is currently nothing to monitor. At this point in time we don't
             //       dynamically add health checks at runtime. If this changes then we
             //       would need to revisit this and scan for transitive health checks
