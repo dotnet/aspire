7acc7814c2f663149aba2b41718e1e7d7d64e4d5
Ratzman, Adam Mortimer <aratzman@iu.edu>
2024-10-01 12:40:21 -0400

    change InstrumentViewModel.DataUpdateSubscriptions to prevent enumeration during collection modification, which can happen asynchronously

--- src/Aspire.Dashboard/Model/InstrumentViewModel.cs
+++ src/Aspire.Dashboard/Model/InstrumentViewModel.cs
@@ -1,6 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Collections.Concurrent;
 using Aspire.Dashboard.Otlp.Model;
 using Aspire.Dashboard.Otlp.Model.MetricValues;
 
@@ -11,7 +12,7 @@ public class InstrumentViewModel
     public OtlpInstrumentSummary? Instrument { get; private set; }
     public List<DimensionScope>? MatchedDimensions { get; private set; }
 
-    public List<Func<Task>> DataUpdateSubscriptions { get; } = [];
+    public ConcurrentBag<Func<Task>> DataUpdateSubscriptions { get; } = [];
     public string? Theme { get; set; }
     public bool ShowCount { get; set; }
 
