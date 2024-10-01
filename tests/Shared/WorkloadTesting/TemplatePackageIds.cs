3be33b4aecf6aa7408e0cb258ee17b92515a8eea
Ankit Jain <radical@gmail.com>
2024-10-01 17:14:36 -0400

    Add new new-and-build template tests

---
+++ tests/Shared/WorkloadTesting/TemplatePackageIds.cs
@@ -0,0 +1,10 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace Aspire.Workload.Tests;
+
+public static class TemplatePackageIdNames
+{
+    public const string AspireProjectTemplates_9_0_net8 = "Aspire.ProjectTemplates.9.0.net8";
+    public const string AspireProjectTemplates_9_0_net9 = "Aspire.ProjectTemplates.9.0.net9";
+}
