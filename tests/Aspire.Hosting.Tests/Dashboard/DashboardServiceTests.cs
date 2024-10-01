f6bfd13c181937e10004c2b64941c605b6a16ed2
James Newton-King <james@newtonking.com>
2024-10-01 17:05:12 +0800

    Add test

--- tests/Aspire.Hosting.Tests/Dashboard/DashboardServiceTests.cs
+++ tests/Aspire.Hosting.Tests/Dashboard/DashboardServiceTests.cs
@@ -4,7 +4,9 @@
 using Aspire.Hosting.Dashboard;
 using Aspire.Hosting.Tests.Utils;
 using Aspire.Hosting.Tests.Utils.Grpc;
+using Aspire.Hosting.Utils;
 using Aspire.ResourceService.Proto.V1;
+using Google.Protobuf.WellKnownTypes;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
@@ -12,6 +14,7 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
 using Xunit;
 using DashboardService = Aspire.Hosting.Dashboard.DashboardService;
+using Resource = Aspire.Hosting.ApplicationModel.Resource;
 
 namespace Aspire.Hosting.Tests.Dashboard;
 
@@ -23,7 +26,7 @@ public class DashboardServiceTests
         // Arrange
         var resourceLoggerService = new ResourceLoggerService();
         var resourceNotificationService = new ResourceNotificationService(NullLogger<ResourceNotificationService>.Instance, new TestHostApplicationLifetime(), new ServiceCollection().BuildServiceProvider(), resourceLoggerService);
-        var dashboardServiceData = new DashboardServiceData(resourceNotificationService, resourceLoggerService, NullLogger<DashboardServiceData>.Instance, new DashboardCommandExecutor(new ServiceCollection().BuildServiceProvider()));
+        await using var dashboardServiceData = new DashboardServiceData(resourceNotificationService, resourceLoggerService, NullLogger<DashboardServiceData>.Instance, new DashboardCommandExecutor(new ServiceCollection().BuildServiceProvider()));
         var dashboardService = new DashboardService(dashboardServiceData, new TestHostEnvironment(), new TestHostApplicationLifetime(), NullLogger<DashboardService>.Instance);
 
         var logger = resourceLoggerService.GetLogger("test-resource");
@@ -54,11 +57,78 @@ public class DashboardServiceTests
         await task;
     }
 
+    [Fact]
+    public async Task WatchResources_ResourceHasCommands_CommandsSentWithResponse()
+    {
+        // Arrange
+        var resourceLoggerService = new ResourceLoggerService();
+        var resourceNotificationService = new ResourceNotificationService(NullLogger<ResourceNotificationService>.Instance, new TestHostApplicationLifetime(), new ServiceCollection().BuildServiceProvider(), resourceLoggerService);
+        await using var dashboardServiceData = new DashboardServiceData(resourceNotificationService, resourceLoggerService, NullLogger<DashboardServiceData>.Instance, new DashboardCommandExecutor(new ServiceCollection().BuildServiceProvider()));
+        var dashboardService = new DashboardService(dashboardServiceData, new TestHostEnvironment(), new TestHostApplicationLifetime(), NullLogger<DashboardService>.Instance);
+
+        var testResource = new TestResource("test-resource");
+        using var applicationBuilder = TestDistributedApplicationBuilder.Create();
+        var builder = applicationBuilder.AddResource(testResource);
+        builder.WithCommand(
+            type: "TestType",
+            displayName: "Display name!",
+            executeCommand: c => Task.FromResult(CommandResults.Success()),
+            updateState: c => ApplicationModel.ResourceCommandState.Enabled,
+            displayDescription: "Display description!",
+            parameter: new [] {"One", "Two"},
+            confirmationMessage: "Confirmation message!",
+            iconName: "Icon name!",
+            iconVariant: ApplicationModel.IconVariant.Filled,
+            isHighlighted: true);
+
+        await resourceNotificationService.PublishUpdateAsync(testResource, s => s);
+
+        var cts = new CancellationTokenSource();
+        var context = TestServerCallContext.Create(cancellationToken: cts.Token);
+        var writer = new TestServerStreamWriter<WatchResourcesUpdate>(context);
+
+        // Act
+        var task = dashboardService.WatchResources(
+            new WatchResourcesRequest(),
+            writer,
+            context);
+
+        // Assert
+        var update = await writer.ReadNextAsync();
+
+        var resourceData = Assert.Single(update.InitialData.Resources);
+        var commandData = Assert.Single(resourceData.Commands);
+
+        Assert.Equal("TestType", commandData.CommandType);
+        Assert.Equal("Display name!", commandData.DisplayName);
+        Assert.Equal("Display description!", commandData.DisplayDescription);
+        Assert.Equal(Value.ForList(Value.ForString("One"), Value.ForString("Two")), commandData.Parameter);
+        Assert.Equal("Confirmation message!", commandData.ConfirmationMessage);
+        Assert.Equal("Icon name!", commandData.IconName);
+        Assert.Equal(ResourceService.Proto.V1.IconVariant.Filled, commandData.IconVariant);
+        Assert.True(commandData.IsHighlighted);
+
+        cts.Cancel();
+
+        try
+        {
+            await task;
+        }
+        catch (OperationCanceledException)
+        {
+            // Ok if this error is thrown.
+        }
+    }
+
     private sealed class TestHostEnvironment : IHostEnvironment
     {
         public string ApplicationName { get; set; } = default!;
         public IFileProvider ContentRootFileProvider { get; set; } = default!;
         public string ContentRootPath { get; set; } = default!;
         public string EnvironmentName { get; set; } = default!;
+    }
+
+    private sealed class TestResource(string name) : Resource(name)
+    {
     }
 }
