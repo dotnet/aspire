f7cb2ea085918f2373f81ca751cd0dd03fd87c5f
David Ebbo <david.ebbo@gmail.com>
2024-10-04 10:11:42 +0200

    More comments

--- src/Aspire.Hosting/ApplicationModel/ExpressionResolver.cs
+++ src/Aspire.Hosting/ApplicationModel/ExpressionResolver.cs
@@ -13,15 +13,15 @@ internal class ExpressionResolver(string containerHostName, CancellationToken ca
         public bool HasPort { get; set; }
     }
 
-    // For each endpoint, store two bools, to track if the host and port are in use
+    // For each endpoint, keep track of whether host and port are in use
     // The key is the unique name of the endpoint, which is the resource name and endpoint name
     readonly Dictionary<string, HostAndPortPresence> _endpointUsage = [];
+    static string EndpointUniqueName(EndpointReference endpointReference) => $"{endpointReference.Resource.Name}/{endpointReference.EndpointName}";
 
     // This marks whether we are in the preprocess phase or not
     // Not thread-safe, but we doesn't matter, since this class is never used concurrently
     bool Preprocess { get; set; }
 
-    static string EndpointUniqueName(EndpointReference endpointReference) => $"{endpointReference.Resource.Name}/{endpointReference.EndpointName}";
 
     async Task<string?> EvalEndpointAsync(EndpointReference endpointReference, EndpointProperty property)
     {
