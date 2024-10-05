b08f053afcc50a45445fc63e1c8b61ddce250295
Damian Edwards <damian@damianedwards.com>
2024-10-04 17:46:55 -0700

    Update starter template from .NET 9 app template updates
    - net8 version gets just a Bootstrap update
    - net9 version gets all changes

--- src/Aspire.ProjectTemplates.9.0.net8/templates/aspire-starter/Aspire-StarterApplication.1.ApiService/Program.cs
+++ src/Aspire.ProjectTemplates.9.0.net8/templates/aspire-starter/Aspire-StarterApplication.1.ApiService/Program.cs
@@ -11,10 +11,10 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 app.UseExceptionHandler();
 
-var summaries = new[]
-{
+string[] summaries =
+[
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+];
 
 app.MapGet("/weatherforecast", () =>
 {
