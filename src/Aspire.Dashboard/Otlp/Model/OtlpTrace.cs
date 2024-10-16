7aec0e3c67c227b123b8935b2fae47e9738e5e49
James Newton-King <james@newtonking.com>
2024-10-16 15:41:18 +0800

    WIP

--- src/Aspire.Dashboard/Otlp/Model/OtlpTrace.cs
+++ src/Aspire.Dashboard/Otlp/Model/OtlpTrace.cs
@@ -1,9 +1,25 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Collections.ObjectModel;
 using System.Diagnostics;
 
 namespace Aspire.Dashboard.Otlp.Model;
+
+public sealed class SpanCollection : KeyedCollection<string, OtlpSpan>
+{
+    // Chosen to balance memory usage overhead of dictionary vs work to find spans by ID.
+    private const int DictionaryCreationThreshold = 128;
+
+    public SpanCollection() : base(StringComparers.OtlpSpanId, DictionaryCreationThreshold)
+    {
+    }
+
+    protected override string GetKeyForItem(OtlpSpan item)
+    {
+        return item.SpanId;
+    }
+}
 
 [DebuggerDisplay("{DebuggerToString(),nq}")]
 public class OtlpTrace
@@ -34,7 +50,7 @@ public class OtlpTrace
         }
     }
 
-    public List<OtlpSpan> Spans { get; } = new List<OtlpSpan>();
+    public SpanCollection Spans { get; } = new SpanCollection();
 
     public int CalculateDepth(OtlpSpan span)
     {
