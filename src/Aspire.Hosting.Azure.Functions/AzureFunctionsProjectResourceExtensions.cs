803a122bd06d8eddac73d2fb2e3c179ed1ab756e
Safia Abdalla <safia@microsoft.com>
2024-10-16 08:34:41 -0700

    Shorten Functions storage prefix and use AppHost name SHA (#6301)
    * Shorten Functions storage prefix and use AppHost name SHA
    
    * Use name only when IsPublishMode == true
    
    * Add tests and change Functions logic
    
    * Update doc string

--- src/Aspire.Hosting.Azure.Functions/AzureFunctionsProjectResourceExtensions.cs
+++ src/Aspire.Hosting.Azure.Functions/AzureFunctionsProjectResourceExtensions.cs
@@ -14,7 +14,16 @@ namespace Aspire.Hosting;
 /// </summary>
 public static class AzureFunctionsProjectResourceExtensions
 {
-    internal const string DefaultAzureFunctionsHostStorageName = "defaultazfuncstorage";
+    /// <remarks>
+    /// The prefix used for configuring the name default Azure Storage account that is used
+    /// for Azure Functions bookkeeping. Locally, the name is generated using a combination of this
+    /// prefix, a hash of the AppHost project name. During publish mode, the name generated
+    /// is a combination of this prefix and  the name of the resource group associated with
+    /// the deployment. We want to keep the total number of characters in the name under
+    /// 24 characters to avoid truncation by Azure and allow
+    /// for unique enough identifiers.
+    /// </remarks>
+    internal const string DefaultAzureFunctionsHostStorageName = "funcstorage";
 
     /// <summary>
     /// Adds an Azure Functions project to the distributed application.
@@ -29,7 +38,7 @@ public static class AzureFunctionsProjectResourceExtensions
 
         // Add the default storage resource if it doesn't already exist.
         var storageResourceName = builder.CreateDefaultStorageName();
-        AzureStorageResource? storage = builder.Resources
+        var storage = builder.Resources
             .OfType<AzureStorageResource>()
             .FirstOrDefault(r => r.Name == storageResourceName);
 
@@ -198,6 +207,10 @@ public static class AzureFunctionsProjectResourceExtensions
 
     private static string CreateDefaultStorageName(this IDistributedApplicationBuilder builder)
     {
+        if (builder.ExecutionContext.IsPublishMode)
+        {
+            return DefaultAzureFunctionsHostStorageName;
+        }
         var applicationHash = builder.Configuration["AppHost:Sha256"]![..5].ToLowerInvariant();
         return $"{DefaultAzureFunctionsHostStorageName}{applicationHash}";
     }
--- tests/Aspire.Hosting.Azure.Tests/AzureFunctionsTests.cs
+++ tests/Aspire.Hosting.Azure.Tests/AzureFunctionsTests.cs
@@ -149,6 +149,17 @@ public class AzureFunctionsTests
         await host.StopAsync();
     }
 
+    [Fact]
+    public void AddAzureFunctionsProject_UsesCorrectNameUnderPublish()
+    {
+        using var builder = TestDistributedApplicationBuilder.Create(DistributedApplicationOperation.Publish);
+        builder.AddAzureFunctionsProject<TestProject>("funcapp");
+
+        var resource = Assert.Single(builder.Resources.OfType<AzureStorageResource>());
+
+        Assert.Equal(AzureFunctionsProjectResourceExtensions.DefaultAzureFunctionsHostStorageName, resource.Name);
+    }
+
     private sealed class TestProject : IProjectMetadata
     {
         public string ProjectPath => "some-path";
