2c38f6c884b5026a7f278cdfe6e1f816810093f9
Jose Perez Rodriguez <joperezr@microsoft.com>
2024-10-19 21:24:03 -0700

    [release/8.2] Update dependencies (#6377)

--- src/Aspire.Hosting.PostgreSQL/PostgresContainerImageTags.cs
+++ src/Aspire.Hosting.PostgreSQL/PostgresContainerImageTags.cs
@@ -7,11 +7,11 @@ internal static class PostgresContainerImageTags
 {
     public const string Registry = "docker.io";
     public const string Image = "library/postgres";
-    public const string Tag = "16.4";
+    public const string Tag = "17.0";
     public const string PgAdminRegistry = "docker.io";
     public const string PgAdminImage = "dpage/pgadmin4";
-    public const string PgAdminTag = "8.11";
+    public const string PgAdminTag = "8.12";
     public const string PgWebRegistry = "docker.io";
     public const string PgWebImage = "sosedoff/pgweb";
-    public const string PgWebTag = "0.15.0";
+    public const string PgWebTag = "0.16.1";
 }
