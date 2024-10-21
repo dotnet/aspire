4a3f64dbfb4db9724f27dcc268e7015487d3afa3
Adam Ratzman <adamratzman@microsoft.com>
2024-10-21 12:20:54 -0400

    re-enable additional test

--- tests/Aspire.Hosting.Tests/Health/ResourceHealthCheckServiceTests.cs
+++ tests/Aspire.Hosting.Tests/Health/ResourceHealthCheckServiceTests.cs
@@ -199,7 +199,7 @@ public class ResourceHealthCheckServiceTests(ITestOutputHelper testOutputHelper)
         await app.StopAsync();
     }
 
-    [Fact(Skip = "")]
+    [Fact()]
     public async Task PoorlyImplementedHealthChecksDontCauseMonitoringLoopToCrashout()
     {
         using var builder = TestDistributedApplicationBuilder.Create(testOutputHelper);
