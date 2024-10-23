32aaab1e6c8806034a1a310c6f1ce0bdd1580e48
Sebastien Ros <sebastienros@gmail.com>
2024-10-22 21:21:34 -0700

    PR feedbcak

--- tests/Aspire.Hosting.Python.Tests/AddPythonAppTests.cs
+++ tests/Aspire.Hosting.Python.Tests/AddPythonAppTests.cs
@@ -1,8 +1,6 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-#pragma warning disable ASPIREHOSTINGPYTHON001 // Test for experimental feature
-
 using Xunit;
 using Microsoft.Extensions.DependencyInjection;
 using Aspire.Hosting.Utils;
