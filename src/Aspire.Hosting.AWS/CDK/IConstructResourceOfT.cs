2e60ee60398402a51f15e17d1115587ed523510e
Eric Erhardt <eric.erhardt@microsoft.com>
2024-11-07 09:30:43 -0600

    Remove AWS specific code. (#6624)
    This code has transferred to https://github.com/aws/integrations-on-dotnet-aspire-for-aws/pull/1.

--- src/Aspire.Hosting.AWS/CDK/IConstructResourceOfT.cs
+++
@@ -1,9 +0,0 @@
-// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-
-using Constructs;
-
-namespace Aspire.Hosting.AWS.CDK;
-
-/// <inheritdoc cref="IConstructResource"/>
-public interface IConstructResource<out T> : IConstructResource, IResourceWithConstruct<T> where T : IConstruct;
