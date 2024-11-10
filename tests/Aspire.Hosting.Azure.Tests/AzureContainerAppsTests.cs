43a2b78468decb5b768748a31cd85ef0327ec7f4
David Fowler <davidfowl@gmail.com>
2024-11-10 14:28:08 -0800

    Changed the order

--- tests/Aspire.Hosting.Azure.Tests/AzureContainerAppsTests.cs
+++ tests/Aspire.Hosting.Azure.Tests/AzureContainerAppsTests.cs
@@ -765,21 +765,21 @@ public class AzureContainerAppsTests(ITestOutputHelper output)
           scope: storage
         }
 
-        resource storage_StorageQueueDataContributor 'Microsoft.Authorization/roleAssignments@2022-04-01' = {
-          name: guid(storage.id, api_identity.id, subscriptionResourceId('Microsoft.Authorization/roleDefinitions', '974c5e8b-45b9-4653-ba55-5f855dd0fb88'))
-          properties: {
-            principalId: api_identity.properties.principalId
-            roleDefinitionId: subscriptionResourceId('Microsoft.Authorization/roleDefinitions', '974c5e8b-45b9-4653-ba55-5f855dd0fb88')
-            principalType: 'ServicePrincipal'
-          }
-          scope: storage
-        }
-
         resource storage_StorageTableDataContributor 'Microsoft.Authorization/roleAssignments@2022-04-01' = {
           name: guid(storage.id, api_identity.id, subscriptionResourceId('Microsoft.Authorization/roleDefinitions', '0a9a7e1f-b9d0-4cc4-a60d-0319b160aaa3'))
           properties: {
             principalId: api_identity.properties.principalId
             roleDefinitionId: subscriptionResourceId('Microsoft.Authorization/roleDefinitions', '0a9a7e1f-b9d0-4cc4-a60d-0319b160aaa3')
+            principalType: 'ServicePrincipal'
+          }
+          scope: storage
+        }
+
+        resource storage_StorageQueueDataContributor 'Microsoft.Authorization/roleAssignments@2022-04-01' = {
+          name: guid(storage.id, api_identity.id, subscriptionResourceId('Microsoft.Authorization/roleDefinitions', '974c5e8b-45b9-4653-ba55-5f855dd0fb88'))
+          properties: {
+            principalId: api_identity.properties.principalId
+            roleDefinitionId: subscriptionResourceId('Microsoft.Authorization/roleDefinitions', '974c5e8b-45b9-4653-ba55-5f855dd0fb88')
             principalType: 'ServicePrincipal'
           }
           scope: storage
