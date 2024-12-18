fd1b038772e36b56570b313fa65499d79b01411d
Paul Jeschke <pjeschke@hmp-solutions.online>
2024-12-18 14:33:12 +0000

    improve dapr sample to be more complex and show what is possible with the new implementation
    Signed-off-by: Paul Jeschke <pjeschke@hmp-solutions.online>

--- playground/dapr/Dapr.AppHost/Program.cs
+++ playground/dapr/Dapr.AppHost/Program.cs
@@ -1,3 +1,6 @@
+using Aspire.Hosting.Dapr;
+using Aspire.Hosting.Dapr.Models.ComponentSpec;
+
 var builder = DistributedApplication.CreateBuilder(args);
 
 var rmq = builder.AddRabbitMQ("rabbitMQ")
@@ -5,14 +8,40 @@ var rmq = builder.AddRabbitMQ("rabbitMQ")
                    .WithEndpoint("tcp", e => e.Port = 5672)
                    .WithEndpoint("management", e => e.Port = 15672);
 
-var stateStore = builder.AddDaprStateStore("statestore");
-var pubSub = builder.AddDaprPubSub("pubsub")
-              .WithReference(rmq);
+var secretStore = builder.AddDaprComponent("secrets", "secretstores.local.file", new(){
+   Configuration = new List<MetadataValue>
+       {
+              new MetadataDirectValue<string> { Name = "secretsFile", Value = "appsettings.json"},
+              new MetadataDirectValue<string> { Name = "nestedSeparator", Value = ":"},
+              new MetadataDirectValue<bool> { Name = "multiValued", Value = false}
+       }
+});
+
+var stateStore = builder.AddDaprStateStore("statestore", options: new DaprComponentOptions
+{
+       Configuration = new List<MetadataValue>
+              {
+                     new MetadataDirectValue<string> { Name = "actorStateStore", Value = "true" },
+                     new MetadataSecretValue { Name = "logginglevel", SecretKeyRef = new("Logging:LogLevel:Default")}
+              },
+       SecretStore = secretStore.Resource
+}
+);
+var pubSub = builder.AddDaprPubSub("pubsub", "rabbitmq", options: new DaprComponentOptions
+{
+       Configuration = new List<MetadataValue>
+       {
+              new MetadataDirectValue<int> { Name = "reconnectWait", Value = 2 },
+       }
+})
+       .WaitFor(rmq)
+       .WithReference(rmq);
 
 builder.AddProject<Projects.DaprServiceA>("servicea")
        .WithDaprSidecar()
        .WithReference(stateStore)
-       .WithReference(pubSub);
+       .WithReference(pubSub)
+       .WithReference(secretStore);
 
 builder.AddProject<Projects.DaprServiceB>("serviceb")
        .WithDaprSidecar()
