831ede3a5b4ee4ecafcb22887ac92cd583597f99
Eric Erhardt <eric.erhardt@microsoft.com>
2025-01-10 17:22:41 -0600

    Disable Azure ServiceBus emulator functional tests (#7067)
    The emulator isn't reliable / consistent in CI. Disabling for now.
    
    See #7066

--- playground/AzureFunctionsEndToEnd/AzureFunctionsEndToEnd.AppHost/Program.cs
+++ playground/AzureFunctionsEndToEnd/AzureFunctionsEndToEnd.AppHost/Program.cs
@@ -4,18 +4,24 @@ var storage = builder.AddAzureStorage("storage").RunAsEmulator();
 var queue = storage.AddQueues("queue");
 var blob = storage.AddBlobs("blob");
 var eventHubs = builder.AddAzureEventHubs("eventhubs").RunAsEmulator().WithHub("myhub");
+#if !SKIP_UNSTABLE_EMULATORS
 var serviceBus = builder.AddAzureServiceBus("messaging").RunAsEmulator().WithQueue("myqueue");
+#endif
 
 var funcApp = builder.AddAzureFunctionsProject<Projects.AzureFunctionsEndToEnd_Functions>("funcapp")
     .WithExternalHttpEndpoints()
     .WithReference(eventHubs).WaitFor(eventHubs)
+#if !SKIP_UNSTABLE_EMULATORS
     .WithReference(serviceBus).WaitFor(serviceBus)
+#endif
     .WithReference(blob)
     .WithReference(queue);
 
 builder.AddProject<Projects.AzureFunctionsEndToEnd_ApiService>("apiservice")
     .WithReference(eventHubs).WaitFor(eventHubs)
+#if !SKIP_UNSTABLE_EMULATORS
     .WithReference(serviceBus).WaitFor(serviceBus)
+#endif
     .WithReference(queue)
     .WithReference(blob)
     .WithReference(funcApp);
