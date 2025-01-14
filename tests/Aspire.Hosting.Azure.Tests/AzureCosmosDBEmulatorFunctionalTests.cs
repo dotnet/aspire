8dd206e476ece1f0721fd4d32d163692d4f6d068
Sebastien Ros <sebastienros@gmail.com>
2025-01-13 17:13:45 -0800

    Fix build

--- tests/Aspire.Hosting.Azure.Tests/AzureCosmosDBEmulatorFunctionalTests.cs
+++ tests/Aspire.Hosting.Azure.Tests/AzureCosmosDBEmulatorFunctionalTests.cs
@@ -1,19 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System;
-using System.Diagnostics;
-using System.IO;
 using System.Net;
-using System.Text;
-using Microsoft.Azure.Cosmos.Diagnostics;
-using Microsoft.Azure.Cosmos.Linq;
-using Microsoft.Azure.Cosmos.Query.Core.Metrics;
-using Microsoft.Azure.Cosmos.Query.Core.QueryAdvisor;
-using Microsoft.Azure.Cosmos.Resource.CosmosExceptions;
-using Microsoft.Azure.Cosmos.Tracing;
-using Microsoft.Azure.Documents;
-
 using Aspire.Components.Common.Tests;
 using Aspire.Hosting.ApplicationModel;
 using Aspire.Hosting.Utils;
