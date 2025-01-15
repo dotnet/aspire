dcfdc643cf50f68bdde3411d01df6a2d1565a6f8
James Newton-King <james@newtonking.com>
2025-01-15 12:16:40 +0800

    More fixes

--- src/Aspire.Dashboard/Components/Controls/Chart/ChartFilters.razor.cs
+++ src/Aspire.Dashboard/Components/Controls/Chart/ChartFilters.razor.cs
@@ -3,9 +3,7 @@
 
 using Aspire.Dashboard.Model;
 using Aspire.Dashboard.Otlp.Model;
-using Aspire.Dashboard.Utils;
 using Microsoft.AspNetCore.Components;
-using Microsoft.FluentUI.AspNetCore.Components;
 
 namespace Aspire.Dashboard.Components;
 
@@ -20,15 +18,10 @@ public partial class ChartFilters
     [Parameter, EditorRequired]
     public required List<DimensionFilterViewModel> DimensionFilters { get; set; }
 
-    private ColumnResizeLabels _resizeLabels = ColumnResizeLabels.Default;
-    private ColumnSortLabels _sortLabels = ColumnSortLabels.Default;
-
     public bool ShowCounts { get; set; }
 
     protected override void OnInitialized()
     {
-        (_resizeLabels, _sortLabels) = DashboardUIHelpers.CreateGridLabels(Loc);
-
         InstrumentViewModel.DataUpdateSubscriptions.Add(() =>
         {
             ShowCounts = InstrumentViewModel.ShowCount;
