d5321f9e621ba55004fdcd2dc55dea8df5eff6e5
Brett Smith <brett@pukekos.co.nz>
2025-01-17 08:48:18 +1300

    Make UseAccessKeyAuthentication public so that it is accessible for package extensibility

--- src/Aspire.Hosting.Azure.Redis/AzureRedisCacheResource.cs
+++ src/Aspire.Hosting.Azure.Redis/AzureRedisCacheResource.cs
@@ -29,8 +29,11 @@ public class AzureRedisCacheResource(string name, Action<AzureResourceInfrastruc
     /// </summary>
     internal BicepSecretOutputReference? ConnectionStringSecretOutput { get; set; }
 
+    /// <summary>
+    /// Whether the resource uses access key authentication.
+    /// </summary>
     [MemberNotNullWhen(true, nameof(ConnectionStringSecretOutput))]
-    internal bool UseAccessKeyAuthentication => ConnectionStringSecretOutput is not null;
+    public bool UseAccessKeyAuthentication => ConnectionStringSecretOutput is not null;
 
     /// <summary>
     /// Gets the inner Redis resource.
