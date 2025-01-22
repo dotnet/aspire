9df53ebe15d4058ca5f56128162077c3d98e65f1
chuongnguyen <chuongnguyen@microsoft.com>
2025-01-22 10:53:43 +0700

    refactor code

--- playground/signalr/SignalR.AppHost/Program.cs
+++ playground/signalr/SignalR.AppHost/Program.cs
@@ -1,7 +1,3 @@
-using Aspire.Hosting.Azure;
-using Azure.Provisioning;
-using Azure.Provisioning.SignalR;
-
 var builder = DistributedApplication.CreateBuilder(args);
 
 // Configure Azure SignalR in default mode
@@ -13,20 +9,7 @@ builder.AddProject<Projects.SignalRWeb>("webfrontend")
 
 // Configure Azure SignalR in serverless mode
 var serverlessSignalr = builder
-    .AddAzureSignalR("signalrServerless")
-    .ConfigureInfrastructure(infra =>
-    {
-        var resource = infra.GetProvisionableResources().OfType<SignalRService>().First(s => s.BicepIdentifier == infra.AspireResource.GetBicepIdentifier());
-        resource.Features.Add(new SignalRFeature()
-        {
-            Flag = SignalRFeatureFlag.ServiceMode,
-            Value = "Serverless"
-        });
-        // Add required role assignment to invoke REST API for serverless mode
-        var principalTypeParameter = infra.GetProvisionableResources().OfType<ProvisioningParameter>().First(o => o.BicepIdentifier == AzureBicepResource.KnownParameters.PrincipalType);
-        var principalIdTypeParameter = infra.GetProvisionableResources().OfType<ProvisioningParameter>().First(o => o.BicepIdentifier == AzureBicepResource.KnownParameters.PrincipalId);
-        infra.Add(resource.CreateRoleAssignment(SignalRBuiltInRole.SignalRRestApiOwner, principalTypeParameter, principalIdTypeParameter));
-    })
+    .AddAzureSignalR("signalrServerless", AzureSignalRServiceMode.Serverless)
     .RunAsEmulator();
 
 builder.AddProject<Projects.SignalRServerlessWeb>("webserverless")
