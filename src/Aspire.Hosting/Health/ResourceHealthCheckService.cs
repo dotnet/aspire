2382636b5be2589d7e0426514429caebca072cbc
David Fowler <davidfowl@gmail.com>
2025-01-24 16:17:06 -0800

    Apply suggestions from code review

--- src/Aspire.Hosting/Health/ResourceHealthCheckService.cs
+++ src/Aspire.Hosting/Health/ResourceHealthCheckService.cs
@@ -25,7 +25,6 @@ internal class ResourceHealthCheckService(ILogger<ResourceHealthCheckService> lo
         public void StopResourceMonitor()
         {
             _cts.Cancel();
-            _cts.Dispose();
         }
     }
 
