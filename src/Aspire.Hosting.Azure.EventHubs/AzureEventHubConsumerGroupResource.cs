7a06ce7a33c3af6829c0f32c4789f5ffb51586e7
Eric Erhardt <eric.erhardt@microsoft.com>
2025-02-06 12:19:45 -0600

    Convert AzureEventHub and ConsumerGroup to Resources
    This allows them to be referenceable and waitable in the future.
    
    Also rename the classes and put them in the appropriate namespace.
    
    Contributes to #7407

--- src/Aspire.Hosting.Azure.EventHubs/AzureEventHubConsumerGroupResource.cs
+++ src/Aspire.Hosting.Azure.EventHubs/AzureEventHubConsumerGroupResource.cs
@@ -2,30 +2,42 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.Text.Json;
+using Aspire.Hosting.ApplicationModel;
 using Azure.Provisioning;
 
 namespace Aspire.Hosting.Azure;
 
 /// <summary>
-/// Represents a Consumer Group.
+/// Represents an Azure Event Hub Consumer Group.
 /// </summary>
 /// <remarks>
 /// Use <see cref="AzureProvisioningResourceExtensions.ConfigureInfrastructure{T}(ApplicationModel.IResourceBuilder{T}, Action{AzureResourceInfrastructure})"/> to configure specific <see cref="Azure.Provisioning"/> properties.
 /// </remarks>
-public class AzureEventHubConsumerGroupResource
+public class AzureEventHubConsumerGroupResource : Resource, IResourceWithParent<AzureEventHubResource>, IResourceWithConnectionString
 {
     /// <summary>
     /// Initializes a new instance of the <see cref="AzureEventHubConsumerGroupResource"/> class.
     /// </summary>
-    public AzureEventHubConsumerGroupResource(string name)
+    public AzureEventHubConsumerGroupResource(string name, string consumerGroupName, AzureEventHubResource parent) : base(name)
     {
-        Name = name;
+        ConsumerGroupName = consumerGroupName;
+        Parent = parent;
     }
 
     /// <summary>
-    /// The event hub name.
+    /// The event hub consumer group name.
     /// </summary>
-    public string Name { get; set; }
+    public string ConsumerGroupName { get; set; }
+
+    /// <summary>
+    /// Gets the parent Azure Event Hub resource.
+    /// </summary>
+    public AzureEventHubResource Parent { get; }
+
+    /// <summary>
+    /// Gets the connection string expression for the Azure Event Hub.
+    /// </summary>
+    public ReferenceExpression ConnectionStringExpression => Parent.ConnectionStringExpression;
 
     /// <summary>
     /// Converts the current instance to a provisioning entity.
@@ -35,7 +47,7 @@ public class AzureEventHubConsumerGroupResource
     {
         var consumerGroup = new global::Azure.Provisioning.EventHubs.EventHubsConsumerGroup(Infrastructure.NormalizeBicepIdentifier(Name));
 
-        consumerGroup.Name = Name;
+        consumerGroup.Name = ConsumerGroupName;
 
         return consumerGroup;
     }
@@ -46,6 +58,6 @@ public class AzureEventHubConsumerGroupResource
     /// <param name="writer">The Utf8JsonWriter to write the JSON object to.</param>
     internal void WriteJsonObjectProperties(Utf8JsonWriter writer)
     {
-        writer.WriteString(nameof(Name), Name);
+        writer.WriteString(nameof(Name), ConsumerGroupName);
     }
 }
