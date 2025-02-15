2682cdeb9efe913f01a904a00f34d688ae35d5f9
ZLoo <Kozlov.s.v.1992@gmail.com>
2025-02-15 21:11:07 +0300

    add azure aI open aI public api tests

--- src/Components/Aspire.OpenAI/AspireOpenAIClientBuilder.cs
+++ src/Components/Aspire.OpenAI/AspireOpenAIClientBuilder.cs
@@ -1,8 +1,10 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using Microsoft.Extensions.Configuration;
 using System.Data.Common;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using OpenAI;
 
@@ -10,46 +12,36 @@ namespace Aspire.OpenAI;
 
 /// <summary>
 /// A builder for configuring an <see cref="OpenAIClient"/> service registration.
+/// Constructs a new instance of <see cref="AspireOpenAIClientBuilder"/>.
 /// </summary>
-public class AspireOpenAIClientBuilder
+/// <param name="hostBuilder">The <see cref="IHostApplicationBuilder"/> with which services are being registered.</param>
+/// <param name="connectionName">The name used to retrieve the connection string from the ConnectionStrings configuration section.</param>
+/// <param name="serviceKey">The service key used to register the <see cref="OpenAIClient"/> service, if any.</param>
+/// <param name="disableTracing">A flag to indicate whether tracing should be disabled.</param>
+public class AspireOpenAIClientBuilder(IHostApplicationBuilder hostBuilder, string connectionName, string? serviceKey, bool disableTracing)
 {
     private const string DeploymentKey = "Deployment";
     private const string ModelKey = "Model";
 
     /// <summary>
-    /// Constructs a new instance of <see cref="AspireOpenAIClientBuilder"/>.
-    /// </summary>
-    /// <param name="hostBuilder">The <see cref="IHostApplicationBuilder"/> with which services are being registered.</param>
-    /// <param name="connectionName">The name used to retrieve the connection string from the ConnectionStrings configuration section.</param>
-    /// <param name="serviceKey">The service key used to register the <see cref="OpenAIClient"/> service, if any.</param>
-    /// <param name="disableTracing">A flag to indicate whether tracing should be disabled.</param>
-    public AspireOpenAIClientBuilder(IHostApplicationBuilder hostBuilder, string connectionName, string? serviceKey, bool disableTracing)
-    {
-        HostBuilder = hostBuilder;
-        ConnectionName = connectionName;
-        ServiceKey = serviceKey;
-        DisableTracing = disableTracing;
-    }
-
-    /// <summary>
     /// Gets the <see cref="IHostApplicationBuilder"/> with which services are being registered.
     /// </summary>
-    public IHostApplicationBuilder HostBuilder { get; }
+    public IHostApplicationBuilder HostBuilder { get; } = hostBuilder ?? throw new ArgumentNullException(nameof(hostBuilder));
 
     /// <summary>
     /// Gets the name used to retrieve the connection string from the ConnectionStrings configuration section.
     /// </summary>
-    public string ConnectionName { get; }
+    public string ConnectionName { get; } = ThrowIfNullOrEmpty(connectionName);
 
     /// <summary>
     /// Gets the service key used to register the <see cref="OpenAIClient"/> service, if any.
     /// </summary>
-    public string? ServiceKey { get; }
+    public string? ServiceKey { get; } = serviceKey;
 
     /// <summary>
     /// Gets a flag indicating whether tracing should be disabled.
     /// </summary>
-    public bool DisableTracing { get; }
+    public bool DisableTracing { get; } = disableTracing;
 
     /// <summary>
     /// Gets the name of the configuration section for this component type.
@@ -95,4 +87,10 @@ public class AspireOpenAIClientBuilder
 
     private static string? ConnectionStringValue(DbConnectionStringBuilder connectionString, string key)
         => connectionString.TryGetValue(key, out var value) ? value as string : null;
+
+    private static string ThrowIfNullOrEmpty([NotNull] string? argument, [CallerArgumentExpression(nameof(argument))] string? paramName = null)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(argument, paramName);
+        return argument;
+    }
 }
