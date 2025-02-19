99616332b38754029201f55aab5fa1e6b29596de
Sebastien Ros <sebastienros@gmail.com>
2025-02-18 16:47:15 -0800

    Don't skip event hub health checks

--- tests/Aspire.Azure.Messaging.EventHubs.Tests/ConformanceTestsBase.cs
+++ tests/Aspire.Azure.Messaging.EventHubs.Tests/ConformanceTestsBase.cs
@@ -70,8 +70,6 @@ public abstract class ConformanceTestsBase<TService, TOptions> : ConformanceTest
     [ConditionalFact]
     public void HealthChecksClientsAreReused()
     {
-        SkipIfHealthChecksAreNotSupported();
-
         // DisableRetries so the test doesn't take so long retrying when the server isn't available.
         using IHost host = CreateHostWithComponent(configureComponent: DisableRetries);
 
