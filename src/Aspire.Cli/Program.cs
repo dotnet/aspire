79e10aef0543c70ee12bdd0dddb0c254138f71f6
Mitch Denny <midenn@microsoft.com>
2025-02-27 04:12:15 +0000

    Some boilerplate test code.

--- src/Aspire.Cli/Program.cs
+++ src/Aspire.Cli/Program.cs
@@ -3,7 +3,15 @@
 
 using System.CommandLine;
 
-var rootCommand = new RootCommand(".NET Aspire CLI");
-var result = rootCommand.Parse(args);
-var exitCode = await result.InvokeAsync().ConfigureAwait(false);
-return exitCode;
+namespace Aspire.Cli;
+
+public class Program
+{
+    public static async Task<int> Main(string[] args)
+    {
+        var rootCommand = new RootCommand(".NET Aspire CLI");
+        var result = rootCommand.Parse(args);
+        var exitCode = await result.InvokeAsync().ConfigureAwait(false);
+        return exitCode;
+    }
+}
---
+++ tests/Aspire.Cli.Tests/CliSmokeTests.cs
@@ -0,0 +1,16 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Xunit;
+
+namespace Aspire.Cli.Tests;
+
+public class CliSmokeTests
+{
+    [Fact]
+    public async Task NoArgsReturnsZeroExitCode()
+    {
+        var exitCode = await Aspire.Cli.Program.Main([]);
+        Assert.Equal(0, exitCode);
+    }
+}
