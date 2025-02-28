025d5f65f22aa23ad9b75723ad8b8b1c6ffc481c
Aaron Powell <me@aaron-powell.com>
2025-02-28 11:34:17 +1100

    First pass on creating a client integration for Azure.AI.Projects
    This allows you to integrate with Azure AI Agent Service, and other services available via the SDK.
    
    Fixes #7768

---
+++ playground/AzureAIProjects/AIProjects.Web/Program.cs
@@ -0,0 +1,35 @@
+using AIProjects.Web.Components;
+using Azure.Identity;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.AddServiceDefaults();
+
+builder.AddAzureAIProjectClient("aiprojects", settings =>
+{
+    settings.Credential = new AzureCliCredential();
+});
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
---
+++ src/Components/Aspire.Azure.AI.Projects/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Aspire;
+using Aspire.Azure.AI.Projects;
+using Azure.AI.Projects;
+
+[assembly: ConfigurationSchema("Aspire:Azure:AI:Projects", typeof(AzureAIProjectSettings))]
+[assembly: ConfigurationSchema("Aspire:Azure:AI:Projects:AIProjectClientOptions", typeof(AIProjectClientOptions))]
+
+[assembly: LoggingCategories(
+    "Azure",
+    "Azure.Core",
+    "Azure.Identity"
+)]
