3211e52fd367282282663ff5e0e7d69cad5031b6
Mitch Denny <midenn@microsoft.com>
2025-03-04 17:21:48 +1100

    Use RemoteExecutor for fake CLI process. (#7875)
    * Fix Windows StubProcess implementation.
    
    * Move over to remote executor.

--- tests/Aspire.Cli.Tests/CliSmokeTests.cs
+++ tests/Aspire.Cli.Tests/CliSmokeTests.cs
@@ -1,7 +1,6 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using Aspire.Cli.Tests.Helpers;
 using Xunit;
 
 namespace Aspire.Cli.Tests;
@@ -13,15 +12,5 @@ public class CliSmokeTests
     {
         var exitCode = await Aspire.Cli.Program.Main([]);
         Assert.Equal(ExitCodeConstants.InvalidCommand, exitCode);
-    }
-
-    [Fact]
-    public void StubProcessWorks()
-    {
-        // This is just an early signal to make sure the executables selected
-        // for the stub processes work.
-        using var stubProcess = StubProcess.Create();
-        var exited = stubProcess.Process.WaitForExit(1000);
-        Assert.False(exited);
     }
 }
