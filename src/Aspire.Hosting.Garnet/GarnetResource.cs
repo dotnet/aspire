316067d0b5fd489e319f22772adfb4d066689779
Alireza Baloochi <alireza.baloochi1380@gmail.com>
2025-03-04 20:00:16 +0330

    Add Password for Garnet (#7618)
    * Add Password for Garnet
    
    * Update tests
    
    * Update playground manifest
    
    * Fix up merge
    
    * PR feedback
    
    1. Don't use special characters until azd gets fixed
    2. Refactor code to be simplified
    
    ---------
    
    Co-authored-by: Eric Erhardt <eric.erhardt@microsoft.com>

--- src/Aspire.Hosting.Garnet/GarnetResource.cs
+++ src/Aspire.Hosting.Garnet/GarnetResource.cs
@@ -9,6 +9,16 @@ namespace Aspire.Hosting.ApplicationModel;
 /// <param name="name">The name of the resource.</param>
 public class GarnetResource(string name) : ContainerResource(name), IResourceWithConnectionString
 {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="GarnetResource"/> class.
+    /// </summary>
+    /// <param name="name">The name of the resource.</param>
+    /// <param name="password">A parameter that contains the Garnet server password.</param>
+    public GarnetResource(string name, ParameterResource password) : this(name)
+    {
+        PasswordParameter = password;
+    }
+
     internal const string PrimaryEndpointName = "tcp";
 
     private EndpointReference? _primaryEndpoint;
@@ -19,8 +29,36 @@ public class GarnetResource(string name) : ContainerResource(name), IResourceWit
     public EndpointReference PrimaryEndpoint => _primaryEndpoint ??= new(this, PrimaryEndpointName);
 
     /// <summary>
+    /// Gets the parameter that contains the Garnet server password.
+    /// </summary>
+    public ParameterResource? PasswordParameter { get; }
+
+    /// <summary>
     /// Gets the connection string expression for the Garnet server.
     /// </summary>
-    public ReferenceExpression ConnectionStringExpression =>
-        ReferenceExpression.Create($"{PrimaryEndpoint.Property(EndpointProperty.HostAndPort)}");
+    public ReferenceExpression ConnectionStringExpression
+    {
+        get
+        {
+            if (this.TryGetLastAnnotation<ConnectionStringRedirectAnnotation>(out var connectionStringAnnotation))
+            {
+                return connectionStringAnnotation.Resource.ConnectionStringExpression;
+            }
+
+            return BuildConnectionString();
+        }
+    }
+
+    private ReferenceExpression BuildConnectionString()
+    {
+        var builder = new ReferenceExpressionBuilder();
+        builder.Append($"{PrimaryEndpoint.Property(EndpointProperty.HostAndPort)}");
+
+        if (PasswordParameter is not null)
+        {
+            builder.Append($",password={PasswordParameter}");
+        }
+
+        return builder.Build();
+    }
 }
