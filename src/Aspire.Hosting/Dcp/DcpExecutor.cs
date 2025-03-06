6fd8b635041b49ac12353b3c5cd4329a210642e5
James Newton-King <james@newtonking.com>
2025-03-06 09:25:11 +0800

    Test

--- src/Aspire.Hosting/Dcp/DcpExecutor.cs
+++ src/Aspire.Hosting/Dcp/DcpExecutor.cs
@@ -1147,10 +1147,6 @@ internal sealed class DcpExecutor : IDcpExecutor, IAsyncDisposable
 
                 try
                 {
-                    // Publish snapshot built from DCP resource. Do this now to populate more values from DCP (URLs, source) to ensure they're
-                    // available if the resource isn't immediately started because it's waiting or is configured for explicit start.
-                    await _executorEvents.PublishAsync(new OnResourceChangedContext(_shutdownCancellation.Token, KnownResourceTypes.Container, cr.ModelResource, cr.DcpResourceName, new ResourceStatus(null, null, null), s => _snapshotBuilder.ToSnapshot((Container)cr.DcpResource, s))).ConfigureAwait(false);
-
                     await CreateContainerAsync(cr, logger, cancellationToken).ConfigureAwait(false);
                 }
                 catch (FailedToApplyEnvironmentException)
@@ -1178,6 +1174,10 @@ internal sealed class DcpExecutor : IDcpExecutor, IAsyncDisposable
 
             foreach (var cr in containerResources)
             {
+                // Publish snapshot built from DCP resource. Do this now to populate more values from DCP (URLs, source) to ensure they're
+                // available if the resource isn't immediately started because it's waiting or is configured for explicit start.
+                await _executorEvents.PublishAsync(new OnResourceChangedContext(_shutdownCancellation.Token, KnownResourceTypes.Container, cr.ModelResource, cr.DcpResourceName, new ResourceStatus(null, null, null), s => _snapshotBuilder.ToSnapshot((Container)cr.DcpResource, s))).ConfigureAwait(false);
+
                 if (cr.ModelResource.TryGetLastAnnotation<ExplicitStartupAnnotation>(out _))
                 {
                     await _executorEvents.PublishAsync(new OnResourceChangedContext(cancellationToken, KnownResourceTypes.Container, cr.ModelResource, cr.DcpResourceName, new ResourceStatus(KnownResourceStates.NotStarted, null, null), s => s with { State = new ResourceStateSnapshot(KnownResourceStates.NotStarted, null) })).ConfigureAwait(false);
