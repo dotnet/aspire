c4e4092a0b49b3a27783c8a224dce612cf91c446
Safia Abdalla <safia@microsoft.com>
2025-03-07 21:17:29 -0800

    Add Functions host support to AzureTableStorageResource (#7961)

--- src/Aspire.Hosting.Azure.Storage/AzureTableStorageResource.cs
+++ src/Aspire.Hosting.Azure.Storage/AzureTableStorageResource.cs
@@ -11,7 +11,7 @@ namespace Aspire.Hosting.Azure;
 /// <param name="name">The name of the resource.</param>
 /// <param name="storage">The <see cref="AzureStorageResource"/> that the resource is stored in.</param>
 public class AzureTableStorageResource(string name, AzureStorageResource storage)
-    : Resource(name), IResourceWithConnectionString, IResourceWithParent<AzureStorageResource>
+    : Resource(name), IResourceWithConnectionString, IResourceWithParent<AzureStorageResource>, IResourceWithAzureFunctionsConfig
 {
     /// <summary>
     /// Gets the parent AzureStorageResource of this AzureTableStorageResource.
@@ -23,4 +23,21 @@ public class AzureTableStorageResource(string name, AzureStorageResource storage
     /// </summary>
     public ReferenceExpression ConnectionStringExpression =>
         Parent.GetTableConnectionString();
+
+    void IResourceWithAzureFunctionsConfig.ApplyAzureFunctionsConfiguration(IDictionary<string, object> target, string connectionName)
+    {
+        if (Parent.IsEmulator)
+        {
+            var connectionString = Parent.GetEmulatorConnectionString();
+            target[connectionName] = connectionString;
+            target[$"{AzureStorageResource.TablesConnectionKeyPrefix}__{connectionName}__ConnectionString"] = connectionString;
+        }
+        else
+        {
+            // Injected to support Azure Functions listener.
+            target[$"{connectionName}__tableServiceUri"] = Parent.TableEndpoint;
+            // Injected to support Aspire client integration for Azure Storage Tables.
+            target[$"{AzureStorageResource.TablesConnectionKeyPrefix}__{connectionName}__ServiceUri"] = Parent.TableEndpoint; // Updated for consistency
+        }
+    }
 }
