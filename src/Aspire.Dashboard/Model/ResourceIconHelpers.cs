401bd0380e1507ab7fc2db00d6469d5bb95792a4
glennc <glennc@microsoft.com>
2025-03-10 22:40:25 -0700

    Fix name so that colors of the graph and the grid match.

--- src/Aspire.Dashboard/Model/ResourceIconHelpers.cs
+++ src/Aspire.Dashboard/Model/ResourceIconHelpers.cs
@@ -3,7 +3,6 @@
 
 namespace Aspire.Dashboard.Model;
 
-using Aspire.Dashboard.Otlp.Model;
 using Microsoft.FluentUI.AspNetCore.Components;
 using Icons = Microsoft.FluentUI.AspNetCore.Components.Icons;
 
@@ -14,14 +13,12 @@ internal static class ResourceIconHelpers
     /// </summary>
     public static Icon GetIconForResource(ResourceViewModel resource)
     {
-        Icon icon = resource.ResourceType switch
+        return resource.ResourceType switch
         {
             KnownResourceTypes.Executable => new Icons.Filled.Size24.Database(),
             KnownResourceTypes.Project => new Icons.Filled.Size24.CodeCircle(),
             KnownResourceTypes.Container => new Icons.Filled.Size24.Box(),
             string t => t.Contains("database", StringComparison.OrdinalIgnoreCase) ? new Icons.Filled.Size24.Database() : new Icons.Filled.Size24.SettingsCogMultiple(),
         };
-        icon = icon.WithColor(ColorGenerator.Instance.GetColorHexByKey(resource.Name));
-        return icon;
     }  
 }
