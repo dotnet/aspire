dc62e82c94d4f3cae22de815a095c1d8e2995c99
Ankit Jain <radical@gmail.com>
2025-03-11 00:31:56 -0400

    split tests to allow running in parallel

--- tests/Aspire.Workload.Tests/StarterTemplateProjectNamesTests.cs
+++ tests/Aspire.Workload.Tests/StarterTemplateProjectNamesTests.cs
@@ -1,36 +1,35 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using Aspire.Components.Common.Tests;
 using Xunit;
 using Xunit.Abstractions;
 
 namespace Aspire.Workload.Tests;
 
-public class StarterTemplateProjectNamesTests : WorkloadTestsBase
+public abstract class StarterTemplateProjectNamesTests : WorkloadTestsBase
 {
-    public StarterTemplateProjectNamesTests(ITestOutputHelper testOutput)
+    private readonly string _testType;
+    public StarterTemplateProjectNamesTests(string testType, ITestOutputHelper testOutput)
         : base(testOutput)
     {
+        _testType = testType;
     }
 
-    public static TheoryData<string, string> ProjectNamesWithTestType_TestData()
+    public static IEnumerable<object[]> ProjectNamesWithTestType_TestData()
     {
-        var data = new TheoryData<string, string>();
-        foreach (var testType in TestFrameworkTypes)
+        foreach (var name in GetProjectNamesForTest())
         {
-            foreach (var name in GetProjectNamesForTest())
-            {
-                data.Add(name, testType);
-            }
+            yield return [name];
         }
-        return data;
     }
 
     [Theory]
     [MemberData(nameof(ProjectNamesWithTestType_TestData))]
-    public async Task StarterTemplateWithTest_ProjectNames(string prefix, string testType)
+    [RequiresSSLCertificate("Needs dashboard, web front end access")]
+    public async Task StarterTemplateWithTest_ProjectNames(string prefix)
     {
-        string id = $"{prefix}-{testType}";
+        string id = $"{prefix}-{_testType}";
         string config = "Debug";
 
         await using var project = await AspireProject.CreateNewTemplateProjectAsync(
@@ -38,13 +37,42 @@ public class StarterTemplateProjectNamesTests : WorkloadTestsBase
             "aspire-starter",
             _testOutput,
             BuildEnvironment.ForDefaultFramework,
-            $"-t {testType}");
+            $"-t {_testType}");
 
         await using var context = PlaywrightProvider.HasPlaywrightSupport ? await CreateNewBrowserContextAsync() : null;
         _testOutput.WriteLine($"Checking the starter template project");
         await AssertStarterTemplateRunAsync(context, project, config, _testOutput);
 
         _testOutput.WriteLine($"Checking the starter template project tests");
-        await AssertTestProjectRunAsync(project.TestsProjectDirectory, testType, _testOutput, config);
+        await AssertTestProjectRunAsync(project.TestsProjectDirectory, _testType, _testOutput, config);
+    }
+}
+
+// Individual class for each test framework so the tests can run in separate helix jobs
+public class None_StarterTemplateProjectNamesTests : StarterTemplateProjectNamesTests
+{
+    public None_StarterTemplateProjectNamesTests(ITestOutputHelper testOutput) : base("none", testOutput)
+    {
+    }
+}
+
+public class MSTest_StarterTemplateProjectNamesTests : StarterTemplateProjectNamesTests
+{
+    public MSTest_StarterTemplateProjectNamesTests(ITestOutputHelper testOutput) : base("mstest", testOutput)
+    {
+    }
+}
+
+public class Xunit_StarterTemplateProjectNamesTests : StarterTemplateProjectNamesTests
+{
+    public Xunit_StarterTemplateProjectNamesTests(ITestOutputHelper testOutput) : base("xunit.net", testOutput)
+    {
+    }
+}
+
+public class Nunit_StarterTemplateProjectNamesTests : StarterTemplateProjectNamesTests
+{
+    public Nunit_StarterTemplateProjectNamesTests(ITestOutputHelper testOutput) : base("nunit", testOutput)
+    {
     }
 }
