79f13aec0e6ba5841d8f6b5ab0fcf6e19bee785f
Eric Erhardt <eric.erhardt@microsoft.com>
2025-03-11 09:42:54 -0500

    Revert "Backchannel between CLI and AppHost (#7991)" (#7999)
    This reverts commit 811341ddf033bbda9c3d07fa1390bcfa25cf70cf.

--- src/Aspire.Cli/Program.cs
+++ src/Aspire.Cli/Program.cs
@@ -24,7 +24,6 @@ public class Program
 
         builder.Services.AddTransient<AppHostRunner>();
         builder.Services.AddTransient<DotNetCliRunner>();
-        builder.Services.AddSingleton<CliRpcTarget>();
         var app = builder.Build();
         return app;
     }
