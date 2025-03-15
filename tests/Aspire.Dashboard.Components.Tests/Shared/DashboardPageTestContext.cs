690ebe98a6f595c3d04eb1b303d5d148a78cfa32
Dan Moseley <danmose@microsoft.com>
2025-03-15 15:03:59 -0600

    nit

--- tests/Aspire.Dashboard.Components.Tests/Shared/DashboardPageTestContext.cs
+++ tests/Aspire.Dashboard.Components.Tests/Shared/DashboardPageTestContext.cs
@@ -2,7 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using Bunit;
-using Xunit.Abstractions;
 
 namespace Aspire.Dashboard.Components.Tests.Shared;
 public abstract class DashboardTestContext : TestContext
