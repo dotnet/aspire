6320ed50ae86eafe46427c9fafa4130d3d9ebad1
James Newton-King <james@newtonking.com>
2025-03-17 14:22:45 +0800

    Fix tests

--- tests/Aspire.Dashboard.Tests/Telemetry/ComponentTelemetryContextTests.cs
+++ tests/Aspire.Dashboard.Tests/Telemetry/ComponentTelemetryContextTests.cs
@@ -1,4 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using Aspire.Dashboard.Components.Pages;
@@ -6,12 +6,12 @@ using Aspire.Dashboard.Telemetry;
 using Microsoft.Extensions.Logging.Abstractions;
 using Xunit;
 
-namespace Aspire.Dashboard.Components.Tests;
+namespace Aspire.Dashboard.Tests.Telemetry;
 
 public class ComponentTelemetryContextTests
 {
     [Fact]
-    public async Task ComponentTelemetryContext_TelemetryEnabled_EndToEnd()
+    public async Task ComponuentTelemetryContext_TelemetryEnabled_EndToEnd()
     {
         var telemetryContext = new ComponentTelemetryContext(nameof(ComponentTelemetryContextTests));
         var telemetrySender = new TestDashboardTelemetrySender { IsTelemetryEnabled = true };
@@ -25,7 +25,7 @@ public class ComponentTelemetryContextTests
         }
 
         Assert.True(telemetrySender.ContextChannel.Reader.TryRead(out var initializeOperation));
-        Assert.Equal(TelemetryEventKeys.ComponentInitialize, initializeOperation.Name);
+        Assert.Equal("/telemetry/userTask - $aspire/dashboard/component/initialize", initializeOperation.Name);
 
         Assert.Single(initializeOperation.Properties);
         Assert.Single(telemetryContext.Properties);
@@ -35,7 +35,7 @@ public class ComponentTelemetryContextTests
         telemetryContext.UpdateTelemetryProperties([new ComponentTelemetryProperty("Test", new AspireTelemetryProperty("Value"))]);
         Assert.Equal(2, telemetryContext.Properties.Count);
         Assert.True(telemetrySender.ContextChannel.Reader.TryRead(out parametersUpdateOperation));
-        Assert.Equal(TelemetryEventKeys.ParametersSet, parametersUpdateOperation.Name);
+        Assert.Equal("/telemetry/operation - $aspire/dashboard/component/paramsSet", parametersUpdateOperation.Name);
         Assert.Single(parametersUpdateOperation.Properties);
 
         // If value didn't change, we shouldn't post again
@@ -49,7 +49,7 @@ public class ComponentTelemetryContextTests
 
         telemetryContext.Dispose();
         Assert.True(telemetrySender.ContextChannel.Reader.TryRead(out var disposeOperation));
-        Assert.Equal(TelemetryEventKeys.ComponentDispose, disposeOperation.Name);
+        Assert.Equal("/telemetry/operation - $aspire/dashboard/component/dispose", disposeOperation.Name);
     }
 
     [Fact]
