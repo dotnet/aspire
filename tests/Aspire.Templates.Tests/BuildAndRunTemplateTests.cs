1328f3b9d5981d2f887f1f6f3c3f23d485f16e7c
Ankit Jain <radical@gmail.com>
2025-03-19 22:34:40 -0400

    Extract the biggest test from BuildAndRunTemplateTests

--- tests/Aspire.Templates.Tests/BuildAndRunTemplateTests.cs
+++ tests/Aspire.Templates.Tests/BuildAndRunTemplateTests.cs
@@ -15,36 +15,6 @@ public partial class BuildAndRunTemplateTests : TemplateTestsBase
         : base(testOutput)
     {}
 
-    public static TheoryData<string, string> TestFrameworkTypeWithConfig()
-    {
-        var data = new TheoryData<string, string>();
-        foreach (var testType in TestFrameworkTypes)
-        {
-            data.Add("Debug", testType);
-            if (!PlatformDetection.IsRunningPRValidation)
-            {
-                data.Add("Release", testType);
-            }
-        }
-        return data;
-    }
-
-    [Theory]
-    [MemberData(nameof(TestFrameworkTypeWithConfig))]
-    [RequiresSSLCertificate]
-    public async Task BuildAndRunStarterTemplateBuiltInTest(string config, string testType)
-    {
-        string id = GetNewProjectId(prefix: $"starter test.{config}-{testType.Replace(".", "_")}");
-        await using var project = await AspireProject.CreateNewTemplateProjectAsync(
-                                            id,
-                                            "aspire-starter",
-                                            _testOutput,
-                                            buildEnvironment: BuildEnvironment.ForDefaultFramework,
-                                            extraArgs: $"-t {testType}");
-
-        await AssertTestProjectRunAsync(project.TestsProjectDirectory, testType, _testOutput, config);
-    }
-
     public static TheoryData<string> BuildConfigurationsForTestData()
     {
         var data = new TheoryData<string>() { "Debug" };
