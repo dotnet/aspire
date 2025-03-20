efd6566120f5ca3ac25cd48ce88b9c9dd6506ec9
Ankit Jain <radical@gmail.com>
2025-03-19 21:33:32 -0400

    Rename Workload tests to Template tests (#8158)
    * Rename Aspire.Workload.Tests to Aspire.Templates.Tests, and Shared/WorkloadTesting to Shared/TemplatesTesting
    * rename send-to-helix-workloadtests.targets to send-to-helix-templatestests.targets
    * Update existing references, and msbuild item/property names to reflect the renaming
    * Drop the package reference to `Microsoft.NET.Runtime.WorkloadTesting.Internal` which is not being used since no workload is installed
    * Update Aspire.sln
    * [CI] Avoid running unnecessary tests
    
    Co-authored-by: Eric Erhardt <eric.erhardt@microsoft.com>

---
+++ tests/Aspire.Templates.Tests/TestSdk.cs
@@ -0,0 +1,11 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace Aspire.Templates.Tests;
+
+public enum TestSdk
+{
+    Previous,
+    Current,
+    CurrentSdkAndPreviousRuntime
+}
