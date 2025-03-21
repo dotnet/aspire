b5752c261dcc363cd78980057ed72785a7119390
Eric Erhardt <eric.erhardt@microsoft.com>
2025-03-21 12:17:43 -0500

    Support Azure SqlServer role assignments on app specific managed identity (#8226)
    * Support Azure SqlServer role assignments on app specific managed identity
    
    Adding role assignment support for PostgreSQL following the pattern set in #8140
    
    Fix #6161
    
    * Update playground manifest

--- tests/Aspire.Hosting.Azure.Tests/RoleAssignmentTests.cs
+++ tests/Aspire.Hosting.Azure.Tests/RoleAssignmentTests.cs
@@ -389,6 +389,43 @@ public class RoleAssignmentTests(ITestOutputHelper output)
             includePrincipalName: true);
     }
 
+    [Fact]
+    public Task SqlSupport()
+    {
+        return RoleAssignmentTest("sql",
+            builder =>
+            {
+                var redis = builder.AddAzureSqlServer("sql");
+
+                builder.AddProject<Project>("api", launchProfileName: null)
+                    .WithReference(redis);
+            },
+            """
+            @description('The location for the resource(s) to be deployed.')
+            param location string = resourceGroup().location
+
+            param sql_outputs_name string
+
+            param principalId string
+
+            param principalName string
+
+            resource sql 'Microsoft.Sql/servers@2021-11-01' existing = {
+              name: sql_outputs_name
+            }
+            
+            resource sql_admin 'Microsoft.Sql/servers/administrators@2021-11-01' = {
+              name: 'ActiveDirectory'
+              properties: {
+                login: principalName
+                sid: principalId
+              }
+              parent: sql
+            }
+            """,
+            includePrincipalName: true);
+    }
+
     private async Task RoleAssignmentTest(
         string azureResourceName,
         Action<IDistributedApplicationBuilder> configureBuilder,
