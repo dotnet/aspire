2f49ff809243fa386eca93c9cdab602c28f53137
Eric Erhardt <eric.erhardt@microsoft.com>
2025-03-24 16:26:06 -0500

    Support getting the PrinicipalName for a user assigned managed identity

--- tests/Aspire.Azure.Npgsql.Tests/FakeTokenCredential.cs
+++ tests/Aspire.Azure.Npgsql.Tests/FakeTokenCredential.cs
@@ -8,7 +8,6 @@ namespace Aspire.Azure.Npgsql.Tests;
 internal sealed class FakeTokenCredential : TokenCredential
 {
     private const string Token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJJc3N1ZXIiOiJJc3N1ZXIiLCJJc3N1ZWQgQXQiOiIyMDI1LTAzLTIxVDAxOjM3OjAwLjE5OFoiLCJFeHBpcmF0aW9uIjoiMjAyNS0wMy0yMVQwMTozNzowMC4xOThaIiwicHJlZmVycmVkX3VzZXJuYW1lIjoibWlrZXlAbW91c2UuY29tIiwiUm9sZSI6IkFkbWluIn0.WdKzHL5CBeMOIlpzqaotvNbo0mmvaMtcW0zpMlB3lUE";
-
     // {
     //   "Issuer": "Issuer",
     //   "Issued At": "2025-03-21T01:37:00.198Z",
@@ -17,17 +16,38 @@ internal sealed class FakeTokenCredential : TokenCredential
     //   "Role": "Admin"
     // }
 
+    private const string ManagedIdentityToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJJc3N1ZXIiOiJJc3N1ZXIiLCJJc3N1ZWQgQXQiOiIyMDI1LTAzLTIxVDAxOjM3OjAwLjE5OFoiLCJFeHBpcmF0aW9uIjoiMjAyNS0wMy0yMVQwMTozNzowMC4xOThaIiwieG1zX21pcmlkIjoiL3N1YnNjcmlwdGlvbnMvMTIzL3Jlc291cmNlZ3JvdXBzL3JnLTEyMy9wcm92aWRlcnMvTWljcm9zb2Z0Lk1hbmFnZWRJZGVudGl0eS91c2VyQXNzaWduZWRJZGVudGl0aWVzL21pLTEyMyIsIlJvbGUiOiJBZG1pbiJ9.luuw0374yNSOWKfswHURCm620UoY9qrZriqLG0668Tw";
+    // {
+    //  "Issuer": "Issuer",
+    //  "Issued At": "2025-03-21T01:37:00.198Z",
+    //  "Expiration": "2025-03-21T01:37:00.198Z",
+    //  "xms_mirid": "/subscriptions/123/resourcegroups/rg-123/providers/Microsoft.ManagedIdentity/userAssignedIdentities/mi-123",
+    //  "Role": "Admin"
+    //}
+
+    private readonly bool _useManagedIdentity;
+
+    public FakeTokenCredential(bool useManagedIdentity = false)
+    {
+        _useManagedIdentity = useManagedIdentity;
+    }
+
     public bool IsGetTokenInvoked { get; private set; }
 
     public override AccessToken GetToken(TokenRequestContext requestContext, CancellationToken cancellationToken)
     {
         IsGetTokenInvoked = true;
-        return new AccessToken(Token, DateTimeOffset.Now);
+        return CreateAccessToken();
     }
 
     public override ValueTask<AccessToken> GetTokenAsync(TokenRequestContext requestContext, CancellationToken cancellationToken)
     {
         IsGetTokenInvoked = true;
-        return new ValueTask<AccessToken>(new AccessToken(Token, DateTimeOffset.Now));
+        return new ValueTask<AccessToken>(CreateAccessToken());
+    }
+
+    private AccessToken CreateAccessToken()
+    {
+        return new AccessToken(_useManagedIdentity ? ManagedIdentityToken : Token, DateTimeOffset.Now);
     }
 }
