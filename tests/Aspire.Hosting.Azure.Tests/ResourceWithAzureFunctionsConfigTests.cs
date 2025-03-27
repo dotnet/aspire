8a70e15a0f51b29b225fdfe440550561dbaef10a
Safia Abdalla <safia@safia.rocks>
2025-03-27 16:56:57 -0700

    Fix Functions connection key for Azure Table Storage

--- tests/Aspire.Hosting.Azure.Tests/ResourceWithAzureFunctionsConfigTests.cs
+++ tests/Aspire.Hosting.Azure.Tests/ResourceWithAzureFunctionsConfigTests.cs
@@ -117,7 +117,7 @@ public class ResourceWithAzureFunctionsConfigTests
         Assert.True(target.ContainsKey("myconnection"));
         Assert.True(target.ContainsKey("Aspire__Azure__Storage__Blobs__myconnection__ConnectionString"));
         Assert.True(target.ContainsKey("Aspire__Azure__Storage__Queues__myconnection__ConnectionString"));
-        Assert.True(target.ContainsKey("Aspire__Azure__Storage__Tables__myconnection__ConnectionString"));
+        Assert.True(target.ContainsKey("Aspire__Azure__Data__Tables__myconnection__ConnectionString"));
     }
 
     [Fact]
@@ -137,7 +137,7 @@ public class ResourceWithAzureFunctionsConfigTests
         Assert.True(target.ContainsKey("myconnection__tableServiceUri"));
         Assert.True(target.ContainsKey("Aspire__Azure__Storage__Blobs__myconnection__ServiceUri"));
         Assert.True(target.ContainsKey("Aspire__Azure__Storage__Queues__myconnection__ServiceUri"));
-        Assert.True(target.ContainsKey("Aspire__Azure__Storage__Tables__myconnection__ServiceUri"));
+        Assert.True(target.ContainsKey("Aspire__Azure__Data__Tables__myconnection__ServiceUri"));
     }
 
     [Fact]
@@ -171,7 +171,7 @@ public class ResourceWithAzureFunctionsConfigTests
 
         // Assert
         Assert.True(target.ContainsKey("mytables"));
-        Assert.True(target.ContainsKey("Aspire__Azure__Storage__Tables__mytables__ConnectionString"));
+        Assert.True(target.ContainsKey("Aspire__Azure__Data__Tables__mytables__ConnectionString"));
     }
 
     [Fact]
