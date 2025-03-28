ef7f6af1ac82a2a7b3cd1761d3a0adbbd6f41d90
Mitch Denny <midenn@microsoft.com>
2025-03-28 11:37:28 +1100

    Show template versions in Aspire commands (#8339)
    * Show versions in aspire new and aspire add.
    
    * PR feedback.
    
    * Add comments.

--- src/Aspire.Cli/DotNetCliRunner.cs
+++ src/Aspire.Cli/DotNetCliRunner.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
 using System.Net.Sockets;
 using System.Text.Json;
@@ -28,7 +29,7 @@ internal sealed class DotNetCliRunner(ILogger<DotNetCliRunner> logger, IServiceP
             cancellationToken: cancellationToken);
     }
 
-    public async Task<int> InstallTemplateAsync(string packageName, string version, string? nugetSource, bool force, CancellationToken cancellationToken)
+    public async Task<(int ExitCode, string? TemplateVersion)> InstallTemplateAsync(string packageName, string version, string? nugetSource, bool force, CancellationToken cancellationToken)
     {
         List<string> cliArgs = ["new", "install", $"{packageName}::{version}"];
 
@@ -43,13 +44,89 @@ internal sealed class DotNetCliRunner(ILogger<DotNetCliRunner> logger, IServiceP
             cliArgs.Add(nugetSource);
         }
 
-        return await ExecuteAsync(
+        string? stdout = null;
+        string? stderr = null;
+
+        var exitCode = await ExecuteAsync(
             args: [.. cliArgs],
             env: null,
             workingDirectory: new DirectoryInfo(Environment.CurrentDirectory),
             backchannelCompletionSource: null,
-            streamsCallback: null,
+            streamsCallback: (_, output, _) => {
+                // We need to read the output of the streams
+                // here otherwise th process will never exit.
+                stdout = output.ReadToEnd();
+                stderr = output.ReadToEnd();
+            },
             cancellationToken: cancellationToken);
+
+        if (exitCode != 0)
+        {
+            logger.LogError(
+                "Failed to install template {PackageName} with version {Version}. See debug logs for more details. Stderr: {Stderr}, Stdout: {Stdout}",
+                packageName,
+                version,
+                stderr,
+                stdout
+            );
+            return (exitCode, null);
+        }
+        else
+        {
+            if (stdout is null)
+            {
+                logger.LogError("Failed to read stdout from the process. This should never happen.");
+                return (ExitCodeConstants.FailedToInstallTemplates, null);
+            }
+
+            // NOTE: This parsing logic is hopefully temporary and in the future we'll
+            //       have structured output:
+            //       
+            //       See: https://github.com/dotnet/sdk/issues/46345
+            //
+            if (!TryParsePackageVersionFromStdout(stdout, out var parsedVersion))
+            {
+                logger.LogError("Failed to parse template version from stdout.");
+
+                // Throwing here because this should never happen - we don't want to return
+                // the zero exit code if we can't parse the version because its possibly a 
+                // signal that the .NET SDK has changed.
+                throw new InvalidOperationException("Failed to parse template version from stdout.");
+            }
+
+            return (exitCode, parsedVersion);
+        }
+    }
+
+    private static bool TryParsePackageVersionFromStdout(string stdout, [NotNullWhen(true)] out string? version)
+    {
+        var lines = stdout.Split(Environment.NewLine);
+        var successLine = lines.SingleOrDefault(x => x.StartsWith("Success: Aspire.ProjectTemplates"));
+
+        if (successLine is null)
+        {
+            version = null;
+            return false;
+        }
+        
+        var templateVersion = successLine.Split(" ") switch { // Break up the success line.
+            { Length: > 2 } chunks => chunks[1].Split("::") switch { // Break up the template+version string
+                { Length: 2 } versionChunks => versionChunks[1], // The version in the second chunk
+                _ => null
+            },
+            _ => null
+        };
+
+        if (templateVersion is not null)
+        {
+            version = templateVersion;
+            return true;
+        }
+        else
+        {
+            version = null;
+            return false;
+        }
     }
 
     public async Task<int> NewProjectAsync(string templateName, string name, string outputPath, CancellationToken cancellationToken)
