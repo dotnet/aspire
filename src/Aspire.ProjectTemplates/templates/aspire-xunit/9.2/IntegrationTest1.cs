6dea39eb3e95b218f6def566422eb61ac03d149e
Dan Moseley <danmose@microsoft.com>
2025-03-31 14:32:11 -0600

    add ct

--- src/Aspire.ProjectTemplates/templates/aspire-xunit/9.2/IntegrationTest1.cs
+++ src/Aspire.ProjectTemplates/templates/aspire-xunit/9.2/IntegrationTest1.cs
@@ -18,30 +18,35 @@ public class IntegrationTest1
     // [Fact]
     // public async Task GetWebResourceRootReturnsOkStatusCode()
     // {
-    //     // Arrange
-    //     var appHost = await DistributedApplicationTestingBuilder.CreateAsync<Projects.MyAspireApp_AppHost>();
-    //     appHost.Services.AddLogging(logging =>
-    //     {
-    //         logging.SetMinimumLevel(LogLevel.Debug);
-    //         // Override the logging filters from the app's configuration
-    //         logging.AddFilter(appHost.Environment.ApplicationName, LogLevel.Debug);
-    //         logging.AddFilter("Aspire.", LogLevel.Debug);
-    //         // To output logs to the xUnit.net ITestOutputHelper, consider adding a package from https://www.nuget.org/packages?q=xunit+logging
-    //     });
-    //     appHost.Services.ConfigureHttpClientDefaults(clientBuilder =>
-    //     {
-    //         clientBuilder.AddStandardResilienceHandler();
-    //     });
+    //      // Arrange
+#if (XUnitVersion == "v2")
+    //      CancellationToken cancellationToken = new CancellationTokenSource(DefaultTimeout).Token;
+#else // XunitVersion v3 or v3mtp
+    //      CancellationToken cancellationToken = TestContext.Current.CancellationToken;
+#endif
+    //      var appHost = await DistributedApplicationTestingBuilder.CreateAsync<Projects.MyAspireApp_AppHost>(cancellationToken);
+    //      appHost.Services.AddLogging(logging =>
+    //      {
+    //          logging.SetMinimumLevel(LogLevel.Debug);
+    //          // Override the logging filters from the app's configuration
+    //          logging.AddFilter(appHost.Environment.ApplicationName, LogLevel.Debug);
+    //          logging.AddFilter("Aspire.", LogLevel.Debug);
+    //          // To output logs to the xUnit.net ITestOutputHelper, consider adding a package from https://www.nuget.org/packages?q=xunit+logging
+    //      });
+    //      appHost.Services.ConfigureHttpClientDefaults(clientBuilder =>
+    //      {
+    //          clientBuilder.AddStandardResilienceHandler();
+    //      });
     //
-    //     await using var app = await appHost.BuildAsync().WaitAsync(DefaultTimeout);
-    //     await app.StartAsync().WaitAsync(DefaultTimeout);
+    //      await using var app = await appHost.BuildAsync(cancellationToken).WaitAsync(DefaultTimeout, cancellationToken);
+    //      await app.StartAsync(cancellationToken).WaitAsync(DefaultTimeout, cancellationToken);
     //
-    //     // Act
-    //     var httpClient = app.CreateHttpClient("webfrontend");
-    //     await app.ResourceNotifications.WaitForResourceHealthyAsync("webfrontend").WaitAsync(DefaultTimeout);
-    //     var response = await httpClient.GetAsync("/");
+    //      // Act
+    //      var httpClient = app.CreateHttpClient("webfrontend");
+    //      await app.ResourceNotifications.WaitForResourceHealthyAsync("webfrontend", cancellationToken).WaitAsync(DefaultTimeout, cancellationToken);
+    //      var response = await httpClient.GetAsync("/", cancellationToken);
     //
-    //     // Assert
-    //     Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+    //      // Assert
+    //      Assert.Equal(HttpStatusCode.OK, response.StatusCode);
     // }
 }
