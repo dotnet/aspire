ce1cf0793da57fa42bb5b52d59079ebab96a9d5f
Ankit Jain <radical@gmail.com>
2025-04-01 13:46:11 -0400

    disable tests failing due to https://github.com/dotnet/aspire/issues/8473

--- tests/Aspire.Templates.Tests/StarterTemplateWithRedisCacheTests_PreviousTFM.cs
+++ tests/Aspire.Templates.Tests/StarterTemplateWithRedisCacheTests_PreviousTFM.cs
@@ -2,11 +2,13 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using Aspire.TestUtilities;
+using Xunit;
 
 namespace Aspire.Templates.Tests;
 
 [RequiresDocker("Needs docker to start redis cache")]
 [RequiresSSLCertificate]
+[ActiveIssue("https://github.com/dotnet/aspire/issues/8473", typeof(PlatformDetection), nameof(PlatformDetection.IsRunningOnGithubActions))]
 public class StarterTemplateWithRedisCacheTests_PreviousTFM : StarterTemplateRunTestsBase<StarterTemplateWithRedisCacheFixture_PreviousTFM>
 {
     protected override int DashboardResourcesWaitTimeoutSecs => 300;
