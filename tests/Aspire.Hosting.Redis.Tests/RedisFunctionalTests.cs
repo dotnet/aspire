614aa4fc47f19fd0cb9894238a7955b65821a889
Alireza Baloochi <Dev.AlirezaBaloochi@gmail.com>
2025-04-03 18:47:28 +0330

    Change Redis Insights to use environments variable for preconfigure database connections

--- tests/Aspire.Hosting.Redis.Tests/RedisFunctionalTests.cs
+++ tests/Aspire.Hosting.Redis.Tests/RedisFunctionalTests.cs
@@ -124,7 +124,7 @@ public class RedisFunctionalTests(ITestOutputHelper testOutputHelper)
 
     [Fact]
     [RequiresDocker]
-    [QuarantinedTest("https://github.com/dotnet/aspire/issues/7291")]
+    //[QuarantinedTest("https://github.com/dotnet/aspire/issues/7291")]
     public async Task VerifyDatabasesAreNotDuplicatedForPersistentRedisInsightContainer()
     {
         var randomResourceSuffix = Random.Shared.Next(10000).ToString();
@@ -229,7 +229,7 @@ public class RedisFunctionalTests(ITestOutputHelper testOutputHelper)
 
     [Fact]
     [RequiresDocker]
-    [QuarantinedTest("https://github.com/dotnet/aspire/issues/6099")]
+    //[QuarantinedTest("https://github.com/dotnet/aspire/issues/6099")]
     public async Task VerifyWithRedisInsightImportDatabases()
     {
         var cts = new CancellationTokenSource(TimeSpan.FromMinutes(5));
@@ -537,7 +537,7 @@ public class RedisFunctionalTests(ITestOutputHelper testOutputHelper)
     [InlineData(false)]
     [InlineData(true)]
     [RequiresDocker]
-    [QuarantinedTest("https://github.com/dotnet/aspire/issues/7176")]
+    //[QuarantinedTest("https://github.com/dotnet/aspire/issues/7176")]
     public async Task RedisInsightWithDataShouldPersistStateBetweenUsages(bool useVolume)
     {
         var cts = new CancellationTokenSource(TimeSpan.FromMinutes(10));
