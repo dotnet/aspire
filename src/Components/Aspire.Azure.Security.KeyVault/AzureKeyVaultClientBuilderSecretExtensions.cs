1070dd1134875b468976ed1ebce3efb73cf6f450
James Gould <hello@jamesgould.dev>
2025-04-04 18:45:02 +0100

    Migrated AddAzureKeyVaultSecrets back to AspireKeyVaultExtensions

--- src/Components/Aspire.Azure.Security.KeyVault/AzureKeyVaultClientBuilderSecretExtensions.cs
+++ src/Components/Aspire.Azure.Security.KeyVault/AzureKeyVaultClientBuilderSecretExtensions.cs
@@ -5,8 +5,6 @@ using Aspire.Azure.Common;
 using Aspire.Azure.Security.KeyVault;
 using Azure.Core;
 using Azure.Core.Extensions;
-using Azure.Extensions.AspNetCore.Configuration.Secrets;
-using Azure.Identity;
 using Azure.Security.KeyVault.Secrets;
 using HealthChecks.Azure.KeyVault.Secrets;
 using Microsoft.Extensions.Azure;
@@ -66,58 +64,6 @@ public static class AzureKeyVaultClientBuilderSecretExtensions
                        configureClientBuilder, builder.ConnectionName, serviceKey);
 
         return builder;
-    }
-
-    /// <summary>
-    /// Adds the Azure KeyVault secrets to be configuration values in the <paramref name="configurationManager"/>.
-    /// </summary>
-    /// <param name="configurationManager">The <see cref="IConfigurationManager"/> to add the secrets to.</param>
-    /// <param name="connectionName">A name used to retrieve the connection string from the ConnectionStrings configuration section.</param>
-    /// <param name="configureSettings">An optional method that can be used for customizing the <see cref="AzureSecurityKeyVaultSettings"/>. It's invoked after the settings are read from the configuration.</param>
-    /// <param name="configureClientOptions">An optional method that can be used for customizing the <see cref="SecretClientOptions"/>.</param>
-    /// <param name="options">An optional <see cref="AzureKeyVaultConfigurationOptions"/> instance to configure the behavior of the configuration provider.</param>
-    public static void AddAzureKeyVaultSecrets(
-        this IConfigurationManager configurationManager,
-        string connectionName,
-        Action<AzureSecurityKeyVaultSettings>? configureSettings = null,
-        Action<SecretClientOptions>? configureClientOptions = null,
-        AzureKeyVaultConfigurationOptions? options = null)
-    {
-        ArgumentNullException.ThrowIfNull(configurationManager);
-        ArgumentException.ThrowIfNullOrEmpty(connectionName);
-
-        var client = configurationManager.GetSecretClient(connectionName, configureSettings, configureClientOptions);
-        configurationManager.AddAzureKeyVault(client, options ?? new AzureKeyVaultConfigurationOptions());
-    }
-
-    private static SecretClient GetSecretClient(
-        this IConfiguration configuration,
-        string connectionName,
-        Action<AzureSecurityKeyVaultSettings>? configureSettings,
-        Action<SecretClientOptions>? configureOptions)
-    {
-        var configSection = configuration.GetSection(AzureKeyVaultComponentConstants.s_defaultConfigSectionName);
-
-        var settings = new AzureSecurityKeyVaultSettings();
-        configSection.Bind(settings);
-
-        if (configuration.GetConnectionString(connectionName) is string connectionString)
-        {
-            ((IConnectionStringSettings)settings).ParseConnectionString(connectionString);
-        }
-
-        configureSettings?.Invoke(settings);
-
-        var clientOptions = new SecretClientOptions();
-        configSection.GetSection("ClientOptions").Bind(clientOptions);
-        configureOptions?.Invoke(clientOptions);
-
-        if (settings.VaultUri is null)
-        {
-            throw new InvalidOperationException($"VaultUri is missing. It should be provided in 'ConnectionStrings:{connectionName}' or under the 'VaultUri' key in the '{AzureKeyVaultComponentConstants.s_defaultConfigSectionName}' configuration section.");
-        }
-
-        return new SecretClient(settings.VaultUri, settings.Credential ?? new DefaultAzureCredential(), clientOptions);
     }
 
     /// <summary>
