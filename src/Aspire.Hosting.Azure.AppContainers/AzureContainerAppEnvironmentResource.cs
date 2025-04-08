53d1d4cb0fd08e82bdfa29cf416765bfa37bd31e
Eric Erhardt <eric.erhardt@microsoft.com>
2025-04-08 12:34:44 -0500

    Fix volume output naming issue
    We weren't discriminating between volumes and bindmounts, which caused a cache collision.

--- src/Aspire.Hosting.Azure.AppContainers/AzureContainerAppEnvironmentResource.cs
+++ src/Aspire.Hosting.Azure.AppContainers/AzureContainerAppEnvironmentResource.cs
@@ -80,8 +80,15 @@ public class AzureContainerAppEnvironmentResource(string name, Action<AzureResou
 
     IManifestExpressionProvider IAzureContainerAppEnvironment.GetVolumeStorage(IResource resource, ContainerMountAnnotation volume, int volumeIndex)
     {
+        var prefix = volume.Type switch
+        {
+            ContainerMountType.BindMount => "bm",
+            ContainerMountType.Volume => "v",
+            _ => throw new NotSupportedException()
+        };
+
         // REVIEW: Should we use the same naming algorithm as azd?
-        var outputName = $"volumes_{resource.Name}_{volumeIndex}";
+        var outputName = $"volumes_{resource.Name}_{prefix}{volumeIndex}";
 
         if (!VolumeNames.TryGetValue(outputName, out var volumeName))
         {
