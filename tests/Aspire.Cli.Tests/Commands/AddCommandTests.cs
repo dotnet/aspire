f47eca02ecab94e0ce84d2f0e258abfcbb69bf87
Mitch Denny <midenn@microsoft.com>
2025-04-15 13:57:09 +1000

    More tests.

--- tests/Aspire.Cli.Tests/Commands/AddCommandTests.cs
+++ tests/Aspire.Cli.Tests/Commands/AddCommandTests.cs
@@ -163,6 +163,171 @@ public class AddCommandTests(ITestOutputHelper outputHelper)
         Assert.False(promptedForIntegrationPackages);
     }
 
+    [Fact]
+    public async Task AddCommandDoesNotPromptForVersionIfSpecifiedOnCommandLine()
+    {
+        var promptedForIntegrationPackages = false;
+        var promptedForVersion = false;
+
+        var services = CliTestHelper.CreateServiceCollection(outputHelper, options => {
+
+            options.AddCommandPrompterFactory = (sp) =>
+            {
+                var interactionService = sp.GetRequiredService<IInteractionService>();
+                var prompter = new TestAddCommandPrompter(interactionService);
+
+                prompter.PromptForIntegrationCallback = (packages) =>
+                {
+                    promptedForIntegrationPackages = true;
+                    throw new InvalidOperationException("Should not have been prompted for integration packages.");
+                };
+
+                prompter.PromptForIntegrationVersionCallback = (packages) =>
+                {
+                    promptedForVersion = true;
+                    throw new InvalidOperationException("Should not have been prompted for integration version.");
+                };
+
+                return prompter;
+            };
+
+            options.ProjectLocatorFactory = _ => new FakeProjectLocator();
+
+            options.DotNetCliRunnerFactory = (sp) =>
+            {
+                var runner = new TestDotNetCliRunner();
+                runner.SearchPackagesAsyncCallback = (dir, query, prerelease, take, skip, nugetSource, cancellationToken) =>
+                {
+                    var dockerPackage = new NuGetPackage()
+                    {
+                        Id = "Aspire.Hosting.Docker",
+                        Source = "nuget",
+                        Version = "9.2.0"
+                    };
+
+                    var redisPackage = new NuGetPackage()
+                    {
+                        Id = "Aspire.Hosting.Redis",
+                        Source = "nuget",
+                        Version = "9.2.0"
+                    };
+
+                    var azureRedisPackage = new NuGetPackage()
+                    {
+                        Id = "Aspire.Hosting.Azure.Redis",
+                        Source = "nuget",
+                        Version = "9.2.0"
+                    };
+
+                    return (
+                        0, // Exit code.
+                        new NuGetPackage[] { dockerPackage, redisPackage, azureRedisPackage } // 
+                        );
+                };
+
+                runner.AddPackageAsyncCallback = (projectFilePath, packageName, packageVersion, cancellationToken) =>
+                {
+                    // Simulate adding the package.
+                    return 0; // Success.
+                };
+
+                return runner;
+            };
+        });
+        var provider = services.BuildServiceProvider();
+
+        var command = provider.GetRequiredService<AddCommand>();
+        var result = command.Parse("add docker --version 9.2.0");
+
+        var exitCode = await result.InvokeAsync().WaitAsync(CliTestConstants.DefaultTimeout);
+        Assert.Equal(0, exitCode);
+        Assert.False(promptedForIntegrationPackages);
+        Assert.False(promptedForVersion);
+    }
+
+    [Fact]
+    public async Task AddCommandPromptsForDisambiguation()
+    {
+        IEnumerable<(string FriendlyName, NuGetPackage Package)>? promptedPackages = null;
+        string? addedPackageName = null;
+        string? addedPackageVersion = null;
+
+        var services = CliTestHelper.CreateServiceCollection(outputHelper, options => {
+
+            options.AddCommandPrompterFactory = (sp) =>
+            {
+                var interactionService = sp.GetRequiredService<IInteractionService>();
+                var prompter = new TestAddCommandPrompter(interactionService);
+
+                prompter.PromptForIntegrationCallback = (packages) =>
+                {
+                    promptedPackages = packages;
+                    return packages.Single(p => p.Package.Id == "Aspire.Hosting.Redis");
+                };
+
+                return prompter;
+            };
+
+            options.ProjectLocatorFactory = _ => new FakeProjectLocator();
+
+            options.DotNetCliRunnerFactory = (sp) =>
+            {
+                var runner = new TestDotNetCliRunner();
+                runner.SearchPackagesAsyncCallback = (dir, query, prerelease, take, skip, nugetSource, cancellationToken) =>
+                {
+                    var dockerPackage = new NuGetPackage()
+                    {
+                        Id = "Aspire.Hosting.Docker",
+                        Source = "nuget",
+                        Version = "9.2.0"
+                    };
+
+                    var redisPackage = new NuGetPackage()
+                    {
+                        Id = "Aspire.Hosting.Redis",
+                        Source = "nuget",
+                        Version = "9.2.0"
+                    };
+
+                    var azureRedisPackage = new NuGetPackage()
+                    {
+                        Id = "Aspire.Hosting.Azure.Redis",
+                        Source = "nuget",
+                        Version = "9.2.0"
+                    };
+
+                    return (
+                        0, // Exit code.
+                        new NuGetPackage[] { dockerPackage, redisPackage, azureRedisPackage } // 
+                        );
+                };
+
+                runner.AddPackageAsyncCallback = (projectFilePath, packageName, packageVersion, cancellationToken) =>
+                {
+                    addedPackageName = packageName;
+                    addedPackageVersion = packageVersion;
+                    return 0; // Success.
+                };
+
+                return runner;
+            };
+        });
+        var provider = services.BuildServiceProvider();
+
+        var command = provider.GetRequiredService<AddCommand>();
+        var result = command.Parse("add red");
+
+        var exitCode = await result.InvokeAsync().WaitAsync(CliTestConstants.DefaultTimeout);
+        Assert.Equal(0, exitCode);
+        Assert.Collection(
+            promptedPackages!,
+            p => Assert.Equal("Aspire.Hosting.Redis", p.Package.Id),
+            p => Assert.Equal("Aspire.Hosting.Azure.Redis", p.Package.Id)
+            );
+        Assert.Equal("Aspire.Hosting.Redis", addedPackageName);
+        Assert.Equal("9.2.0", addedPackageVersion);
+    }
+
 }
 
 internal sealed class FakeProjectLocator : IProjectLocator
