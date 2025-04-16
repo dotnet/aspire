8b6f2b0716d187a6dc617c8d616ad647bda89989
Ankit Jain <radical@gmail.com>
2025-04-15 20:51:51 -0400

    Revert "Unquarantine Aspire.Hosting.Tests.DistributedApplicationTests.ProxylessEndpointWorks"
    This reverts commit b5bef79e2f2180ba199e27e4eb702adcadca06a4.

--- tests/Aspire.Hosting.Tests/DistributedApplicationTests.cs
+++ tests/Aspire.Hosting.Tests/DistributedApplicationTests.cs
@@ -914,6 +914,7 @@ public class DistributedApplicationTests
     }
 
     [Fact]
+    [QuarantinedTest("https://github.com/dotnet/aspire/issues/8728")]
     public async Task ProxylessEndpointWorks()
     {
         const string testName = "proxyless-endpoint-works";
