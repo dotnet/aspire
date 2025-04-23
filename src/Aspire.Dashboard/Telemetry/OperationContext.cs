a4ea2a98a528c3b62daeaa065bdcfa8a673e4a47
James Newton-King <james@newtonking.com>
2025-04-23 12:03:33 +0800

    Debugging

--- src/Aspire.Dashboard/Telemetry/OperationContext.cs
+++ src/Aspire.Dashboard/Telemetry/OperationContext.cs
@@ -1,8 +1,11 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Diagnostics;
 
 namespace Aspire.Dashboard.Telemetry;
 
+[DebuggerDisplay("Name = {Name}, Properties = {Properties.Length}")]
 public sealed class OperationContext
 {
     public static readonly OperationContext Empty = new(name: string.Empty);
