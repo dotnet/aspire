3bfef19f95ab31bbcaa92017e21c1ae3648eab79
Sebastien Ros <sebastienros@gmail.com>
2025-04-30 14:00:33 -0700

    Use free sku

--- playground/SqlServerScript/AppHost1/Program.cs
+++ playground/SqlServerScript/AppHost1/Program.cs
@@ -2,13 +2,22 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using Aspire.Hosting;
+using Azure.Provisioning.Sql;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddAzureContainerAppEnvironment("env");
 
-var dbServer = builder.AddAzureSqlServer("sqlserver")
-    // .RunAsContainer(c => c.WithLifetime(ContainerLifetime.Persistent))
+var dbServer = builder.AddAzureSqlServer("mysqlserver")
+    .ConfigureInfrastructure(c =>
+    {
+        const string FREE_DB_SKU = "GP_S_Gen5_2";
+
+        foreach (var database in c.GetProvisionableResources().OfType<SqlDatabase>())
+        {
+            database.Sku = new SqlSku() { Name = FREE_DB_SKU };
+        }
+    })
     ;
 
 var todosDb = dbServer.AddDatabase("todosdb");
