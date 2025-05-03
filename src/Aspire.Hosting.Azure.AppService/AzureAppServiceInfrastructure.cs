70ea39b70acbd0ece74bd7def48c1170ae55bb6b
David Fowler <davidfowl@gmail.com>
2025-05-03 08:01:12 -0700

    Remove unused parameter resolution logic for Azure Bicep resources in BeforeStartAsync method

--- src/Aspire.Hosting.Azure.AppService/AzureAppServiceInfrastructure.cs
+++ src/Aspire.Hosting.Azure.AppService/AzureAppServiceInfrastructure.cs
@@ -52,35 +52,5 @@ internal sealed class AzureAppServiceInfrastructure(
             });
 #pragma warning restore ASPIRECOMPUTE001 // Type is for evaluation purposes only and is subject to change or removal in future updates.
         }
-
-        static void SetKnownParameterValue(AzureBicepResource r, string key, Func<AzureBicepResource, object> factory)
-        {
-            if (r.Parameters.TryGetValue(key, out var existingValue) && existingValue is null)
-            {
-                var value = factory(r);
-
-                r.Parameters[key] = value;
-            }
-        }
-
-        // Resolve the known parameters for the container app environment
-        foreach (var r in appModel.Resources.OfType<AzureBicepResource>())
-        {
-            // HACK: This forces parameters to be resolved for any AzureProvisioningResource
-            r.GetBicepTemplateFile();
-
-            // REVIEW: the secret key vault resources aren't coupled to the container app environment. This
-            // is a side effect from how azd worked. We can move them to another service in the future.
-            SetKnownParameterValue(r, AzureBicepResource.KnownParameters.KeyVaultName, _ => throw new NotSupportedException("Key vault name is not supported."));
-
-            // Set the known parameters for the container app environment
-            SetKnownParameterValue(r, AzureBicepResource.KnownParameters.PrincipalId, _ => appServiceEnvironment.ContainerRegistryManagedIdentityId);
-            SetKnownParameterValue(r, AzureBicepResource.KnownParameters.PrincipalType, _ => "ServicePrincipal");
-            SetKnownParameterValue(r, AzureBicepResource.KnownParameters.PrincipalName, _ => throw new NotSupportedException("principalName is not supported."));
-            SetKnownParameterValue(r, AzureBicepResource.KnownParameters.LogAnalyticsWorkspaceId, _ => throw new NotSupportedException("logAnalyticsWorkspaceId is not supported."));
-
-            SetKnownParameterValue(r, "containerAppEnvironmentId", _ => throw new NotSupportedException("containerAppEnvironmentId name is not supported."));
-            SetKnownParameterValue(r, "containerAppEnvironmentName", _ => throw new NotSupportedException("containerAppEnvironmentNamee is not supported."));
-        }
     }
 }
