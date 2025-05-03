5df9d4f278b97e32e10c1736f9b9137506f4c69f
Adam Ratzman <adam@adamratzman.com>
2025-05-02 20:13:37 -0700

    Use IsHidden instead of KnownResourceStates.Hidden

--- src/Aspire.Hosting/Dashboard/DashboardLifecycleHook.cs
+++ src/Aspire.Hosting/Dashboard/DashboardLifecycleHook.cs
@@ -170,11 +170,7 @@ internal sealed class DashboardLifecycleHook(IConfiguration configuration,
                 ContainerResource => KnownResourceTypes.Container,
                 _ => dashboardResource.GetType().Name
             },
-            State = configuration.GetBool(KnownConfigNames.ShowDashboardResources, KnownConfigNames.Legacy.ShowDashboardResources) is true
-                ? null
-#pragma warning disable CS0618 // Type or member is obsolete
-                : KnownResourceStates.Hidden
-#pragma warning restore CS0618 // Type or member is obsolete
+            IsHidden = !configuration.GetBool(KnownConfigNames.ShowDashboardResources, KnownConfigNames.Legacy.ShowDashboardResources) is true
         };
 
         dashboardResource.Annotations.Add(new ResourceSnapshotAnnotation(snapshot));
