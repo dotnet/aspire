a537b5a5c57c77e0dcc407823f6036a06fe11363
David Fowler <davidfowl@gmail.com>
2025-05-04 09:54:28 -0700

    Reorganize using directives for improved clarity

--- src/Aspire.Hosting/Publishing/Publisher.cs
+++ src/Aspire.Hosting/Publishing/Publisher.cs
@@ -1,11 +1,11 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using Aspire.Hosting;
 using Aspire.Hosting.ApplicationModel;
-using Aspire.Hosting.Publishing;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+
+namespace Aspire.Hosting.Publishing;
 
 internal class Publisher(
     ILogger<Publisher> logger,
