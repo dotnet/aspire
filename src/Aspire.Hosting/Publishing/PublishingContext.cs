3f8687c28c77f3fc7fb9dfe5f1b7c38c76b9002f
David Fowler <davidfowl@gmail.com>
2025-05-04 09:53:37 -0700

    Reorganize using directives and namespace declaration for clarity

--- src/Aspire.Hosting/Publishing/PublishingContext.cs
+++ src/Aspire.Hosting/Publishing/PublishingContext.cs
@@ -1,9 +1,9 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using Aspire.Hosting;
-using Aspire.Hosting.ApplicationModel;
 using Microsoft.Extensions.Logging;
+
+namespace Aspire.Hosting.ApplicationModel;
 
 /// <summary>
 /// Provides contextual information and services for the publishing process of a distributed application.
