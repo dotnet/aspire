3f8fcc1de06b055330030d6d0aeb5b96b8af68ae
David Fowler <davidfowl@gmail.com>
2025-05-05 10:33:41 -0700

    Added initial support for app service as a compute environment (#9090)
    * Added initial support for app service as a compute environment
    This will allow us to start experimenting with cross compute endpoint references (which doesn't work today).
    - The mirrors what we have with azure container apps pretty closely with some limitations.
       - Only support for projects in this pass
       - On support for public http endpoints (we don't do anything with private networking)
       - Single app service plan, which means you can't scale compute independently
    - Added tests

---
+++ src/Aspire.Hosting.Azure.AppService/AzureAppServiceInfrastructure.cs
@@ -0,0 +1,56 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Aspire.Hosting.ApplicationModel;
+using Aspire.Hosting.Lifecycle;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace Aspire.Hosting.Azure.AppService;
+
+internal sealed class AzureAppServiceInfrastructure(
+    ILogger<AzureAppServiceInfrastructure> logger,
+    IOptions<AzureProvisioningOptions> provisioningOptions,
+    DistributedApplicationExecutionContext executionContext) :
+    IDistributedApplicationLifecycleHook
+{
+    public async Task BeforeStartAsync(DistributedApplicationModel appModel, CancellationToken cancellationToken = default)
+    {
+        if (!executionContext.IsPublishMode)
+        {
+            return;
+        }
+
+        var appServiceEnvironment = appModel.Resources.OfType<AzureAppServiceEnvironmentResource>().FirstOrDefault()
+            ?? throw new InvalidOperationException("AppServiceEnvironmentResource not found.");
+
+        var appServiceEnvironmentContext = new AzureAppServiceEnvironmentContext(
+            logger,
+            executionContext, 
+            appServiceEnvironment);
+
+        foreach (var resource in appModel.Resources)
+        {
+            if (resource.TryGetLastAnnotation<ManifestPublishingCallbackAnnotation>(out var lastAnnotation) && lastAnnotation == ManifestPublishingCallbackAnnotation.Ignore)
+            {
+                continue;
+            }
+
+            // We only support project resources for now.
+            if (resource is not ProjectResource)
+            {
+                continue;
+            }
+
+            var website = await appServiceEnvironmentContext.CreateAppServiceAsync(resource, provisioningOptions.Value, cancellationToken).ConfigureAwait(false);
+
+#pragma warning disable ASPIRECOMPUTE001 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+            resource.Annotations.Add(new DeploymentTargetAnnotation(website)
+            {
+                ContainerRegistry = appServiceEnvironment,
+                ComputeEnvironment = appServiceEnvironment
+            });
+#pragma warning restore ASPIRECOMPUTE001 // Type is for evaluation purposes only and is subject to change or removal in future updates.
+        }
+    }
+}
