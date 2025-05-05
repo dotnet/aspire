5f101918b9322ddfbb3d155473f95b23fd07e90d
Aaron Powell <me@aaron-powell.com>
2025-05-05 16:02:28 +1000

    Making properties public

--- src/Components/Aspire.Azure.AI.Inference/AspireChatCompletionsClientBuilder.cs
+++ src/Components/Aspire.Azure.AI.Inference/AspireChatCompletionsClientBuilder.cs
@@ -11,18 +11,33 @@ namespace Microsoft.Extensions.Hosting;
 /// <remarks>This class is used to configure the necessary parameters for creating an Aspire Chat Completions
 /// client, such as the host application builder, service key, and optional model ID. It is intended for internal use
 /// within the application setup process.</remarks>
-/// <param name="builder">The <see cref="IHostApplicationBuilder"/> with which services are being registered.</param>
+/// <param name="hostBuilder">The <see cref="IHostApplicationBuilder"/> with which services are being registered.</param>
 /// <param name="serviceKey">The service key used to register the <see cref="ChatCompletionsClient"/> service, if any.</param>
-/// <param name="modelId">The name of the model (deployment) in Azure AI Foundry.</param>
+/// <param name="deploymentId">The id of the deployment in Azure AI Foundry.</param>
 /// <param name="disableTracing">A flag to indicate whether tracing should be disabled.</param>
 public class AspireChatCompletionsClientBuilder(
-    IHostApplicationBuilder builder,
+    IHostApplicationBuilder hostBuilder,
     string? serviceKey,
-    string? modelId,
+    string? deploymentId,
     bool disableTracing)
 {
-    internal bool DisableTracing { get; } = disableTracing;
-    internal IHostApplicationBuilder Builder { get; } = builder;
-    internal string? ServiceKey { get; } = serviceKey;
-    internal string? ModelId { get; } = modelId;
+    /// <summary>
+    /// Gets a flag indicating whether tracing should be disabled.
+    /// </summary>
+    public bool DisableTracing { get; } = disableTracing;
+
+    /// <summary>
+    /// Gets the <see cref="IHostApplicationBuilder"/> with which services are being registered.
+    /// </summary>
+    public IHostApplicationBuilder HostBuilder { get; } = hostBuilder ?? throw new ArgumentNullException(nameof(hostBuilder));
+
+    /// <summary>
+    /// Gets the service key used to register the <see cref="ChatCompletionsClient"/> service, if any.
+    /// </summary>
+    public string? ServiceKey { get; } = serviceKey;
+
+    /// <summary>
+    /// The ID of the deployment in Azure AI Foundry.
+    /// </summary>
+    public string? DeploymentId { get; } = deploymentId;
 }
