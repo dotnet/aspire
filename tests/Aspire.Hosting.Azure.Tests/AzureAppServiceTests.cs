cf17e4c68750339467e6a19c782099250fa41f52
Ankit Jain <radical@gmail.com>
2025-05-05 17:11:48 -0400

    adjust new test to use the verifier

--- tests/Aspire.Hosting.Azure.Tests/AzureAppServiceTests.cs
+++ tests/Aspire.Hosting.Azure.Tests/AzureAppServiceTests.cs
@@ -7,12 +7,11 @@ using Aspire.Hosting.ApplicationModel;
 using Aspire.Hosting.Azure.AppService;
 using Aspire.Hosting.Utils;
 using Microsoft.Extensions.DependencyInjection;
-using Xunit;
 using static Aspire.Hosting.Utils.AzureManifestUtils;
 
 namespace Aspire.Hosting.Azure.Tests;
 
-public class AzureAppServiceTests(ITestOutputHelper output)
+public class AzureAppServiceTests
 {
 
     [Fact]
@@ -68,67 +67,8 @@ public class AzureAppServiceTests(ITestOutputHelper output)
 
         Assert.Equal(expectedManifest, m);
 
-        var expectedBicep =
-        """
-        @description('The location for the resource(s) to be deployed.')
-        param location string = resourceGroup().location
-        
-        param env_outputs_azure_container_registry_endpoint string
-        
-        param env_outputs_planid string
-        
-        param env_outputs_azure_container_registry_managed_identity_id string
-        
-        param env_outputs_azure_container_registry_managed_identity_client_id string
-        
-        param api_containerimage string
-        
-        param api_containerport string
-        
-        resource webapp 'Microsoft.Web/sites@2024-04-01' = {
-          name: take('${toLower('api')}-${uniqueString(resourceGroup().id)}', 60)
-          location: location
-          properties: {
-            serverFarmId: env_outputs_planid
-            siteConfig: {
-              linuxFxVersion: 'DOCKER|${api_containerimage}'
-              acrUseManagedIdentityCreds: true
-              acrUserManagedIdentityID: env_outputs_azure_container_registry_managed_identity_client_id
-              appSettings: [
-                {
-                  name: 'OTEL_DOTNET_EXPERIMENTAL_OTLP_EMIT_EXCEPTION_LOG_ATTRIBUTES'
-                  value: 'true'
-                }
-                {
-                  name: 'OTEL_DOTNET_EXPERIMENTAL_OTLP_EMIT_EVENT_LOG_ATTRIBUTES'
-                  value: 'true'
-                }
-                {
-                  name: 'OTEL_DOTNET_EXPERIMENTAL_OTLP_RETRY'
-                  value: 'in_memory'
-                }
-                {
-                  name: 'ASPNETCORE_FORWARDEDHEADERS_ENABLED'
-                  value: 'true'
-                }
-                {
-                  name: 'HTTP_PORTS'
-                  value: api_containerport
-                }
-              ]
-              webSocketsEnabled: true
-            }
-          }
-          identity: {
-            type: 'UserAssigned'
-            userAssignedIdentities: {
-              '${env_outputs_azure_container_registry_managed_identity_id}': { }
-            }
-          }
-        }
-        """;
-        output.WriteLine(bicep);
-        Assert.Equal(expectedBicep, bicep);
+        await Verifier.Verify(bicep, extension: "bicep")
+            .UseHelixAwareDirectory("Snapshots");
     }
 
     [Fact]
@@ -163,66 +103,8 @@ public class AzureAppServiceTests(ITestOutputHelper output)
 
         Assert.Equal(expectedManifest, m);
 
-        string expectedBicep =
-        """
-        @description('The location for the resource(s) to be deployed.')
-        param location string = resourceGroup().location
-        
-        param userPrincipalId string
-        
-        param tags object = { }
-        
-        resource env_mi 'Microsoft.ManagedIdentity/userAssignedIdentities@2023-01-31' = {
-          name: take('env_mi-${uniqueString(resourceGroup().id)}', 128)
-          location: location
-          tags: tags
-        }
-        
-        resource env_acr 'Microsoft.ContainerRegistry/registries@2023-07-01' = {
-          name: take('envacr${uniqueString(resourceGroup().id)}', 50)
-          location: location
-          sku: {
-            name: 'Basic'
-          }
-          tags: tags
-        }
-        
-        resource env_acr_env_mi_AcrPull 'Microsoft.Authorization/roleAssignments@2022-04-01' = {
-          name: guid(env_acr.id, env_mi.id, subscriptionResourceId('Microsoft.Authorization/roleDefinitions', '7f951dda-4ed3-4680-a7ca-43fe172d538d'))
-          properties: {
-            principalId: env_mi.properties.principalId
-            roleDefinitionId: subscriptionResourceId('Microsoft.Authorization/roleDefinitions', '7f951dda-4ed3-4680-a7ca-43fe172d538d')
-            principalType: 'ServicePrincipal'
-          }
-          scope: env_acr
-        }
-        
-        resource env_asplan 'Microsoft.Web/serverfarms@2024-04-01' = {
-          name: take('envasplan-${uniqueString(resourceGroup().id)}', 60)
-          location: location
-          properties: {
-            reserved: true
-          }
-          kind: 'Linux'
-          sku: {
-            name: 'B1'
-            tier: 'Basic'
-          }
-        }
-        
-        output planId string = env_asplan.id
-        
-        output AZURE_CONTAINER_REGISTRY_NAME string = env_acr.name
-        
-        output AZURE_CONTAINER_REGISTRY_ENDPOINT string = env_acr.properties.loginServer
-        
-        output AZURE_CONTAINER_REGISTRY_MANAGED_IDENTITY_ID string = env_mi.id
-        
-        output AZURE_CONTAINER_REGISTRY_MANAGED_IDENTITY_CLIENT_ID string = env_mi.properties.clientId
-        """;
-
-        output.WriteLine(bicep);
-        Assert.Equal(expectedBicep, bicep);
+        await Verifier.Verify(bicep, extension: "bicep")
+            .UseHelixAwareDirectory("Snapshots");
     }
 
     [Fact]
@@ -276,81 +158,8 @@ public class AzureAppServiceTests(ITestOutputHelper output)
 
         Assert.Equal(expectedManifest, m);
 
-        var expectedBicep =
-        """
-        @description('The location for the resource(s) to be deployed.')
-        param location string = resourceGroup().location
-        
-        param env_outputs_azure_container_registry_endpoint string
-        
-        param env_outputs_planid string
-        
-        param env_outputs_azure_container_registry_managed_identity_id string
-        
-        param env_outputs_azure_container_registry_managed_identity_client_id string
-        
-        param api_containerimage string
-        
-        param mydb_kv_outputs_name string
-        
-        param api_identity_outputs_id string
-        
-        param api_identity_outputs_clientid string
-        
-        resource mydb_kv_outputs_name_kv 'Microsoft.KeyVault/vaults@2023-07-01' existing = {
-          name: mydb_kv_outputs_name
-        }
-        
-        resource mydb_kv_outputs_name_kv_connectionstrings__mydb 'Microsoft.KeyVault/vaults/secrets@2023-07-01' existing = {
-          name: 'connectionstrings--mydb'
-          parent: mydb_kv_outputs_name_kv
-        }
-        
-        resource webapp 'Microsoft.Web/sites@2024-04-01' = {
-          name: take('${toLower('api')}-${uniqueString(resourceGroup().id)}', 60)
-          location: location
-          properties: {
-            serverFarmId: env_outputs_planid
-            keyVaultReferenceIdentity: api_identity_outputs_id
-            siteConfig: {
-              linuxFxVersion: 'DOCKER|${api_containerimage}'
-              acrUseManagedIdentityCreds: true
-              acrUserManagedIdentityID: env_outputs_azure_container_registry_managed_identity_client_id
-              appSettings: [
-                {
-                  name: 'OTEL_DOTNET_EXPERIMENTAL_OTLP_EMIT_EXCEPTION_LOG_ATTRIBUTES'
-                  value: 'true'
-                }
-                {
-                  name: 'OTEL_DOTNET_EXPERIMENTAL_OTLP_EMIT_EVENT_LOG_ATTRIBUTES'
-                  value: 'true'
-                }
-                {
-                  name: 'OTEL_DOTNET_EXPERIMENTAL_OTLP_RETRY'
-                  value: 'in_memory'
-                }
-                {
-                  name: 'ConnectionStrings__mydb'
-                  value: '@Microsoft.KeyVault(SecretUri=${mydb_kv_outputs_name_kv_connectionstrings__mydb.properties.secretUri})'
-                }
-                {
-                  name: 'AZURE_CLIENT_ID'
-                  value: api_identity_outputs_clientid
-                }
-              ]
-            }
-          }
-          identity: {
-            type: 'UserAssigned'
-            userAssignedIdentities: {
-              '${env_outputs_azure_container_registry_managed_identity_id}': { }
-              '${api_identity_outputs_id}': { }
-            }
-          }
-        }
-        """;
-        output.WriteLine(bicep);
-        Assert.Equal(expectedBicep, bicep);
+        await Verifier.Verify(bicep, extension: "bicep")
+            .UseHelixAwareDirectory("Snapshots");
     }
 
     [Fact]
@@ -375,7 +184,7 @@ public class AzureAppServiceTests(ITestOutputHelper output)
         await ExecuteBeforeStartHooksAsync(app, default);
 
         var model = app.Services.GetRequiredService<DistributedApplicationModel>();
-        
+
         project2.Resource.TryGetLastAnnotation<DeploymentTargetAnnotation>(out var target);
 
         var resource = target?.DeploymentTarget as AzureProvisioningResource;
@@ -404,71 +213,8 @@ public class AzureAppServiceTests(ITestOutputHelper output)
 
         Assert.Equal(expectedManifest, m);
 
-        var expectedBicep =
-        """
-        @description('The location for the resource(s) to be deployed.')
-        param location string = resourceGroup().location
-
-        param env_outputs_azure_container_registry_endpoint string
-
-        param env_outputs_planid string
-
-        param env_outputs_azure_container_registry_managed_identity_id string
-
-        param env_outputs_azure_container_registry_managed_identity_client_id string
-
-        param project2_containerimage string
-
-        param project2_containerport string
-
-        resource webapp 'Microsoft.Web/sites@2024-04-01' = {
-          name: take('${toLower('project2')}-${uniqueString(resourceGroup().id)}', 60)
-          location: location
-          properties: {
-            serverFarmId: env_outputs_planid
-            siteConfig: {
-              linuxFxVersion: 'DOCKER|${project2_containerimage}'
-              acrUseManagedIdentityCreds: true
-              acrUserManagedIdentityID: env_outputs_azure_container_registry_managed_identity_client_id
-              appSettings: [
-                {
-                  name: 'OTEL_DOTNET_EXPERIMENTAL_OTLP_EMIT_EXCEPTION_LOG_ATTRIBUTES'
-                  value: 'true'
-                }
-                {
-                  name: 'OTEL_DOTNET_EXPERIMENTAL_OTLP_EMIT_EVENT_LOG_ATTRIBUTES'
-                  value: 'true'
-                }
-                {
-                  name: 'OTEL_DOTNET_EXPERIMENTAL_OTLP_RETRY'
-                  value: 'in_memory'
-                }
-                {
-                  name: 'ASPNETCORE_FORWARDEDHEADERS_ENABLED'
-                  value: 'true'
-                }
-                {
-                  name: 'HTTP_PORTS'
-                  value: project2_containerport
-                }
-                {
-                  name: 'services__project1__http__0'
-                  value: 'http://${take('${toLower('project1')}-${uniqueString(resourceGroup().id)}', 60)}.azurewebsites.net'
-                }
-              ]
-            }
-          }
-          identity: {
-            type: 'UserAssigned'
-            userAssignedIdentities: {
-              '${env_outputs_azure_container_registry_managed_identity_id}': { }
-            }
-          }
-        }
-        """;
-
-        output.WriteLine(bicep);
-        Assert.Equal(expectedBicep, bicep);
+        await Verifier.Verify(bicep, extension: "bicep")
+            .UseHelixAwareDirectory("Snapshots");
     }
 
     private static Task<(JsonNode ManifestNode, string BicepText)> GetManifestWithBicep(IResource resource) =>
--- tests/Aspire.Hosting.Azure.Tests/AzureKeyVaultTests.cs
+++ tests/Aspire.Hosting.Azure.Tests/AzureKeyVaultTests.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Aspire.Hosting.Azure.Tests;
 
-public class AzureKeyVaultTests(ITestOutputHelper output)
+public class AzureKeyVaultTests
 {
     [Fact]
     public async Task AddKeyVaultViaRunMode()
@@ -57,32 +57,8 @@ public class AzureKeyVaultTests(ITestOutputHelper output)
 
         var kvRoles = Assert.Single(model.Resources.OfType<AzureProvisioningResource>(), r => r.Name == "mykv-roles");
         var kvRolesManifest = await AzureManifestUtils.GetManifestWithBicep(kvRoles, skipPreparer: true);
-        var expectedBicep = """
-            @description('The location for the resource(s) to be deployed.')
-            param location string = resourceGroup().location
-
-            param mykv_outputs_name string
-
-            param principalType string
-
-            param principalId string
-
-            resource mykv 'Microsoft.KeyVault/vaults@2023-07-01' existing = {
-              name: mykv_outputs_name
-            }
-
-            resource mykv_KeyVaultSecretsUser 'Microsoft.Authorization/roleAssignments@2022-04-01' = {
-              name: guid(mykv.id, principalId, subscriptionResourceId('Microsoft.Authorization/roleDefinitions', '4633458b-17de-408a-b874-0445c86b69e6'))
-              properties: {
-                principalId: principalId
-                roleDefinitionId: subscriptionResourceId('Microsoft.Authorization/roleDefinitions', '4633458b-17de-408a-b874-0445c86b69e6')
-                principalType: principalType
-              }
-              scope: mykv
-            }
-            """;
-        output.WriteLine(kvRolesManifest.BicepText);
-        Assert.Equal(expectedBicep, kvRolesManifest.BicepText);
+        await Verifier.Verify(kvRolesManifest.BicepText, extension: "bicep")
+            .UseHelixAwareDirectory("Snapshots");
     }
 
     [Fact]
@@ -161,7 +137,7 @@ public class AzureKeyVaultTests(ITestOutputHelper output)
             }
             """;
 
-        var m = manifest.ToString();
+        var m = manifest.ManifestNode.ToString();
         Assert.Equal(expectedManifest, m);
 
         await Verifier.Verify(manifest.BicepText, extension: "bicep")
