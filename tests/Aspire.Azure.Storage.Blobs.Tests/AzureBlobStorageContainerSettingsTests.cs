94d0bb5cfd4b8db42e8b5e95f257f3d227287e5f
Igor Velikorossov <igor.velikorossov@microsoft.com>
2025-05-07 19:52:31 +1000

    Reinstate warnings

--- tests/Aspire.Azure.Storage.Blobs.Tests/AzureBlobStorageContainerSettingsTests.cs
+++ tests/Aspire.Azure.Storage.Blobs.Tests/AzureBlobStorageContainerSettingsTests.cs
@@ -1,7 +1,6 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System.Configuration;
 using Aspire.Azure.Common;
 using Aspire.Azure.Storage.Blobs;
 using Xunit;
