22223feb1590b627eeab5e7bd589921bbb13071b
Safia Abdalla <safia@microsoft.com>
2025-05-06 23:23:38 -0700

    Address feedback on Azure Container Registry (#9131)

--- src/Aspire.Hosting/ApplicationModel/ContainerRegistryReferenceAnnotation.cs
+++ src/Aspire.Hosting/ApplicationModel/ContainerRegistryReferenceAnnotation.cs
@@ -3,9 +3,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using Aspire.Hosting.ApplicationModel;
-
-namespace Aspire.Hosting.Azure;
+namespace Aspire.Hosting.ApplicationModel;
 
 /// <summary>
 /// Annotation that indicates a resource is using a specific container registry.
