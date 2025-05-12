39b434fc0d46e11de5e4cbfaf9d8b4a586cae4a3
Zhiyuan Liang <141655842+zhiyuanliang-ms@users.noreply.github.com>
2025-05-13 05:45:40 +0800

    Azure App Configuration client integration (#8945)
    * add app configuration provider as an aspire component
    
    * update
    
    * revert unintended change
    
    * update comment typo
    
    * resolve comments
    
    * update
    
    * remove all tracing related code
    
    * update readme
    
    * update
    
    * move Optional parameter to AzureAppConfigurationSettings
    
    * upgrade app config package version
    
    * revert unintended change
    
    * change package name to Aspire.Microsoft.Extensions.Configuration.AzureAppConfiguration
    
    * Update telemetry.md
    
    * Update configuration sectopm
    
    * Fix json format
    
    * Simplify sample in readme
    
    ---------
    
    Co-authored-by: Sébastien Ros <sebastienros@gmail.com>

---
+++ src/Components/Aspire.Microsoft.Extensions.Configuration.AzureAppConfiguration/AzureAppConfigurationSettings.cs
@@ -0,0 +1,39 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Aspire.Azure.Common;
+using Azure.Core;
+
+namespace Aspire.Microsoft.Extensions.Configuration.AzureAppConfiguration;
+
+/// <summary>
+/// Provides the client configuration settings for connecting to Azure App Configuration.
+/// </summary>
+public sealed class AzureAppConfigurationSettings : IConnectionStringSettings
+{
+    /// <summary>
+    /// A <see cref="Uri"/> to the App Configuration store on which the client operates. Appears as "Endpoint" in the Azure portal.
+    /// This is likely to be similar to "https://{store_name}.azconfig.io".
+    /// </summary>
+    public Uri? Endpoint { get; set; }
+
+    /// <summary>
+    /// Gets or sets the credential used to authenticate to the Azure App Configuration.
+    /// </summary>
+    public TokenCredential? Credential { get; set; }
+
+    /// <summary>
+    /// Determines the behavior of the App Configuration provider when an exception occurs while loading data from server.
+    /// If false, the exception is thrown. If true, the exception is suppressed and no configuration values are populated from Azure App Configuration.
+    /// </summary>
+    public bool Optional { get; set; }
+
+    void IConnectionStringSettings.ParseConnectionString(string? connectionString)
+    {
+        if (!string.IsNullOrEmpty(connectionString) &&
+            Uri.TryCreate(connectionString, UriKind.Absolute, out var uri))
+        {
+            Endpoint = uri;
+        }
+    }
+}
---
+++ tests/Aspire.Microsoft.Extensions.Configuration.AzureAppConfiguration.Tests/ConfigurationTests.cs
@@ -0,0 +1,16 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Xunit;
+
+namespace Aspire.Microsoft.Extensions.Configuration.AzureAppConfiguration.Tests;
+
+public class ConfigurationTests
+{
+    [Fact]
+    public void EndpointUriIsNullByDefault()
+        => Assert.Null(new AzureAppConfigurationSettings().Endpoint);
+
+    // WIP: https://github.com/Azure/AppConfiguration-DotnetProvider/pull/645
+    // Tracing will be supported in the next 8.2.0 release
+}
