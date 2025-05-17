d349d3a5d0579553e9ce83e03ad705ebd827f97a
Sébastien Ros <sebastienros@gmail.com>
2025-05-17 12:41:27 -0700

    Fix build

--- tests/Shared/TestModuleInitializer.cs
+++ tests/Shared/TestModuleInitializer.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.Runtime.CompilerServices;
+using Aspire.TestUtilities;
 using EmptyFiles;
 
 namespace Aspire.Hosting.Azure.Tests;
