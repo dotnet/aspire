8d3a17c586733ef11512eb9474f9a5b846ca0b32
Adam Ratzman <adam@adamratzman.com>
2025-05-30 00:16:46 -0400

    check that a failed test will fail the build

--- extension/src/test/extension.test.ts
+++ extension/src/test/extension.test.ts
@@ -11,5 +11,6 @@ suite('Extension Test Suite', () => {
 	test('Sample test', () => {
 		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
 		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+		assert.strictEqual(3, [1, 2, 3].indexOf(0));
 	});
 });
