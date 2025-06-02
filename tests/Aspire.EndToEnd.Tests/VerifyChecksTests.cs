c3d0e3128ecbd63887af53bc4f1057bf8fc699d1
Simon Cropp <simon.cropp@gmail.com>
2025-06-02 15:39:40 +1000

    Apply Verify conventions

---
+++ tests/Aspire.EndToEnd.Tests/VerifyChecksTests.cs
@@ -0,0 +1,9 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+public class VerifyChecksTests
+{
+    [Fact]
+    public Task Run() =>
+        VerifyChecks.Run();
+}
--- tests/Shared/TemplatesTesting/ProjectInfo.cs
+++ tests/Shared/TemplatesTesting/ProjectInfo.cs
@@ -3,7 +3,6 @@
 
 using System.Text.Json;
 using System.Text.Json.Serialization;
-using Xunit;
 
 namespace Aspire.Templates.Tests;
 
