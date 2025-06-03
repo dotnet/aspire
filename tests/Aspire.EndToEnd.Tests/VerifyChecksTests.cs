90fd58b87b3ab1d80bb0dfeb7b249aba325bd20e
Simon Cropp <simon.cropp@gmail.com>
2025-06-03 20:42:09 +1000

    Update VerifyChecksTests.cs

--- tests/Aspire.EndToEnd.Tests/VerifyChecksTests.cs
+++ tests/Aspire.EndToEnd.Tests/VerifyChecksTests.cs
@@ -1,12 +1,11 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-namespace Aspire.EndToEnd.Tests
+namespace Aspire.EndToEnd.Tests;
+
+public class VerifyChecksTests
 {
-    public class VerifyChecksTests
-    {
-        [Fact]
-        public Task Run() =>
-            VerifyChecks.Run();
-    }
+    [Fact]
+    public Task Run() =>
+        VerifyChecks.Run();
 }
