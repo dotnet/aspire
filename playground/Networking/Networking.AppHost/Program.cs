ae899d67ba83124944114ab0f9d07197a1fd3a15
James Newton-King <james@newtonking.com>
2025-06-07 07:09:09 +0800

    WIP

--- playground/Networking/Networking.AppHost/Program.cs
+++ playground/Networking/Networking.AppHost/Program.cs
@@ -5,12 +5,14 @@ var builder = DistributedApplication.CreateBuilder(args);
     // The HTTP endpoint is also available from the host machine at http://localhost:8080
     var s1Echo = builder.AddContainer("s1-echo", "hashicorp/http-echo")
         .WithHttpEndpoint(8080, targetPort: 5678);
-    s1Echo.WithEnvironment("ECHO_TEXT", $"Hello from {s1Echo.GetEndpoint("http")}");
+    var s1endpoint = s1Echo.GetEndpoint("http");
+    s1Echo.WithEnvironment("ECHO_TEXT", $"Hello from {s1endpoint}");
 
     builder.AddContainer("s1-http-client", "curlimages/curl")
         .WithArgs("-c", "sleep 1 && curl \"$TARGET_URL\"")
         .WithEntrypoint("/bin/sh")
-        .WithEnvironment("TARGET_URL", s1Echo.GetEndpoint("http"))
+        .WithEnvironment("TARGET_URL", s1endpoint)
+        .WithEnvironment(c => c.EnvironmentVariables.Add("TARGET_URL_ALT", s1endpoint.Url))
         .WaitFor(s1Echo);
 }
 
@@ -30,12 +32,14 @@ var builder = DistributedApplication.CreateBuilder(args);
 {
     var s3Echo = builder.AddProject<Projects.Networking_Echo>("s3-echo", launchProfileName: null)
         .WithHttpEndpoint(5158);
-    s3Echo.WithEnvironment("ECHO_TEXT", $"Hello from {s3Echo.GetEndpoint("http")}");
+    var s3endpoint = s3Echo.GetEndpoint("http");
+    s3Echo.WithEnvironment("ECHO_TEXT", $"Hello from {s3endpoint}");
 
     builder.AddContainer("s3-http-client", "curlimages/curl")
         .WithArgs("-c", "sleep 1 && curl \"$TARGET_URL\"")
         .WithEntrypoint("/bin/sh")
-        .WithEnvironment("TARGET_URL", s3Echo.GetEndpoint("http"))
+        .WithEnvironment("TARGET_URL", s3endpoint)
+        .WithEnvironment(c => c.EnvironmentVariables.Add("TARGET_URL_ALT", s3endpoint.Url))
         .WaitFor(s3Echo);
 }
 
@@ -43,10 +47,12 @@ var builder = DistributedApplication.CreateBuilder(args);
 {
     var s4Echo = builder.AddProject<Projects.Networking_Echo>("s4-echo", launchProfileName: null)
         .WithHttpEndpoint(5159);
-    s4Echo.WithEnvironment("ECHO_TEXT", $"Hello from {s4Echo.GetEndpoint("http")}");
+    var s4endpoint = s4Echo.GetEndpoint("http");
+    s4Echo.WithEnvironment("ECHO_TEXT", $"Hello from {s4endpoint}");
 
     builder.AddProject<Projects.Networking_HttpClient>("s4-http-client")
-        .WithEnvironment("TARGET_URL", s4Echo.GetEndpoint("http"))
+        .WithEnvironment("TARGET_URL", s4endpoint)
+        .WithEnvironment(c => c.EnvironmentVariables.Add("TARGET_URL_ALT", s4endpoint.Url))
         .WaitFor(s4Echo);
 }
 
@@ -54,10 +60,12 @@ var builder = DistributedApplication.CreateBuilder(args);
 {
     var s5Echo = builder.AddContainer("s5-echo", "hashicorp/http-echo")
         .WithHttpEndpoint(8081, targetPort: 5678);
-    s5Echo.WithEnvironment("ECHO_TEXT", $"Hello from {s5Echo.GetEndpoint("http")}");
+    var s5endpoint = s5Echo.GetEndpoint("http");
+    s5Echo.WithEnvironment("ECHO_TEXT", $"Hello from {s5endpoint}");
 
     builder.AddProject<Projects.Networking_HttpClient>("s5-http-client")
-        .WithEnvironment("TARGET_URL", s5Echo.GetEndpoint("http"))
+        .WithEnvironment("TARGET_URL", s5endpoint)
+        .WithEnvironment(c => c.EnvironmentVariables.Add("TARGET_URL_ALT", s5endpoint.Url))
         .WaitFor(s5Echo);
 }
 
