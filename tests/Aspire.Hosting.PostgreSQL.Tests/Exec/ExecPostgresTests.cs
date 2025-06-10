cc8dc8b55c48b6971b5406c9e315e2c83da1f50a
Dmitrii Korolev <dmkorolev@microsoft.com>
2025-06-10 11:01:11 -0700

    podman preparation

--- tests/Aspire.Hosting.PostgreSQL.Tests/Exec/ExecPostgresTests.cs
+++ tests/Aspire.Hosting.PostgreSQL.Tests/Exec/ExecPostgresTests.cs
@@ -5,6 +5,7 @@ using Aspire.Hosting.ApplicationModel;
 using Aspire.Hosting.Testing;
 using Aspire.Hosting.Tests.Utils;
 using Aspire.Hosting.Utils;
+using Aspire.TestUtilities;
 using Microsoft.Extensions.Hosting;
 using Projects;
 using Xunit;
@@ -14,16 +15,30 @@ namespace Aspire.Hosting.PostgreSQL.Tests.Exec;
 public class ExecPostgresTests(ITestOutputHelper output)
 {
     [Fact]
+    [RequiresDocker]
     public async Task Postgre()
     {
         // if you run aspire.cli against some random appHost csproj,
         // it will end up running something like:
         // 'run --no-build  --project C:\code\aspire\tests\TestingAppHost1\TestingAppHost1.AppHost\TestingAppHost1.AppHost.csproj --'
 
-        string[] args = [ " " ];
-        Action<DistributedApplicationOptions, HostApplicationBuilderSettings> configureBuilder = (_, _) => { };
+        string[] args = [
+            // separate type of command 'exec'
+            "exec",
+            // what AppHost to target
+            "--project", @"C:\code\aspire\tests\TestingAppHost1\TestingAppHost1.AppHost\TestingAppHost1.AppHost.csproj",
+            // what resource to target
+            "--resource", "mywebapp1",
+            // command to execute against resource
+            "--command", "dotnet ef migrations add Initial"
+        ];
+        Action<DistributedApplicationOptions, HostApplicationBuilderSettings> configureBuilder = (appOptions, _) =>
+        {
+        };
 
-        var builder = DistributedApplicationTestingBuilder.Create(args, configureBuilder)
+        Environment.SetEnvironmentVariable("ASPIRE_CONTAINER_RUNTIME", "podman");
+
+        var builder = DistributedApplicationTestingBuilder.Create(args, configureBuilder, typeof(TestingAppHost1_AppHost).Assembly)
             .WithTestAndResourceLogging(output);
 
         var postgres = builder
@@ -32,6 +47,7 @@ public class ExecPostgresTests(ITestOutputHelper output)
 
         var project = builder
             .AddProject<TestingAppHost1_MyWebApp>("mywebapp1")
+            .WithEnvironment("LAUNCH_PROFILE_VAR_FROM_APP_HOST", builder.Configuration["LAUNCH_PROFILE_VAR_FROM_APP_HOST"])
             .WithReference(postgres);
 
         await using var app = await builder.BuildAsync();
