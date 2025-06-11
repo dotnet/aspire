f51b9c20f11d15ed2a991fd0c11f26165e5c5f75
Mitch Denny <midenn@microsoft.com>
2025-06-11 04:12:06 +0000

    Use default colors for prompts in CLI.

--- src/Aspire.Cli/Interaction/InteractionService.cs
+++ src/Aspire.Cli/Interaction/InteractionService.cs
@@ -21,7 +21,6 @@ internal class InteractionService : IInteractionService
     {
         return await _ansiConsole.Status()
             .Spinner(Spinner.Known.Dots3)
-            .SpinnerStyle(Style.Parse("purple"))
             .StartAsync(statusText, (context) => action());
     }
 
@@ -29,7 +28,6 @@ internal class InteractionService : IInteractionService
     {
         _ansiConsole.Status()
             .Spinner(Spinner.Known.Dots3)
-            .SpinnerStyle(Style.Parse("purple"))
             .Start(statusText, (context) => action());
     }
 
@@ -69,8 +67,7 @@ internal class InteractionService : IInteractionService
             .UseConverter(choiceFormatter)
             .AddChoices(choices)
             .PageSize(10)
-            .EnableSearch()
-            .HighlightStyle(Style.Parse("darkmagenta"));
+            .EnableSearch();
 
         return await _ansiConsole.PromptAsync(prompt, cancellationToken);
     }
