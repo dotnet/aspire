1d805f413f90e4bbb79fc6f9ba9cf3c89e9e9f69
Matt Kotsenas <matt.kotsenas@gmail.com>
2025-06-12 19:14:16 -0700

    Move snapshots to directory

---
+++ tests/Aspire.Hosting.Tests/TestModuleInitializer.cs
@@ -0,0 +1,27 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Runtime.CompilerServices;
+using Aspire.TestUtilities;
+
+namespace Aspire.Hosting.Azure.Tests;
+
+sealed class TestModuleInitializer
+{
+    [ModuleInitializer]
+    internal static void Setup()
+    {
+        // Set the directory for all Verify calls in test projects
+        var target = PlatformDetection.IsRunningOnHelix
+            ? Path.Combine(Environment.GetEnvironmentVariable("HELIX_CORRELATION_PAYLOAD")!, "Snapshots")
+            : "Snapshots";
+
+        // If target contains an absolute path it will use it as is.
+        // If it contains a relative path, it will be combined with the project directory.
+        DerivePathInfo(
+            (sourceFile, projectDirectory, type, method) => new(
+                directory: Path.Combine(projectDirectory, target),
+                typeName: type.Name,
+                methodName: method.Name));
+    }
+}
