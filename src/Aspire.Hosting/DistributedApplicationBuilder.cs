08e6e224b00a8117b7b834ef60e45f835419ca9c
Dmitrii Korolev <dmkorolev@microsoft.com>
2025-06-17 09:54:06 +0200

    fix build

--- src/Aspire.Hosting/DistributedApplicationBuilder.cs
+++ src/Aspire.Hosting/DistributedApplicationBuilder.cs
@@ -265,7 +265,6 @@ public class DistributedApplicationBuilder : IDistributedApplicationBuilder
         // Aspire CLI support
         _innerBuilder.Services.AddHostedService<CliOrphanDetector>();
         _innerBuilder.Services.AddSingleton<BackchannelService>();
-        _innerBuilder.Services.TryAddSingleton<ICliRpcTarget, BackchannelService>();
         _innerBuilder.Services.AddHostedService<BackchannelService>(sp => sp.GetRequiredService<BackchannelService>());
         _innerBuilder.Services.AddSingleton<AppHostRpcTarget>();
 
