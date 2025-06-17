24bb8af944a7881c369795bd4e00f9472588c996
James Newton-King <james@newtonking.com>
2025-06-17 13:50:08 +0800

    Comments

--- tests/Aspire.Dashboard.Tests/ConsoleLogsTests/LogEntriesTests.cs
+++ tests/Aspire.Dashboard.Tests/ConsoleLogsTests/LogEntriesTests.cs
@@ -32,15 +32,18 @@ public class LogEntriesTests
         // Act
         var logParser = new LogParser(ConsoleColor.Black);
 
+        // Insert log with no timestamp.
         var logEntry1 = logParser.CreateLogEntry("Test", isErrorOutput: false);
         logEntries.InsertSorted(logEntry1);
 
+        // Insert log with timestamp that ensures collections timestamp value is not zero.
         var logEntry2 = logParser.CreateLogEntry("2024-08-19T06:12:01.000Z Test", isErrorOutput: false);
         logEntries.InsertSorted(logEntry2);
 
         logEntries.Clear(keepActivePauseEntries: true);
         logEntries.BaseLineNumber = 0;
 
+        // Insert another log entry after clearing.
         var logEntry3 = logParser.CreateLogEntry("2024-08-19T06:12:02.000Z Test", isErrorOutput: false);
         logEntries.InsertSorted(logEntry3);
 
