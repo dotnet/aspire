589ba2d58fd8cc40c9db6a0701f2a7b621a9866c
Adam Ratzman <adam@adamratzman.com>
2025-06-18 00:06:02 -0400

    fix tests

--- extension/src/test/rpc/e2eServerTests.test.ts
+++ extension/src/test/rpc/e2eServerTests.test.ts
@@ -42,7 +42,7 @@ suite('End-to-end RPC server auth tests', () => {
 		const rpcServerInfo = extension.exports.getRpcServerInfo() as RpcServerInformation;
 
 		// Connect as a client
-		const client = net.createConnection(rpcServerInfo.address);
+		const client = net.createConnection(rpcServerInfo.address.replace('localhost:', ''));
 		await new Promise<void>((resolve) => client.once('connect', resolve));
 
 		const connection = createMessageConnection(
