7d8a415215914a23933b232876e753c4ba9e32d2
Safia Abdalla <safia@safia.rocks>
2025-06-25 18:29:22 -0700

    Derive status from CompletionState and update TestAppHostBackchannel

--- src/Aspire.Cli/Backchannel/BackchannelDataTypes.cs
+++ src/Aspire.Cli/Backchannel/BackchannelDataTypes.cs
@@ -107,17 +107,17 @@ internal sealed class PublishingActivityData
     /// <summary>
     /// Gets a value indicating whether the publishing activity is complete.
     /// </summary>
-    public bool IsComplete { get; init; }
+    public bool IsComplete => CompletionState is not CompletionStates.InProgress;
 
     /// <summary>
     /// Gets a value indicating whether the publishing activity encountered an error.
     /// </summary>
-    public bool IsError { get; init; }
+    public bool IsError => CompletionState is CompletionStates.CompletedWithError;
 
     /// <summary>
     /// Gets a value indicating whether the publishing activity completed with warnings.
     /// </summary>
-    public bool IsWarning { get; init; }
+    public bool IsWarning => CompletionState is CompletionStates.CompletedWithWarning;
 
     /// <summary>
     /// Gets the identifier of the step this task belongs to (only applicable for tasks).
