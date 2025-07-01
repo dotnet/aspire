96f15edf5bddc6706391e8dab265b2c64b21a6e6
James Newton-King <james@newtonking.com>
2025-07-01 16:22:45 +0800

    Add Aspire upgrade check (#10034)

--- src/Shared/KnownConfigNames.cs
+++ src/Shared/KnownConfigNames.cs
@@ -7,6 +7,7 @@ internal static class KnownConfigNames
 {
     public const string AspNetCoreUrls = "ASPNETCORE_URLS";
     public const string AllowUnsecuredTransport = "ASPIRE_ALLOW_UNSECURED_TRANSPORT";
+    public const string VersionCheckDisabled = "ASPIRE_VERSION_CHECK_DISABLED";
     public const string DashboardOtlpGrpcEndpointUrl = "ASPIRE_DASHBOARD_OTLP_ENDPOINT_URL";
     public const string DashboardOtlpHttpEndpointUrl = "ASPIRE_DASHBOARD_OTLP_HTTP_ENDPOINT_URL";
     public const string DashboardFrontendBrowserToken = "ASPIRE_DASHBOARD_FRONTEND_BROWSERTOKEN";
