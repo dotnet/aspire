6ed6d999feffaad5ffc4b2a6d86e92495d3e3710
James Newton-King <james@newtonking.com>
2025-07-03 06:00:19 +0800

    Fix build

--- src/Aspire.Hosting/ApplicationModel/InteractionService.cs
+++ src/Aspire.Hosting/ApplicationModel/InteractionService.cs
@@ -3,7 +3,6 @@
 
 using System.Collections.ObjectModel;
 using System.Diagnostics;
-using System.Diagnostics.CodeAnalysis;
 using System.Runtime.CompilerServices;
 using System.Threading.Channels;
 using Microsoft.Extensions.Logging;
