040491a5a97b0fd7799098cb8f0d0fcf13f59b8a
Damian Edwards <damian@damianedwards.com>
2025-07-02 11:50:02 -0700

    Remove warning on publish of external service with parameterized URL

--- playground/ExternalServices/ExternalServices.AppHost/AppHost.cs
+++ playground/ExternalServices/ExternalServices.AppHost/AppHost.cs
@@ -21,6 +21,7 @@ var externalGateway = builder.AddYarp("gateway")
 
 builder.AddProject<Projects.WebFrontEnd>("frontend")
        .WithReference(nuget)
+       .WithReference(externalService)
        .WithEnvironment("EXTERNAL_SERVICE_URL", externalService)
        .WithReference(externalGateway);
 
