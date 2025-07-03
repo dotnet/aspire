4108a13df9bc593c31866e45ad1e9c43cfbc44d5
Sebastien Ros <sebastienros@gmail.com>
2025-07-03 17:33:39 +0200

    Add nameless storage resources

--- src/Components/Aspire.Azure.Storage.Blobs/AspireBlobStorageExtensions.cs
+++ src/Components/Aspire.Azure.Storage.Blobs/AspireBlobStorageExtensions.cs
@@ -45,24 +45,7 @@ public static partial class AspireBlobStorageExtensions
         new BlobStorageComponent().AddClient(builder, DefaultConfigSectionName, configureSettings, configureClientBuilder, connectionName, serviceKey: null);
     }
 
-    /// <summary>
-    ///  Registers <see cref="BlobServiceClient"/> as a singleton in the services provided by the <paramref name="builder"/>.
-    ///  Enables retries, corresponding health check, logging and telemetry.
-    /// </summary>
-    /// <param name="builder">The <see cref="IHostApplicationBuilder" /> to read config from and add services to.</param>
-    /// <param name="connectionName">
-    ///  A name used to retrieve the connection string from the ConnectionStrings configuration section.
-    /// </param>
-    /// <param name="configureSettings">
-    ///  An optional method that can be used for customizing the <see cref="AzureStorageBlobsSettings"/>. It's invoked after the settings are read from the configuration.
-    /// </param>
-    /// <param name="configureClientBuilder">
-    ///  An optional method that can be used for customizing the <see cref="IAzureClientBuilder{TClient, TOptions}"/>.
-    /// </param>
-    /// <remarks>Reads the configuration from "Aspire:Azure:Storage:Blobs" section.</remarks>
-    /// <exception cref="InvalidOperationException">
-    ///  Neither <see cref="AzureStorageBlobsSettings.ConnectionString"/> nor <see cref="AzureStorageBlobsSettings.ServiceUri"/> is provided.
-    /// </exception>
+    /// <inheritdoc cref="AddAzureBlobServiceClient"/>
     [Obsolete("Use AddAzureBlobServiceClient instead. This method will be removed in a future version.")]
     public static void AddAzureBlobClient(
         this IHostApplicationBuilder builder,
@@ -108,26 +91,8 @@ public static partial class AspireBlobStorageExtensions
         new BlobStorageComponent().AddClient(builder, DefaultConfigSectionName, configureSettings, configureClientBuilder, connectionName: name, serviceKey: name);
     }
 
-    /// <summary>
-    ///  Registers <see cref="BlobServiceClient"/> as a singleton for given <paramref name="name"/> in the services provided by the <paramref name="builder"/>.
-    ///  Enables retries, corresponding health check, logging and telemetry.
-    /// </summary>
-    /// <param name="builder">The <see cref="IHostApplicationBuilder" /> to read config from and add services to.</param>
-    /// <param name="name">
-    ///  The name of the component, which is used as the <see cref="ServiceDescriptor.ServiceKey"/> of the service and also to retrieve
-    ///  the connection string from the ConnectionStrings configuration section.
-    /// </param>
-    /// <param name="configureSettings">
-    ///  An optional method that can be used for customizing the <see cref="AzureStorageBlobsSettings"/>.
-    ///  It's invoked after the settings are read from the configuration.
-    /// </param>
-    /// <param name="configureClientBuilder">
-    ///  An optional method that can be used for customizing the <see cref="IAzureClientBuilder{TClient, TOptions}"/>.
-    /// </param>
-    /// <remarks>Reads the configuration from "Aspire:Azure:Storage:Blobs:{name}" section.</remarks>
-    /// <exception cref="InvalidOperationException">
-    ///  Neither <see cref="AzureStorageBlobsSettings.ConnectionString"/> nor <see cref="AzureStorageBlobsSettings.ServiceUri"/> is provided.
-    /// </exception>
+    /// <inheritdoc cref="AddKeyedAzureBlobServiceClient"/>
+    [Obsolete("Use AddKeyedAzureBlobServiceClient instead. This method will be removed in a future version.")]
     public static void AddKeyedAzureBlobClient(
         this IHostApplicationBuilder builder,
         string name,
