d5e5d03cfee3ef0ec45ec408212dc2903692c33f
James Newton-King <james@newtonking.com>
2025-07-11 10:47:52 +0800

    Clean up

--- src/Aspire.Dashboard/Components/Pages/Login.razor.js
+++ src/Aspire.Dashboard/Components/Pages/Login.razor.js
@@ -1,6 +1,6 @@
 export function validateToken(token) {
-    // Module import errors in the browser could have been caused by this function using async/await
-    // and some browsers not supporting it. Use promises instead.
+    // Module import errors in the browser could have been caused by this function using async/await and some browsers not supporting it.
+    // Use promises instead.
     var url = `/api/validatetoken?token=${encodeURIComponent(token)}`;
     return fetch(url, { method: 'POST' })
         .then(response => response.text())
