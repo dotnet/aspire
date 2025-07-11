4ef72e4a901b22f1c411c45dd66fb1b5c9bf99a4
Eric Erhardt <eric.erhardt@microsoft.com>
2025-07-11 11:06:24 -0500

    Remove ToProvisioningEntity on AzureTableStorageResource
    This doesn't work because of https://github.com/Azure/azure-sdk-for-net/issues/51210. Reverting back to the 9.3 behavior of not creating the bicep resource at all, which is fine for now since we don't have child table resources yet.

--- src/Aspire.Hosting.Azure.Storage/AzureTableStorageResource.cs
+++ src/Aspire.Hosting.Azure.Storage/AzureTableStorageResource.cs
@@ -2,7 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using Aspire.Hosting.ApplicationModel;
-using Azure.Provisioning;
 
 namespace Aspire.Hosting.Azure;
 
@@ -40,48 +39,5 @@ public class AzureTableStorageResource(string name, AzureStorageResource storage
             // Injected to support Aspire client integration for Azure Storage Tables.
             target[$"{AzureStorageResource.TablesConnectionKeyPrefix}__{connectionName}__ServiceUri"] = Parent.TableEndpoint; // Updated for consistency
         }
-    }
-
-    /// <summary>
-    /// Converts the current instance to a provisioning entity.
-    /// </summary>
-    /// <returns>A <see cref="global::Azure.Provisioning.Storage.TableService"/> instance.</returns>
-    internal global::Azure.Provisioning.Storage.TableService ToProvisioningEntity()
-    {
-        // Create the TableService with the correct name
-        global::Azure.Provisioning.Storage.TableService service = new(Infrastructure.NormalizeBicepIdentifier(Name));
-        
-        // Set the name using internal infrastructure, similar to how it's done for other storage services
-        // TableService requires the name to be "default" like BlobService and QueueService
-        try
-        {
-            // Use reflection to access the internal _name property/field
-            var nameProperty = service.GetType().GetProperty("Name", System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Instance);
-            if (nameProperty is not null && nameProperty.CanWrite)
-            {
-                nameProperty.SetValue(service, "default");
-            }
-            else
-            {
-                // Try to set via backing field if property is read-only
-                var fields = service.GetType().GetFields(System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
-                foreach (var field in fields)
-                {
-                    if (field.Name.EndsWith("name>k__BackingField", StringComparison.OrdinalIgnoreCase) ||
-                        field.Name.Equals("_name", StringComparison.OrdinalIgnoreCase) ||
-                        field.FieldType == typeof(string) && field.Name.Contains("name", StringComparison.OrdinalIgnoreCase))
-                    {
-                        field.SetValue(service, "default");
-                        break;
-                    }
-                }
-            }
-        }
-        catch (Exception)
-        {
-            // If reflection fails, the service will still work but may not have the name set
-        }
-        
-        return service;
     }
 }
