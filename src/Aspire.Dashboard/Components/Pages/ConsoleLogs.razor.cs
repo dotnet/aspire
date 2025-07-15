1042722bfb749e12c97b0532c720f4025f620faf
Adam Ratzman <adam@adamratzman.com>
2025-07-14 23:20:16 -0400

    move updateresourcelist down, add return after view model changed call

--- src/Aspire.Dashboard/Components/Pages/ConsoleLogs.razor.cs
+++ src/Aspire.Dashboard/Components/Pages/ConsoleLogs.razor.cs
@@ -346,15 +346,16 @@ public sealed partial class ConsoleLogs : ComponentBase, IComponentWithTelemetry
             value =>
             {
                 _showHiddenResources = value;
-                UpdateResourcesList();
 
                 if (!_showHiddenResources && PageViewModel.SelectedResource?.IsResourceHidden(showHiddenResources: false) is true)
                 {
                     PageViewModel.SelectedResource = null;
                     PageViewModel.SelectedOption = _noSelection;
                     await this.AfterViewModelChangedAsync(_contentLayout, false);
+                    return;
                 }
 
+                UpdateResourcesList();
                 UpdateMenuButtons();
             }));
 
