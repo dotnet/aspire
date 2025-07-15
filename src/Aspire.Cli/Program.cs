f74714d9180e214ce6a84000d92de8712316db43
Eric Erhardt <eric.erhardt@microsoft.com>
2025-07-15 18:23:53 -0500

    Use CreateEmptyApplicationBuilder in Aspire.Cli
    We are currently creating a default HostApplicationBuilder, which tries to load appsettings.json files in the CWD. This in turn is creating a FileWatcher for the CWD and all directories under it. Causing the startup of the CLI to suffer depending on the contents of the CWD.
    
    The fix is to use an empty HostApplicationBuilder, which doesn't add appsettings.json config files by default. It also means we don't have to clear logging providers anymore, because there aren't any in an empty builder.
    
    Fix #9765

--- src/Aspire.Cli/Program.cs
+++ src/Aspire.Cli/Program.cs
@@ -46,7 +46,7 @@ public class Program
 
     private static async Task<IHost> BuildApplicationAsync(string[] args)
     {
-        var builder = Host.CreateApplicationBuilder();
+        var builder = Host.CreateEmptyApplicationBuilder(settings: null);
 
         // Set up settings with appropriate paths.
         var globalSettingsFilePath = GetGlobalSettingsPath();
@@ -55,8 +55,6 @@ public class Program
         ConfigurationHelper.RegisterSettingsFiles(builder.Configuration, workingDirectory, globalSettingsFile);
 
         await TrySetLocaleOverrideAsync(builder.Configuration);
-
-        builder.Logging.ClearProviders();
 
         // Always configure OpenTelemetry.
         builder.Logging.AddOpenTelemetry(logging => {
