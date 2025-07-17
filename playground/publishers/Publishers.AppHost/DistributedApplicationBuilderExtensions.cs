c5666f89efe7f159a42fade7f98d47e3219fe299
James Newton-King <james@newtonking.com>
2025-07-17 22:08:56 +0800

    Fix interaction provider in dashboard handling duplicate messages

--- playground/publishers/Publishers.AppHost/DistributedApplicationBuilderExtensions.cs
+++ playground/publishers/Publishers.AppHost/DistributedApplicationBuilderExtensions.cs
@@ -5,6 +5,7 @@
 #pragma warning disable ASPIRECOMPUTE001
 #pragma warning disable ASPIREINTERACTION001
 
+using System.Diagnostics;
 using Microsoft.Extensions.DependencyInjection;
 
 internal static class IDistributedApplicationBuilderExtensions
@@ -88,6 +89,8 @@ internal static class IDistributedApplicationBuilderExtensions
             var appName = multiInputResult.Canceled ? "default-app" : (multiInputResult.Data?.FirstOrDefault(i => i.Label == "Application Name")?.Value ?? "default-app");
             var appVersion = multiInputResult.Canceled ? "1.0.0" : (multiInputResult.Data?.FirstOrDefault(i => i.Label == "Application Version")?.Value ?? "1.0.0");
             var sslType = multiInputResult.Canceled ? "self-signed" : (multiInputResult.Data?.FirstOrDefault(i => i.Label == "SSL Certificate Type")?.Value ?? "self-signed");
+
+            Debugger.Launch();
 
             // Test Text input
             var envResult = await interactionService.PromptInputAsync(
