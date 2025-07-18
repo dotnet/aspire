bb59c89c900bdd446892d202296a6ab3079e974d
Safia Abdalla <safia@microsoft.com>
2025-07-18 04:59:38 +0000

    Check for OutputPath

--- tests/Aspire.Hosting.Tests/Publishing/ResourceContainerImageBuilderTests.cs
+++ tests/Aspire.Hosting.Tests/Publishing/ResourceContainerImageBuilderTests.cs
@@ -407,7 +407,8 @@ public class ResourceContainerImageBuilderTests(ITestOutputHelper output)
 
         // This should not fail despite the fake container runtime being configured to fail
         // because we only have project resources (no DockerfileBuildAnnotation)
-        await imageBuilder.BuildImagesAsync([servicea.Resource], options: new ContainerBuildOptions { ImageFormat = ContainerImageFormat.Oci }, cts.Token);
+        var options = new ContainerBuildOptions { ImageFormat = ContainerImageFormat.Oci, OutputPath = "/tmp/test-path" };
+        await imageBuilder.BuildImagesAsync([servicea.Resource], options: options, cts.Token);
 
         // Validate that the container runtime health check was not called
         Assert.False(fakeContainerRuntime.WasHealthCheckCalled);
