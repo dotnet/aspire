5447e45ea826e5c51c96b8c277c55502e72ed61f
Mitch Denny <midenn@microsoft.com>
2025-07-19 20:35:33 +1000

    [release/9.4] Backport of 10422 (#10555)
    * drop dependencies (#10422)
    
    * Add missing using.
    
    * Add missing usings.
    
    ---------
    
    Co-authored-by: Korolev Dmitry <dmkorolev@microsoft.com>

---
+++ tests/Aspire.Hosting.Tests/Backchannel/Exec/ExecTestsBase.cs
@@ -0,0 +1,68 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Aspire.Hosting.Backchannel;
+using Aspire.Hosting.Testing;
+using Aspire.Hosting.Tests.Utils;
+using Aspire.Hosting.Utils;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace Aspire.Hosting.Tests.Backchannel.Exec;
+
+public abstract class ExecTestsBase(ITestOutputHelper outputHelper)
+{
+    protected readonly ITestOutputHelper _outputHelper = outputHelper;
+
+    /// <summary>
+    /// Performs an `exec` against the apphost,
+    /// collecting the logs of the `exec` resource apphost is being run against.
+    ///
+    /// Also awaits the app startup. It has to be built before running this method.
+    /// </summary>
+    internal async Task<List<CommandOutput>> ExecWithLogCollectionAsync(
+        DistributedApplication app,
+        int timeoutSec = 30)
+    {
+        var cts = new CancellationTokenSource(TimeSpan.FromSeconds(timeoutSec));
+
+        var appHostRpcTarget = app.Services.GetRequiredService<AppHostRpcTarget>();
+        var outputStream = appHostRpcTarget.ExecAsync(cts.Token);
+
+        var logs = new List<CommandOutput>();
+        var startTask = app.StartAsync(cts.Token);
+        await foreach (var message in outputStream)
+        {
+            var logLevel = message.IsErrorMessage ? "error" : "info";
+            var log = $"Received output: #{message.LineNumber} [level={logLevel}] [type={message.Type}] {message.Text}";
+
+            logs.Add(message);
+            _outputHelper.WriteLine(log);
+        }
+
+        await startTask;
+        return logs;
+    }
+
+    internal static void AssertLogsContain(List<CommandOutput> logs, params string[] expectedLogMessages)
+    {
+        if (expectedLogMessages.Length == 0)
+        {
+            Assert.Empty(logs);
+            return;
+        }
+
+        foreach (var expectedMessage in expectedLogMessages)
+        {
+            var logFound = logs.Any(x => x.Text.Contains(expectedMessage));
+            Assert.True(logFound, $"Expected log message '{expectedMessage}' not found in logs.");
+        }
+    }
+
+    protected IDistributedApplicationTestingBuilder PrepareBuilder(string[] args)
+    {
+        var builder = TestDistributedApplicationBuilder.Create(_outputHelper, args).WithTestAndResourceLogging(_outputHelper);
+        builder.Configuration[KnownConfigNames.UnixSocketPath] = UnixSocketHelper.GetBackchannelSocketPath();
+        return builder;
+    }
+}
---
+++ tests/Aspire.Hosting.Tests/Backchannel/Exec/ProjectResourceExecTests.cs
@@ -0,0 +1,144 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Aspire.Hosting.Testing;
+using Xunit;
+
+namespace Aspire.Hosting.Tests.Backchannel.Exec;
+
+public class ProjectResourceExecTests : ExecTestsBase
+{
+    public ProjectResourceExecTests(ITestOutputHelper outputHelper)
+        : base(outputHelper)
+    {
+    }
+
+    [Fact]
+    public async Task Exec_NotFoundTargetResource_ShouldProduceLogs()
+    {
+        string[] args = [
+            "--operation", "run",
+            "--resource", "randomnonexistingresource",
+            "--command", "\"dotnet --info\"",
+        ];
+
+        using var builder = PrepareBuilder(args);
+        WithTestProjectResource(builder);
+
+        using var app = builder.Build();
+
+        var logs = await ExecWithLogCollectionAsync(app);
+        AssertLogsContain(logs, "Target resource randomnonexistingresource not found in the model resources");
+
+        await app.StopAsync().WaitAsync(TimeSpan.FromSeconds(60));
+    }
+
+    [Fact]
+    public async Task Exec_DotnetBuildFail_ProducesLogs_Fail()
+    {
+        string[] args = [
+            "--operation", "run",
+            "--resource", "test",
+                         // not existing csproj, but we dont care if that succeeds or not - we are expecting
+                         // whatever log output from the command
+            "--command", "\"dotnet build \"MyRandom.csproj\"\"",
+        ];
+
+        using var builder = PrepareBuilder(args);
+        WithTestProjectResource(builder);
+
+        using var app = builder.Build();
+
+        var logs = await ExecWithLogCollectionAsync(app);
+        AssertLogsContain(logs, "Project file does not exist", "Aspire exec exit code: 1");
+
+        await app.StopAsync().WaitAsync(TimeSpan.FromSeconds(60));
+    }
+
+    [Fact]
+    public async Task Exec_NonExistingCommand_ProducesLogs_Fail()
+    {
+        string[] args = [
+            "--operation", "run",
+            "--resource", "test",
+                         // not existing command. Executable should fail without start basically
+            "--command", "\"randombuildcommand doit\"",
+        ];
+
+        using var builder = PrepareBuilder(args);
+        WithTestProjectResource(builder);
+
+        using var app = builder.Build();
+
+        var logs = await ExecWithLogCollectionAsync(app);
+        AssertLogsContain(logs, "Aspire exec failed to start");
+
+        await app.StopAsync().WaitAsync(TimeSpan.FromSeconds(60));
+    }
+
+    [Fact]
+    public async Task Exec_DotnetInfo_ProducesLogs_Success()
+    {
+        string[] args = [
+            "--operation", "run",
+            "--resource", "test",
+            "--command", "\"dotnet --info\"",
+        ];
+
+        using var builder = PrepareBuilder(args);
+        WithTestProjectResource(builder);
+
+        using var app = builder.Build();
+
+        var logs = await ExecWithLogCollectionAsync(app);
+        AssertLogsContain(logs,
+            ".NET SDKs installed", // command logs
+            "Aspire exec exit code: 0" // exit code is submitted separately from the command logs
+        );
+
+        await app.StopAsync().WaitAsync(TimeSpan.FromSeconds(60));
+    }
+
+    [Fact]
+    public async Task Exec_DotnetHelp_ProducesLogs_Success()
+    {
+        string[] args = [
+            "--operation", "run",
+            "--resource", "test",
+            "--command", "\"dotnet --help\"",
+        ];
+
+        using var builder = PrepareBuilder(args);
+        WithTestProjectResource(builder);
+
+        using var app = builder.Build();
+
+        var logs = await ExecWithLogCollectionAsync(app);
+        AssertLogsContain(logs,
+            "Usage: dotnet [sdk-options] [command] [command-options] [arguments]", // command logs
+            "Aspire exec exit code: 0" // exit code is submitted separately from the command logs
+        );
+
+        await app.StopAsync().WaitAsync(TimeSpan.FromSeconds(60));
+    }
+
+    private static void WithTestProjectResource(IDistributedApplicationTestingBuilder builder, string name = "test")
+    {
+        builder.AddResource(new TestProjectResource(name))
+               .WithInitialState(new()
+               {
+                   ResourceType = "TestProjectResource",
+                   State = new("Running", null),
+                   Properties = [new("A", "B"), new("c", "d")],
+                   EnvironmentVariables = [new("e", "f", true), new("g", "h", false)]
+               })
+               .WithAnnotation<IProjectMetadata>(new ProjectMetadata(Directory.GetCurrentDirectory()));
+    }
+}
+
+file sealed class TestProjectResource : ProjectResource
+{
+    public TestProjectResource(string name) : base(name)
+    {
+    }
+}
