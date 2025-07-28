f9e374170d173ab83c9fc6dab9855a825108898e
David Fowler <davidfowl@gmail.com>
2025-07-28 03:09:56 -0400

    Revert "Apply suggestions from code review"
    This reverts commit cfd02310f977be37848ad44b4936dfc07f397a0e.

--- tests/Shared/TemplatesTesting/AspireProject.cs
+++ tests/Shared/TemplatesTesting/AspireProject.cs
@@ -169,6 +169,7 @@ public partial class AspireProject : IAsyncDisposable
         var output = new StringBuilder();
         var projectsParsed = new TaskCompletionSource();
         var appRunning = new TaskCompletionSource();
+        var dashboardUrlParsed = new TaskCompletionSource();
         var stdoutComplete = new TaskCompletionSource();
         var stderrComplete = new TaskCompletionSource();
         AppExited = new();
@@ -213,6 +214,7 @@ public partial class AspireProject : IAsyncDisposable
             if (m.Success)
             {
                 DashboardUrl = m.Groups["url"].Value;
+                dashboardUrlParsed.SetResult();
             }
 
             if (line?.StartsWith("$ENDPOINTS: ") == true)
@@ -263,7 +265,7 @@ public partial class AspireProject : IAsyncDisposable
         AppHostProcess.BeginOutputReadLine();
         AppHostProcess.BeginErrorReadLine();
 
-        var successfulStartupTask = Task.WhenAll(appRunning.Task, projectsParsed.Task);
+        var successfulStartupTask = Task.WhenAll(appRunning.Task, dashboardUrlParsed.Task, projectsParsed.Task);
         var startupTimeoutTask = Task.Delay(TimeSpan.FromSeconds(AppStartupWaitTimeoutSecs), token);
 
         string outputMessage;
