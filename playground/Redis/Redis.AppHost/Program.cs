1a82dc35d7dc8f05d7169c2c92cdf24047993dbc
Eric Erhardt <eric.erhardt@microsoft.com>
2025-07-29 15:28:58 -0500

    Add support for Azure Redis enterprise
    This commit introduces a new Azure Redis Enterprise resource, enhancing the existing Redis functionality. Key changes include:
    
    - Introduced new Bicep modules for defining Azure Redis infrastructure.
    - Implemented extension methods in `AzureRedisEnterpriseExtensions.cs` for resource management.
    - Defined the Azure Redis Enterprise resource structure in `AzureRedisEnterpriseResource.cs`.
    
    Fix #6831

--- playground/Redis/Redis.AppHost/Program.cs
+++ playground/Redis/Redis.AppHost/Program.cs
@@ -1,9 +1,6 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var redis = builder.AddRedis("redis");
-redis.WithDataVolume()
-    .WithRedisCommander(c => c.WithHostPort(33803).WithParentRelationship(redis))
-    .WithRedisInsight(c => c.WithHostPort(41567).WithParentRelationship(redis));
+var redis = builder.AddAzureRedisEnterprise("redis");
 
 var garnet = builder.AddGarnet("garnet")
     .WithDataVolume();
