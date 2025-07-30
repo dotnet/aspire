fd2c3439934ae05e7859c974db96d932b89b1f0d
Jose Perez Rodriguez <joperezr@microsoft.com>
2025-07-30 14:15:42 -0700

    [Automated] Update API Surface Area

--- src/Aspire.Hosting.Azure.CognitiveServices/api/Aspire.Hosting.Azure.CognitiveServices.cs
+++ src/Aspire.Hosting.Azure.CognitiveServices/api/Aspire.Hosting.Azure.CognitiveServices.cs
@@ -72,6 +72,8 @@ namespace Aspire.Hosting.ApplicationModel
         [System.Obsolete("AzureOpenAIDeployment is deprecated.")]
         public System.Collections.Generic.IReadOnlyList<AzureOpenAIDeployment> Deployments { get { throw null; } }
 
+        public Azure.BicepOutputReference NameOutputReference { get { throw null; } }
+
         public override global::Azure.Provisioning.Primitives.ProvisionableResource AddAsExistingResource(Azure.AzureResourceInfrastructure infra) { throw null; }
     }
 }
