04d92cf8c1c3fe4ed29f92ff6cd59b8872952b5d
James Newton-King <james@newtonking.com>
2025-08-01 08:39:15 +0800

    Apply suggestions from code review

--- playground/Stress/Stress.AppHost/Program.cs
+++ playground/Stress/Stress.AppHost/Program.cs
@@ -15,15 +15,13 @@ builder.Services.AddHealthChecks().AddAsyncCheck("health-test", async (ct) =>
 for (var i = 0; i < 5; i++)
 {
     var name = $"test-{i:0000}";
-    var rb = builder.AddTestResource(name)
-                   .WithResourceIcon("TestBeaker");
+    var rb = builder.AddTestResource(name);
     IResource parent = rb.Resource;
 
     for (var j = 0; j < 3; j++)
     {
         name += $"-n{j}";
-        var nestedRb = builder.AddNestedResource(name, parent)
-                             .WithResourceIcon("FlaskConical", IconVariant.Regular);
+        var nestedRb = builder.AddNestedResource(name, parent);
         parent = nestedRb.Resource;
     }
 }
@@ -35,8 +33,7 @@ builder.AddContainer("hiddenContainer", "alpine")
         ResourceType = "CustomHiddenContainerType",
         Properties = [],
         IsHidden = true
-    })
-    .WithResourceIcon("Package");
+    });
 
 // TODO: OTEL env var can be removed when OTEL libraries are updated to 1.9.0
 // See https://github.com/open-telemetry/opentelemetry-dotnet/blob/main/RELEASENOTES.md#1100
@@ -93,7 +90,6 @@ builder.AddProject<Projects.Stress_TelemetryService>("stress-telemetryservice")
        .WithUrl("https://someotherplace.com/some-path", "Some other place")
        .WithUrl("https://extremely-long-url.com/abcdefghijklmnopqrstuvwxyz/abcdefghijklmnopqrstuvwxyz/abcdefghijklmnopqrstuvwxyz//abcdefghijklmnopqrstuvwxyz/abcdefghijklmnopqrstuvwxyz/abcdefghijklmnopqrstuvwxyz/abcdefghijklmnopqrstuvwxyz/abcdefghijklmno")
        .AddInteractionCommands()
-       .WithResourceIcon("ChartLineUp")
        .WithCommand(
            name: "long-command",
            displayName: "This is a custom command with a very long command display name",
