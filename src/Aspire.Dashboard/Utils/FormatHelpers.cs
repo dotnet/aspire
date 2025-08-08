03a23d7cb2035cc1db115bbc7c3e0451cd188649
James Newton-King <james@newtonking.com>
2025-08-08 10:22:15 +0800

    Truncate large messages displayed in log grid

--- src/Aspire.Dashboard/Utils/FormatHelpers.cs
+++ src/Aspire.Dashboard/Utils/FormatHelpers.cs
@@ -18,6 +18,10 @@ public enum MillisecondsDisplay
 
 internal static partial class FormatHelpers
 {
+    // Limit size of very long data that is written in large grids.
+    public const int ColumnMaximumTextLength = 250;
+    public const int TooltipMaximumTextLength = 1500;
+
     // There are an unbound number of CultureInfo instances so we don't want to use it as the key.
     // Someone could have also customized their culture so we don't want to use the name as the key.
     // This struct contains required information from the culture that is used in cached format strings.
@@ -132,5 +136,22 @@ internal static partial class FormatHelpers
             _ => throw new ArgumentException("Unexpected value.", nameof(maxDecimalPlaces))
         };
         return value.ToString(formatString, provider ?? CultureInfo.CurrentCulture);
+    }
+
+    public static string TruncateText(string text, int maxLength)
+    {
+        if (string.IsNullOrEmpty(text) || maxLength <= 0)
+        {
+            return string.Empty;
+        }
+
+        if (text.Length <= maxLength)
+        {
+            return text;
+        }
+
+        const string ellipsis = "…";
+
+        return string.Concat(text.AsSpan(0, maxLength - ellipsis.Length), ellipsis);
     }
 }
