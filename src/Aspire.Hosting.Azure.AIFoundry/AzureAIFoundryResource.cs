c49db4e2b33c0a68d54359ddba21740c886d72e4
Sebastien Ros <sebastienros@gmail.com>
2025-08-12 11:19:38 -0700

    Fix Azure AI Foundry resource name

--- src/Aspire.Hosting.Azure.AIFoundry/AzureAIFoundryResource.cs
+++ src/Aspire.Hosting.Azure.AIFoundry/AzureAIFoundryResource.cs
@@ -73,7 +73,4 @@ public class AzureAIFoundryResource(string name, Action<AzureResourceInfrastruct
 
         _deployments.Add(deployment);
     }
-
-    internal ReferenceExpression GetConnectionString(string deploymentName) =>
-        ReferenceExpression.Create($"{ConnectionStringExpression};DeploymentId={deploymentName};Model={deploymentName}");
 }
