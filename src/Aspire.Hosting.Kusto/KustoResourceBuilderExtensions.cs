a2f50fb65dfc8923199e9415c00f54ea4f597475
Matt Kotsenas <matt.kotsenas@gmail.com>
2025-08-12 13:56:39 -0700

    Add additional docs

--- src/Aspire.Hosting.Kusto/KustoResourceBuilderExtensions.cs
+++ src/Aspire.Hosting.Kusto/KustoResourceBuilderExtensions.cs
@@ -20,6 +20,14 @@ public static class KustoResourceBuilderExtensions
     /// <summary>
     /// Adds a Kusto resource to the application model.
     /// </summary>
+    /// <remarks>
+    /// <para>
+    /// When adding a <see cref="KustoResource"/> to your application model the resource can then
+    /// be referenced by other resources using the resource name. When the dependent resource is using
+    /// the extension method <see cref="ResourceBuilderExtensions.WaitFor{T}(IResourceBuilder{T}, IResourceBuilder{IResource})"/>
+    /// then the dependent resource will wait until the Kusto database is available.
+    /// </para>
+    /// </remarks>
     /// <param name="builder">The <see cref="IDistributedApplicationBuilder"/>.</param>
     /// <param name="name">The name of the resource. This name will be used as the connection string name when referenced in a dependency.</param>
     /// <returns>A reference to the <see cref="IResourceBuilder{T}"/>.</returns>
@@ -89,6 +97,9 @@ public static class KustoResourceBuilderExtensions
     /// <summary>
     /// Configures the Kusto resource to run as an emulator using the Kustainer container.
     /// </summary>
+    /// <remarks>
+    /// This version of the package defaults to the <inheritdoc cref="KustoEmulatorContainerImageTags.Tag"/> tag of the <inheritdoc cref="KustoEmulatorContainerImageTags.Registry"/>/<inheritdoc cref="KustoEmulatorContainerImageTags.Image"/> container image.
+    /// </remarks>
     /// <param name="builder">The resource builder to configure.</param>
     /// <param name="httpPort">The host port that the Kusto HTTP endpoint is bound to.</param>
     /// <param name="configureContainer">
@@ -125,6 +136,15 @@ public static class KustoResourceBuilderExtensions
     /// <summary>
     /// Configures the Kusto resource with any control script (e.g. to create tables or ingest data with the .ingest command).
     /// </summary>
+    /// <remarks>
+    /// <para>
+    /// This method allows you to specify a KQL script that will be executed against the Kusto database when the resource is ready.
+    /// </para>
+    /// <para>
+    /// When creating and populating databases, the creation operations must be split across multiple scripts, as the database must first be
+    /// created, and then a subsequent script must run using a connection to the newly created database.
+    /// </para>
+    /// </remarks>
     /// <param name="builder">The resource builder to configure.</param>
     /// <param name="script">KQL script to create databases, tables, or data.</param>
     /// <param name="database">The database to use when executing the script. If <c langword="null" /> use the default database.</param>
