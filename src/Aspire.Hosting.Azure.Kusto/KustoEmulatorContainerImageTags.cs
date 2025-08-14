64fd55985f2f2851969797d48d66af5bed518492
Matt Kotsenas <matt.kotsenas@gmail.com>
2025-08-14 14:41:30 -0700

    Fix remarks for documentation reference

--- src/Aspire.Hosting.Azure.Kusto/KustoEmulatorContainerImageTags.cs
+++ src/Aspire.Hosting.Azure.Kusto/KustoEmulatorContainerImageTags.cs
@@ -11,15 +11,18 @@ internal static class KustoEmulatorContainerImageTags
     /// <summary>
     /// The container registry hosting the Kusto emulator image.
     /// </summary>
+    /// <remarks>mcr.microsoft.com</remarks>
     public static string Registry { get; } = "mcr.microsoft.com";
 
     /// <summary>
     /// The Kusto emulator container image name.
     /// </summary>
+    /// <remarks>azuredataexplorer/kustainer-linux</remarks>
     public static string Image { get; } = "azuredataexplorer/kustainer-linux";
 
     /// <summary>
     /// The tag for the Kusto emulator container image.
     /// </summary>
+    /// <remarks>latest</remarks>
     public static string Tag { get; } = "latest";
 }
