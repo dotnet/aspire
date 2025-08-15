f9af29c336d9e4552d017d96953bee83f08d7124
Matt Kotsenas <matt.kotsenas@gmail.com>
2025-08-14 17:31:33 -0700

    Add skeleton playground app for Kusto

---
+++ playground/AzureKusto/AzureKusto.AppHost/Program.cs
@@ -0,0 +1,14 @@
+using Aspire.Hosting.Azure.Kusto;
+
+var builder = DistributedApplication.CreateBuilder(args);
+
+var kusto = builder.AddKusto("kusto")
+    .RunAsEmulator();
+var db = kusto.AddDatabase("testdb");
+
+builder.AddProject<Projects.AzureKusto_Worker>("worker")
+    .WithReference(db)
+    .WaitFor(db);
+
+var app = builder.Build();
+app.Run();
