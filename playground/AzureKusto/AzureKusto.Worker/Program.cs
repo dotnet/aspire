bcf47246d489e8716d61c9eba87d494d95631362
Matt Kotsenas <matt.kotsenas@gmail.com>
2025-08-18 18:16:25 -0700

    Show seeding both via AppHost and as a background worker in an app

--- playground/AzureKusto/AzureKusto.Worker/Program.cs
+++ playground/AzureKusto/AzureKusto.Worker/Program.cs
@@ -1,8 +1,5 @@
-
-using Kusto.Cloud.Platform.Data;
-using Kusto.Cloud.Platform.Utils;
+using AzureKusto.Worker;
 using Kusto.Data;
-using Kusto.Data.Common;
 using Kusto.Data.Net.Client;
 
 var builder = Host.CreateApplicationBuilder(args);
@@ -25,41 +22,14 @@ builder.Services.AddSingleton(sp =>
     return KustoClientFactory.CreateCslAdminProvider(kcsb);
 });
 
-builder.Services.AddHostedService<Worker>();
+builder.Services.AddOptions<WorkerOptions>();
+
+builder.Services.AddHostedService<QueryWorker>();
+if (builder.Environment.IsDevelopment())
+{
+    builder.Services.AddHostedService<IngestionWorker>();
+}
 
 var app = builder.Build();
 
 app.Run();
-
-internal sealed class Worker : BackgroundService
-{
-    private readonly ICslQueryProvider _queryClient;
-    private readonly ICslAdminProvider _adminClient;
-    private readonly IHostApplicationLifetime _hostApplicationLifetime;
-    private readonly Microsoft.Extensions.Logging.ILogger<Worker> _logger;
-
-    public Worker(
-        ICslQueryProvider queryClient,
-        ICslAdminProvider adminClient,
-        IHostApplicationLifetime hostApplicationLifetime,
-        Microsoft.Extensions.Logging.ILogger<Worker> logger)
-    {
-        _queryClient = queryClient;
-        _adminClient = adminClient;
-        _hostApplicationLifetime = hostApplicationLifetime;
-        _logger = logger;
-    }
-
-    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
-    {
-        var reader = await _queryClient.ExecuteQueryAsync(_queryClient.DefaultDatabaseName, ".show version", new ClientRequestProperties(), stoppingToken);
-        var results = reader.ToJObjects().StringJoin(",");
-
-        _logger.LogInformation("Query Results: {results}", results);
-
-        await Task.Delay(1_000, stoppingToken);
-
-        // When completed, the entire app host will stop.
-        _hostApplicationLifetime.StopApplication();
-    }
-}
