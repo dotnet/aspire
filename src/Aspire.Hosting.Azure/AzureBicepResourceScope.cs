a27fa93af0042d832f65b3511278df1eecc18704
Safia Abdalla <safia@safia.rocks>
2025-08-20 13:30:07 -0700

    Fix up AzureBicepResourceScope

--- src/Aspire.Hosting.Azure/AzureBicepResourceScope.cs
+++ src/Aspire.Hosting.Azure/AzureBicepResourceScope.cs
@@ -7,9 +7,18 @@ namespace Aspire.Hosting.Azure;
 /// Represents the scope associated with the resource.
 /// </summary>
 /// <param name="resourceGroup">The name of the existing resource group.</param>
-/// <param name="subscription">The subscription identifier associated with the resource group. If null, the default subscription is used.</param>
-public sealed class AzureBicepResourceScope(object resourceGroup, object? subscription = null)
+public sealed class AzureBicepResourceScope(object resourceGroup)
 {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="AzureBicepResourceScope"/> class.
+    /// </summary>
+    /// <param name="resourceGroup">The name of the existing resource group.</param>
+    /// <param name="subscription">The subscription identifier associated with the resource group.</param>
+    public AzureBicepResourceScope(object resourceGroup, object subscription) : this(resourceGroup)
+    {
+        Subscription = subscription;
+    }
+
     /// <summary>
     /// Represents the resource group to encode in the scope.
     /// </summary>
@@ -18,5 +27,5 @@ public sealed class AzureBicepResourceScope(object resourceGroup, object? subscr
     /// <summary>
     /// Represents the subscription to encode in the scope.
     /// </summary>
-    public object? Subscription { get; } = subscription;
+    public object? Subscription { get; }
 }
