0893d2bb3602d4d32190bde64db9f5ed34669646
David Negstad <David.Negstad@microsoft.com>
2025-09-05 14:55:07 -0700

    Remove whitespace

--- src/Aspire.Hosting.Azure/Provisioning/Provisioners/AzureProvisioner.cs
+++ src/Aspire.Hosting.Azure/Provisioning/Provisioners/AzureProvisioner.cs
@@ -288,7 +288,7 @@ internal sealed class AzureProvisioner(
         {
             eventing.Subscribe<BeforeStartEvent>(OnBeforeStartAsync);
         }
-            
+
         return Task.CompletedTask;
     }
 }
