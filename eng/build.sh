b7914a785128f739a0065144c3e107947f3eef30
Adam Ratzman <adam@adamratzman.com>
2025-09-10 11:50:22 -0400

    Build aspire extension and allow acquisition from pr cli shell script (#11245)
    * add options to build the aspire extension in build scripts
    
    * publish the vsix as well
    
    * add aspire extension acquisition to sh script
    
    * fix syntax errors
    
    * fix path, install vsce, build extension in packaging step
    
    * see if this stops building extension instead of tests
    
    * try removing entirely
    
    * add use insiders parameters
    
    * use skipextension instead of skipextensioninstall

--- eng/build.sh
+++ eng/build.sh
@@ -45,6 +45,7 @@ usage()
 
   echo "Libraries settings:"
   echo "  --testnobuild              Skip building tests when invoking -test."
+  echo "  --build-extension          Build the VS Code extension."
   echo ""
 
   echo "Command line arguments starting with '/p:' are passed through to MSBuild."
@@ -56,7 +57,7 @@ arguments=''
 extraargs=''
 
 # Check if an action is passed in
-declare -a actions=("b" "build" "r" "restore" "rebuild" "testnobuild" "sign" "publish" "clean" "t" "test")
+declare -a actions=("b" "build" "r" "restore" "rebuild" "testnobuild" "sign" "publish" "clean" "t" "test" "build-extension")
 actInt=($(comm -12 <(printf '%s\n' "${actions[@]/#/-}" | sort) <(printf '%s\n' "${@/#--/-}" | sort)))
 
 while [[ $# > 0 ]]; do
@@ -133,6 +134,11 @@ while [[ $# > 0 ]]; do
 
      -testnobuild)
       arguments="$arguments /p:VSTestNoBuild=true"
+      shift 1
+      ;;
+
+     -build-extension)
+      arguments="$arguments --build-extension"
       shift 1
       ;;
 
