a1fac2e58f20be4d584330fc3ae8277b3ff7f155
James Newton-King <james@newtonking.com>
2025-09-12 07:51:25 +0800

    Compiled expressions

--- src/Aspire.Dashboard/Utils/MaxItemCountHelper.cs
+++ src/Aspire.Dashboard/Utils/MaxItemCountHelper.cs
@@ -1,6 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Linq.Expressions;
 using System.Reflection;
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 using Microsoft.FluentUI.AspNetCore.Components;
@@ -11,48 +12,85 @@ namespace Aspire.Dashboard.Utils;
 // Required because dashboard currently targets .NET 8 and MaxItemCount isn't available.
 public static class VirtualizeHelper<TItem>
 {
-    private static readonly PropertyInfo? s_setMaxItemCountPropertyInfo = GetSetMaxItemCountMethodInfo();
+    private static readonly Func<Virtualize<TItem>, int>? s_getMaxItemCount =
+        CreateGetter();
 
-    private static PropertyInfo? GetSetMaxItemCountMethodInfo()
+    private static readonly Action<Virtualize<TItem>, int>? s_setMaxItemCount =
+        CreateSetter();
+
+    private static Func<Virtualize<TItem>, int>? CreateGetter()
     {
         var type = typeof(Virtualize<TItem>);
-        return type.GetProperty("MaxItemCount", BindingFlags.Instance | BindingFlags.Public);
+        var prop = type.GetProperty("MaxItemCount", BindingFlags.Instance | BindingFlags.Public);
+
+        if (prop == null || !prop.CanRead)
+        {
+            return null;
+        }
+
+        var instance = Expression.Parameter(type, "virtualize");
+        var body = Expression.Property(instance, prop);
+
+        return Expression.Lambda<Func<Virtualize<TItem>, int>>(body, instance).Compile();
+    }
+
+    private static Action<Virtualize<TItem>, int>? CreateSetter()
+    {
+        var type = typeof(Virtualize<TItem>);
+        var prop = type.GetProperty("MaxItemCount", BindingFlags.Instance | BindingFlags.Public);
+
+        if (prop == null || !prop.CanWrite)
+        {
+            return null;
+        }
+
+        var instance = Expression.Parameter(type, "virtualize");
+        var valueParam = Expression.Parameter(typeof(int), "value");
+        var body = Expression.Assign(Expression.Property(instance, prop), valueParam);
+
+        return Expression.Lambda<Action<Virtualize<TItem>, int>>(body, instance, valueParam).Compile();
     }
 
     public static bool TrySetMaxItemCount(Virtualize<TItem> virtualize, int max)
     {
-        if (s_setMaxItemCountPropertyInfo == null)
+        if (s_getMaxItemCount == null || s_setMaxItemCount == null)
         {
             return false;
         }
 
-        var currentMaxItemCount = (int)s_setMaxItemCountPropertyInfo.GetValue(virtualize)!;
-        if (currentMaxItemCount == max)
+        if (s_getMaxItemCount(virtualize) == max)
         {
             return false;
         }
 
-        s_setMaxItemCountPropertyInfo.SetValue(virtualize, max);
+        s_setMaxItemCount(virtualize, max);
         return true;
     }
 }
 
 public static class FluentDataGridHelper<TGridItem>
 {
-    private static readonly FieldInfo? s_virtualizeComponentFieldInfo = GetVirtualizeComponentFieldInfo();
+    private static readonly Func<FluentDataGrid<TGridItem>, Virtualize<(int, TGridItem)>>? s_getVirtualize =
+        CreateGetter();
 
-    private static FieldInfo? GetVirtualizeComponentFieldInfo()
+    private static Func<FluentDataGrid<TGridItem>, Virtualize<(int, TGridItem)>>? CreateGetter()
     {
         var type = typeof(FluentDataGrid<TGridItem>);
         var field = type.GetField("_virtualizeComponent", BindingFlags.Instance | BindingFlags.NonPublic);
 
-        return field;
+        if (field == null)
+        {
+            return null;
+        }
+
+        var instance = Expression.Parameter(type, "dataGrid");
+        var body = Expression.Convert(Expression.Field(instance, field), typeof(Virtualize<(int, TGridItem)>));
+
+        return Expression.Lambda<Func<FluentDataGrid<TGridItem>, Virtualize<(int, TGridItem)>>> (body, instance).Compile();
     }
 
     private static Virtualize<(int, TGridItem)>? GetVirtualize(FluentDataGrid<TGridItem> dataGrid)
-    {
-        return s_virtualizeComponentFieldInfo?.GetValue(dataGrid) as Virtualize<(int, TGridItem)>;
-    }
+        => s_getVirtualize?.Invoke(dataGrid);
 
     public static bool TrySetMaxItemCount(FluentDataGrid<TGridItem> dataGrid, int max)
     {
@@ -61,7 +99,7 @@ public static class FluentDataGridHelper<TGridItem>
         {
             return false;
         }
+
         return VirtualizeHelper<(int, TGridItem)>.TrySetMaxItemCount(virtualize, max);
     }
 }
-
