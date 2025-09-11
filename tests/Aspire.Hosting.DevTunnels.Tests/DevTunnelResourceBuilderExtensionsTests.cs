582a56a2fccb59c8d805713ad954019448a63b71
Damian Edwards <damian@damianedwards.com>
2025-09-10 17:21:36 -0700

    Test env vars set when referencing resources via the tunnel

---
+++ tests/Aspire.Hosting.DevTunnels.Tests/DevTunnelResourceBuilderExtensionsTests.cs
@@ -0,0 +1,49 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Aspire.Hosting.ApplicationModel;
+using Aspire.Hosting.Utils;
+
+namespace Aspire.Hosting.DevTunnels.Tests;
+
+public class DevTunnelResourceBuilderExtensionsTests
+{
+    [Fact]
+    public async Task WithReference_InjectsServiceDiscoveryEnvironmentVariablesWhenReferencingOtherResourcesViaTheTunnel()
+    {
+        using var builder = TestDistributedApplicationBuilder.Create();
+
+        var target = builder.AddProject<ProjectA>("target")
+            .WithHttpsEndpoint();
+        var tunnel = builder.AddDevTunnel("tunnel")
+            .WithReference(target);
+        var consumer = builder.AddResource(new TestResource("consumer"))
+            .WithReference(target, tunnel);
+
+        var tunnelPort = tunnel.Resource.Ports.FirstOrDefault();
+        Assert.NotNull(tunnelPort);
+
+        tunnelPort.TunnelEndpointAnnotation.AllocatedEndpoint = new(tunnelPort.TunnelEndpointAnnotation, "test123.devtunnels.ms", 443);
+        tunnelPort.TunnelEndpointAllocatedTcs.SetResult();
+
+        var values = await consumer.Resource.GetEnvironmentVariableValuesAsync();
+
+        var expectedKey = $"services__target__https__0";
+        Assert.Contains(expectedKey, values.Keys);
+
+        var expectedValue = "https://test123.devtunnels.ms:443";
+        Assert.Equal(expectedValue, values[expectedKey]);
+    }
+
+    private sealed class ProjectA : IProjectMetadata
+    {
+        public string ProjectPath => "projectA";
+
+        public LaunchSettings LaunchSettings { get; } = new();
+    }
+
+    private sealed class TestResource(string name) : Resource(name), IResourceWithEnvironment
+    {
+
+    }
+}
