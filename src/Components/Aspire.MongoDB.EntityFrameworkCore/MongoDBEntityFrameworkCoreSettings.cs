1b7c61c9750570b049b49dfa7c0592ae6f550d51
James Alickolli <jalickolli@gmail.com>
2025-09-12 10:46:21 +1000

    Created MongoDB.EntityFrameworkCore component

---
+++ src/Components/Aspire.MongoDB.EntityFrameworkCore/MongoDBEntityFrameworkCoreSettings.cs
@@ -0,0 +1,47 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace Aspire.MongoDB.EntityFrameworkCore;
+
+/// <summary>
+/// Provides the client configuration settings for connecting to a MongoDB database using EntityFrameworkCore.
+/// </summary>
+public sealed class MongoDBEntityFrameworkCoreSettings
+{
+    /// <summary>
+    /// Gets or sets the connection string of the MongoDB database to connect to.
+    /// </summary>
+    public string? ConnectionString { get; set; }
+    /// <summary>
+    /// Gets or sets whether retries should be disabled.
+    /// </summary>
+    /// <value>
+    /// The default value is <see langword="false"/>.
+    /// </value>
+    public bool DisableRetry { get; set; }
+    /// <summary>
+    /// Gets or sets a boolean value that indicates whether the database health check is disabled or not.
+    /// </summary>
+    /// <value>
+    /// The default value is <see langword="false"/>.
+    /// </value>
+    public bool DisableHealthChecks { get; set; }
+    /// <summary>
+    /// Gets or sets a boolean value that indicates whether the OpenTelemetry tracing is disabled or not.
+    /// </summary>
+    /// <value>
+    /// The default value is <see langword="false"/>.
+    /// </value>
+    public bool DisableTracing { get; set; }
+    /// <summary>
+    /// Gets or sets a boolean value that indicates whether the OpenTelemetry metrics are disabled or not.
+    /// </summary>
+    /// <value>
+    /// The default value is <see langword="false"/>.
+    /// </value>
+    public bool DisableMetrics { get; set; }
+    /// <summary>
+    /// Gets or sets the time in seconds to wait for the command to execute.
+    /// </summary>
+    public int? CommandTimeout { get; set; }
+}
