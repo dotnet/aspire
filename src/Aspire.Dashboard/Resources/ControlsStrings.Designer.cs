77fdadbe803bb3671fa09b098f26328f20969daf
James Newton-King <james@newtonking.com>
2025-09-16 06:46:12 +0800

    Add cloud type filter to tracing pages (#11324)

--- src/Aspire.Dashboard/Resources/ControlsStrings.Designer.cs
+++ src/Aspire.Dashboard/Resources/ControlsStrings.Designer.cs
@@ -19,7 +19,7 @@ namespace Aspire.Dashboard.Resources {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "18.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class ControlsStrings {
@@ -993,6 +993,15 @@ namespace Aspire.Dashboard.Resources {
         public static string SpanDetailsStartTime {
             get {
                 return ResourceManager.GetString("SpanDetailsStartTime", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Cloud.
+        /// </summary>
+        public static string SpanTypeCloud {
+            get {
+                return ResourceManager.GetString("SpanTypeCloud", resourceCulture);
             }
         }
         
