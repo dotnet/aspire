2143b005fc4275d392709738b1eb80ba7973b586
Mitch Denny <midenn@microsoft.com>
2025-09-15 15:08:52 +1000

    Move PublishAot into SDK.

--- src/Aspire.ProjectTemplates/templates/aspire-apphost-singlefile/9.5/apphost.cs
+++ src/Aspire.ProjectTemplates/templates/aspire-apphost-singlefile/9.5/apphost.cs
@@ -1,7 +1,6 @@
 #:sdk Microsoft.NET.Sdk
 #:sdk Aspire.AppHost.Sdk@aspireVersion
 #:package Aspire.Hosting.AppHost@aspireVersion
-#:property PublishAot=false
 
 var builder = DistributedApplication.CreateBuilder(args);
 
