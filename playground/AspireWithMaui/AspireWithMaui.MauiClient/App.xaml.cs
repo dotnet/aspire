45305440de235a6bd99a63fd477b564aa008ca05
Gerald Versluis <gerald.versluis@microsoft.com>
2025-09-16 16:28:06 +0200

    Add MAUI playground

---
+++ playground/AspireWithMaui/AspireWithMaui.MauiClient/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace AspireWithMaui.MauiClient;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+	}
+
+	protected override Window CreateWindow(IActivationState? activationState)
+	{
+		return new Window(new AppShell());
+	}
+}
