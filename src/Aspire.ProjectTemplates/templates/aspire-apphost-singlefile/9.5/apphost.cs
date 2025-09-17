112e4e969d89785ae7d67479bec6386b3497be58
Damian Edwards <damian@damianedwards.com>
2025-09-17 09:54:59 -0700

    rebase on single-file stuff & simplify template

--- src/Aspire.ProjectTemplates/templates/aspire-apphost-singlefile/9.5/apphost.cs
+++ src/Aspire.ProjectTemplates/templates/aspire-apphost-singlefile/9.5/apphost.cs
@@ -1,7 +1,4 @@
-#:sdk Microsoft.NET.Sdk
 #:sdk Aspire.AppHost.Sdk@!!REPLACE_WITH_LATEST_VERSION!!
-#:package Aspire.Hosting.AppHost@!!REPLACE_WITH_LATEST_VERSION!!
-#:property PublishAot=false
 
 var builder = DistributedApplication.CreateBuilder(args);
 
