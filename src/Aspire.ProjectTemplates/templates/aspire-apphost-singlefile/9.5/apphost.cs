38b040c4dbba278dae2765786777696776e2cd42
Mitch Denny <midenn@microsoft.com>
2025-09-17 12:22:50 +1000

    Put PublishAot back in the template for now.

--- src/Aspire.ProjectTemplates/templates/aspire-apphost-singlefile/9.5/apphost.cs
+++ src/Aspire.ProjectTemplates/templates/aspire-apphost-singlefile/9.5/apphost.cs
@@ -1,7 +1,7 @@
 #:sdk Microsoft.NET.Sdk
 #:sdk Aspire.AppHost.Sdk@aspireVersion
-
 #:package Aspire.Hosting.AppHost@aspireVersion
+#:property PublishAot=false
 
 var builder = DistributedApplication.CreateBuilder(args);
 
