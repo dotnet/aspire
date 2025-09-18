adc8aeec154cac7d040dd6327f1ef37c2b4a61da
Gerald Versluis <gerald.versluis@microsoft.com>
2025-09-18 13:49:44 +0200

    Add webapi reference in playground

--- playground/AspireWithMaui/AspireWithMaui.AppHost/AppHost.cs
+++ playground/AspireWithMaui/AspireWithMaui.AppHost/AppHost.cs
@@ -1,11 +1,12 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
 // Add the Weather API service with external HTTP endpoints for mobile device access
-builder.AddProject("AspireWithMaui-WeatherApi", @"..\AspireWithMaui.WeatherApi\AspireWithMaui.WeatherApi.csproj")
+var weatherApi = builder.AddProject("webapi", @"..\AspireWithMaui.WeatherApi\AspireWithMaui.WeatherApi.csproj")
     .WithExternalHttpEndpoints();
 
 builder.AddMauiProject("mauiclient", @"..\AspireWithMaui.MauiClient\AspireWithMaui.MauiClient.csproj")
     .WithWindows()
-    .WithAndroid();
+    .WithAndroid()
+    .WithReference(weatherApi);
 
 builder.Build().Run();
