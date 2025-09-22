64bd86c97ad636868291794aacfc915ec719d000
James Newton-King <james@newtonking.com>
2025-09-22 17:22:46 +0800

    Support dynamic choice inputs

--- src/Aspire.Hosting/Dashboard/DashboardServiceData.cs
+++ src/Aspire.Hosting/Dashboard/DashboardServiceData.cs
@@ -2,8 +2,8 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.Runtime.CompilerServices;
-using Aspire.Hosting.ApplicationModel;
 using Aspire.DashboardService.Proto.V1;
+using Aspire.Hosting.ApplicationModel;
 using Microsoft.Extensions.Logging;
 
 namespace Aspire.Hosting.Dashboard;
@@ -158,9 +158,9 @@ internal sealed class DashboardServiceData : IDisposable
 
     internal async Task SendInteractionRequestAsync(WatchInteractionsRequestUpdate request, CancellationToken cancellationToken)
     {
-        await _interactionService.CompleteInteractionAsync(
+        await _interactionService.PrcoessInteractionFromClientAsync(
             request.InteractionId,
-            (interaction, serviceProvider) =>
+            (interaction, serviceProvider, logger) =>
             {
                 switch (request.KindCase)
                 {
@@ -172,28 +172,69 @@ internal sealed class DashboardServiceData : IDisposable
                         var inputsInfo = (Interaction.InputsInteractionInfo)interaction.InteractionInfo;
                         var options = (InputsDialogInteractionOptions)interaction.Options;
 
-                        for (var i = 0; i < inputsInfo.Inputs.Count; i++)
-                        {
-                            var modelInput = inputsInfo.Inputs[i];
-                            var requestInput = request.InputsDialog.InputItems[i];
+                        ProcessInputs(serviceProvider, logger, inputsInfo, request.InputsDialog, interaction.CancellationToken);
 
-                            var incomingValue = requestInput.Value;
-
-                            // Ensure checkbox value is either true or false.
-                            if (requestInput.InputType == Aspire.DashboardService.Proto.V1.InputType.Boolean)
-                            {
-                                incomingValue = (bool.TryParse(incomingValue, out var b) && b) ? "true" : "false";
-                            }
-
-                            modelInput.Value = incomingValue;
-                        }
-
-                        return new InteractionCompletionState { Complete = true, State = inputsInfo.Inputs };
+                        // If the interaction was sent to the server because an input changed, don't try to complete the interaction.
+                        return new InteractionCompletionState { Complete = !request.InputsDialog.DependOnChange, State = inputsInfo.Inputs };
                     default:
                         return new InteractionCompletionState { Complete = true };
                 }
             },
             cancellationToken).ConfigureAwait(false);
+    }
+
+    private static void ProcessInputs(IServiceProvider serviceProvider, ILogger logger, Interaction.InputsInteractionInfo inputsInfo, InteractionInputsDialog inputsDialog, CancellationToken cancellationToken)
+    {
+        var choiceInteractionsToUpdate = new HashSet<InteractionInput>();
+
+        for (var i = 0; i < inputsDialog.InputItems.Count; i++)
+        {
+            var requestInput = inputsDialog.InputItems[i];
+            if (!inputsInfo.Inputs.TryGetByName(requestInput.Name, out var modelInput))
+            {
+                continue;
+            }
+
+            var incomingValue = requestInput.Value;
+
+            // Ensure checkbox value is either true or false.
+            if (requestInput.InputType == Aspire.DashboardService.Proto.V1.InputType.Boolean)
+            {
+                incomingValue = (bool.TryParse(incomingValue, out var b) && b) ? "true" : "false";
+            }
+
+            if (modelInput.Value != incomingValue)
+            {
+                modelInput.Value = incomingValue;
+
+                // If we're processing updates because of a dependency change, check to see if this input is depended on.
+                if (inputsDialog.DependOnChange)
+                {
+                    var dependentInputs = inputsInfo.Inputs.Where(
+                        i => i.InputType == InputType.Choice &&
+                        i.OptionsProvider is { } optionsProvider &&
+                        (optionsProvider.DependsOnInputs?.Any(d => string.Equals(modelInput.Name, d, StringComparisons.InteractionInputName)) ?? false));
+
+                    foreach (var dependentInput in dependentInputs)
+                    {
+                        choiceInteractionsToUpdate.Add(dependentInput);
+                    }
+                }
+            }
+        }
+
+        // Refresh options for choice inputs that depend on other inputs.
+        foreach (var inputToUpdate in choiceInteractionsToUpdate)
+        {
+            var context = new LoadOptionsContext
+            {
+                CancellationToken = cancellationToken,
+                ServiceProvider = serviceProvider,
+                InputName = inputToUpdate.Name,
+                Inputs = inputsInfo.Inputs
+            };
+            inputToUpdate.OptionsProviderState!.RefreshData(context, logger);
+        }
     }
 }
 
