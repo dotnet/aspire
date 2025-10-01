3a7ed1737b1dbb8e3997c70125a34f05a1dd1433
Matt Kotsenas <matt.kotsenas@gmail.com>
2025-10-01 16:22:59 -0700

    Use persistent dbs

--- src/Aspire.Hosting.Azure.Kusto/AzureKustoEmulatorContainerDefaults.cs
+++ src/Aspire.Hosting.Azure.Kusto/AzureKustoEmulatorContainerDefaults.cs
@@ -13,4 +13,11 @@ internal static class AzureKustoEmulatorContainerDefaults
     /// Based on Azure Data Explorer emulator documentation, it typically uses port 8080.
     /// </summary>
     public const int DefaultTargetPort = 8080;
+
+    /// <summary>
+    /// The default (emulator local) path used for persisting Kusto databases. This path
+    /// can be mounted as a volume to persist database data across container restarts.
+    /// </summary>
+    /// <remarks>/kustodata/dbs/</remarks>
+    public const string DefaultPersistencePath = "/kustodata/dbs/";
 }
