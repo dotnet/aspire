aa61232bdaef86519212a56790e7290f0714ef62
David Negstad <David.Negstad@microsoft.com>
2025-10-09 13:02:52 -0700

    Update yarp tests

--- tests/Aspire.Hosting.Yarp.Tests/AddYarpTests.cs
+++ tests/Aspire.Hosting.Yarp.Tests/AddYarpTests.cs
@@ -1,6 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Security.Cryptography.X509Certificates;
 using Aspire.Hosting.ApplicationModel;
 using Aspire.Hosting.Tests.Utils;
 using Aspire.Hosting.Utils;
@@ -22,21 +23,34 @@ public class AddYarpTests(ITestOutputHelper testOutputHelper)
         Assert.Equal(5000, endpoint.TargetPort);
     }
 
-    [Fact]
-    public async Task VerifyRunEnvVariablesAreSet()
+    [Theory]
+    [InlineData(true)]
+    [InlineData(false)]
+    public async Task VerifyRunEnvVariablesAreSet(bool containerCertificateSupport)
     {
         var builder = TestDistributedApplicationBuilder.Create(DistributedApplicationOperation.Run);
 
+        // Report that developer certificates won't support container scenarios
+        var testProvider = new TestServiceProvider();
+        testProvider.AddService<IDeveloperCertificateService>(new TestDeveloperCertificateService(new List<X509Certificate2>(), containerCertificateSupport));
+
         var yarp = builder.AddYarp("yarp");
 
-        var env = await EnvironmentVariableEvaluator.GetEnvironmentVariablesAsync(yarp.Resource, DistributedApplicationOperation.Run, TestServiceProvider.Instance);
+        var env = await EnvironmentVariableEvaluator.GetEnvironmentVariablesAsync(yarp.Resource, DistributedApplicationOperation.Run, testProvider);
 
         Assert.Contains("OTEL_EXPORTER_OTLP_ENDPOINT", env);
         Assert.Contains("OTEL_EXPORTER_OTLP_PROTOCOL", env);
         Assert.Contains("ASPNETCORE_ENVIRONMENT", env);
 
-        var value = Assert.Contains("YARP_UNSAFE_OLTP_CERT_ACCEPT_ANY_SERVER_CERTIFICATE", env);
-        Assert.Equal("true", value);
+        if (containerCertificateSupport)
+        {
+            Assert.DoesNotContain("YARP_UNSAFE_OLTP_CERT_ACCEPT_ANY_SERVER_CERTIFICATE", env);
+        }
+        else
+        {
+            var value = Assert.Contains("YARP_UNSAFE_OLTP_CERT_ACCEPT_ANY_SERVER_CERTIFICATE", env);
+            Assert.Equal("true", value);
+        }
     }
 
     [Fact]
@@ -60,9 +74,13 @@ public class AddYarpTests(ITestOutputHelper testOutputHelper)
     {
         var builder = TestDistributedApplicationBuilder.Create(DistributedApplicationOperation.Run);
 
+        // Yarp requires an IDeveloperCertificateService in run mode when building it's environment variables.
+        var testProvider = new TestServiceProvider();
+        testProvider.AddService<IDeveloperCertificateService>(new TestDeveloperCertificateService(new List<X509Certificate2>(), false));
+
         var yarp = builder.AddYarp("yarp").WithStaticFiles();
 
-        var env = await EnvironmentVariableEvaluator.GetEnvironmentVariablesAsync(yarp.Resource, DistributedApplicationOperation.Run, TestServiceProvider.Instance);
+        var env = await EnvironmentVariableEvaluator.GetEnvironmentVariablesAsync(yarp.Resource, DistributedApplicationOperation.Run, testProvider);
 
         var value = Assert.Contains("YARP_ENABLE_STATIC_FILES", env);
         Assert.Equal("true", value);
@@ -73,9 +91,13 @@ public class AddYarpTests(ITestOutputHelper testOutputHelper)
     {
         var builder = TestDistributedApplicationBuilder.Create(DistributedApplicationOperation.Publish);
 
+        // Yarp requires an IDeveloperCertificateService in run mode when building it's environment variables.
+        var testProvider = new TestServiceProvider();
+        testProvider.AddService<IDeveloperCertificateService>(new TestDeveloperCertificateService(new List<X509Certificate2>(), false));
+
         var yarp = builder.AddYarp("yarp").WithStaticFiles();
 
-        var env = await EnvironmentVariableEvaluator.GetEnvironmentVariablesAsync(yarp.Resource, DistributedApplicationOperation.Publish, TestServiceProvider.Instance);
+        var env = await EnvironmentVariableEvaluator.GetEnvironmentVariablesAsync(yarp.Resource, DistributedApplicationOperation.Publish, testProvider);
 
         var value = Assert.Contains("YARP_ENABLE_STATIC_FILES", env);
         Assert.Equal("true", value);
@@ -86,11 +108,15 @@ public class AddYarpTests(ITestOutputHelper testOutputHelper)
     {
         var builder = TestDistributedApplicationBuilder.Create(DistributedApplicationOperation.Run);
 
+        // Yarp requires an IDeveloperCertificateService in run mode when building it's environment variables.
+        var testProvider = new TestServiceProvider();
+        testProvider.AddService<IDeveloperCertificateService>(new TestDeveloperCertificateService(new List<X509Certificate2>(), false));
+
         using var tempDir = new TempDirectory();
         
         var yarp = builder.AddYarp("yarp").WithStaticFiles(tempDir.Path);
 
-        var env = await EnvironmentVariableEvaluator.GetEnvironmentVariablesAsync(yarp.Resource, DistributedApplicationOperation.Run, TestServiceProvider.Instance);
+        var env = await EnvironmentVariableEvaluator.GetEnvironmentVariablesAsync(yarp.Resource, DistributedApplicationOperation.Run, testProvider);
 
         var value = Assert.Contains("YARP_ENABLE_STATIC_FILES", env);
         Assert.Equal("true", value);
