ee91ee13b3da0fd7ad9551c32694bc6039bd1de8
James Newton-King <james@newtonking.com>
2025-10-14 13:53:39 +0800

    Project icons

--- playground/AspireWithNode/AspireWithNode.AppHost/AppHost.cs
+++ playground/AspireWithNode/AspireWithNode.AppHost/AppHost.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Hosting;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
@@ -21,5 +21,15 @@ if (builder.Environment.IsDevelopment() && launchProfile == "https")
 {
     frontend.RunWithHttpsDevCertificate("HTTPS_CERT_FILE", "HTTPS_CERT_KEY_FILE");
 }
+
+#if !SKIP_DASHBOARD_REFERENCE
+// This project is only added in playground projects to support development/debugging
+// of the dashboard. It is not required in end developer code. Comment out this code
+// or build with `/p:SkipDashboardReference=true`, to test end developer
+// dashboard launch experience, Refer to Directory.Build.props for the path to
+// the dashboard binary (defaults to the Aspire.Dashboard bin output in the
+// artifacts dir).
+builder.AddProject<Projects.Aspire_Dashboard>(KnownResourceNames.AspireDashboard);
+#endif
 
 builder.Build().Run();
