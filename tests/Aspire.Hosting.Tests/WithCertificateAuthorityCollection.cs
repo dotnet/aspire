18839c2e3d75d11c6d1a157594668e9f882b47af
David Negstad <David.Negstad@microsoft.com>
2025-10-14 14:20:33 -0700

    Get test working

--- tests/Aspire.Hosting.Tests/WithCertificateAuthorityCollection.cs
+++ tests/Aspire.Hosting.Tests/WithCertificateAuthorityCollection.cs
@@ -1,12 +1,14 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using Aspire.Hosting.Utils;
+
 namespace Aspire.Hosting.Tests;
 
 public class WithCertificateAuthorityCollectionTests
 {
     [Fact]
-    public async Task AddingCertificateAuthorityCollectionsIsIdempotent()
+    public Task AddingCertificateAuthorityCollectionsIsIdempotent()
     {
         using var builder = TestDistributedApplicationBuilder.Create();
 
@@ -25,9 +27,12 @@ public class WithCertificateAuthorityCollectionTests
                                .WithCertificateAuthorityCollection(bundle2)
                                .WithCertificateAuthorityCollection(bundle1); // Add bundle1 again to test idempotency
 
-        Assert.True(container.Resource.TryGetAnnotationsOfType<CertificateAuthorityCollectionAnnotation>(out var annotation));
+        Assert.True(container.Resource.TryGetAnnotationsOfType<CertificateAuthorityCollectionAnnotation>(out var annotations));
+        var annotation = Assert.Single(annotations);
         Assert.Equal(2, annotation.CertificateAuthorityCollections.Count);
         Assert.Contains(bundle1.Resource, annotation.CertificateAuthorityCollections);
         Assert.Contains(bundle2.Resource, annotation.CertificateAuthorityCollections);
+
+        return Task.CompletedTask;
     }
 }
