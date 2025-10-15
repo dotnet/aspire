06278475bed066df446799c0abd01d548a8cdeb0
Sebastien Ros <sebastienros@gmail.com>
2025-10-15 12:23:24 -0700

    Fix class name

--- src/Aspire.Hosting.Azure.AIFoundry/AIFoundryModel.Local.Generated.cs
+++ src/Aspire.Hosting.Azure.AIFoundry/AIFoundryModel.Local.Generated.cs
@@ -10,7 +10,7 @@ public partial class AIFoundryModel
     /// <summary>
     /// Models available on Foundry Local.
     /// </summary>
-    public static class FoundryLocal
+    public static class Local
     {
         /// <summary>
         /// This model is an optimized version of DeepSeek-R1-Distill-Qwen-14B to enable local inference. This model uses RTN quantization. # Model Description - **Developed by:** Microsoft - **Model type:** ONNX - **License:** MIT - **Model Description:** This is a conversion of the DeepSeek-R1-Distill-Qwen-14B for local inference. - **Disclaimer:** Model is only an optimization of the base model, any risk associated with the model is the responsibility of the user of the model. Please verify and test for your scenarios. There may be a slight difference in output from the base model with the optimizations applied. Note that optimizations applied are distinct from fine tuning and thus do not alter the intended uses or capabilities of the model. # Base Model Information See Hugging Face model [DeepSeek-R1-Distill-Qwen-14B](https://huggingface.co/deepseek-ai/DeepSeek-R1-Distill-Qwen-14B) for details.
