e531977c7664d66953dfade28e70e7e71efeae55
David Negstad <David.Negstad@microsoft.com>
2025-10-17 10:57:52 -0700

    Remove extra using

--- src/Aspire.Hosting.Python/PythonAppResourceBuilderExtensions.cs
+++ src/Aspire.Hosting.Python/PythonAppResourceBuilderExtensions.cs
@@ -3,7 +3,6 @@
 
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
-using System.Security.Cryptography.X509Certificates;
 
 #pragma warning disable ASPIREEXTENSION001
 using Aspire.Hosting.ApplicationModel;
