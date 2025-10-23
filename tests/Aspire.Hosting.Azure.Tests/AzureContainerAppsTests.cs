e13f1b0ce7ac7df001c00c5e6724fc59c2d7e5f3
Shilpi Rachna <shilpir@microsoft.com>
2025-10-23 12:27:30 -0700

    Removing unrelated changes

--- tests/Aspire.Hosting.Azure.Tests/AzureContainerAppsTests.cs
+++ tests/Aspire.Hosting.Azure.Tests/AzureContainerAppsTests.cs
@@ -1990,9 +1990,7 @@ public class AzureContainerAppsTests
     }
 
     [Fact]
-#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
     public async Task GetHostAddressExpression()
-#pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
     {
         var builder = TestDistributedApplicationBuilder.Create(DistributedApplicationOperation.Publish);
 
