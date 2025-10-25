9c5fd093533e23e864ba07cfdcebc9ebfc0b249b
sebastienros <sebastienros@users.noreply.github.com>
2025-10-25 05:03:11 +0000

    [Automated] Update AI Foundry Models

--- src/Aspire.Hosting.Azure.AIFoundry/AIFoundryModel.Local.Generated.cs
+++ src/Aspire.Hosting.Azure.AIFoundry/AIFoundryModel.Local.Generated.cs
@@ -53,16 +53,6 @@ public partial class AIFoundryModel
         public static readonly AIFoundryModel Phi4 = new() { Name = "phi-4", Version = "1", Format = "Microsoft" };
 
         /// <summary>
-        /// This model is an optimized version of Phi-4-mini-instruct to enable local inference. This model uses RTN quantization. # Model Description - **Developed by:** Microsoft - **Model type:** ONNX - **License:** MIT - **Model Description:** This is a conversion of the Phi-4-mini-instruct for local inference. - **Disclaimer:** Model is only an optimization of the base model, any risk associated with the model is the responsibility of the user of the model. Please verify and test for your scenarios. There may be a slight difference in output from the base model with the optimizations applied. Note that optimizations applied are distinct from fine tuning and thus do not alter the intended uses or capabilities of the model. # Base Model Information See Hugging Face model [Phi-4-mini-instruct](https://huggingface.co/microsoft/Phi-4-mini-instruct) for details.
-        /// </summary>
-        public static readonly AIFoundryModel Phi4Mini = new() { Name = "phi-4-mini", Version = "4", Format = "Microsoft" };
-
-        /// <summary>
-        /// This model is an optimized version of Phi-4-mini-reasoning to enable local inference. This model uses RTN quantization. # Model Description - **Developed by:** Microsoft - **Model type:** ONNX - **License:** MIT - **Model Description:** This is a conversion of the Phi-4-mini-reasoning for local inference. - **Disclaimer:** Model is only an optimization of the base model, any risk associated with the model is the responsibility of the user of the model. Please verify and test for your scenarios. There may be a slight difference in output from the base model with the optimizations applied. Note that optimizations applied are distinct from fine tuning and thus do not alter the intended uses or capabilities of the model. # Base Model Information See Hugging Face model [Phi-4-mini-reasoning](https://huggingface.co/microsoft/Phi-4-mini-reasoning) for details.
-        /// </summary>
-        public static readonly AIFoundryModel Phi4MiniReasoning = new() { Name = "phi-4-mini-reasoning", Version = "2", Format = "Microsoft" };
-
-        /// <summary>
         /// This model is an optimized version of Qwen2.5-0.5B-Instruct to enable local inference. This model uses RTN quantization. # Model Description - **Developed by:** Microsoft - **Model type:** ONNX - **License:** apache-2.0 - **Model Description:** This is a conversion of the Qwen2.5-0.5B-Instruct for local inference. - **Disclaimer:** Model is only an optimization of the base model, any risk associated with the model is the responsibility of the user of the model. Please verify and test for your scenarios. There may be a slight difference in output from the base model with the optimizations applied. Note that optimizations applied are distinct from fine tuning and thus do not alter the intended uses or capabilities of the model. # Base Model Information See Hugging Face model [Qwen2.5-0.5B-Instruct](https://huggingface.co/Qwen/Qwen2.5-0.5B-Instruct) for details.
         /// </summary>
         public static readonly AIFoundryModel Qwen2505b = new() { Name = "qwen2.5-0.5b", Version = "3", Format = "Microsoft" };
