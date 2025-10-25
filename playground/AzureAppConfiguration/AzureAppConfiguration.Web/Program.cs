c009d573103a48f78455ffbc7e92d705c6daff05
Mitch Denny <mitch@mitchdeny.com>
2025-10-25 12:48:43 +1100

    Checkpoint from VS Code for coding agent session

--- playground/AzureAppConfiguration/AzureAppConfiguration.Web/Program.cs
+++ playground/AzureAppConfiguration/AzureAppConfiguration.Web/Program.cs
@@ -1,6 +1,8 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using Azure.Data.AppConfiguration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.AddAzureAppConfiguration("appconfig", configureOptions: options =>
@@ -12,12 +14,19 @@ builder.AddAzureAppConfiguration("appconfig", configureOptions: options =>
     });
 });
 
+builder.AddAzureAppConfigurationClient("appconfig");
+
 var app = builder.Build();
 
 app.MapGet("/message", (IConfiguration config) =>
 {
     var message = config["Message"];
     return new { Message = message };
+});
+
+app.MapGet("/setmessage", async (string message, ConfigurationClient client) =>
+{
+    await client.SetConfigurationSettingAsync("Message", message);
 });
 
 // Use Azure App Configuration middleware for dynamic configuration refresh.
