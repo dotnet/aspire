5e519825a408302004b273e5636dabf2f6552670
Eric Erhardt <eric.erhardt@microsoft.com>
2025-10-27 17:41:35 -0500

    Remove ASPIREHOSTINGPYTHON001
    The Python APIs should be considered stable for this release.
    
    Fix #12163

--- playground/python/Python.AppHost/Program.cs
+++ playground/python/Python.AppHost/Program.cs
@@ -1,8 +1,6 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-#pragma warning disable ASPIREHOSTINGPYTHON001 // Test for experimental feature
-
 var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddPythonScript("script-only", "../script_only", "main.py");
