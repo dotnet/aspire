8a916f76e20baafe2f45c782faa21687576db571
Eric Erhardt <eric.erhardt@microsoft.com>
2025-10-28 17:20:29 -0500

    Fix the playground app

--- playground/AspireWithJavaScript/AspireJavaScript.AppHost/AppHost.cs
+++ playground/AspireWithJavaScript/AspireJavaScript.AppHost/AppHost.cs
@@ -1,9 +1,32 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddAzureContainerAppEnvironment("env");
-
 var weatherApi = builder.AddProject<Projects.AspireJavaScript_MinimalApi>("weatherapi")
     .WithExternalHttpEndpoints();
+
+builder.AddNpmApp("angular", "../AspireJavaScript.Angular")
+    .WithNpm(install: true)
+    .WithReference(weatherApi)
+    .WaitFor(weatherApi)
+    .WithHttpEndpoint(env: "PORT")
+    .WithExternalHttpEndpoints()
+    .PublishAsDockerFile();
+
+builder.AddNpmApp("react", "../AspireJavaScript.React")
+    .WithNpm(install: true)
+    .WithReference(weatherApi)
+    .WaitFor(weatherApi)
+    .WithEnvironment("BROWSER", "none") // Disable opening browser on npm start
+    .WithHttpEndpoint(env: "PORT")
+    .WithExternalHttpEndpoints()
+    .PublishAsDockerFile();
+
+builder.AddNpmApp("vue", "../AspireJavaScript.Vue")
+    .WithInstallCommand("npm", ["ci"]) // Use 'npm ci' for clean install
+    .WithReference(weatherApi)
+    .WaitFor(weatherApi)
+    .WithHttpEndpoint(env: "PORT")
+    .WithExternalHttpEndpoints()
+    .PublishAsDockerFile();
 
 var reactvite = builder.AddViteApp("reactvite", "../AspireJavaScript.Vite")
     .WithNpm(install: true)
