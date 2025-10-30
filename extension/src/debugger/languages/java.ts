20e1805536d9712dc3029d32810f70737f0ee86f
Marshal Hayes <17213165+marshalhayes@users.noreply.github.com>
2025-10-29 22:25:44 -0500

    Follow pattern from other debugger extensions

--- extension/src/debugger/languages/java.ts
+++ extension/src/debugger/languages/java.ts
@@ -1,21 +1,24 @@
-import { isJavaLaunchConfiguration } from "../../dcp/types";
+import { ExecutableLaunchConfiguration, isJavaLaunchConfiguration } from "../../dcp/types";
 import { invalidLaunchConfiguration } from "../../loc/strings";
 import { ResourceDebuggerExtension } from "../debuggerExtensions";
+import * as vscode from 'vscode';
+
+function getProjectFile(launchConfig: ExecutableLaunchConfiguration): string {
+    if (isJavaLaunchConfiguration(launchConfig)) {
+        const programPath = launchConfig.main_class_path || launchConfig.project_path;
+        if (programPath) {
+            return programPath;
+        }
+    }
+
+    throw new Error(invalidLaunchConfiguration(JSON.stringify(launchConfig)));
+}
 
 export const javaDebuggerExtension: ResourceDebuggerExtension = {
     resourceType: 'java',
     debugAdapter: 'java',
     extensionId: 'vscjava.vscode-java-pack',
-    displayName: 'Java',
+    getDisplayName: (launchConfiguration: ExecutableLaunchConfiguration) => `Java: ${vscode.workspace.asRelativePath(getProjectFile(launchConfiguration))}`,
     getSupportedFileTypes: () => ['.java'],
-    getProjectFile: (launchConfig) => {
-        if (isJavaLaunchConfiguration(launchConfig)) {
-            const programPath = launchConfig.main_class_path || launchConfig.project_path;
-            if (programPath) {
-                return programPath;
-            }
-        }
-
-        throw new Error(invalidLaunchConfiguration(JSON.stringify(launchConfig)));
-    }
+    getProjectFile: (launchConfig) => getProjectFile(launchConfig),
 };
