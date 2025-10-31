75724a321f973e67837461f052a62c4c21037df6
James Newton-King <james@newtonking.com>
2025-10-31 09:45:28 +0800

    Fix extra tests

--- tests/Aspire.Dashboard.Tests/Integration/StartupTests.cs
+++ tests/Aspire.Dashboard.Tests/Integration/StartupTests.cs
@@ -710,6 +710,13 @@ public class StartupTests(ITestOutputHelper testOutputHelper)
             },
             w =>
             {
+                Assert.Equal("MCP listening on: {McpEndpointUri}", GetValue(w.State, "{OriginalFormat}"));
+
+                var uri = new Uri((string)GetValue(w.State, "McpEndpointUri")!);
+                Assert.NotEqual(0, uri.Port);
+            },
+            w =>
+            {
                 Assert.Equal("OTLP server is unsecured. Untrusted apps can send telemetry to the dashboard. For more information, visit https://go.microsoft.com/fwlink/?linkid=2267030", GetValue(w.State, "{OriginalFormat}"));
                 Assert.Equal(LogLevel.Warning, w.LogLevel);
             },
