c5d815f6b9d8d4dacdaf115505630cb7428b3914
Adam Ratzman <adam@adamratzman.com>
2025-11-06 01:32:15 -0500

    fix double disposal issue

--- src/Aspire.Hosting/DistributedApplicationBuilder.cs
+++ src/Aspire.Hosting/DistributedApplicationBuilder.cs
@@ -179,9 +179,7 @@ public class DistributedApplicationBuilder : IDistributedApplicationBuilder
 
         _innerBuilder.Services.AddSingleton(TimeProvider.System);
 
-        _innerBuilder.Services.AddSingleton<BackchannelLoggerProvider>();
-        _innerBuilder.Services.AddSingleton<IBackchannelLoggerProvider>(sp => sp.GetRequiredService<BackchannelLoggerProvider>());
-        _innerBuilder.Services.AddSingleton<ILoggerProvider>(sp => sp.GetRequiredService<BackchannelLoggerProvider>());
+        _innerBuilder.Services.AddSingleton<IBackchannelLoggerProvider, BackchannelLoggerProvider>();
         _innerBuilder.Logging.AddFilter("Microsoft.Hosting.Lifetime", LogLevel.Warning);
         _innerBuilder.Logging.AddFilter("Microsoft.AspNetCore.Server.Kestrel", LogLevel.Error);
         _innerBuilder.Logging.AddFilter("Aspire.Hosting.Dashboard", LogLevel.Error);
