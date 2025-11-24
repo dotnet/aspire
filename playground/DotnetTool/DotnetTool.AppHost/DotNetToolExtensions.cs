2c67f06b1f48e78b228488a0f738ed233d674110
Alex Crome <289860+afscrome@users.noreply.github.com>
2025-11-24 18:53:51 +0000

    Rework to use `dotnet tool exec`

--- playground/DotnetTool/DotnetTool.AppHost/DotNetToolExtensions.cs
+++ playground/DotnetTool/DotnetTool.AppHost/DotNetToolExtensions.cs
@@ -1,5 +1,3 @@
-using Microsoft.Extensions.DependencyInjection;
-
 namespace DotnetTool.AppHost;
 
 /// <summary>
@@ -7,118 +5,55 @@ namespace DotnetTool.AppHost;
 /// </summary>
 public static class DotNetToolExtensions
 {
-    public static IResourceBuilder<DotnetToolResource> AddDotnetTool(this IDistributedApplicationBuilder builder, string name, string command, string packageId, Action<IResourceBuilder<DotnetToolResource>> configure)
-    {
-        var tool = builder.AddDotnetTool(name, packageId, command);
-        configure(tool);
-        return tool;
-    }
-
-    public static IResourceBuilder<DotnetToolResource> AddDotnetTool(this IDistributedApplicationBuilder builder, string name, string command, string packageId)
-        => builder.AddDotnetTool(new DotnetToolResource(name, packageId, command));
+    public static IResourceBuilder<DotnetToolResource> AddDotnetTool(this IDistributedApplicationBuilder builder, string name, string packageId)
+        => builder.AddDotnetTool(new DotnetToolResource(name, packageId));
 
     public static IResourceBuilder<T> AddDotnetTool<T>(this IDistributedApplicationBuilder builder, T resource)
-        where T: DotnetToolResource
+        where T : DotnetToolResource
     {
-        var tool = builder.AddResource(resource)
-           .WithIconName("Toolbox");
+        return builder.AddResource(resource)
+           .WithIconName("Toolbox")
+           .WithCommand("dotnet")
+           .WithArgs(x =>
+           {
+               if (!x.Resource.TryGetLastAnnotation<DotNetToolAnnotation>(out var toolConfig))
+               {
+                   // If the annotation has been removed, don't add any dotnet tool arguments.
+                   return;
+               }
 
-        var installer = BuildInstaller();
-        RewriteToolCommand();
+               x.Args.Add("tool");
+               x.Args.Add("exec");
+               x.Args.Add(toolConfig.PackageId);
 
-        return tool.WaitForCompletion(installer);
+               var sourceArg = toolConfig.IgnoreExistingFeeds ? "--source" : "--add-source";
 
-        void RewriteToolCommand()
-        {
-            // To avoid excess redownloading, want to set the tool path to a 
-            // .Net 10's `dotnet tool exec` would handle a lot of that natively
-            // Although https://github.com/dotnet/sdk/issues/50579 is a complication
-            // In the meantime, download tool to a path based on IAspireStore
-            //
-            // Using BeforeStartEvent rather than BeforeResoruceStart as the latter event
-            // gets called multiple times, and prepending would break the path
-            builder.Eventing.Subscribe<BeforeStartEvent>((evt, ct) =>
-            {
-                if (Path.IsPathFullyQualified(resource.Command))
-                {
-                    throw new ArgumentException("Executable must not have an absolute path to run as a tool", nameof(builder));
-                }
+               foreach (var source in toolConfig.Sources)
+               {
+                   x.Args.Add(sourceArg);
+                   x.Args.Add(source);
+               }
 
-                var toolDirectory = GetToolDirectory(evt.Services, tool);
-                tool.WithCommand(Path.Combine(toolDirectory, resource.Command));
+               if (toolConfig.IgnoreFailedSources)
+               {
+                   x.Args.Add("--ignore-failed-sources");
+               }
 
-                return Task.CompletedTask;
-            });
-        }
+               if (toolConfig.Version is not null)
+               {
+                   x.Args.Add("--version");
+                   x.Args.Add(toolConfig.Version);
+               }
+               else if (toolConfig.Prerelease)
+               {
+                   x.Args.Add("--prerelease");
+               }
 
-        IResourceBuilder<DotnetToolInstaller> BuildInstaller()
-        {
-            var installerResource = new DotnetToolInstaller($"{tool.Resource.Name}-installer", "dotnet") { Parent = tool.Resource };
-
-            return builder
-                .AddResource(installerResource)
-                .WithArgs(x =>
-                {
-                    var toolDirectory = GetToolDirectory(x.ExecutionContext.ServiceProvider, tool);
-                    var toolConfig = tool.Resource.ToolConfiguration;
-
-                    x.Args.Add("tool");
-                    x.Args.Add("install");
-                    x.Args.Add(toolConfig.PackageId);
-                    x.Args.Add("--tool-path");
-                    x.Args.Add(toolDirectory);
-
-                    var sourceArg = toolConfig.IgnoreExistingFeeds ? "--source" : "--add-source";
-
-                    foreach (var source in toolConfig.Sources)
-                    {
-                        x.Args.Add(sourceArg);
-                        x.Args.Add(source);
-                    }
-
-                    if (toolConfig.IgnoreFailedSources)
-                    {
-                        x.Args.Add("--ignore-failed-sources");
-                    }
-
-                    if (toolConfig.Version is not null)
-                    {
-                        x.Args.Add("--version");
-                        x.Args.Add(toolConfig.Version);
-                    }
-                    else if (toolConfig.Prerelease)
-                    {
-                        x.Args.Add("--prerelease");
-                    }
-
-                    if (toolConfig.AllowDowngrade)
-                    {
-                        x.Args.Add("--allow-downgrade");
-                    }
-
-                    x.Args.Add("--verbosity");
-                    x.Args.Add("detailed");
-                })
-                .WithIconName("ArrowDownload")
-                .WithParentRelationship(tool)
-                .WithOfflineFallback();
-        }
-
-        string GetToolDirectory(IServiceProvider serviceProvider, IResourceBuilder<DotnetToolResource> tool)
-        {
-            var builder = tool.ApplicationBuilder;
-            var explicitPath = builder.Configuration["ASPIRE_TOOLBASEPATH"];
-
-            if (!string.IsNullOrEmpty(explicitPath))
-            {
-                return Path.Combine(explicitPath, builder.Environment.ApplicationName, tool.Resource.Name);
-            }
-            else
-            {
-                var store = serviceProvider.GetRequiredService<IAspireStore>();
-                return Path.Combine(store.BasePath, "tools", tool.Resource.Name);
-            }
-        }
+               x.Args.Add("--verbosity");
+               x.Args.Add("detailed");
+               x.Args.Add("--yes");
+               x.Args.Add("--");
+           });
     }
 
     public static IResourceBuilder<T> WithPackageId<T>(this IResourceBuilder<T> builder, string packageId)
@@ -168,25 +103,5 @@ public static class DotNetToolExtensions
     {
         builder.Resource.ToolConfiguration.IgnoreFailedSources = true;
         return builder;
-    }
-
-    public static IResourceBuilder<T> WithPackageAllowDowngrade<T>(this IResourceBuilder<T> builder)
-        where T : DotnetToolResource
-    {
-        builder.Resource.ToolConfiguration.AllowDowngrade = true;
-        return builder;
-    }
-
-    private static IResourceBuilder<DotnetToolInstaller> WithOfflineFallback(this IResourceBuilder<DotnetToolInstaller> builder)
-    {
-        return builder.WithArgs(x =>
-        {
-            var settings = NuGet.Configuration.Settings.LoadDefaultSettings(root: builder.Resource.WorkingDirectory);
-            var packagesPath = NuGet.Configuration.SettingsUtility.GetGlobalPackagesFolder(settings);
-
-            builder.ApplicationBuilder.CreateResourceBuilder(builder.Resource.Parent)
-                .WithPackageSource(packagesPath)
-                .WithPackageIgnoreFailedSources();
-        });
     }
 }
