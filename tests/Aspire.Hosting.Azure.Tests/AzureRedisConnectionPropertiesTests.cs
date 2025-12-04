3fe5b34cbe27fbee68409f49d7f14805a0f459b2
Sebastien Ros <sebastienros@gmail.com>
2025-12-04 12:41:21 -0800

    Fix Azure Redis tests

--- tests/Aspire.Hosting.Azure.Tests/AzureRedisConnectionPropertiesTests.cs
+++ tests/Aspire.Hosting.Azure.Tests/AzureRedisConnectionPropertiesTests.cs
@@ -1,8 +1,6 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-#pragma warning disable ASPIREAZUREREDIS001 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
-
 using Aspire.Hosting.ApplicationModel;
 using Aspire.Hosting.Utils;
 
@@ -11,10 +9,10 @@ namespace Aspire.Hosting.Azure.Tests;
 public class AzureRedisConnectionPropertiesTests
 {
     [Fact]
-    public void AzureRedisEnterpriseResourceGetConnectionPropertiesReturnsExpectedValues()
+    public void AzureManagedRedisResourceGetConnectionPropertiesReturnsExpectedValues()
     {
         using var builder = TestDistributedApplicationBuilder.Create();
-        var redis = builder.AddAzureRedisEnterprise("redis");
+        var redis = builder.AddAzureManagedRedis("redis");
 
         var properties = ((IResourceWithConnectionString)redis.Resource).GetConnectionProperties().ToArray();
 
