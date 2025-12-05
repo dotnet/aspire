027ece623ce151226e1b7dc0ee91df62ca5b90e8
Sebastien Ros <sebastienros@gmail.com>
2025-12-05 12:00:39 -0800

    Update Kusto

--- src/Aspire.Hosting.Azure.Kusto/README.md
+++ src/Aspire.Hosting.Azure.Kusto/README.md
@@ -29,11 +29,20 @@ var myService = builder.AddProject<Projects.MyService>()
 
 When you reference Azure Kusto resources using `WithReference`, the following connection properties are made available to the consuming project:
 
+### Cluster Resource
+
 | Property Name | Description |
 |---------------|-------------|
 | `Uri`         | The cluster endpoint URI, typically `https://<cluster-name>.<region>.kusto.windows.net/` (or the HTTP endpoint when using the emulator) |
 
-Aspire exposes each property as an environment variable named `[RESOURCE]_[PROPERTY]`. For instance, the `Uri` property of a resource called `db1` becomes `DB1_URI`.
+### Database Resource
+
+| Property Name | Description |
+|---------------|-------------|
+| `Uri`         | The cluster endpoint URI (inherited from parent cluster) |
+| `Database`    | The name of the database |
+
+Aspire exposes each property as an environment variable named `[RESOURCE]_[PROPERTY]`. For instance, the `Uri` property of a resource called `mydb` becomes `MYDB_URI`, and the `Database` property becomes `MYDB_DATABASE`.
 
 ## Additional documentation
 
--- tests/Aspire.Hosting.Azure.Kusto.Tests/AzureKustoConnectionPropertiesTests.cs
+++ tests/Aspire.Hosting.Azure.Kusto.Tests/AzureKustoConnectionPropertiesTests.cs
@@ -25,4 +25,74 @@ public class AzureKustoConnectionPropertiesTests
                 Assert.Equal("{kusto.outputs.clusterUri}", property.Value.ValueExpression);
             });
     }
+
+    [Fact]
+    public void AzureKustoClusterResourceWithEmulatorGetConnectionPropertiesReturnsExpectedValues()
+    {
+        using var builder = TestDistributedApplicationBuilder.Create();
+        var kusto = builder.AddAzureKustoCluster("kusto").RunAsEmulator();
+
+        var properties = ((IResourceWithConnectionString)kusto.Resource).GetConnectionProperties().ToArray();
+
+        Assert.Single(properties);
+        Assert.Collection(
+            properties,
+            property =>
+            {
+                Assert.Equal("Uri", property.Key);
+                Assert.Equal("{kusto.bindings.http.url}", property.Value.ValueExpression);
+            });
+    }
+
+    [Fact]
+    public void AzureKustoDatabaseResourceGetConnectionPropertiesReturnsExpectedValues()
+    {
+        using var builder = TestDistributedApplicationBuilder.Create();
+        var kusto = builder.AddAzureKustoCluster("kusto");
+        var database = kusto.AddReadWriteDatabase("testdb");
+
+        var resource = Assert.Single(builder.Resources.OfType<AzureKustoReadWriteDatabaseResource>());
+        var properties = ((IResourceWithConnectionString)resource).GetConnectionProperties().ToDictionary(x => x.Key, x => x.Value);
+
+        // Should have parent properties (Uri) + Database
+        Assert.Equal(2, properties.Count);
+        Assert.Collection(
+            properties,
+            property =>
+            {
+                Assert.Equal("Uri", property.Key);
+                Assert.Equal("{kusto.outputs.clusterUri}", property.Value.ValueExpression);
+            },
+            property =>
+            {
+                Assert.Equal("Database", property.Key);
+                Assert.Equal("testdb", property.Value.ValueExpression);
+            });
+    }
+
+    [Fact]
+    public void AzureKustoDatabaseResourceWithEmulatorGetConnectionPropertiesReturnsExpectedValues()
+    {
+        using var builder = TestDistributedApplicationBuilder.Create();
+        var kusto = builder.AddAzureKustoCluster("kusto").RunAsEmulator();
+        var database = kusto.AddReadWriteDatabase("testdb");
+
+        var resource = Assert.Single(builder.Resources.OfType<AzureKustoReadWriteDatabaseResource>());
+        var properties = ((IResourceWithConnectionString)resource).GetConnectionProperties().ToDictionary(x => x.Key, x => x.Value);
+
+        // Should have parent properties (Uri from emulator endpoint) + Database
+        Assert.Equal(2, properties.Count);
+        Assert.Collection(
+            properties,
+            property =>
+            {
+                Assert.Equal("Uri", property.Key);
+                Assert.Equal("{kusto.bindings.http.url}", property.Value.ValueExpression);
+            },
+            property =>
+            {
+                Assert.Equal("Database", property.Key);
+                Assert.Equal("testdb", property.Value.ValueExpression);
+            });
+    }
 }
