2dd008014f68e0359800963db70ef100df47f06d
Safia Abdalla <safia@safia.rocks>
2025-12-15 14:36:45 -0800

    Fix pragma disable for experimental

--- tests/Aspire.Hosting.Tests/Pipelines/DistributedApplicationPipelineTests.cs
+++ tests/Aspire.Hosting.Tests/Pipelines/DistributedApplicationPipelineTests.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 #pragma warning disable CS0618 // Type or member is obsolete
+#pragma warning disable ASPIRECOMPUTE003
 #pragma warning disable ASPIREPIPELINES001
 #pragma warning disable ASPIREPIPELINES002
 #pragma warning disable IDE0005
