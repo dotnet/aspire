032f93020a726ecc73e226cdc65654e1b6dd54e5
Phillip Hoff <phillip@orst.edu>
2025-12-30 10:24:03 -0800

    Update sample host.

--- playground/AzureFunctionsWithDts/AzureFunctionsWithDts.AppHost/Program.cs
+++ playground/AzureFunctionsWithDts/AzureFunctionsWithDts.AppHost/Program.cs
@@ -1,17 +1,14 @@
-using Azure.Hosting;
-
 var builder = DistributedApplication.CreateBuilder(args);
 
 var storage = builder.AddAzureStorage("storage").RunAsEmulator();
 
-var dts = builder.AddDurableTaskScheduler("dts").RunAsEmulator();
+var scheduler = builder.AddDurableTaskScheduler("scheduler").RunAsEmulator();
 
-var taskHub = dts.AddTaskHub("default");
+var taskHub = scheduler.AddTaskHub("taskhub");
 
 builder.AddAzureFunctionsProject<Projects.AzureFunctionsWithDts_Functions>("funcapp")
     .WithHostStorage(storage)
-    .WaitFor(dts)
-    .WithEnvironment("DURABLE_TASK_SCHEDULER_CONNECTION_STRING", dts)
-    .WithEnvironment("TASKHUB_NAME", taskHub.Resource.Name);
+    .WithEnvironment("DURABLE_TASK_SCHEDULER_CONNECTION_STRING", scheduler)
+    .WithEnvironment("TASKHUB_NAME", taskHub.Resource.TaskHubName);
 
 builder.Build().Run();
