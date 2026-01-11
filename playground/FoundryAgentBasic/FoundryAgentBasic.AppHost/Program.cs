cec8462e5a41fe342f740e3bfbf788218f45e58a
Eric Suh <ericsuh@microsoft.com>
2026-01-10 23:11:51 -0800

    Fix demo accounts

--- playground/FoundryAgentBasic/FoundryAgentBasic.AppHost/Program.cs
+++ playground/FoundryAgentBasic/FoundryAgentBasic.AppHost/Program.cs
@@ -1,15 +1,16 @@
 ﻿using Aspire.Hosting.Azure;
+using Microsoft.Extensions.Configuration;
 
 var builder = DistributedApplication.CreateBuilder(args);
+var name = builder.Configuration.GetSection("App").GetValue<string>("Name") ?? "my-app";
 
-var project = builder.AddFoundryProject("my-app-project");
+var project = builder.AddFoundryProject($"{name}-proj");
 
-builder.AddPythonApp("my-app", "../app", "main.py")
-    .WithUv()
-    .WithHttpEndpoint(port: 9999, name: "api")
+builder.AddPythonApp(name, "../app", "main.py")
+    .WithHttpEndpoint(port: 9999, name: "api", env: "PORT")
     .WithExternalHttpEndpoints()
     .WithReference(project)
-    .WithReference(project.AddModelDeployment("my-gpt-5", AIFoundryModel.OpenAI.Gpt5))
+    .WithReference(project.AddModelDeployment($"{name}-gpt41mini", AIFoundryModel.OpenAI.Gpt41Mini))
     .PublishAsHostedAgent();
 
 builder.Build().Run();
