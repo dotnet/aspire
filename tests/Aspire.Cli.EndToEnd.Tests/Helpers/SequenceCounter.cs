494507a0589b803e7a5c0879e1b1ecfe5529d1a9
Ankit Jain <radical@gmail.com>
2026-01-15 23:10:15 -0500

    fix build

--- tests/Aspire.Cli.EndToEnd.Tests/Helpers/SequenceCounter.cs
+++ tests/Aspire.Cli.EndToEnd.Tests/Helpers/SequenceCounter.cs
@@ -1,7 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-namespace Aspire.Cli.EndToEndTests.Helpers;
+namespace Aspire.Cli.EndToEnd.Tests.Helpers;
 
 public class SequenceCounter
 {
