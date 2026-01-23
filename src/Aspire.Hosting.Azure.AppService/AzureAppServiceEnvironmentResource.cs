e06c71bc2941a0683fa5ab2d746fda35da976968
Shilpi Rachna <shilpir@microsoft.com>
2026-01-23 10:47:19 +0530

    Removing annotation file no longer needed

--- src/Aspire.Hosting.Azure.AppService/AzureAppServiceEnvironmentResource.cs
+++ src/Aspire.Hosting.Azure.AppService/AzureAppServiceEnvironmentResource.cs
@@ -310,7 +310,7 @@ public class AzureAppServiceEnvironmentResource :
     /// <summary>
     /// Enables or disables regional DNL for the App Services.
     /// </summary>
-    internal bool EnableRegionalDNL { get; set; } = true;
+    internal bool EnableRegionalDNL { get; set; }
 
     /// <summary>
     /// Gets the name of the App Service Plan.
