a75f7fcac1a9f31397889a28847a8059e21a1f8a
Adam Ratzman <adam@adamratzman.com>
2026-01-28 22:30:15 -0500

    avoid !

--- src/Aspire.Cli/Projects/DotNetAppHostProject.cs
+++ src/Aspire.Cli/Projects/DotNetAppHostProject.cs
@@ -218,7 +218,7 @@ internal sealed class DotNetAppHostProject : IAppHostProject
             if (!watch)
             {
                 // Build in CLI if either not running under extension host, or the extension reports 'build-dotnet-using-cli' capability.
-                var extensionHasBuildCapability = isExtensionHost && await extensionBackchannel!.HasCapabilityAsync(KnownCapabilities.BuildDotnetUsingCli, cancellationToken);
+                var extensionHasBuildCapability = extensionBackchannel is not null && await extensionBackchannel.HasCapabilityAsync(KnownCapabilities.BuildDotnetUsingCli, cancellationToken);
                 var shouldBuildInCli = !isExtensionHost || extensionHasBuildCapability;
                 if (shouldBuildInCli)
                 {
