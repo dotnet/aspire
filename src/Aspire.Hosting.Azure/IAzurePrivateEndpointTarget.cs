060aac61c47f94ca603945f768f5c3bfea3e3e4d
Eric Erhardt <eric.erhardt@microsoft.com>
2026-01-30 17:23:50 -0600

    Make new Aspire.Hosting.Azure APIs experimental

--- src/Aspire.Hosting.Azure/IAzurePrivateEndpointTarget.cs
+++ src/Aspire.Hosting.Azure/IAzurePrivateEndpointTarget.cs
@@ -1,6 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Diagnostics.CodeAnalysis;
 using Aspire.Hosting.ApplicationModel;
 
 namespace Aspire.Hosting.Azure;
@@ -8,6 +9,7 @@ namespace Aspire.Hosting.Azure;
 /// <summary>
 /// Represents an Azure resource that can be connected to via a private endpoint.
 /// </summary>
+[Experimental("ASPIREAZURE003", UrlFormat = "https://aka.ms/dotnet/aspire/diagnostics#{0}")]
 public interface IAzurePrivateEndpointTarget : IResource
 {
     /// <summary>
--- src/Aspire.Hosting.Azure/PrivateEndpointTargetAnnotation.cs
+++ src/Aspire.Hosting.Azure/PrivateEndpointTargetAnnotation.cs
@@ -1,6 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Diagnostics.CodeAnalysis;
 using Aspire.Hosting.ApplicationModel;
 
 namespace Aspire.Hosting.Azure;
@@ -9,6 +10,7 @@ namespace Aspire.Hosting.Azure;
 /// An annotation that indicates a resource is the target of a private endpoint.
 /// When this annotation is present, the resource should be configured to deny public network access.
 /// </summary>
+[Experimental("ASPIREAZURE003", UrlFormat = "https://aka.ms/dotnet/aspire/diagnostics#{0}")]
 public sealed class PrivateEndpointTargetAnnotation : IResourceAnnotation
 {
 }
