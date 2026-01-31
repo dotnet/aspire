61aa09d298e53a4823436fc49baa2507a13db39a
tmat <tomas.matousek@microsoft.com>
2026-01-31 12:37:57 -0800

    1

--- src/WatchPrototype/Watch.Aspire/AspireHostLauncher.cs
+++ src/WatchPrototype/Watch.Aspire/AspireHostLauncher.cs
@@ -62,7 +62,6 @@ internal static class AspireHostLauncher
             RootProjectOptions = rootProjectOptions,
             BuildArguments = rootProjectOptions.BuildArguments,
             TargetFramework = rootProjectOptions.TargetFramework,
-            LaunchProfileName = rootProjectOptions.NoLaunchProfile ? null : rootProjectOptions.LaunchProfileName,
             RootProjects = [options.EntryPoint],
             BrowserRefreshServerFactory = new BrowserRefreshServerFactory(),
             BrowserLauncher = new BrowserLauncher(logger, reporter, environmentOptions),
