e12dab9b802de3160d825fbf6e04f6351612dd38
David Fowler <davidfowl@gmail.com>
2026-02-02 20:37:53 -0800

    Refactor: extract ResourceCommandBase for Start/Restart commands

--- src/Aspire.Cli/Commands/RestartCommand.cs
+++ src/Aspire.Cli/Commands/RestartCommand.cs
@@ -1,7 +1,6 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System.CommandLine;
 using Aspire.Cli.Backchannel;
 using Aspire.Cli.Configuration;
 using Aspire.Cli.Interaction;
@@ -13,21 +12,12 @@ using Microsoft.Extensions.Logging;
 
 namespace Aspire.Cli.Commands;
 
-internal sealed class RestartCommand : BaseCommand
+internal sealed class RestartCommand : ResourceCommandBase
 {
-    private readonly IInteractionService _interactionService;
-    private readonly AppHostConnectionResolver _connectionResolver;
-    private readonly ILogger<RestartCommand> _logger;
-
-    private static readonly Argument<string> s_resourceArgument = new("resource")
-    {
-        Description = ResourceCommandStrings.RestartResourceArgumentDescription
-    };
-
-    private static readonly Option<FileInfo?> s_projectOption = new("--project")
-    {
-        Description = ResourceCommandStrings.ProjectOptionDescription
-    };
+    protected override string CommandName => KnownResourceCommands.RestartCommand;
+    protected override string DisplayVerb => "Restarting";
+    protected override string PastTenseVerb => "restarted";
+    protected override string ResourceArgumentDescription => ResourceCommandStrings.RestartResourceArgumentDescription;
 
     public RestartCommand(
         IInteractionService interactionService,
@@ -37,43 +27,9 @@ internal sealed class RestartCommand : BaseCommand
         CliExecutionContext executionContext,
         ILogger<RestartCommand> logger,
         AspireCliTelemetry telemetry)
-        : base("restart", ResourceCommandStrings.RestartDescription, features, updateNotifier, executionContext, interactionService, telemetry)
+        : base("restart", ResourceCommandStrings.RestartDescription,
+               interactionService, backchannelMonitor, features, updateNotifier,
+               executionContext, logger, telemetry)
     {
-        _interactionService = interactionService;
-        _connectionResolver = new AppHostConnectionResolver(backchannelMonitor, interactionService, executionContext, logger);
-        _logger = logger;
-
-        Arguments.Add(s_resourceArgument);
-        Options.Add(s_projectOption);
-    }
-
-    protected override async Task<int> ExecuteAsync(ParseResult parseResult, CancellationToken cancellationToken)
-    {
-        var resourceName = parseResult.GetValue(s_resourceArgument)!;
-        var passedAppHostProjectFile = parseResult.GetValue(s_projectOption);
-
-        var result = await _connectionResolver.ResolveConnectionAsync(
-            passedAppHostProjectFile,
-            ResourceCommandStrings.ScanningForRunningAppHosts,
-            ResourceCommandStrings.SelectAppHost,
-            ResourceCommandStrings.NoInScopeAppHostsShowingAll,
-            ResourceCommandStrings.NoRunningAppHostsFound,
-            cancellationToken);
-
-        if (!result.Success)
-        {
-            _interactionService.DisplayError(result.ErrorMessage ?? ResourceCommandStrings.NoRunningAppHostsFound);
-            return ExitCodeConstants.FailedToFindProject;
-        }
-
-        return await ResourceCommandHelper.ExecuteResourceCommandAsync(
-            result.Connection!,
-            _interactionService,
-            _logger,
-            resourceName,
-            KnownResourceCommands.RestartCommand,
-            "Restarting",
-            "restarted",
-            cancellationToken);
     }
 }
--- src/Aspire.Cli/Commands/StartCommand.cs
+++ src/Aspire.Cli/Commands/StartCommand.cs
@@ -1,7 +1,6 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System.CommandLine;
 using Aspire.Cli.Backchannel;
 using Aspire.Cli.Configuration;
 using Aspire.Cli.Interaction;
@@ -13,21 +12,12 @@ using Microsoft.Extensions.Logging;
 
 namespace Aspire.Cli.Commands;
 
-internal sealed class StartCommand : BaseCommand
+internal sealed class StartCommand : ResourceCommandBase
 {
-    private readonly IInteractionService _interactionService;
-    private readonly AppHostConnectionResolver _connectionResolver;
-    private readonly ILogger<StartCommand> _logger;
-
-    private static readonly Argument<string> s_resourceArgument = new("resource")
-    {
-        Description = ResourceCommandStrings.StartResourceArgumentDescription
-    };
-
-    private static readonly Option<FileInfo?> s_projectOption = new("--project")
-    {
-        Description = ResourceCommandStrings.ProjectOptionDescription
-    };
+    protected override string CommandName => KnownResourceCommands.StartCommand;
+    protected override string DisplayVerb => "Starting";
+    protected override string PastTenseVerb => "started";
+    protected override string ResourceArgumentDescription => ResourceCommandStrings.StartResourceArgumentDescription;
 
     public StartCommand(
         IInteractionService interactionService,
@@ -37,43 +27,9 @@ internal sealed class StartCommand : BaseCommand
         CliExecutionContext executionContext,
         ILogger<StartCommand> logger,
         AspireCliTelemetry telemetry)
-        : base("start", ResourceCommandStrings.StartDescription, features, updateNotifier, executionContext, interactionService, telemetry)
+        : base("start", ResourceCommandStrings.StartDescription,
+               interactionService, backchannelMonitor, features, updateNotifier,
+               executionContext, logger, telemetry)
     {
-        _interactionService = interactionService;
-        _connectionResolver = new AppHostConnectionResolver(backchannelMonitor, interactionService, executionContext, logger);
-        _logger = logger;
-
-        Arguments.Add(s_resourceArgument);
-        Options.Add(s_projectOption);
-    }
-
-    protected override async Task<int> ExecuteAsync(ParseResult parseResult, CancellationToken cancellationToken)
-    {
-        var resourceName = parseResult.GetValue(s_resourceArgument)!;
-        var passedAppHostProjectFile = parseResult.GetValue(s_projectOption);
-
-        var result = await _connectionResolver.ResolveConnectionAsync(
-            passedAppHostProjectFile,
-            ResourceCommandStrings.ScanningForRunningAppHosts,
-            ResourceCommandStrings.SelectAppHost,
-            ResourceCommandStrings.NoInScopeAppHostsShowingAll,
-            ResourceCommandStrings.NoRunningAppHostsFound,
-            cancellationToken);
-
-        if (!result.Success)
-        {
-            _interactionService.DisplayError(result.ErrorMessage ?? ResourceCommandStrings.NoRunningAppHostsFound);
-            return ExitCodeConstants.FailedToFindProject;
-        }
-
-        return await ResourceCommandHelper.ExecuteResourceCommandAsync(
-            result.Connection!,
-            _interactionService,
-            _logger,
-            resourceName,
-            KnownResourceCommands.StartCommand,
-            "Starting",
-            "started",
-            cancellationToken);
     }
 }
