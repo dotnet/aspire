920a961cded15d815b11b17746d428ed9162709c
David Negstad <David.Negstad@microsoft.com>
2026-02-04 18:24:51 -0800

    Removed adapter files

--- extension/adapters/python/debugpy/_vendored/pydevd/tests_python/resources/_debugger_case_sysexit_unhandled_break.py
+++
@@ -1,7 +0,0 @@
-import time
-
-wait = True
-while wait:
-    time.sleep(1)  # break here
-
-print('attached')
--- extension/adapters/python/debugpy/_vendored/pydevd/tests_runfiles/samples_integrated/root/test_in_root.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-
-class MyTest(unittest.TestCase):
-    def test1(self):
-        print("--on-test-1")
-
-    def test2(self):
-        print("--on-test-2")
