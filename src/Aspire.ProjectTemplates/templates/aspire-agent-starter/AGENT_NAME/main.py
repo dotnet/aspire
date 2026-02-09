df4fb932f46b9abe90a22e488f5a523844fd093d
Eric Suh <ericsuh@microsoft.com>
2026-02-09 21:23:28 +0000

    Fix responses error and instrumentation

--- src/Aspire.ProjectTemplates/templates/aspire-agent-starter/AGENT_NAME/main.py
+++ src/Aspire.ProjectTemplates/templates/aspire-agent-starter/AGENT_NAME/main.py
@@ -3,15 +3,32 @@ import logging
 import os
 import sys
 
-from azure.ai.projects.aio import AIProjectClient
-from azure.identity.aio import DefaultAzureCredential
 from rich.console import Console
 from rich.logging import RichHandler
 
-from agent.agent import Agent
+
+AGENT_NAME = "Example Agent"
 
 
 def main():
+    from azure.ai.projects.aio import AIProjectClient
+    from azure.identity.aio import DefaultAzureCredential
+    from agent.agent import Agent
+
+    FOUNDRY_ENDPOINT = os.environ['AZURE_AI_FOUNDRY_PROJECT_ENDPOINT']
+    MODEL_DEPLOYMENT_NAME = os.environ['AZURE_AI_DEPLOYMENT_NAME']
+    with contextlib.closing(AIProjectClient(FOUNDRY_ENDPOINT, DefaultAzureCredential())) as client:
+        # Serve traffic
+        Agent(client, MODEL_DEPLOYMENT_NAME).run()
+
+
+def init_telemetry():
+    from opentelemetry.instrumentation.openai_agents import OpenAIAgentsInstrumentor # type: ignore
+    # Run before any other third party imports to ensure instrumentation is applied correctly
+    OpenAIAgentsInstrumentor().instrument(
+        capture_message_content='span_and_event',
+    )
+
     log_level = os.getenv('LOG_LEVEL', 'INFO').upper()
     root_logger = logging.getLogger()
     root_logger.setLevel(getattr(logging, log_level, logging.INFO))
@@ -22,21 +39,20 @@ def main():
             show_path=False,
             rich_tracebacks=True,
         )
+        console_handler.name = 'console'
         console_handler.setFormatter(logging.Formatter("%(message)s"))
     else:
-        console_handler = logging.StreamHandler(sys.stderr)
+        console_handler = logging.StreamHandler(sys.stdout)
         console_handler.setFormatter(
             logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
         )
+        console_handler.name = 'console'
     root_logger.addHandler(console_handler)
-
-    FOUNDRY_ENDPOINT = os.environ['AZURE_AI_FOUNDRY_PROJECT_ENDPOINT']
-    MODEL_DEPLOYMENT_NAME = os.environ['AZURE_AI_DEPLOYMENT_NAME']
-    PORT = int(os.getenv('PORT', '8088'))
-    print(f'Starting agent on port {PORT}')
-    with contextlib.closing(AIProjectClient(FOUNDRY_ENDPOINT, DefaultAzureCredential())) as client:
-        # Serve traffic
-        Agent(client, MODEL_DEPLOYMENT_NAME).run(PORT)
+    for logger_name in ["uvicorn", "uvicorn.error", "uvicorn.access"]:
+        uv_logger = logging.getLogger(logger_name)
+        uv_logger.addHandler(console_handler)
+        uv_logger.setLevel(root_logger.level)
+        uv_logger.propagate = False
 
 
 if __name__ == "__main__":
