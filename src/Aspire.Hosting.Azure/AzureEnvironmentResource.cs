efb3bbda9b320c1cac735c34cbc06919402c33fa
Eric Erhardt <eric.erhardt@microsoft.com>
2026-02-13 17:22:14 -0600

    Clean up the code

--- src/Aspire.Hosting.Azure/AzureEnvironmentResource.cs
+++ src/Aspire.Hosting.Azure/AzureEnvironmentResource.cs
@@ -133,29 +133,19 @@ public sealed class AzureEnvironmentResource : Resource
     /// <param name="provisioningContext">The Azure provisioning context.</param>
     private static void AddToPipelineSummary(PipelineStepContext ctx, ProvisioningContext provisioningContext)
     {
-        // Safely access the nested properties with null checks for reference types
-        // AzureLocation is a struct so it cannot be null
-        var resourceGroupName = provisioningContext.ResourceGroup?.Name ?? "unknown";
-        var subscriptionId = provisioningContext.Subscription?.Id.Name ?? "unknown";
+        var resourceGroupName = provisioningContext.ResourceGroup.Name;
+        var subscriptionId = provisioningContext.Subscription.Id.Name;
         var location = provisioningContext.Location.Name;
 
-        // Build resource group value with a portal link when possible
-        var resourceGroupValue = resourceGroupName;
-        if (resourceGroupName != "unknown" && subscriptionId != "unknown")
-        {
-            var tenantId = provisioningContext.Tenant?.TenantId;
-            var portalUrl = tenantId.HasValue
-                ? $"https://portal.azure.com/#@{tenantId.Value}/resource/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/overview"
-                : $"https://portal.azure.com/#/resource/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/overview";
-            resourceGroupValue = $"{resourceGroupName} ([link]({portalUrl}))";
-        }
+        var tenantId = provisioningContext.Tenant.TenantId;
+        var tenantSegment = tenantId.HasValue ? $"#@{tenantId.Value}" : "#";
+        var portalUrl = $"https://portal.azure.com/{tenantSegment}/resource/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/overview";
+        var resourceGroupValue = $"[{resourceGroupName}]({portalUrl})";
 
-#pragma warning disable ASPIREPIPELINES001 // PipelineSummary is experimental
         ctx.Summary.Add("☁️ Target", "Azure");
         ctx.Summary.Add("📦 Resource Group", resourceGroupValue);
         ctx.Summary.Add("🔑 Subscription", subscriptionId);
         ctx.Summary.Add("🌐 Location", location);
-#pragma warning restore ASPIREPIPELINES001
     }
 
     private Task PublishAsync(PipelineStepContext context)
