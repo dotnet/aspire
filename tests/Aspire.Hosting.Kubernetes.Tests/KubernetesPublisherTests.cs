8df7446aa77f8f8b41acba2eed283d41356a4e22
bbartels <benjamin@bartels.dev>
2026-02-21 01:42:12 +0000

    Adds Chart.yaml to snapshot

--- tests/Aspire.Hosting.Kubernetes.Tests/KubernetesPublisherTests.cs
+++ tests/Aspire.Hosting.Kubernetes.Tests/KubernetesPublisherTests.cs
@@ -424,6 +424,7 @@ public class KubernetesPublisherTests()
         // Assert
         var expectedFiles = new[]
         {
+            "Chart.yaml",
             "values.yaml",
             "templates/api/deployment.yaml",
             "templates/api/service.yaml",
