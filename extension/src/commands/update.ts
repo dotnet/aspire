163177f3634c0663145b833b1e406627ac0f605a
Adam Ratzman <adam@adamratzman.com>
2026-02-25 13:57:23 -0500

    Pass open AppHost to CLI commands via --project flag
    When the user has an AppHost file open in the editor or configured in
    .aspire/settings.json, the extension now passes --project <path> to CLI
    commands that support it (deploy, publish, add, update).
    
    Fixes #11024

--- extension/src/commands/update.ts
+++ extension/src/commands/update.ts
@@ -1,5 +1,8 @@
+import { AspireEditorCommandProvider } from '../editor/AspireEditorCommandProvider';
 import { AspireTerminalProvider } from '../utils/AspireTerminalProvider';
+import { getAppHostArgs } from '../utils/appHostArgs';
 
-export async function updateCommand(terminalProvider: AspireTerminalProvider) {
-    await terminalProvider.sendAspireCommandToAspireTerminal('update');
+export async function updateCommand(terminalProvider: AspireTerminalProvider, editorCommandProvider: AspireEditorCommandProvider) {
+    const appHostArgs = await getAppHostArgs(editorCommandProvider);
+    await terminalProvider.sendAspireCommandToAspireTerminal('update', true, appHostArgs);
 }
