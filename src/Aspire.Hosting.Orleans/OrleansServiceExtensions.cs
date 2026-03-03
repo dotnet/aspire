645bb7d2fbe78fb3d9a24191ddf36ab83ae2ec21
Sébastien Ros <sebastienros@gmail.com>
2026-03-03 10:25:47 -0800

    Fix XML documentation return tags in OrleansServiceExtensions

--- src/Aspire.Hosting.Orleans/OrleansServiceExtensions.cs
+++ src/Aspire.Hosting.Orleans/OrleansServiceExtensions.cs
@@ -34,7 +34,7 @@ public static class OrleansServiceExtensions
     /// </summary>
     /// <param name="orleansServiceBuilder">The target Orleans service builder.</param>
     /// <param name="clusterId">The ClusterId value.</param>
-    /// <returns>>The Orleans service builder.</returns>
+    /// <returns>The Orleans service builder.</returns>
     [AspireExport("withClusterId", Description = "Sets the Orleans cluster ID")]
     public static OrleansService WithClusterId(
         this OrleansService orleansServiceBuilder,
@@ -49,7 +49,7 @@ public static class OrleansServiceExtensions
     /// </summary>
     /// <param name="orleansServiceBuilder">The target Orleans service builder.</param>
     /// <param name="clusterId">The ClusterId value.</param>
-    /// <returns>>The Orleans service builder.</returns>
+    /// <returns>The Orleans service builder.</returns>
     /// <remarks>This method is not available in polyglot app hosts. Use <see cref="WithClusterId(OrleansService, string)"/> instead.</remarks>
     [AspireExportIgnore(Reason = "ParameterResource handle overload is not needed in polyglot. Use the string overload instead.")]
     public static OrleansService WithClusterId(
@@ -65,7 +65,7 @@ public static class OrleansServiceExtensions
     /// </summary>
     /// <param name="orleansServiceBuilder">The target Orleans service builder.</param>
     /// <param name="serviceId">The ServiceId value.</param>
-    /// <returns>>The Orleans service builder.</returns>
+    /// <returns>The Orleans service builder.</returns>
     [AspireExport("withServiceId", Description = "Sets the Orleans service ID")]
     public static OrleansService WithServiceId(
         this OrleansService orleansServiceBuilder,
@@ -80,7 +80,7 @@ public static class OrleansServiceExtensions
     /// </summary>
     /// <param name="orleansServiceBuilder">The target Orleans service builder.</param>
     /// <param name="serviceId">The ServiceId value.</param>
-    /// <returns>>The Orleans service builder.</returns>
+    /// <returns>The Orleans service builder.</returns>
     /// <remarks>This method is not available in polyglot app hosts. Use <see cref="WithServiceId(OrleansService, string)"/> instead.</remarks>
     [AspireExportIgnore(Reason = "ParameterResource handle overload is not needed in polyglot. Use the string overload instead.")]
     public static OrleansService WithServiceId(
@@ -96,7 +96,7 @@ public static class OrleansServiceExtensions
     /// </summary>
     /// <param name="orleansServiceBuilder">The target Orleans service builder.</param>
     /// <param name="provider">The provider.</param>
-    /// <returns>>The Orleans service builder.</returns>
+    /// <returns>The Orleans service builder.</returns>
     [AspireExport("withClustering", Description = "Configures Orleans clustering using a resource connection")]
     public static OrleansService WithClustering(
         this OrleansService orleansServiceBuilder,
@@ -108,7 +108,7 @@ public static class OrleansServiceExtensions
     /// </summary>
     /// <param name="orleansServiceBuilder">The target Orleans service builder.</param>
     /// <param name="provider">The provider.</param>
-    /// <returns>>The Orleans service builder.</returns>
+    /// <returns>The Orleans service builder.</returns>
     /// <remarks>This method is not available in polyglot app hosts. Use <see cref="WithClustering(OrleansService, IResourceBuilder{IResourceWithConnectionString})"/> instead.</remarks>
     [AspireExportIgnore(Reason = "IProviderConfiguration cannot be created directly by polyglot callers. Use the resource-based overload instead.")]
     public static OrleansService WithClustering(
@@ -123,7 +123,7 @@ public static class OrleansServiceExtensions
     /// Configures the Orleans service to use development-only clustering.
     /// </summary>
     /// <param name="orleansServiceBuilder">The target Orleans service builder.</param>
-    /// <returns>>The Orleans service builder.</returns>
+    /// <returns>The Orleans service builder.</returns>
     [AspireExport("withDevelopmentClustering", Description = "Configures Orleans development clustering")]
     public static OrleansService WithDevelopmentClustering(
         this OrleansService orleansServiceBuilder)
@@ -134,7 +134,7 @@ public static class OrleansServiceExtensions
     /// </summary>
     /// <param name="orleansServiceBuilder">The target Orleans service builder.</param>
     /// <param name="provider">The provider to add.</param>
-    /// <returns>>The Orleans service builder.</returns>
+    /// <returns>The Orleans service builder.</returns>
     /// <remarks>This resource name is the name the application will use to resolve the provider. This method is not available in polyglot app hosts. Use <see cref="WithGrainStorage(OrleansService, string, IResourceBuilder{IResourceWithConnectionString})"/> instead.</remarks>
     [AspireExportIgnore(Reason = "Convenience overload. Use the overload with explicit provider name instead.")]
     public static OrleansService WithGrainStorage(
@@ -148,7 +148,7 @@ public static class OrleansServiceExtensions
     /// <param name="orleansServiceBuilder">The target Orleans service builder.</param>
     /// <param name="name">The name of the provider. This is the name the application will use to resolve the provider.</param>
     /// <param name="provider">The provider to add.</param>
-    /// <returns>>The Orleans service builder.</returns>
+    /// <returns>The Orleans service builder.</returns>
     [AspireExport("withGrainStorage", Description = "Adds an Orleans grain storage provider")]
     public static OrleansService WithGrainStorage(
         this OrleansService orleansServiceBuilder,
@@ -162,7 +162,7 @@ public static class OrleansServiceExtensions
     /// <param name="orleansServiceBuilder">The target Orleans service builder.</param>
     /// <param name="name">The name of the provider. This is the name the application will use to resolve the provider.</param>
     /// <param name="provider">The provider to add.</param>
-    /// <returns>>The Orleans service builder.</returns>
+    /// <returns>The Orleans service builder.</returns>
     /// <remarks>This method is not available in polyglot app hosts. Use <see cref="WithGrainStorage(OrleansService, string, IResourceBuilder{IResourceWithConnectionString})"/> instead.</remarks>
     [AspireExportIgnore(Reason = "IProviderConfiguration cannot be created directly by polyglot callers. Use the resource-based overload instead.")]
     public static OrleansService WithGrainStorage(
@@ -179,7 +179,7 @@ public static class OrleansServiceExtensions
     /// </summary>
     /// <param name="orleansServiceBuilder">The target Orleans service builder.</param>
     /// <param name="name">The name of the provider. This is the name the application will use to resolve the provider.</param>
-    /// <returns>>The Orleans service builder.</returns>
+    /// <returns>The Orleans service builder.</returns>
     [AspireExport("withMemoryGrainStorage", Description = "Adds in-memory Orleans grain storage")]
     public static OrleansService WithMemoryGrainStorage(
         this OrleansService orleansServiceBuilder,
@@ -191,7 +191,7 @@ public static class OrleansServiceExtensions
     /// </summary>
     /// <param name="orleansServiceBuilder">The target Orleans service builder.</param>
     /// <param name="provider">The provider to add.</param>
-    /// <returns>>The Orleans service builder.</returns>
+    /// <returns>The Orleans service builder.</returns>
     /// <remarks>This resource name is the name the application will use to resolve the provider. This method is not available in polyglot app hosts. Use <see cref="WithStreaming(OrleansService, string, IResourceBuilder{IResourceWithConnectionString})"/> instead.</remarks>
     [AspireExportIgnore(Reason = "Convenience overload. Use the overload with explicit provider name instead.")]
     public static OrleansService WithStreaming(
@@ -205,7 +205,7 @@ public static class OrleansServiceExtensions
     /// <param name="orleansServiceBuilder">The target Orleans service builder.</param>
     /// <param name="name">The name of the provider. This is the name the application will use to resolve the provider.</param>
     /// <param name="provider">The provider to add.</param>
-    /// <returns>>The Orleans service builder.</returns>
+    /// <returns>The Orleans service builder.</returns>
     [AspireExport("withStreaming", Description = "Adds an Orleans stream provider")]
     public static OrleansService WithStreaming(
         this OrleansService orleansServiceBuilder,
@@ -219,7 +219,7 @@ public static class OrleansServiceExtensions
     /// <param name="orleansServiceBuilder">The target Orleans service builder.</param>
     /// <param name="name">The name of the provider. This is the name the application will use to resolve the provider.</param>
     /// <param name="provider">The provider to add.</param>
-    /// <returns>>The Orleans service builder.</returns>
+    /// <returns>The Orleans service builder.</returns>
     /// <remarks>This method is not available in polyglot app hosts. Use <see cref="WithStreaming(OrleansService, string, IResourceBuilder{IResourceWithConnectionString})"/> instead.</remarks>
     [AspireExportIgnore(Reason = "IProviderConfiguration cannot be created directly by polyglot callers. Use the resource-based overload instead.")]
     public static OrleansService WithStreaming(
@@ -236,7 +236,7 @@ public static class OrleansServiceExtensions
     /// </summary>
     /// <param name="orleansServiceBuilder">The target Orleans service builder.</param>
     /// <param name="name">The name of the provider. This is the name the application will use to resolve the provider.</param>
-    /// <returns>>The Orleans service builder.</returns>
+    /// <returns>The Orleans service builder.</returns>
     [AspireExport("withMemoryStreaming", Description = "Adds in-memory Orleans streaming")]
     public static OrleansService WithMemoryStreaming(
         this OrleansService orleansServiceBuilder,
@@ -249,7 +249,7 @@ public static class OrleansServiceExtensions
     /// <param name="orleansServiceBuilder">The target Orleans service builder.</param>
     /// <param name="name">The name of the provider. This is the name the application will use to resolve the provider.</param>
     /// <param name="provider">The provider to add.</param>
-    /// <returns>>The Orleans service builder.</returns>
+    /// <returns>The Orleans service builder.</returns>
     /// <remarks>This method is not available in polyglot app hosts. Use <see cref="WithBroadcastChannel(OrleansService, string)"/> instead.</remarks>
     [AspireExportIgnore(Reason = "IProviderConfiguration cannot be created directly by polyglot callers. Use the default provider overload instead.")]
     public static OrleansService WithBroadcastChannel(
@@ -266,7 +266,7 @@ public static class OrleansServiceExtensions
     /// </summary>
     /// <param name="orleansServiceBuilder">The target Orleans service builder.</param>
     /// <param name="name">The name of the provider. This is the name the application will use to resolve the provider.</param>
-    /// <returns>>The Orleans service builder.</returns>
+    /// <returns>The Orleans service builder.</returns>
     [AspireExport("withBroadcastChannel", Description = "Adds an Orleans broadcast channel provider")]
     public static OrleansService WithBroadcastChannel(
         this OrleansService orleansServiceBuilder,
@@ -278,7 +278,7 @@ public static class OrleansServiceExtensions
     /// </summary>
     /// <param name="orleansServiceBuilder">The target Orleans service builder.</param>
     /// <param name="provider">The reminder storage provider.</param>
-    /// <returns>>The Orleans service builder.</returns>
+    /// <returns>The Orleans service builder.</returns>
     [AspireExport("withReminders", Description = "Configures Orleans reminder storage")]
     public static OrleansService WithReminders(
         this OrleansService orleansServiceBuilder,
@@ -290,7 +290,7 @@ public static class OrleansServiceExtensions
     /// </summary>
     /// <param name="orleansServiceBuilder">The target Orleans service builder.</param>
     /// <param name="provider">The reminder storage provider to use.</param>
-    /// <returns>>The Orleans service builder.</returns>
+    /// <returns>The Orleans service builder.</returns>
     /// <remarks>This method is not available in polyglot app hosts. Use <see cref="WithReminders(OrleansService, IResourceBuilder{IResourceWithConnectionString})"/> instead.</remarks>
     [AspireExportIgnore(Reason = "IProviderConfiguration cannot be created directly by polyglot callers. Use the resource-based overload instead.")]
     public static OrleansService WithReminders(
@@ -305,7 +305,7 @@ public static class OrleansServiceExtensions
     /// Configures in-memory reminder storage for the Orleans service.
     /// </summary>
     /// <param name="orleansServiceBuilder">The target Orleans service builder.</param>
-    /// <returns>>The Orleans service builder.</returns>
+    /// <returns>The Orleans service builder.</returns>
     [AspireExport("withMemoryReminders", Description = "Configures in-memory Orleans reminders")]
     public static OrleansService WithMemoryReminders(
         this OrleansService orleansServiceBuilder)
@@ -319,7 +319,7 @@ public static class OrleansServiceExtensions
     /// </summary>
     /// <param name="orleansServiceBuilder">The target Orleans service builder.</param>
     /// <param name="provider">The provider to add.</param>
-    /// <returns>>The Orleans service builder.</returns>
+    /// <returns>The Orleans service builder.</returns>
     /// <remarks>This resource name is the name the application will use to resolve the provider. This method is not available in polyglot app hosts. Use <see cref="WithGrainDirectory(OrleansService, string, IResourceBuilder{IResourceWithConnectionString})"/> instead.</remarks>
     [AspireExportIgnore(Reason = "Convenience overload. Use the overload with explicit provider name instead.")]
     public static OrleansService WithGrainDirectory(
@@ -333,7 +333,7 @@ public static class OrleansServiceExtensions
     /// <param name="orleansServiceBuilder">The target Orleans service builder.</param>
     /// <param name="name">The name of the provider. This is the name the application will use to resolve the provider.</param>
     /// <param name="provider">The provider to add.</param>
-    /// <returns>>The Orleans service builder.</returns>
+    /// <returns>The Orleans service builder.</returns>
     [AspireExport("withGrainDirectory", Description = "Adds an Orleans grain directory provider")]
     public static OrleansService WithGrainDirectory(
         this OrleansService orleansServiceBuilder,
@@ -347,7 +347,7 @@ public static class OrleansServiceExtensions
     /// <param name="orleansServiceBuilder">The target Orleans service builder.</param>
     /// <param name="name">The name of the provider. This is the name the application will use to resolve the provider.</param>
     /// <param name="provider">The provider to add.</param>
-    /// <returns>>The Orleans service builder.</returns>
+    /// <returns>The Orleans service builder.</returns>
     /// <remarks>This method is not available in polyglot app hosts. Use <see cref="WithGrainDirectory(OrleansService, string, IResourceBuilder{IResourceWithConnectionString})"/> instead.</remarks>
     [AspireExportIgnore(Reason = "IProviderConfiguration cannot be created directly by polyglot callers. Use the resource-based overload instead.")]
     public static OrleansService WithGrainDirectory(
