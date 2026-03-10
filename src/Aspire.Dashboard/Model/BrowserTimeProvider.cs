5b7805537fe7b422b17bf9f96b08c18b5261220d
Abdallah Selim <77124997+abdallahsellem@users.noreply.github.com>
2026-03-10 06:09:02 +0200

    Add dashboard time format setting with 12/24-hour override (#14490)
    Co-authored-by: James Newton-King <james@newtonking.com>

--- src/Aspire.Dashboard/Model/BrowserTimeProvider.cs
+++ src/Aspire.Dashboard/Model/BrowserTimeProvider.cs
@@ -1,6 +1,8 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using Aspire.Dashboard.Utils;
+
 namespace Aspire.Dashboard.Model;
 
 /// <summary>
@@ -10,10 +12,13 @@ namespace Aspire.Dashboard.Model;
 /// - BrowserTimeProvider must be scoped to the user's session.
 /// - The built-in TimeProvider registration must be singleton for the system time (used by auth).
 /// </summary>
-public class BrowserTimeProvider : TimeProvider
+public class BrowserTimeProvider : TimeProvider, ITimeFormatProvider
 {
     private readonly ILogger _logger;
     private TimeZoneInfo? _browserLocalTimeZone;
+    private TimeFormat? _browserTimeFormat;
+
+    public TimeFormat ConfiguredTimeFormat { get; set; } = TimeFormat.System;
 
     public BrowserTimeProvider(ILoggerFactory loggerFactory)
     {
@@ -35,5 +40,28 @@ public class BrowserTimeProvider : TimeProvider
 
         _logger.LogDebug("Browser time zone set to '{TimeZone}' with UTC offset {UtcOffset}.", timeZoneInfo.Id, timeZoneInfo.BaseUtcOffset);
         _browserLocalTimeZone = timeZoneInfo;
+    }
+
+    public TimeFormat ResolvedTimeFormat
+    {
+        get
+        {
+            if (ConfiguredTimeFormat == TimeFormat.System)
+            {
+                return _browserTimeFormat ?? TimeFormat.System;
+            }
+
+            return ConfiguredTimeFormat;
+        }
+    }
+
+    public void SetBrowserTimeFormat(TimeFormat timeFormat)
+    {
+        _browserTimeFormat = timeFormat;
+    }
+
+    public void SetConfiguredTimeFormat(TimeFormat timeFormat)
+    {
+        ConfiguredTimeFormat = timeFormat;
     }
 }
