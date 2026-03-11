54c271050ba90d245e0e6973abe64c53ddebbf4a
Adam Ratzman <adam@adamratzman.com>
2026-03-11 19:45:22 -0400

    add new functions js sample

---
+++ playground/AzureFunctionsEndToEnd.TypeScript/TypeScriptFunctions/src/functions/eventHubTrigger.ts
@@ -0,0 +1,15 @@
+import { app, InvocationContext } from "@azure/functions";
+
+export async function eventHubTrigger(messages: unknown[], context: InvocationContext): Promise<void> {
+    context.log(`EventHub trigger function processed ${messages.length} messages`);
+    for (const message of messages) {
+        context.log("EventHub message:", message);
+    }
+}
+
+app.eventHub("eventHubTrigger", {
+    connection: "eventhubs",
+    eventHubName: "myhub",
+    cardinality: "many",
+    handler: eventHubTrigger,
+});
